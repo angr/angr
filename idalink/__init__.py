75c558aae064160e1bc3f79e15a486f484847a07
Yan <yans@yancomm.net>
2013-09-11 14:50:37 -0700

    better functionality

--- idalink/__init__.py
+++ idalink/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import time
+import random
 import subprocess
 import logging
 import rpyc
@@ -7,13 +9,18 @@ import os
 import standard_logging
 
 l = logging.getLogger("idalink")
-l.setLevel(logging.INFO)
-script_dir = os.path.dirname(os.path.realpath(__file__))
+l.setLevel(logging.DEBUG)
+
+# various locations
+module_dir = os.path.dirname(os.path.realpath(__file__))
+log_file = module_dir + "/ida.log"
+ida_script = module_dir + "/run_ida.sh"
+ida_dir = module_dir
 
 def spawn_ida(filename, port):
 	fullpath = os.path.realpath(os.path.expanduser(filename))
 	l.info("Launching IDA on %s" % fullpath)
-	subprocess.call([ "screen", "-d", "-m", "--", script_dir + "/run_ida.sh", fullpath, script_dir + "/ida.log", script_dir + "/server.py", str(port) ])
+	subprocess.call([ "screen", "-d", "-m", "--", ida_script, ida_dir, fullpath, log_file, module_dir + "/server.py", str(port) ])
 
 def connect_ida(port):
 	global ida, idc, idaapi, idautils
@@ -23,3 +30,16 @@ def connect_ida(port):
 	idc = ida.root.getmodule("idc")
 	idaapi = ida.root.getmodule("idaapi")
 	idautils = ida.root.getmodule("idautils")
+
+def make_idalink(filename):
+	port = random.randint(40000, 49999)
+	spawn_ida(filename, port)
+
+	while True:
+		try:
+			time.sleep(1)
+			l.debug("Trying to connect to IDA on port %d" % port)
+			connect_ida(port)
+			break
+		except Exception:
+			l.debug("... failed. Retrying.")
--- idalink/run_ida.sh
+++ idalink/run_ida.sh
@@ -2,6 +2,8 @@
 
 export TERM=xterm
 
+IDADIR=$1
+shift
 FILE=$1
 shift
 LOGFILE=$1
@@ -10,6 +12,13 @@ SCRIPT=$1
 shift
 ARGS="$@"
 
-echo "Launching IDA with script: $SCRIPT" >> $LOGFILE
+echo "Launching IDA on file $FILE with script: $SCRIPT" >> $LOGFILE
+echo "... log: $LOGFILE" >> $LOGFILE
 echo "... arguments: $ARGS" >> $LOGFILE
-/opt/analrepo/bin/idal64 -A -S"$SCRIPT $ARGS" -L$LOGFILE $FILE
+
+( file $FILE | egrep -q "[^[:digit:]]64[^[:digit:]]" ) && IDABIN=idal64
+( file $FILE | egrep -q "[^[:digit:]]32[^[:digit:]]" ) && IDABIN=idal
+
+echo "... detected executable: $IDABIN" >> $LOGFILE
+
+$IDADIR/$IDABIN -A -S"$SCRIPT $ARGS" -L$LOGFILE $FILE
