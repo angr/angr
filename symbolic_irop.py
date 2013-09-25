701330ae09313ac0810237ee3ca965e08d0db49b
Yan <yans@yancomm.net>
2013-09-25 11:23:43 -0700

    cleaning up op handling

--- symbolic_irop.py
+++ symbolic_irop.py
@@ -16,14 +16,14 @@ l.setLevel(logging.DEBUG)
 
 def generic_Sub(args, size, state):
 	#l.debug("OP: %s - %s" % (args[0], args[1]))
-	return symbolic_irexpr.translate(args[0], state) - symbolic_irexpr.translate(args[1], state)
+	return args[0] - args[1]
 
 def generic_Add(args, size, state):
 	#l.debug("OP: %s - %s" % (args[0], args[1]))
-	return symbolic_irexpr.translate(args[0], state) + symbolic_irexpr.translate(args[1], state)
+	return args[0] + args[1]
 
 def handle_64to32(args, state):
-	z3.Extract(63, 32, symbolic_irexpr.translate(args[0], state))
+	z3.Extract(63, 32, args[0])
 
 op_handlers = { }
 op_handlers["Iop_64to32"] = handle_64to32
@@ -32,9 +32,11 @@ op_handlers["Iop_64to32"] = handle_64to32
 ### Op Handler ###
 ##################
 def translate(op, args, state):
+	symbolic_args = [ symbolic_irexpr.translate(a, state) for a in args ]
+
 	if op in op_handlers:
 		l.debug("Calling %s" % op_handlers)
-		constraints = op_handlers[op](args, state)
+		constraints = op_handlers[op](symbolic_args, state)
 		l.debug("Generated constraints: %s" % constraints)
 		return constraints
 
@@ -45,7 +47,7 @@ def translate(op, args, state):
 	func_name = "generic_" + name
 	l.debug("Calling %s" % func_name)
 	if hasattr(sys.modules[__name__], func_name):
-		constraints = getattr(sys.modules[__name__], func_name)(args, size, state)
+		constraints = getattr(sys.modules[__name__], func_name)(symbolic_args, size, state)
 		l.debug("Generated constraints: %s" % constraints)
 		return constraints
 
