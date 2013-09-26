f6b67be5ac43cfffd01e99336b93880e89b35cf2
Nilo Redini <n.redini@gmail.com>
2013-09-26 10:44:00 -0700

    changed file name and added binary search

---
+++ symbolic_memory.py
@@ -0,0 +1,118 @@
+import platform
+import fractions
+from z3 import *
+import logging
+
+# This class manages memory blocks in the Bintrimmer projects
+class MemoryMap(object):
+
+    # Gets the highest representable value on the machine
+    def _get_highest_arch_value(self):
+        steps = range(self.arch_bits >> 2)
+        value = 0
+        for i in steps:
+            value |= 0xF
+            if i != steps[-1]:
+                value <<= 4
+        return value
+
+    # Initializes the class object
+    def __init__(self, arch_type = None):
+        logging.basicConfig(level = logging.DEBUG)
+        self.mmap = {}
+        self.sym_var = []
+        self.arch_bits = int(platform.architecture()[0].split('bit')[0]) if (arch_type == None) else arch_type
+        self.h_value = self._get_highest_arch_value()
+
+
+    def _lower_bound(self, expr, lo, hi):
+        # Necessary check since Pyhton doesn't allow private functions
+        lo = 0 if (lo < 0) else lo
+        hi = ((1 << self.arch_bits) - 1) if (hi < 0) else hi
+        s = Solver()
+        ret = -1
+        # workaround for the constant simplifying bug
+        try:
+            expr_smpl = simplify(expr)
+        except:
+            expr_smpl = expr
+
+        old_bnd = -1
+        while 1:
+            bnd = lo + ((hi - lo) >> 1)
+            if bnd == old_bnd:
+                break
+            s.add(expr_smpl <= bnd)
+            if  s.check() == sat:
+                hi = bnd
+                ret = bnd
+                logging.debug("Lower bound: Model %s" % s.model());
+            else:
+                lo = bnd + 1
+            s.reset()
+            old_bnd = bnd
+        return ret
+
+    def _upper_bound(self, expr, lo, hi):
+        # Necessary check since Pyhton doesn't allow private functions
+        lo = 0 if (lo < 0) else lo
+        hi = ((1 << self.arch_bits) - 1) if (hi < 0) else hi
+        s = Solver()
+        ret = -1
+        end = hi
+        # workaround for the constant simplifying bug
+        try:
+            expr_smpl = simplify(expr)
+        except:
+            expr_smpl = expr
+
+        old_bnd = -1
+        while 1:
+            bnd = lo + ((hi - lo) >> 1)
+            if bnd == old_bnd:
+                break
+            s.add(expr_smpl >= bnd)
+            # s.add(expr_smpl <= hi) #are you serious?
+            if  s.check() == sat:
+                logging.debug("Upper bound: Model %s" % s.model());
+                lo = bnd
+                ret = bnd
+            else:
+                hi = bnd - 1
+            s.reset()
+            old_bnd = bnd
+        return ret
+
+    # def _get_step(self, expr, start, stop, incr):
+    #     lo = 0 if (start < 0) else start
+    #     hi = ((1 << self.arch_bits) - 1) if (stop < 0) else stop
+    #     incr = 1 if (incr <= 0) else incr
+    #     s = Solver()
+
+    #     gcd = -1
+    #     unsat_steps = 0
+
+    #     while lo <= hi:
+    #         s.add(expr == lo)
+    #         if  s.check() == sat:
+    #             gcd = unsat_steps if (gcd == -1) else fractions.gcd(gcd, unsat_steps)
+    #             if gcd == 1:
+    #                 break
+    #             unsat_steps = 1
+    #         else:
+    #             unsat_steps += 1
+    #             s.reset()
+    #         lo = lo + incr
+
+    #     return gcd
+
+    # Gets the memory scope of the index
+    def get_index_scope(self, index_expr, start = None, end = None):
+        expr = index_expr
+        start = 0 if (start == None or start < 0) else start
+        end = self.h_value if (end == None or end < 0) else end
+        lo = self._lower_bound(expr, start, end)
+        hi = self._upper_bound(expr, start, end)
+        # st = self._get_step(expr, lo, hi, 1) #too slow
+        st = 1
+        return [lo, hi, st]
--- testmem.py
+++ testmem.py
@@ -1,10 +1,10 @@
 from z3 import *
-import symmemory
+import symbolic_memory
 
 x = BitVec('x', 64)
 j = BitVec('j', 64)
 z = BitVec('z', 64)
 
-expr = (2*x + 1)
-mm = symmemory.MemoryMap()
+expr = 2*x + 1
+mm = symbolic_memory.MemoryMap()
 print 'Range: ', mm.get_index_scope(expr, x)
