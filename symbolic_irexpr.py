d59dcba8807d1e38bb36673105891ad8ea1255e8
Nilo Redini <n.redini@gmail.com>
2013-09-30 16:45:46 -0700

    implemented first part of the memory algorithm

--- symbolic_irexpr.py
+++ symbolic_irexpr.py
@@ -9,6 +9,8 @@ import symbolic_ccall
 import random
 import logging
 import symbolic_memory
+
+
 l = logging.getLogger("symbolic_irexpr")
 #l.setLevel(logging.DEBUG)
 
@@ -18,52 +20,52 @@ l = logging.getLogger("symbolic_irexpr")
 
 # TODO: make sure the way we're handling reads of parts of registers is correct
 def handle_get(expr, state):
-	# TODO: proper SSO registers
-	size = symbolic_helpers.get_size(expr.type)
+        # TODO: proper SSO registers
+        size = symbolic_helpers.get_size(expr.type)
 
-	if expr.offset not in state.registers:
-		# TODO: handle register partials (ie, ax) as symbolic pieces of the full register
-		state.registers[expr.offset] = [ z3.BitVec("%s_reg_%d_%d" % (state.id, expr.offset, 0), size) ]
-	return z3.Extract(size - 1, 0, state.registers[expr.offset][-1])
+        if expr.offset not in state.registers:
+                # TODO: handle register partials (ie, ax) as symbolic pieces of the full register
+                state.registers[expr.offset] = [ z3.BitVec("%s_reg_%d_%d" % (state.id, expr.offset, 0), size) ]
+        return z3.Extract(size - 1, 0, state.registers[expr.offset][-1])
 
 def handle_op(expr, state):
-	args = expr.args()
-	return symbolic_irop.translate(expr.op, args, state)
+        args = expr.args()
+        return symbolic_irop.translate(expr.op, args, state)
 
 def handle_rdtmp(expr, state):
-	return state.temps[expr.tmp]
+        return state.temps[expr.tmp]
 
 def handle_const(expr, state):
-	return symbolic_helpers.translate_irconst(expr.con)
+        return symbolic_helpers.translate_irconst(expr.con)
 
 def handle_load(expr, state):
-	# TODO: symbolic memory
-	symbolic_memory.load(translate(expr.addr, state), state.past_constraints)
+        state.memory = sym_mem.load(state.memory, translate(expr.addr, state), state.past_constraints)
 
-	# temporary
-	size = symbolic_helpers.get_size(expr.type)
-	l.debug("Load of size %d" % size)
-	m_id = random.randint(0, 100)
-	l.debug("... ID: %d" % m_id)
-	m = z3.BitVec("tmp_memory_%d" % m_id, size)
-	return m
+        # temporary
+        size = symbolic_helpers.get_size(expr.type)
+        l.debug("Load of size %d" % size)
+        m_id = random.randint(0, 100)
+        l.debug("... ID: %d" % m_id)
+        m = z3.BitVec("tmp_memory_%d" % m_id, size)
+        return m
 
 def handle_ccall(expr, state):
-	symbolic_args = [ translate(a, state) for a in expr.args() ]
-	if hasattr(symbolic_ccall, expr.callee.name):
-		func = getattr(symbolic_ccall, expr.callee.name)
-		return func(*symbolic_args)
+        symbolic_args = [ translate(a, state) for a in expr.args() ]
+        if hasattr(symbolic_ccall, expr.callee.name):
+                func = getattr(symbolic_ccall, expr.callee.name)
+                return func(*symbolic_args)
 
-	raise Exception("Unsupported callee %s" % expr.callee.name)
+        raise Exception("Unsupported callee %s" % expr.callee.name)
 
 def handle_mux0x(expr, state):
-	cond = translate(expr.cond, state)
-	expr0 = translate(expr.expr0, state)
-	exprX = translate(expr.exprX, state)
+        cond = translate(expr.cond, state)
+        expr0 = translate(expr.expr0, state)
+        exprX = translate(expr.exprX, state)
 
-	return z3.If(cond == 0, expr0, exprX)
+        return z3.If(cond == 0, expr0, exprX)
 
 expr_handlers = { }
+sym_mem = symbolic_memory.Memory()
 expr_handlers[pyvex.IRExpr.Get] = handle_get
 expr_handlers[pyvex.IRExpr.Unop] = handle_op
 expr_handlers[pyvex.IRExpr.Binop] = handle_op
@@ -76,8 +78,8 @@ expr_handlers[pyvex.IRExpr.CCall] = handle_ccall
 expr_handlers[pyvex.IRExpr.Mux0X] = handle_mux0x
 
 def translate(expr, state):
-	t = type(expr)
-	if t not in expr_handlers:
-		raise Exception("Unsupported expression type %s." % str(t))
-	l.debug("Handling IRExpr %s" % t)
-	return expr_handlers[t](expr, state)
+        t = type(expr)
+        if t not in expr_handlers:
+                raise Exception("Unsupported expression type %s." % str(t))
+        l.debug("Handling IRExpr %s" % t)
+        return expr_handlers[t](expr, state)
