2e2dbb68aa3f90ca6d32f432341d7f253507be85
Nilo Redini <n.redini@gmail.com>
2013-10-02 16:37:28 -0700

    load completed

--- pysex/s_memory.py
+++ pysex/s_memory.py
@@ -4,8 +4,7 @@ import s_value
 import random
 import copy
 import pdb
-import itertools
-
+import collections
 import logging
 
 
@@ -16,18 +15,25 @@ var_mem_counter = 0
 
 l.setLevel(logging.DEBUG)
 
+def default_mem_value():
+    global var_mem_counter
+    var = z3.BitVec("mem_%d" %var_mem_counter, 8)
+    var_mem_counter += 1
+    return var
+
 class Memory:
+
     def __init__(self, initial=None, sys=None):
         #TODO: copy-on-write behaviour
-        self.__mem = copy.copy(initial) if ( initial != None) else {}
+        self.__mem = copy.copy(initial) if ( initial != None) else collections.defaultdict(default_mem_value)
         self.__limit = 1024
-        self.__freemem = [(0, (2**64) - 1)]
-        self.__max_mem = 2**64
-        self.__sys = sys if (sys != None) else 8
+        self.__bits = sys if (sys != None) else 64
+        self.__max_mem = 2**self.__bits
+        self.__freemem = [(0, self.__max_mem - 1)]
 
 
     def store(self, dst, cnt, constraints):
-        if len(self.__mem) == self.__max_mem:
+        if len(self.__mem) + cnt.size() >= self.__max_mem:
             raise Exception("Memory is full.")
 
         v = s_value.Value(dst, constraints)
@@ -44,7 +50,7 @@ class Memory:
                 con = z3.Or(z3.And(z3.UGE(dst, self.__freemem[i][0]), z3.ULE(dst, self.__freemem[i][1])), con)
 
             con = z3.simplify(con)
-            if con == True: #it is always satisfiable%
+            if con == True: # if it is always satisfiable%
                 #TODO: pick up one random instead
                 addr = (long(self.__mem.keys()[-1]) + 1) if len(self.__mem.keys()) != 0 else 0
                 ret = [dst == addr]
@@ -58,7 +64,7 @@ class Memory:
         for off in range(0, cnt.size() / 8):
             self.__mem[(addr + off)] = z3.Extract((off << 3) + 7, (off << 3), cnt)
 
-        keys = [ -1 ] + self.__mem.keys() + [ 2**64 ]
+        keys = [ -1 ] + self.__mem.keys() + [ self.__max_mem ]
         self.__freemem = [ j for j in [ ((keys[i] + 1, keys[i+1] - 1) if keys[i+1] - keys[i] > 1 else ()) for i in range(len(keys)-1) ] if j ]
 
         return ret
@@ -80,45 +86,39 @@ class Memory:
         v = s_value.Value(dst, constraints)
         r = ( v.min, v.max )
 
-        if abs(v.max - v.min) <= self.__limit:
+        # specific read
+        if v.min == v.max:
+            addr = v.min
+            expr = self.__mem[addr] if (size_b == 1) else z3.Concat(*[self.__mem[addr + i] for i in range( 0, size_b)])
+            expr = z3.simplify(expr)
+            ret = expr, []
+
+        elif abs(v.max - v.min) <= self.__limit:
             w_k = range(v.min, v.max)
             w_k.append(v.max)
             p_k = list(set(w_k) & set(self.__mem.keys()))
 
             if len(p_k) == 0:
                 l.debug("Loading operation outside its boundaries, symbolic variable found")
-                expr = z3.BitVec("mem_%s" %(var_mem_counter), size)
+                expr = z3.BitVec("mem_%s" %(var_mem_counter), size), []
                 var_mem_counter += 1
             else:
-                var = z3.BitVec("%s_addr_%s" %(dst, addr_mem_counter), self.__sys)
+                var = z3.BitVec("%s_addr_%s" %(dst, addr_mem_counter), self.__bits)
                 addr_mem_counter += 1
-
-                # specific read
-                if len(p_k)/size_b == 0:
-                    addr = p_k[0]
-                    expr = z3.Concat(*[ self.__mem[addr + i] for i in range( 0, size_b)])
-                    expr = z3.simplify(expr)
-                else:
-                    try:
-                        for cnt in range(0, len(p_k)/size_b):
-                            cnc = z3.Concat(*[ self.__mem[p_k[i + (cnt*size_b)]] for i in range( 0, size_b)])
-                            new_expr = z3.Or(var == cnc, expr)
-                            expr = z3.simplify(new_expr)
-                    except:
-                        l.debug("Loading not mutiple of %d" % size_b)
-            ret = expr, []
+                for addr in p_k:
+                    cnc = z3.Concat(*[self.__mem[addr + i] for i in range( 0, size_b)])
+                    expr = z3.simplify(z3.Or(var == cnc, expr))
+                ret = expr, []
         else:
-            pos = range(0, len(self.__mem.keys()) / size_b)
-            addr = random.choice(pos)
-            cnc = z3.Concat(*[ self.__mem[(addr*size_b) + i] for i in range( 0, size_b)])
+            addr = random.choice(self.__mem.keys())
+            cnc = z3.Concat(*[ self.__mem[addr + i] for i in range( 0, size_b)])
             cnc = z3.simplify(cnc)
-            pdb.set_trace()
             ret = cnc, [dst == addr]
 
         return ret
 
     def get_bit_address(self):
-        return self.__sys
+        return self.__bits
 
     #TODO: copy-on-write behaviour
     def copy(self):
