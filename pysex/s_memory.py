1fe2cd5d20f8c9033922ab0f76fac54d149100b4
Nilo Redini <n.redini@gmail.com>
2013-10-07 11:15:45 -0700

    added memory type, and cleaned code

--- pysex/s_memory.py
+++ pysex/s_memory.py
@@ -11,26 +11,58 @@ l = logging.getLogger("s_memory")
 addr_mem_counter = 0
 var_mem_counter = 0
 
+class Cell:
+        # Type: RWX bits
+        def __init__(self, ctype, cnt):
+                self.type = ctype
+                self.cnt = cnt
+
 class Memory:
         def __init__(self, initial=None, sys=None, id="mem"):
                 def default_mem_value():
                         global var_mem_counter
                         var = z3.BitVec("%s_%d" % (id, var_mem_counter), 8)
                         var_mem_counter += 1
-                        return var
+                        return Cell(6, var)
 
                 #TODO: copy-on-write behaviour
-                self.__mem = copy.copy(initial) if initial else collections.defaultdict(default_mem_value)
+                # copy.copy(initial) if initial else
+                self.__mem = collections.defaultdict(default_mem_value)
                 self.__limit = 1024
                 self.__bits = sys if sys else 64
                 self.__max_mem = 2**self.__bits
                 self.__freemem = [(0, self.__max_mem - 1)]
 
+        def is_readable(self, addr):
+                return self.__mem[addr].type & 4
+
+        def is_writable(self, addr):
+                return self.__mem[addr].type & 2
+
+        def is_executable(self, addr):
+                return self.__mem[addr].type & 1
+
         def read_from(self, addr, num_bytes):
-                if num_bytes == 1:
-                        return self.__mem[addr]
+                # Check every addresses insted only the first one?
+                if self.is_readable(addr):
+                        if num_bytes == 1:
+                                return self.__mem[addr].cnt
+                        else:
+                                return z3.Concat(*[self.__mem[addr + i].cnt for i in range( 0, num_bytes)])
                 else:
-                        return z3.Concat(*[self.__mem[addr + i] for i in range( 0, num_bytes)])
+                        l.info("Attempted reading in a not readable location")
+                        # FIX ME
+                        return []
+
+        def write_to(self, addr, cnt):
+                if self.is_writable(addr):
+                        for off in range(0, cnt.size() / 8):
+                                self.__mem[(addr + off)].cnt = z3.Extract((off << 3) + 7, (off << 3), cnt)
+
+                        keys = [ -1 ] + self.__mem.keys() + [ self.__max_mem ]
+                        self.__freemem = [ j for j in [ ((keys[i] + 1, keys[i+1] - 1) if keys[i+1] - keys[i] > 1 else ()) for i in range(len(keys)-1) ] if j ]
+                else:
+                        l.info("Attempted writing in a not writable location")
 
         def store(self, dst, cnt, constraints):
                 v = s_value.Value(dst, constraints)
@@ -40,13 +72,6 @@ class Memory:
                         # if there's only one option, let's do it
                         addr = v.any()
                 else:
-                        # otherwise, let's first try to find some free memory
-                        # This becomes useless!
-                        # if len(self.__mem) == 0:
-                        #       # do a page-aligned address just in case?
-                        #       addr = random.randint(0, self.__max_mem) % 0x1000
-                        #       ret = [dst == addr]
-
                         fcon = z3.Or([ z3.And(z3.UGE(dst,a), z3.ULE(dst,b)) for a,b in self.__freemem ])
                         v_free = s_value.Value(dst, constraints + [ fcon ])
 
@@ -59,11 +84,7 @@ class Memory:
                                 addr = v.any()
                                 ret = [dst == addr]
 
-                for off in range(0, cnt.size() / 8):
-                        self.__mem[(addr + off)] = z3.Extract((off << 3) + 7, (off << 3), cnt)
-
-                keys = [ -1 ] + self.__mem.keys() + [ self.__max_mem ]
-                self.__freemem = [ j for j in [ ((keys[i] + 1, keys[i+1] - 1) if keys[i+1] - keys[i] > 1 else ()) for i in range(len(keys)-1) ] if j ]
+                self.write_to(addr, cnt)
 
                 return ret
 
@@ -98,8 +119,7 @@ class Memory:
                         var = z3.BitVec("%s_addr_%s" %(dst, addr_mem_counter), self.__bits)
                         addr_mem_counter += 1
                         for addr in to_iterate.iter():
-                                #ERROR: Check whether the i-th address is really attainable by th eexpression
-                                cnc = z3.Concat(*[self.__mem[addr + i] for i in range( 0, size_b)])
+                                cnc = self.read_from(addr, size_b)
                                 expr = z3.simplify(z3.Or(var == cnc, expr))
 
                         ret = expr, []
@@ -109,14 +129,14 @@ class Memory:
                                 #first try to point it somewhere valid
                                 #ERROR: Check whether the i-th address is really attainable by th eexpression
                                 addr = random.choice(self.__mem.keys())
-                                cnc = z3.Concat(*[ self.__mem[addr + i] for i in range( 0, size_b)])
+                                cnc = self.read_from(addr, size_b)
                                 cnc = z3.simplify(cnc)
                                 ret = cnc, [dst == addr]
                         else:
                                 # otherwise, concretize to a random, page-aligned location, just for fun
                                 addr = random.randint(0, self.__max_mem) % 0x1000
                                 #ERROR: Check whether the i-th address is really attainable by th eexpression
-                                cnc = z3.Concat(*[ self.__mem[addr + i] for i in range( 0, size_b)])
+                                cnc = self.read_from(addr, size_b)
                                 cnc = z3.simplify(cnc)
                                 ret = cnc, [dst == addr]
 
