0e87ed1391560e42a03216d48286159c3538d23f
Nilo Redini <n.redini@gmail.com>
2013-10-15 17:45:02 -0700

    rebasing shared libraries

--- loader.py
+++ loader.py
@@ -16,7 +16,7 @@ l = logging.getLogger("loader")
 l.setLevel(logging.DEBUG)
 
 loaded_libs = {}
-default_offset = 1024
+default_offset = 10240 #10k
 
 
 def get_tmp_fs_copy(src_filename):
@@ -30,15 +30,19 @@ def get_tmp_fs_copy(src_filename):
 def load_binary(ida):
     mem = pysex.s_memory.Memory()
     loaded_libs[ida.get_filename()] = {}
-    link_and_load(ida, mem, 0)
+    link_and_load(ida, mem)
     l.debug("MEM: Lowest addr: %d, Highest addr: %d" %(min(mem.get_addresses()), max(mem.get_addresses())))
     return mem
 
-# TODO relocating everything, start variable has this purpose!
-def link_and_load(ida, mem, start=0):
+# TODO relocating everything, start variable serves this purpose!
+def link_and_load(ida, mem, rebase=0, start=0):
     dst = z3.BitVec('dst', mem.get_bit_address())
     lib_name = os.path.realpath(os.path.expanduser(ida.get_filename())).split("/")[-1]
     l.debug("Loading Binary: %s, instructions: #%d" %(lib_name, len(ida.mem.keys())))
+
+    if rebase:
+        real_start = min(ida.mem.keys()) + start
+        ida.idaapi.rebase_program(real_start, ida.idaapi.MSF_FIXONCE | ida.idaapi.MSF_LDKEEP)
 
     # dynamic stuff
     bin_names = names.Names(ida)
@@ -50,14 +54,16 @@ def link_and_load(ida, mem, start=0):
         size = 8
         if sym_name and bin_names[sym_name].ntype == 'E':
             extrnlib_name = bin_names[sym_name].extrn_lib_name
-            ## REMOVE WHEN READY ###
+            REMOVE WHEN READY ###
             if "libc" in extrnlib_name:
                 l.debug("Skipping LibC")
                 continue
-             #########################
+             
             if extrnlib_name not in loaded_libs.keys():
                 ida_bin = binary.Binary(get_tmp_fs_copy(bin_names[sym_name].extrn_fs_path)).ida
-                link_and_load(ida_bin, mem)
+                start_current = min(ida.mem.keys())
+                size_bin = len(ida_bin.mem)
+                link_and_load(ida_bin, mem, 1, (((start_current - (default_offset + size_bin)) % mem.get_max()) & 0x1000)
             ## got EXTRN change address!
             cnt = loaded_libs[bin_names[sym_name].extrn_lib_name][sym_name].addr
             size = ida.idautils.DecodeInstruction(addr).size * 8
