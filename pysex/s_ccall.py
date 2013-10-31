8e65a51a8008fd2720df69f31b6cbd047ad3cb7b
Yan <yans@yancomm.net>
2013-10-31 12:42:16 -0700

    added ARM rflags support

--- pysex/s_ccall.py
+++ pysex/s_ccall.py
@@ -24,6 +24,9 @@ def calc_zerobit(p):
 		b = b | z3.Extract(i, i, p)
 	return b ^ z3.BitVecVal(1, 1)
 
+def boolean_extend(O, a, b, size):
+	return z3.If(O(a, b), z3.BitVecVal(1, size), z3.BitVecVal(0, size))
+
 # There might be a faster way of doing this
 #def calc_overflow(a, b):
 #	ax = z3.ZeroExt(1, a)
@@ -169,16 +172,7 @@ def amd64g_calculate_rflags_all_WRK(cc_op, cc_dep1_formal, cc_dep2_formal, cc_nd
 		return cc_dep1_formal & (AMD64G_CC_MASK_O | AMD64G_CC_MASK_S | AMD64G_CC_MASK_Z
 			   | AMD64G_CC_MASK_A | AMD64G_CC_MASK_C | AMD64G_CC_MASK_P)
 
-	#if cc_op in [ AMD64G_CC_OP_ADDB, AMD64G_CC_OP_ADCB, AMD64G_CC_OP_SUBB, AMD64G_CC_OP_SBBB, AMD64G_CC_OP_LOGICB, AMD64G_CC_OP_INCB, AMD64G_CC_OP_DECB, AMD64G_CC_OP_SHLB, AMD64G_CC_OP_SHRB, AMD64G_CC_OP_ROLB, AMD64G_CC_OP_RORB, AMD64G_CC_OP_UMULB, AMD64G_CC_OP_SMULB ]:
-	#	nbits = 8
-	#elif cc_op in [ AMD64G_CC_OP_ADDW, AMD64G_CC_OP_ADCW, AMD64G_CC_OP_SUBW, AMD64G_CC_OP_SBBW, AMD64G_CC_OP_LOGICW, AMD64G_CC_OP_INCW, AMD64G_CC_OP_DECW, AMD64G_CC_OP_SHLW, AMD64G_CC_OP_SHRW, AMD64G_CC_OP_ROLW, AMD64G_CC_OP_RORW, AMD64G_CC_OP_UMULW, AMD64G_CC_OP_SMULW ]:
-	#	nbits = 16
-	#elif cc_op in [ AMD64G_CC_OP_ADDL, AMD64G_CC_OP_ADCL, AMD64G_CC_OP_SUBL, AMD64G_CC_OP_SBBL, AMD64G_CC_OP_LOGICL, AMD64G_CC_OP_INCL, AMD64G_CC_OP_DECL, AMD64G_CC_OP_SHLL, AMD64G_CC_OP_SHRL, AMD64G_CC_OP_ROLL, AMD64G_CC_OP_RORL, AMD64G_CC_OP_UMULL, AMD64G_CC_OP_SMULL ]:
-	#	nbits = 32
-	#if cc_op in [ AMD64G_CC_OP_ADDQ, AMD64G_CC_OP_ADCQ, AMD64G_CC_OP_SUBQ, AMD64G_CC_OP_SBBQ, AMD64G_CC_OP_LOGICQ, AMD64G_CC_OP_INCQ, AMD64G_CC_OP_DECQ, AMD64G_CC_OP_SHLQ, AMD64G_CC_OP_SHRQ, AMD64G_CC_OP_ROLQ, AMD64G_CC_OP_RORQ, AMD64G_CC_OP_UMULQ, AMD64G_CC_OP_SMULQ ]:
-	#	nbits = 64
 	nbits = 2 ** ((cc_op % 4) + 3)
-
 	l.debug("nbits == %d" % nbits)
 
 	if cc_op in [ AMD64G_CC_OP_ADDB, AMD64G_CC_OP_ADDW, AMD64G_CC_OP_ADDL, AMD64G_CC_OP_ADDQ ]:
@@ -305,3 +299,183 @@ def amd64g_calculate_rflags_c(cc_op, cc_dep1, cc_dep2, cc_ndep):
 		return z3.BitVecVal(0, 1)
 
 	return z3.LShR(amd64g_calculate_rflags_all_WRK(cc_op,cc_dep1,cc_dep2,cc_ndep), AMD64G_CC_SHIFT_C) & 1
+
+#################
+### ARM Flags ###
+#################
+
+ARMCondEQ = 0 #   /* equal                         : Z=1 */
+ARMCondNE = 1 #   /* not equal                     : Z=0 */
+ARMCondHS = 2 #   /* >=u (higher or same)          : C=1 */
+ARMCondLO = 3 #   /* <u  (lower)                   : C=0 */
+ARMCondMI = 4 #   /* minus (negative)              : N=1 */
+ARMCondPL = 5 #   /* plus (zero or +ve)            : N=0 */
+ARMCondVS = 6 #   /* overflow                      : V=1 */
+ARMCondVC = 7 #   /* no overflow                   : V=0 */
+ARMCondHI = 8 #   /* >u   (higher)                 : C=1 && Z=0 */
+ARMCondLS = 9 #   /* <=u  (lower or same)          : C=0 || Z=1 */
+ARMCondGE = 10 #  /* >=s (signed greater or equal) : N=V */
+ARMCondLT = 11 #  /* <s  (signed less than)        : N!=V */
+ARMCondGT = 12 #  /* >s  (signed greater)          : Z=0 && N=V */
+ARMCondLE = 13 #  /* <=s (signed less or equal)    : Z=1 || N!=V */
+ARMCondAL = 14 #  /* always (unconditional)        : 1 */
+ARMCondNV = 15 #   /* never (unconditional):        : 0 */
+
+ARMG_CC_OP_COPY = 0   # /* DEP1 = NZCV in 31:28, DEP2 = 0, DEP3 = 0 just copy DEP1 to output */ 
+ARMG_CC_OP_ADD = 1    # /* DEP1 = argL (Rn) =  DEP2 = argR (shifter_op) =  DEP3 = 0 */ 
+ARMG_CC_OP_SUB = 2    # /* DEP1 = argL (Rn) =  DEP2 = argR (shifter_op) =  DEP3 = 0 */ 
+ARMG_CC_OP_ADC = 3    # /* DEP1 = argL (Rn) =  DEP2 = arg2 (shifter_op) =  DEP3 = oldC (in LSB) */ 
+ARMG_CC_OP_SBB = 4    # /* DEP1 = argL (Rn) =  DEP2 = arg2 (shifter_op) =  DEP3 = oldC (in LSB) */ 
+ARMG_CC_OP_LOGIC = 5  # /* DEP1 = result =  DEP2 = shifter_carry_out (in LSB) =  DEP3 = old V flag (in LSB) */ 
+ARMG_CC_OP_MUL = 6    # /* DEP1 = result =  DEP2 = 0 =  DEP3 = oldC:old_V (in bits 1:0) */ 
+ARMG_CC_OP_MULL = 7   # /* DEP1 = resLO32 =  DEP2 = resHI32 =  DEP3 = oldC:old_V (in bits 1:0) */ 
+ARMG_CC_OP_NUMBER = 8
+
+ARMG_CC_SHIFT_N = 31
+ARMG_CC_SHIFT_Z = 30
+ARMG_CC_SHIFT_C = 29
+ARMG_CC_SHIFT_V = 28
+ARMG_CC_SHIFT_Q = 27
+
+def armg_calculate_flag_n(cc_op, cc_dep1, cc_dep2, cc_dep3):
+	if cc_op == ARMG_CC_OP_COPY:
+		return z3.LShR(cc_dep1, ARMG_CC_SHIFT_N) & 1
+	elif cc_op == ARMG_CC_OP_ADD:
+		res = cc_dep1 + cc_dep2
+		return z3.LShR(res, 31)
+	elif cc_op == ARMG_CC_OP_SUB:
+		res = cc_dep1 - cc_dep2
+		return z3.LShR(res, 31)
+	elif cc_op == ARMG_CC_OP_ADC:
+		res = cc_dep1 + cc_dep2 + cc_dep3
+		return z3.LShR(res, 31)
+	elif cc_op == ARMG_CC_OP_SBB:
+		res = cc_dep1 - cc_dep2 - (cc_dep3^1)
+		return z3.LShR(res, 31)
+	elif cc_op == ARMG_CC_OP_LOGIC:
+		return z3.LShR(cc_dep1, 31)
+	elif cc_op == ARMG_CC_OP_MUL:
+		return z3.LShR(cc_dep1, 31)
+	elif cc_op == ARMG_CC_OP_MULL:
+		return z3.LShR(cc_dep2, 31)
+
+	raise Exception("Unknown cc_op %s" % cc_op)
+
+def amd_zerobit(x):
+	z3.ZeroExt(31, calc_zerobit(x))
+
+def armg_calculate_flag_z(cc_op, cc_dep1, cc_dep2, cc_dep3):
+	if cc_op == ARMG_CC_OP_COPY:
+		return z3.LShR(cc_dep1, ARMG_CC_SHIFT_Z) & 1
+	elif cc_op == ARMG_CC_OP_ADD:
+		res = cc_dep1 + cc_dep2
+		return amd_zerobit(z3.LShR(res, 31))
+	elif cc_op == ARMG_CC_OP_SUB:
+		res = cc_dep1 - cc_dep2
+		return amd_zerobit(z3.LShR(res, 31))
+	elif cc_op == ARMG_CC_OP_ADC:
+		res = cc_dep1 + cc_dep2 + cc_dep3
+		return amd_zerobit(z3.LShR(res, 31))
+	elif cc_op == ARMG_CC_OP_SBB:
+		res = cc_dep1 - cc_dep2 - (cc_dep3^1)
+		return amd_zerobit(z3.LShR(res, 31))
+	elif cc_op == ARMG_CC_OP_LOGIC:
+		return amd_zerobit(z3.LShR(cc_dep1, 31))
+	elif cc_op == ARMG_CC_OP_MUL:
+		return amd_zerobit(z3.LShR(cc_dep1, 31))
+	elif cc_op == ARMG_CC_OP_MULL:
+		return amd_zerobit(z3.LShR(cc_dep1 | cc_dep2, 31))
+
+	raise Exception("Unknown cc_op %s" % cc_op)
+
+def armg_calculate_flag_c(cc_op, cc_dep1, cc_dep2, cc_dep3):
+	if cc_op == ARMG_CC_OP_COPY:
+		return z3.LShR(cc_dep1, ARMG_CC_SHIFT_C) & 1
+	elif cc_op == ARMG_CC_OP_ADD:
+		res = cc_dep1 + cc_dep2
+		return boolean_extend(z3.LT, res, cc_dep1, 32)
+	elif cc_op == ARMG_CC_OP_SUB:
+		return boolean_extend(z3.GTE, cc_dep1, cc_dep2, 32)
+	elif cc_op == ARMG_CC_OP_ADC:
+		res = cc_dep1 + cc_dep2 + cc_dep3
+		return z3.If(cc_dep2 != 0, boolean_extend(z3.LTE, res, cc_dep1, 32), boolean_extend(z3.LT, res, cc_dep1, 32))
+	elif cc_op == ARMG_CC_OP_SBB:
+		return z3.If(cc_dep2 != 0, boolean_extend(z3.GTE, cc_dep1, cc_dep2, 32), boolean_extend(z3.GT, cc_dep1, cc_dep2, 32))
+	elif cc_op == ARMG_CC_OP_LOGIC:
+		return cc_dep2
+	elif cc_op == ARMG_CC_OP_MUL:
+		return (cc_dep3 >> 1) & 1
+	elif cc_op == ARMG_CC_OP_MULL:
+		return (cc_dep3 >> 1) & 1
+
+	raise Exception("Unknown cc_op %s" % cc_op)
+
+def armg_calculate_flag_v(cc_op, cc_dep1, cc_dep2, cc_dep3):
+	if cc_op == ARMG_CC_OP_COPY:
+		return z3.LShR(cc_dep1, ARMG_CC_SHIFT_V) & 1
+	elif cc_op == ARMG_CC_OP_ADD:
+		res = cc_dep1 + cc_dep2
+		v = ((res ^ cc_dep1) & (res ^ cc_dep2))
+		return z3.LShR(v, 31)
+	elif cc_op == ARMG_CC_OP_SUB:
+		res = cc_dep1 - cc_dep2
+		v = ((cc_dep1 ^ cc_dep2) & (cc_dep1 ^ res))
+		return z3.LShR(v, 31)
+	elif cc_op == ARMG_CC_OP_ADC:
+		res = cc_dep1 + cc_dep2 + cc_dep3
+		v = ((res ^ cc_dep1) & (res ^ cc_dep2))
+		return z3.LShR(v, 31)
+	elif cc_op == ARMG_CC_OP_SBB:
+		res = cc_dep1 - cc_dep2 - (cc_dep3^1)
+		v = ((cc_dep1 ^ cc_dep2) & (cc_dep1 ^ res))
+		return z3.LShR(v, 31)
+	elif cc_op == ARMG_CC_OP_LOGIC:
+		return cc_dep3
+	elif cc_op == ARMG_CC_OP_MUL:
+		return cc_dep3 & 1
+	elif cc_op == ARMG_CC_OP_MULL:
+		return cc_dep3 & 1
+
+	raise Exception("Unknown cc_op %s" % cc_op)
+
+def armg_calculate_flags_nzcv(cc_op, cc_dep1, cc_dep2, cc_dep3):
+	n = armg_calculate_flag_n(cc_op, cc_dep1, cc_dep2, cc_dep3)
+	z = armg_calculate_flag_z(cc_op, cc_dep1, cc_dep2, cc_dep3)
+	c = armg_calculate_flag_c(cc_op, cc_dep1, cc_dep2, cc_dep3)
+	v = armg_calculate_flag_v(cc_op, cc_dep1, cc_dep2, cc_dep3)
+	return (n << ARMG_CC_SHIFT_N) | (z << ARMG_CC_SHIFT_Z) | (c << ARMG_CC_SHIFT_C) | (v << ARMG_CC_SHIFT_V)
+
+def armg_calculate_condition(cond_n_op, cc_dep1, cc_dep2, cc_dep3):
+	cond = cond_n_op.as_long() >> 4
+	cc_op = cond_n_op & 0xF
+	inv = cond & 1
+
+	if cond == ARMCondAL:
+		return z3.BitVecVal(1, 32)
+	elif cond in [ ARMCondEQ, ARMCondNE ]:
+		zf = armg_calculate_flag_z(cc_op, cc_dep1, cc_dep2, cc_dep3)
+		return inv ^ zf;
+	elif cond in [ ARMCondHS, ARMCondLO ]:
+		cf = armg_calculate_flag_c(cc_op, cc_dep1, cc_dep2, cc_dep3)
+		return inv ^ cf;
+	elif cond in [ ARMCondMI, ARMCondPL ]:
+		nf = armg_calculate_flag_n(cc_op, cc_dep1, cc_dep2, cc_dep3)
+		return inv ^ nf;
+	elif cond in [ ARMCondVS, ARMCondVC ]:
+		vf = armg_calculate_flag_v(cc_op, cc_dep1, cc_dep2, cc_dep3)
+		return inv ^ vf
+	elif cond in [ ARMCondHI, ARMCondLS ]:
+		cf = armg_calculate_flag_c(cc_op, cc_dep1, cc_dep2, cc_dep3)
+		zf = armg_calculate_flag_z(cc_op, cc_dep1, cc_dep2, cc_dep3)
+		return inv ^ (cf & ~zf)
+	elif cond in [ ARMCondGE, ARMCondLT ]:
+		nf = armg_calculate_flag_n(cc_op, cc_dep1, cc_dep2, cc_dep3)
+		vf = armg_calculate_flag_v(cc_op, cc_dep1, cc_dep2, cc_dep3)
+		return inv ^ (1 & ~(nf ^ vf))
+	elif cond in [ ARMCondGT, ARMCondLE ]:
+		nf = armg_calculate_flag_n(cc_op, cc_dep1, cc_dep2, cc_dep3)
+		vf = armg_calculate_flag_v(cc_op, cc_dep1, cc_dep2, cc_dep3)
+		zf = armg_calculate_flag_z(cc_op, cc_dep1, cc_dep2, cc_dep3)
+		return inv ^ (1 & ~(zf | (nf ^ vf)))
+
+	raise Exception("Unrecognized condition in armg_calculate_condition")
