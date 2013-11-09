3bf96029e4224bdf9a9f35df041de48f5f55d42a
Yan <yans@yancomm.net>
2013-11-09 01:19:28 -0800

    support ARM procedure emulation

--- pysex/s_arch.py
+++ pysex/s_arch.py
@@ -52,7 +52,14 @@ class SymbolicARM:
 		self.vex_arch = "VexArchARM"
 
 	def emulate_subroutine(self, call_imark, state):
-		return None
+		l.debug("Emulating return for ARM at 0x%x" % call_imark.addr)
+		if len(state.block_path) == 0:
+			raise CallEmulationError("unable to emulate return with no call stack")
+
+		# NOTE: ARM stuff
+		ret_irsb = pyvex.IRSB(bytes="\xE1\xA0\xF0\x0E", mem_addr=call_imark.addr, arch="VexArchARM")
+		ret_sirsb = s_irsb.SymbolicIRSB(ret_irsb, state.copy_after(), ethereal=True)
+		return ret_sirsb.exits()[0]
 
 class SymbolicMIPS32:
 	def __init__(self):
