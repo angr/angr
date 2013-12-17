d78608d64ea40c69ef07a34d51e493d16ce24cd9
Yan <yans@yancomm.net>
2013-12-17 12:43:43 -0700

    looks like it's now functional for static stuff

--- simuvex/s_slice.py
+++ simuvex/s_slice.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
+''' This file contains support for SimSlice. '''
+
 import logging
 l = logging.getLogger("s_slice")
 
 import pyvex
-import symexec
 from .s_irsb import SimIRSB
 from .s_exception import SimError
 from .s_exit import SimExit
@@ -13,72 +14,112 @@ class SimSliceError(SimError):
 	pass
 
 class SimSlice:
+	# SimSlice adds support for program slicing. It accepts a set of addresses and analyzes a slice containing those addresses.
+	# Currently, code changes during the runtime of the slice are *not* supported.
 	def __init__(self, initial_state, addresses, mode):
-		max_inst_bytes = initial_state.arch.max_inst_bytes
 		self.data_reads = [ ]
 		self.data_writes = [ ]
 		self.memory_refs = [ ]
 		self.code_refs = [ ]
-		self.mode = mode;
+		self.sim_blocks = [ ]
+		self.mode = mode
+
+		# prepare the states
+		self.initial_state = initial_state
+		initial_exit = SimExit(addr = addresses[0], addr_state = initial_state)
+		
+		self.final_exits = [ initial_exit ]
+		self.blockstacks = [ [ ] ]
+		self.add_addresses(addresses)
+
+	def add_addresses(self, addresses):
+		max_inst_bytes = self.initial_state.arch.max_inst_bytes
+
+		l.debug("Adding %d addresses to slice.", len(addresses))
 
 		# make a one-instruction IRSB at the addresses
 		instructions = [ ]
 		for addr in addresses:
-			irsb = pyvex.IRSB(bytes=initial_state.memory[addr:addr + max_inst_bytes], max_inst = 1)
+			irsb = pyvex.IRSB(bytes=self.initial_state.memory.read_from(addr, max_inst_bytes, constraints=[ ]), num_inst = 1, mem_addr=addr)
 			#sirsb = SimIRSB(irsb, initial_state, mode=mode)
-			first_imark = [ s for s in irsb.statements() if type(s) == pyvex.IMark ][0]
+			first_imark = [ s for s in irsb.statements() if type(s) == pyvex.IRStmt.IMark ][0]
 
-			instructions.append(irsb, first_imark)
+			l.debug("Instruction of size %d at 0x%x", first_imark.len, first_imark.addr)
+			instructions.append((irsb, first_imark))
 
-		# prepare the states
-		self.initial_state = initial_state
-		initial_exit = [ SimExit(empty = True) ]
-		initial_exit.state = initial_state.copy_after()
-		initial_exit.s_target = symexec.BitVecVal(addresses[0], initial_state.arch.bits)
-		initial_state.jumpkind = "Ijk_Boring"
-		
-		self.final_exits = [ initial_exit ]
+		l.debug("Sanity check: %d IMarks", len(instructions))
 
 		# add the parts of the slice
-		start_addr = 0
+		start_addr = None
 		next_addr = None
 		num_inst = 0
 		num_bytes = 0
-		for imark,_ in instructions:
+		for _, imark in instructions:
+			l.debug("Looking at IMark with addr 0x%x and len %d", imark.addr, imark.len)
+			if start_addr is None:
+				l.debug("... first imark")
+				start_addr = imark.addr
+				next_addr = imark.addr
+				num_inst = 0
+				num_bytes = 0
+
 			if next_addr != imark.addr:
-				# we're done with our slice; add it
-				self.add_to_slice(start_addr, next_addr, num_inst, num_bytes)
-			next_addr += imark.addr
+				# we're done with this part of the slice; add it
+				l.debug("... adding block of %d instructions and %d bytes starting at 0x%x", num_inst, num_bytes, start_addr)
+				self.add_block(start_addr, num_inst, num_bytes)
+
+				start_addr = imark.addr
+				next_addr = imark.addr
+				num_bytes = 0
+				num_inst = 0
+
+			next_addr += imark.len
 			num_bytes += imark.len
 			num_inst += 1
 
+		l.debug("Adding final block of %d instructions and %d bytes starting at 0x%x", num_inst, num_bytes, start_addr)
+		self.add_block(start_addr, num_inst, num_bytes)
 
-	def add_to_slice(self, addr, num_inst, num_bytes):
+
+	def add_block(self, addr, num_inst, num_bytes):
 		# check current exit states for one that points to addr
 		# if it doesn't exist, debug and just take the last one for now
 		# replace the current exits with the new last one's exits
 
-		reachable_exits = [ exit for exit in self.final_exits if exit.reachable() ]
+		reachable_exits = [ (e,s) for e,s in zip(self.final_exits, self.blockstacks) if e.reachable() ]
 		l.debug("%d reachable exits", len(reachable_exits))
+		if len(reachable_exits) == 0:
+			raise SimSliceError("No sat path when starting block 0x%x while building slice.", addr)
 
 		feasible_states = [ ]
 		unfeasible_states = [ ]
-		for exit in reachable_exits:
-			if exit.simvalue.is_solution(addr):
-				feasible_states.append(exit.state)
+		for e,s in reachable_exits:
+			if e.simvalue.is_solution(addr):
+				feasible_states.append((e.state, s))
 			else:
-				unfeasible_states.append(exit.state)
+				unfeasible_states.append((e.state, s))
+
+		l.debug("%d feasible and %d unfeasible exits", len(feasible_states), len(unfeasible_states))
 
 		# if there are no feasible solutions (which can happen if we're skipping instructions), use the unfeasible states
-		if len(feasible_states) == 0: feasible_states = unfeasible_states
+		if len(feasible_states) == 0:
+			feasible_states = unfeasible_states
 
 		self.final_exits = [ ]
-		for state in feasible_states:
+		self.blockstacks = [ ]
+		for state, stack in feasible_states:
 			try:
-				irsb = pyvex.IRSB(bytes=state.memory[addr:addr + num_bytes], max_inst = num_inst)
+				irsb = pyvex.IRSB(bytes=state.memory.read_from(addr, num_bytes, constraints=[ ]), num_inst = num_inst, mem_addr=addr)
 				sirsb = SimIRSB(irsb, state, mode=self.mode)
+				stack.append(sirsb)
+
 				# TODO: track the data refs for the different possibilities
 				# TODO: fix up the instruction pointer to the next location if it's known.
-				self.final_exits.extend(sirsb.exits())
+				new_exits = sirsb.exits()
+				for e in new_exits:
+					self.final_exits.append(e)
+					self.blockstacks.append(stack)
 			except SimError:
 				l.warning("SimError caught while adding a state to a slice.", exc_info=True)
+
+		l.debug("%d exits", len(self.final_exits))
