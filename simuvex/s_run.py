66a82d5bd08b8d3f3f647739daf70053c2acb789
Yan <yans@yancomm.net>
2014-01-09 13:29:00 -0800

    exits for SimRun!

--- simuvex/s_run.py
+++ simuvex/s_run.py
@@ -2,11 +2,8 @@
 
 from .s_ref import RefTypes
 import s_options as o
-from abc import ABCMeta, abstractmethod
 
 class SimRun:
-	__metaclass__ = ABCMeta
-
 	def __init__(self, options = None, mode = None):
 		# the options and mode
 		if options is None:
@@ -14,24 +11,34 @@ class SimRun:
 		self.options = options
 		self.mode = mode
 
+		self._exits = [ ]
+
 		self._refs = { }
 		self.options = options
 		for t in RefTypes:
 			self._refs[t] = [ ]
 
-	@abstractmethod
-	def exits(self): pass
-
 	def refs(self):
 		return self._refs
 
-	# Categorize and add a sequence of refs to this IRSB
+	def exits(self):
+		return self._exits
+
+	# Categorize and add a sequence of refs to this run
 	def add_refs(self, *refs):
 		for r in refs:
 			if o.SYMBOLIC not in self.options and r.is_symbolic():
 				continue
 
 			self._refs[type(r)].append(r)
+
+	# Categorize and add a sequence of exits to this run
+	def add_exits(self, *exits):
+		for e in exits:
+			if o.SYMBOLIC not in self.options and e.sim_value.is_symbolic():
+				continue
+
+			self._exits.append(e)
 
 	# Copy the references
 	def copy_refs(self, other):
