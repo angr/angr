10378e45c0c506cd87a399cd5f7e5e2cb500ca99
Yan <yans@yancomm.net>
2014-01-10 17:05:25 -0800

    calling conventions

--- simuvex/s_procedure.py
+++ simuvex/s_procedure.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from .s_run import SimRun
+from .s_exception import SimProcedureError
 import itertools
 
 import logging
@@ -10,25 +11,55 @@ symbolic_count = itertools.count()
 
 class SimProcedure(SimRun):
 	# The SimProcedure constructor, when receiving a None mode and options, defaults to mode="static"
-	def __init__(self, state, procedure_id=None, options=None, mode=None, convention="cdecl"):
+	#
+	#	calling convention is one of: "systemv_x64", "syscall", "microsoft_x64", "cdecl", "arm", "mips"
+	def __init__(self, state, procedure_id=None, options=None, mode=None, convention=None):
 		SimRun.__init__(self, options=options, mode=mode)
 		self.id = procedure_id
 		self.initial_state = state.copy_after()
 		self.addr_from = -1
+
+		if convention is None:
+			if self.initial_state.arch.name == "AMD64":
+				convention = "systemv_x64"
+			elif self.initial_state.arch.name == "x86":
+				convention = "cdecl"
+			elif self.initial_state.arch.name == "arm":
+				convention = "arm"
+			elif self.initial_state.arch.name == "mips":
+				convention = "os2_mips"
+
 		self.convention = convention
 
-	# Returns a bitvector expression representing the nth argument of a function
-	def get_function_arg_expr(self, index):
-		pass
+	# Helper function to get an argument, given a list of register locations it can be and stack information for overflows.
+	def arg_reg_stack(self, reg_offsets, stack_skip, stack_step, index):
+		if index < len(reg_offsets):
+			return self.initial_state.reg_expr(reg_offsets[index])
+		else:
+			index -= len(reg_offsets)
+			return self.initial_state.stack_read(stack_step * (index + stack_skip))
 
-	# Returns a bitvector expression representing the nth argument of a syscall
-	def get_syscall_arg_expr(self, index):
-		pass
+	# Returns a bitvector expression representing the nth argument of a function
+	def get_arg_expr(self, index):
+		if self.convention == "systemv_x64" and self.initial_state.arch.name == "AMD64":
+			reg_offsets = [ 72, 64, 32, 24, 80, 88 ] # rdi, rsi, rdx, rcx, r8, r9
+			return self.arg_reg_stack(reg_offsets, 1, -8, index)
+		elif self.convention == "syscall" and self.initial_state.arch.name == "AMD64":
+			reg_offsets = [ 72, 64, 32, 96, 80, 88 ] # rdi, rsi, rdx, r10, r8, r9
+			return self.arg_reg_stack(reg_offsets, 1, -8, index)
+
+		raise SimProcedureError("Unsupported calling convention %s for arguments", self.convention)
 
 	# Sets an expression as the return value. Also updates state.
 	def set_return_expr(self, expr):
-		pass
+		if self.initial_state.arch.name == "AMD64":
+			self.initial_state.store_reg(16, expr)
+
+		raise SimProcedureError("Unsupported calling convention %s for returns", self.convention)
 
 	# Does a return (pop, etc) and returns an bitvector expression representing the return value. Also updates state.
 	def do_return(self):
-		pass
+		if self.initial_state.arch.name == "AMD64":
+			return self.initial_state.stack_pop()
+
+		raise SimProcedureError("Unsupported platform %s for return emulation.", self.initial_state.arch.name)
