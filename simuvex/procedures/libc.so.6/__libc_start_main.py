485e1d96823a7abe55922db9032b5a617622dddc
Fish <fish@cs.ucsb.edu>
2014-01-11 15:22:54 -0800

    Updated procedure modules according to new SimProcedure interfaces.

--- simuvex/procedures/libc.so.6/__libc_start_main.py
+++ simuvex/procedures/libc.so.6/__libc_start_main.py
@@ -8,19 +8,17 @@ import symexec
 import struct
 
 class __libc_start_main(simuvex.SimProcedure):
-	def __init__(self, state, options=[], mode="static"):
-		simuvex.SimProcedure.__init__(self, state, options=options, mode=mode)
-
+	def handle():
 		# TODO: Access different registers on different archs
 		# TODO: handle symbolic and static modes
-		if isinstance(self.initial_state.arch, simuvex.SimAMD64):
+		if isinstance(self.state.arch, simuvex.SimAMD64):
 			# Get main pc from registers
-			rdi_val = self.initial_state.registers.read_from(72, 8)
+			rdi_val = self.state.registers.read_from(72, 8)
 			main_addr = simuvex.SimValue(rdi_val)
-			self.add_exits(simuvex.s_exit.SimExit(expr = main_addr.expr, state = state))
+			self.add_exits(simuvex.s_exit.SimExit(expr = main_addr.expr, state = self.state))
 
 			# TODO: address from
 			# TODO: What should we do for the addr parameter?
 			self.add_refs(simuvex.SimCodeRef(-1, -1, main_addr, [ ], [ ]))
 		else:
-			raise Exception("Architecture %s is not supported yet." % self.initial_state.arch)
+			raise Exception("Architecture %s is not supported yet." % self.state.arch)
