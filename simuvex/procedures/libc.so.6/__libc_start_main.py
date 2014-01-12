7d4d2a002cd4b6fd12c7ca8e6eddc8af0a0a0a47
Fish <fish@cs.ucsb.edu>
2014-01-11 20:52:54 -0800

    wtf I don't remember what I have modified before an hour.

--- simuvex/procedures/libc.so.6/__libc_start_main.py
+++ simuvex/procedures/libc.so.6/__libc_start_main.py
@@ -1,12 +1,8 @@
 import simuvex
-import symexec
 
 ######################################
 # __libc_start_main
 ######################################
-
-import struct
-
 class __libc_start_main(simuvex.SimProcedure):
 	def __init__(self):
 		# TODO: Access different registers on different archs
@@ -15,10 +11,10 @@ class __libc_start_main(simuvex.SimProcedure):
 			# Get main pc from registers
 			rdi_val = self.state.registers.read_from(72, 8)
 			main_addr = simuvex.SimValue(rdi_val)
-			self.add_exits(simuvex.s_exit.SimExit(expr = main_addr.expr, state = self.state))
+			self.add_exits(simuvex.s_exit.SimExit(expr=main_addr.expr, state=self.state))
 
 			# TODO: address from
 			# TODO: What should we do for the addr parameter?
-			self.add_refs(simuvex.SimCodeRef(-1, -1, main_addr, [ ], [ ]))
+			self.add_refs(simuvex.SimCodeRef(-1, -1, main_addr, [], []))
 		else:
 			raise Exception("Architecture %s is not supported yet." % self.state.arch)
