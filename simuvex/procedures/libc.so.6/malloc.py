2902ab639fd2684156bf14fdaf96242725ada2c1
Nilo Redini <n.redini@gmail.com>
2014-01-13 15:25:39 -0800

    fixed malloc and free

--- simuvex/procedures/libc.so.6/malloc.py
+++ simuvex/procedures/libc.so.6/malloc.py
@@ -6,22 +6,30 @@ import itertools
 ######################################
 
 malloc_mem_counter = itertools.count()
+heap_id = 'heap'
 
 class malloc(simuvex.SimProcedure):
         def __init__(self):
-            simuvex.SimProcedure.__init__(self, convention='cdecl')
-            if isinstance(self.state.arch, simuvex.SimAMD64):
-                size = self.get_arg_expr(0)
-                
-                #TODO symbolic size!
-                addr = self.state.plugin('libc').heap_location
-                self.state.plugin('libc').heap_location += size
-                mem_id = "%x_%d" % (addr, malloc_mem_counter.next())
-                v = symexec.BitVec(mem_id, size)
-                self.state.store_mem(addr, v)
+                #TODO add convention cdecl
+                simuvex.SimProcedure.__init__(self)
+                if isinstance(self.state.arch, simuvex.SimAMD64):
+                        sim_size = simuvex.SimValue(self.get_arg_expr(0))
+                        if sim_size.is_symbolic():
+                                #TODO: find a better way
+                                size = sim_size.max()
+                                pass
+                        else:
+                                size = sim_size.any()
 
-                #TODO: also SimMemRef??
-                #ask idx???
-                self.exit_return(addr)
-            else:
-                raise Exception("Architecture %s is not supported yet." % self.state.arch)
+                        size *= 8
+                        addr = self.state.get_plugin('libc').heap_location
+                        self.state.get_plugin('libc').heap_location += size
+                        mem_id = "%s_%x_%d" % (heap_id, addr, malloc_mem_counter.next())
+                        v = symexec.BitVec(mem_id, size)
+                        self.state.store_mem(addr, v)
+
+                        #TODO: also SimMemRef??
+                        #ask idx???
+                        self.exit_return(addr)
+                else:
+                        raise Exception("Architecture %s is not supported yet." % self.state.arch)
