bbc9713a59be590e3b75f5381deb9a812182d789
Nilo Redini <n.redini@gmail.com>
2014-01-14 00:09:03 -0800

    added close syscall

---
+++ simuvex/procedures/syscalls/close.py
@@ -0,0 +1,18 @@
+import simuvex
+
+######################################
+# close
+######################################
+
+class close(simuvex.SimProcedure):
+        def __init__(self, ret_expr): # pylint: disable=W0231
+                # TODO: Symbolic fd
+                fd = self.get_arg_value(0)
+                
+                # TODO handle errors
+                plugin.close(fd.expr)
+
+                v = symexec.BitVecVal(0, self.state.arch.bits)
+                self.set_return_expr(simuvex.SimValue(v))
+                # TODO: code referencies?
+                self.add_exits(simuvex.SimExit(expr=ret_expr, state=self.state))
