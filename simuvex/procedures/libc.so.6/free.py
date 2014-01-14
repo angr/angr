048f61994e11431cd336ea77768d9d98e1218268
Fish <fish@cs.ucsb.edu>
2014-01-13 17:22:40 -0800

    Modified the indents of procedures/libc.so.6/free.py

--- simuvex/procedures/libc.so.6/free.py
+++ simuvex/procedures/libc.so.6/free.py
@@ -5,9 +5,9 @@ import simuvex
 ######################################
 class free(simuvex.SimProcedure):
 	def __init__(self):
-            simuvex.SimProcedure.__init__(self, convention='cdecl')            
+		simuvex.SimProcedure.__init__(self, convention='cdecl')
 		if isinstance(self.state.arch, simuvex.SimAMD64):
-                    # TODO: if the return address cannot be concretized? 
-                    self.exit_return()
+			# TODO: if the return address cannot be concretized?
+			self.exit_return()
 		else:
 			raise Exception("Architecture %s is not supported yet." % self.state.arch)
