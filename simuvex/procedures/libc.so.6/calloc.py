b288972eb9c0238f4e23f5322c66d60518e8f6e1
Nilo Redini <n.redini@gmail.com>
2014-01-13 17:25:04 -0800

    fixed malloc and calloc

--- simuvex/procedures/libc.so.6/calloc.py
+++ simuvex/procedures/libc.so.6/calloc.py
@@ -7,22 +7,41 @@ import itertools
 ######################################
 
 calloc_mem_counter = itertools.count()
+#TODO move them in a shared location
+heap_id = 'heap'
+max_mem_allocable_per_variable = 2 ** 16
 
 class calloc(simuvex.SimProcedure):
         def __init__(self):
-            simuvex.SimProcedure.__init__(self, convention='cdecl')
-            if isinstance(self.initial_state.arch, simuvex.SimAMD64):
-                nmemb = self.get_arg_expr(0)
-                size = self.get_arg_expr(1)
+                #add convention
+                simuvex.SimProcedure.__init__(self)
+                if isinstance(self.initial_state.arch, simuvex.SimAMD64):
+                        sim_nmemb = simuvex.SimValue(self.get_arg_expr(0))
+                        sim_size = simuvex.SimValue(self.get_arg_expr(1))
 
-                #TODO symbolic size!
-                addr = self.initial_state.plugin('libc').heap_location
-                self.initial_state.plugin('libc').heap_location += (size*nmemb)
-                v = symexec.BitVecVal(0, size*nmemb)
-                self.state.store_mem(addr, v)
-                #TODO: also SimMemRef??
-                #ask idx???
-                self.exit_return(addr)
+                        if sim_nmemb.is_symbolic():
+                                #TODO: find a better way
+                                nmemb = sim_nmemb.max()
+                        else:
+                                nmemb = sim_nmemb.any()
 
-            else:
-                raise Exception("Architecture %s is not supported yet." % self.initial_state.arch)
+                        if sim_size.is_symbolic():
+                                #TODO: find a better way
+                                size = sim_size.max()
+                        else:
+                                size = sim_size.any()
+
+                        final_size = size * nmemb * 8
+                        if final_size > max_mem_allocable_per_variable:
+                                final_size = max_mem_allocable_per_variable
+
+                        addr = self.initial_state.get_plugin('libc').heap_location
+                        self.initial_state.get_plugin('libc').heap_location += final_size
+                        v = symexec.BitVecVal(0, final_size)
+                        self.state.store_mem(addr, v)
+                        #TODO: also SimMemRef??
+                        #ask idx???
+                        self.exit_return(addr)
+
+                else:
+                        raise Exception("Architecture %s is not supported yet." % self.initial_state.arch)
--- simuvex/procedures/libc.so.6/malloc.py
+++ simuvex/procedures/libc.so.6/malloc.py
@@ -7,21 +7,22 @@ import itertools
 
 malloc_mem_counter = itertools.count()
 heap_id = 'heap'
+max_mem_allocable_per_variable = 2 ** 16
 
 class malloc(simuvex.SimProcedure):
         def __init__(self):
-                #TODO add convention cdecl
+                #TODO add convention
                 simuvex.SimProcedure.__init__(self)
                 if isinstance(self.state.arch, simuvex.SimAMD64):
                         sim_size = simuvex.SimValue(self.get_arg_expr(0))
                         if sim_size.is_symbolic():
                                 #TODO: find a better way
-                                size = sim_size.max()
-                                pass
+                                size = sim_size.max() * 8
+                                if size > max_mem_allocable_per_variable:
+                                        size = max_mem_allocable_per_variable
                         else:
-                                size = sim_size.any()
+                                size = sim_size.any() * 8
 
-                        size *= 8
                         addr = self.state.get_plugin('libc').heap_location
                         self.state.get_plugin('libc').heap_location += size
                         mem_id = "%s_%x_%d" % (heap_id, addr, malloc_mem_counter.next())
