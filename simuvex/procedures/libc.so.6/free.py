1d9322a18bc421f40651e1939131081c49bce484
Yan <yans@yancomm.net>
2014-01-15 21:45:02 -0800

    modernization

--- simuvex/procedures/libc.so.6/free.py
+++ simuvex/procedures/libc.so.6/free.py
@@ -5,9 +5,5 @@ import simuvex
 ######################################
 class free(simuvex.SimProcedure):
 	def __init__(self):
-		simuvex.SimProcedure.__init__(self, convention='cdecl')
-		if isinstance(self.state.arch, simuvex.SimAMD64):
-			# TODO: if the return address cannot be concretized?
-			self.exit_return()
-		else:
-			raise Exception("Architecture %s is not supported yet." % self.state.arch)
+		# TODO: if the return address cannot be concretized?
+		self.exit_return()
