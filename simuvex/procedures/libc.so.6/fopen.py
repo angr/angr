cbbf2675fa3d3825b998aefa0f96df806c4dd502
Nilo Redini <n.redini@gmail.com>
2014-02-02 00:13:41 -0800

    fixed errors

--- simuvex/procedures/libc.so.6/fopen.py
+++ simuvex/procedures/libc.so.6/fopen.py
@@ -7,12 +7,12 @@ import simuvex
 def mode_to_flag(mode):
     #TODO improve this: handle mode = strings
     return {
-        'r'  : simuvex.Flags.O_RDONLY, 
-        'r+' : simuvex.Flags.O_RDWR,
-        'w'  : simuvex.Flags.O_WRTONLY | simuvex.Flags.O_CREAT,
-        'w+' : simuvex.Flags.O_RDWR | simuvex.Flags.O_CREAT,
-        'a'  : simuvex.Flags.O_WRTONLY | simuvex.Flags.O_CREAT | simuvex.O_APPEND
-        'a+' : simuvex.Flags.O_RDWR | simuvex.Flags.O_CREAT | simuvex.O_APPEND
+        "r"  : simuvex.Flags.O_RDONLY, 
+        "r+" : simuvex.Flags.O_RDWR,
+        "w"  : simuvex.Flags.O_WRTONLY | simuvex.Flags.O_CREAT,
+        "w+" : simuvex.Flags.O_RDWR | simuvex.Flags.O_CREAT,
+        "a"  : simuvex.Flags.O_WRTONLY | simuvex.Flags.O_CREAT | simuvex.O_APPEND,
+        "a+" : simuvex.Flags.O_RDWR | simuvex.Flags.O_CREAT | simuvex.O_APPEND
         }[mode]
 
 class fopen(simuvex.SimProcedure):
