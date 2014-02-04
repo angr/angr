0c3485140c2c6782f2ac854ede5d37f0e37e8290
Nilo Redini <n.redini@gmail.com>
2014-02-03 16:27:39 -0800

    fixed return value

--- simuvex/procedures/libc.so.6/__vsnprintf_chk.py
+++ simuvex/procedures/libc.so.6/__vsnprintf_chk.py
@@ -8,6 +8,8 @@ class __vsnprintf_chk(simuvex.SimProcedure):
 	def __init__(self):
 		# This function returns
 		# Add another exit to the retn_addr that is at the top of the stack now
-		retn_addr = self.do_return()
-		# TODO: What if the return address cannot be concretized? Then it won't add any exit there
-		self.add_exits(simuvex.s_exit.SimExit(expr = retn_addr, state = self.state))
+		str_ptr = self.get_arg_value(0)
+		size = self.get_arg_value(1)
+		format = self.get_arg_value(2)
+		ap = self.get_arg_value(3)
+		self.exit_return(size.expr - 1)
