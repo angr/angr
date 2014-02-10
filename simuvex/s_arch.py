d234a22a6126d5608bfe57fbe477c7b76b87d200
Fish <fish@cs.ucsb.edu>
2014-02-09 23:40:43 -0800

    Fixed a bug in SimArch.get_ret_irsb(). Added support for get_nop_irsb() in SimArch.

--- simuvex/s_arch.py
+++ simuvex/s_arch.py
@@ -14,7 +14,7 @@ class SimArchError(s_exception.SimError):
 	pass
 
 class SimArch:
-	def __init__(self, bits, vex_arch, name, max_inst_bytes, ip_offset, sp_offset, bp_offset, ret_offset, stack_change, endness, ret_instruction):
+	def __init__(self, bits, vex_arch, name, max_inst_bytes, ip_offset, sp_offset, bp_offset, ret_offset, stack_change, endness, ret_instruction, nop_instruction):
 		self.bits = bits
 		self.vex_arch = vex_arch
 		self.name = name
@@ -26,28 +26,32 @@ class SimArch:
 		self.endness = endness
 		self.stack_change = stack_change
 		self.ret_instruction = ret_instruction
+		self.nop_instruction = nop_instruction
 
 	def get_ret_irsb(self, inst_addr):
-		return pyvex.IRSB(bytes=self.ret_instruction, mem_addr=inst_addr, arch="VexArchAMD64")
+		return pyvex.IRSB(bytes=self.ret_instruction, mem_addr=inst_addr, arch=self.vex_arch)
+	
+	def get_nop_irsb(self, inst_addr):
+		return pyvex.IRSB(bytes=self.nop_instruction, mem_addr=inst_addr, arch=self.vex_arch)
 
 class SimAMD64(SimArch):
 	def __init__(self):
-		SimArch.__init__(self, 64, "VexArchAMD64", "AMD64", 15, 184, 48, 56, 16, -8, "Iend_LE", "\xc3")
+		SimArch.__init__(self, 64, "VexArchAMD64", "AMD64", 15, 184, 48, 56, 16, -8, "Iend_LE", "\xc3", "\x90")
 
 class SimX86(SimArch):
 	def __init__(self):
-		SimArch.__init__(self, 32, "VexArchX86", "X86", 15, 68, 24, 28, 8, -4, "Iend_LE", "\xc3")
+		SimArch.__init__(self, 32, "VexArchX86", "X86", 15, 68, 24, 28, 8, -4, "Iend_LE", "\xc3", "\x90")
 
 class SimARM(SimArch):
 	def __init__(self):
 		# TODO: determine proper base register (if it exists)
 		# TODO: handle multiple return registers?
-		SimArch.__init__(self, 32, "VexArchARM", "ARM", 4, 68, 60, 60, 8, -4, "Iend_LE", "\xE1\xA0\xF0\x0E")
+		SimArch.__init__(self, 32, "VexArchARM", "ARM", 4, 68, 60, 60, 8, -4, "Iend_LE", "\xE1\xA0\xF0\x0E", "\x00\x00\x00\x00")
 
 class SimMIPS32(SimArch):
 	def __init__(self):
 		# TODO: multiple return registers?
-		SimArch.__init__(self, 32, "VexArchMIPS32", "MIPS32", 4, 128, 116, 120, 8, -4, "Iend_BE", "TODO")
+		SimArch.__init__(self, 32, "VexArchMIPS32", "MIPS32", 4, 128, 116, 120, 8, -4, "Iend_BE", "TODO", "\x00\x00\x00\x00")
 
 class SimPPC32(SimArch):
 	def __init__(self):
@@ -55,7 +59,7 @@ class SimPPC32(SimArch):
 		# PowerPC doesn't have stack base pointer, so bp_offset is set to -1 below
 		# Normally r1 is used as stack pointer
 		# TODO: Return instruction
-		SimArch.__init__(self, 32, "VexArchPPC32", "PPC32", 4, 1152, 4, -1, 8, -4, "Iend_BE", "\x4e\x80\x00\x20") # 4e800020: blr
+		SimArch.__init__(self, 32, "VexArchPPC32", "PPC32", 4, 1152, 4, -1, 8, -4, "Iend_BE", "\x4e\x80\x00\x20", "\x60\x00\x00\x00") # 4e800020: blr
 
 Architectures = { }
 Architectures["AMD64"] = SimAMD64()
