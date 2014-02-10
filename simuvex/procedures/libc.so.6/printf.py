3511b41dfa9fdb1540992ea4d9aea5f26377e2af
Yan <yans@yancomm.net>
2014-02-10 14:06:31 +0100

    fixed return

--- simuvex/procedures/libc.so.6/printf.py
+++ simuvex/procedures/libc.so.6/printf.py
@@ -12,7 +12,5 @@ class printf(simuvex.SimProcedure):
 	def __init__(self):
 		# This function returns
 		# Add another exit to the retn_addr that is at the top of the stack now
-		retn_addr = self.do_return()
-		# TODO: What if the return address cannot be concretized? Then it won't add any exit there
-		self.add_exits(simuvex.s_exit.SimExit(expr = retn_addr, state = self.state))
+		retn_addr = self.exit_return()
 		l.debug("Got return address for %s: 0x%08x.", __file__, self._exits[0].concretize())
