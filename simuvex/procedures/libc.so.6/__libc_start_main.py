e205802d7543caecb9ae00c30e79b03d85880f21
Yan <yans@yancomm.net>
2014-02-10 14:06:40 +0100

    added bizarre case for PPC32

--- simuvex/procedures/libc.so.6/__libc_start_main.py
+++ simuvex/procedures/libc.so.6/__libc_start_main.py
@@ -7,8 +7,12 @@ class __libc_start_main(simuvex.SimProcedure):
 	def __init__(self):
 		# TODO: handle symbolic and static modes
 
-		# Get main pc from registers
-		main_addr = self.get_arg_value(0)
-		self.add_exits(simuvex.s_exit.SimExit(expr=main_addr.expr, state=self.state))
+		if self.state.arch.name == "PPC32":
+			# for some dumb reason, PPC32 passes arguments to libc_start_main in some completely absurd way
+			main_addr = self.state.mem_value(self.state.reg_expr(48) + 4, 4)
+		else:
+			# Get main pc from arguments
+			main_addr = self.get_arg_value(0)
 
+		self.add_exits(simuvex.s_exit.SimExit(expr=main_addr.expr, state=self.state))
 		self.add_refs(simuvex.SimCodeRef(self.addr, self.stmt_from, main_addr, [], []))
