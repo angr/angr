5db1d18b5a9c0d19d256402a73e4c840a00680c6
Fish <fish@cs.ucsb.edu>
2014-02-12 09:34:26 -0800

    Added a SimProcedure stub PathTerminator.

--- simuvex/procedures/__init__.py
+++ simuvex/procedures/__init__.py
@@ -7,18 +7,23 @@ import imp
 # lib names.
 SimProcedures = defaultdict(dict)
 path = os.path.dirname(os.path.abspath(__file__))
+skip_dirs = []
 
-for libname in [f for f in os.listdir(path) if f != "__init__.py"]:
+for libname in [f for f in os.listdir(path)
+				if f != "__init__.py" and f not in skip_dirs]:
 	lib_path = os.path.join(path, libname)
 	if not os.path.isdir(lib_path):
 		continue
-	for py in [f[ : -3 ] for f in os.listdir(lib_path) if f.endswith(".py")]:
+	for py in [f[: -3] for f in os.listdir(lib_path)
+			if f.endswith(".py")]:
 		module_file_path = os.path.join(lib_path, py) + ".py"
 		with open(module_file_path) as module_file:
-			mod = imp.load_module(py, module_file, module_file_path, (".py", "rb", imp.PY_SOURCE))
+			mod = imp.load_module(py, module_file, module_file_path,
+						 (".py", "rb", imp.PY_SOURCE))
 			if py == '__init__':
 				continue
 
-			classes = [getattr(mod, x) for x in dir(mod) if isinstance(getattr(mod, x), type)]
+			classes = [getattr(mod, x) for x in dir(mod)
+						if isinstance(getattr(mod, x), type)]
 			for class_ in classes:
 				SimProcedures[libname][class_.__name__] = class_
---
+++ simuvex/procedures/stubs/PathTerminator.py
@@ -0,0 +1,9 @@
+import simuvex
+
+######################################
+# Path terminator
+######################################
+
+class PathTerminator(simuvex.SimProcedure):
+    def __init__(self):
+        return
