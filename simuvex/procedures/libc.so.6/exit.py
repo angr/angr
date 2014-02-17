165c716769e99e2a43c69beee27d0ff342f18bad
Fish <fish@cs.ucsb.edu>
2014-02-16 19:00:09 -0800

    Added libc.exit in SimProcedure.

---
+++ simuvex/procedures/libc.so.6/exit.py
@@ -0,0 +1,9 @@
+import simuvex
+
+######################################
+# exit
+######################################
+
+class exit(simuvex.SimProcedure):
+    def __init__(self):
+        return
