d04d124e340a9c79c6f8a1b8c75a40305acb3a8e
Fish <fish@cs.ucsb.edu>
2014-02-24 17:12:11 -0800

    Make Executor compliant with the new Surveyor design.

--- surveyors/executor.py
+++ surveyors/executor.py
@@ -11,19 +11,21 @@ class Executor(Surveyor):
     No state splitting is ever allowed.
     '''
     def __init__(self, project, start, final_addr=None, \
-                 max_run=500):
+                 max_run=50000):
         if simuvex.o.SYMBOLIC in start.state.options:
             raise Exception("Executor doesn't support symbolic execution.")
         Surveyor.__init__(self, project, start=start)
         self._project = project
         self._final_addr = final_addr
-        self._max_run = 5000
+        self._max_run = max_run
         self._done = False
         self._error_occured = False
         self._run_counter = 0
 
     @property
     def done(self):
+        if self.error_occured:
+            return True
         if len(self.active) > 1:
             raise Exception("We have more than one path in concrete mode." + \
                             " Something is wrong.")
@@ -56,9 +58,7 @@ class Executor(Surveyor):
     def tick(self):
         Surveyor.tick(self)
         self._run_counter += 1
-        l.debug("Running %d run...", self._run_counter)
-
-    def run(self):
-        while not (self.done or self.error_occured):
-            self.tick()
-        return self
+        if len(self.active) > 0:
+            l.debug("Running %d run %s...", self._run_counter, self.active[0].last_run)
+        else:
+            l.debug("Running %d run...", self._run_counter)
