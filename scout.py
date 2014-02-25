85def34fe94d3d76bac015d762731252536d8664
Fish <fish@cs.ucsb.edu>
2014-02-24 17:11:44 -0800

    Many bug fixes in scout.py.

--- scout.py
+++ scout.py
@@ -1,5 +1,6 @@
 import logging
 import string
+from collections import defaultdict
 
 import networkx
 
@@ -56,12 +57,16 @@ class SegmentList(object):
             else:
                 self._list.insert(idx, (address, address + size))
 
+    def has_blocks(self):
+        return len(self._list) > 0
+
     def next_free_pos(self, address):
         idx = self._search(address + 1)
-        if address >= self._list[idx][0] and address < self._list[idx][1]:
+        if idx < len(self._list) and \
+                address >= self._list[idx][0] and address < self._list[idx][1]:
             # Occupied
             i = idx
-            while i < len(self._list) and \
+            while i + 1 < len(self._list) and \
                     self._list[i][1] == self._list[i + 1][0]:
                 i += 1
             if i == len(self._list):
@@ -81,7 +86,7 @@ class SegmentList(object):
         return False
 
     def occupy(self, address, size):
-        l.debug("Occpuying 0x%08x-0x%08x", address, address + size)
+        # l.debug("Occpuying 0x%08x-0x%08x", address, address + size)
         if len(self._list) == 0:
             self._list.append((address, address + size))
             return
@@ -117,7 +122,7 @@ class SegmentList(object):
                 # Shall we merge it with the previous one?
                 self._merge_check(address, size, idx)
         # l.debug(self._dbg_output())
-        self._debug_check()
+        # self._debug_check()
 
     def _dbg_output(self):
         s = "["
@@ -154,34 +159,35 @@ class Scout(object):
         # Create the segment list
         self._seg_list = SegmentList()
 
-    def _get_next_addr_to_search(self):
+    def _get_next_addr_to_search(self, alignment=None):
         # TODO: Take care of those functions that are already generated
         curr_addr = self._next_addr
         # Determine the size of that IRSB
         # Note: we don't care about SimProcedure at this moment, as we want to
         # get as many functions as possible
-        s_irsb = None
-        instr_alignment = \
-            self._project.initial_state(mode="static").arch.instruction_alignment
-        while s_irsb is None:
-            s_ex = self._project.exit_to(addr=curr_addr, \
-                            state=self._project.initial_state(mode="static"))
-            try:
-                s_irsb = self._project.sim_block(s_ex)
-            except simuvex.s_irsb.SimIRSBError:
-                # We cannot build functions there
-                # Move on to next possible position
-                s_irsb = None
-                # TODO: Handle strings
-                curr_addr = \
-                    self._seg_list.next_free_pos(curr_addr + instr_alignment - 1)
-                if curr_addr % instr_alignment > 0:
-                    curr_addr = curr_addr - curr_addr % instr_alignment + \
-                        instr_alignment
+        # s_irsb = None
+        # while s_irsb is None:
+        #     s_ex = self._project.exit_to(addr=curr_addr, \
+        #                     state=self._project.initial_state(mode="static"))
+        #     try:
+        #         s_irsb = self._project.sim_block(s_ex)
+        #     except simuvex.s_irsb.SimIRSBError:
+        #         # We cannot build functions there
+        #         # Move on to next possible position
+        #         s_irsb = None
+        #         # TODO: Handle strings
+        #         curr_addr = \
+        #             self._seg_list.next_free_pos(curr_addr)
+        if self._seg_list.has_blocks:
+            curr_addr = self._seg_list.next_free_pos(curr_addr)
 
-        block_size = s_irsb.irsb.size()
-        self._next_addr = curr_addr + block_size
-        l.debug("Returning new recon address: 0x%08x", curr_addr)
+        if alignment is not None:
+            if curr_addr % alignment > 0:
+                curr_addr = curr_addr - curr_addr % alignment + alignment
+        # block_size = s_irsb.irsb.size()
+        # self._next_addr = curr_addr + block_size
+        self._next_addr = curr_addr
+        # l.debug("Returning new recon address: 0x%08x", curr_addr)
         return curr_addr
 
     def _get_next_code_addr(self, initial_state):
@@ -190,30 +196,78 @@ class Scout(object):
         address seems to be code or not. If not, we'll move on to request for
         next valid address.
         '''
-        next_addr = 0
+        next_addr = self._get_next_addr_to_search()
+        start_addr = next_addr
+        sz = ""
         is_sz = True
         while is_sz:
-            next_addr = self._get_next_addr_to_search()
             # Get data until we meet a 0
-            sz = ""
             while True:
                 try:
                     val = initial_state.mem_concrete(next_addr, 1)
                     if val == 0:
-                        if len(sz) == 0:
+                        if len(sz) < 4:
                             is_sz = False
+                        else:
+                            reach_end = True
                         break
                     if chr(val) not in string.printable:
                         is_sz = False
                         break
                     sz += chr(val)
+                    next_addr += 1
                 except symexec.SymbolicError:
                     # Not concretizable
                     break
+
             if is_sz:
                 l.debug("Got string [%s]", sz)
+                # l.debug("Occpuy %x - %x", start_addr, start_addr + len(sz) + 1)
+                self._seg_list.occupy(start_addr, len(sz) + 1)
+                sz = ""
+                next_addr = self._get_next_addr_to_search()
+                # l.debug("next addr = %x", next_addr)
+                start_addr = next_addr
 
-        return next_addr
+        # Let's search for function prologs
+        instr_alignment = initial_state.arch.instruction_alignment
+        if start_addr % instr_alignment > 0:
+            start_addr = start_addr - start_addr % instr_alignment + \
+                instr_alignment
+
+        while True:
+            try:
+                s = initial_state.mem_value(start_addr, 4).any_str()
+            except simuvex.ConcretizingException:
+                # Memory doesn't exist
+                return None
+            if s.startswith(initial_state.arch.function_prologs):
+                break
+            start_addr = self._get_next_addr_to_search(alignment=instr_alignment)
+
+        return start_addr
+
+    def _symbolic_reconnoiter(self, addr, target_addr, max_depth=10):
+        '''
+        When an IRSB has more than two exits (for example, a jumptable), we
+        cannot concretize their exits in concrete mode. Hence we statically
+        execute the function from beginning in this method, and then switch to
+        symbolic mode for the final IRSB to get all possible exits of that
+        IRSB.
+        '''
+        state = self._project.initial_state(mode="symbolic")
+        state.options.add(simuvex.o.CALLLESS)
+        initial_exit = self._project.exit_to(addr=addr, state=state)
+        explorer = angr.surveyors.Explorer(self._project, \
+                                           start=initial_exit, \
+                                           max_depth=max_depth, \
+                                           find=(target_addr), num_find=1).run()
+        if len(explorer.found) > 0:
+            path = explorer.found[0]
+            last_run = path.last_run
+            return last_run.flat_exits()
+        else:
+            return []
 
     def reconnoiter(self):
         '''
@@ -239,20 +293,35 @@ class Scout(object):
         self._functions = set()
         self._call_map = networkx.DiGraph()
         initial_state = self._project.initial_state(mode="concrete")
-        initial_state.options.remove(simuvex.o.COW_STATES)
+        initial_options = initial_state.options
+        # initial_options.remove(simuvex.o.COW_STATES)
+        initial_state.options = initial_options
+        # Sadly, not all call to functions are explicitly made by call
+        # instruction - they could be a jmp or b, or something else. So we
+        # should record all exits from a single function, and then add
+        # necessary calling edges in our call map during the post-processing
+        # phase.
+        function_exits = defaultdict(set)
         while True:
             if len(remaining_exits) == 0:
                 # Load a possible position
-                remaining_exits.add((-1, self._get_next_addr_to_search(), \
+                next_addr = self._get_next_code_addr(initial_state)
+                if next_addr is None:
+                    break
+                remaining_exits.add((next_addr, \
+                                     next_addr, \
                                      initial_state.copy_after()))
+                self._call_map.add_node(next_addr)
 
-            current_function_addr, exit_addr, state = remaining_exits.pop()
+            current_function_addr, exit_addr, parent_addr, state = \
+                remaining_exits.pop()
+            if exit_addr in traced_address:
+                continue
             if current_function_addr != -1:
                 l.debug("Tracing new exit 0x%08x in function 0x%08x", \
                         exit_addr, current_function_addr)
             else:
                 l.debug("Tracing new exit 0x%08x", exit_addr)
-            l.debug("Function calls: %d", len(self._call_map.nodes()))
             traced_address.add(exit_addr)
             # Get a basic block
             s_ex = self._project.exit_to(addr=exit_addr, state=state)
@@ -263,12 +332,49 @@ class Scout(object):
             except angr.errors.AngrException:
                 # "No memory at xxx"
                 continue
+            except simuvex.ConcretizingException:
+                # Cannot concretize something when executing the SimRun
+                continue
 
             # Mark that part as occupied
             if isinstance(s_run, simuvex.SimIRSB):
                 self._seg_list.occupy(exit_addr, s_run.irsb.size())
             new_exits = s_run.exits()
             has_call_exit = False
+            tmp_exit_set = set()
+            for new_exit in new_exits:
+                if new_exit.jumpkind == "Ijk_Call":
+                    has_call_exit = True
+
+            if current_function_addr == 0xff8d9074:
+                import ipdb
+                ipdb.set_trace()
+            all_symbolic = True
+            for new_exit in new_exits:
+                if new_exit.jumpkind == "Ijk_Ret":
+                    all_symbolic = False
+                    break
+                if not new_exit.target_value.is_symbolic():
+                    all_symbolic = False
+                    break
+
+            if all_symbolic:
+                # We want to try executing this function in symbolic mode to
+                # get more exits
+                l.debug("Trying to symbolically solve IRSB at 0x%08x " + \
+                        "in function 0x%08x", \
+                        exit_addr, current_function_addr)
+                if current_function_addr == exit_addr:
+                    new_exits = self._symbolic_reconnoiter(current_function_addr, \
+                                                       exit_addr)
+                else:
+                    new_exits = self._symbolic_reconnoiter(parent_addr, \
+                                                       exit_addr)
+                # Replace the states with concrete-mode states!
+                for ex in new_exits:
+                    ex.state.options = initial_options
+                l.debug("Got %d exits from symbolic solving.", len(new_exits))
+
             for new_exit in new_exits:
                 if not has_call_exit and \
                         new_exit.jumpkind == "Ijk_Ret":
@@ -279,21 +385,45 @@ class Scout(object):
                     target_addr = new_exit.concretize()
                 except simuvex.s_value.ConcretizingException:
                     continue
-                if target_addr in traced_address:
-                    continue
+
+                # Log it before we cut the tracing :)
                 if new_exit.jumpkind == "Ijk_Call":
-                    has_call_exit = True
-                    # This is a call. Let's record it
-                    new_state = new_exit.state.copy_after()
-                    remaining_exits.add((target_addr, target_addr, new_state))
                     if current_function_addr != -1:
                         self._call_map.add_edge(current_function_addr, target_addr)
                     else:
                         self._call_map.add_node(target_addr)
                 elif new_exit.jumpkind == "Ijk_Boring" or \
                         new_exit.jumpkind == "Ijk_Ret":
+                    if current_function_addr != -1:
+                        function_exits[current_function_addr].add(target_addr)
+
+                # If we have traced it before, don't trace it anymore
+                if target_addr in traced_address:
+                    continue
+                # If we have traced it in current loop, don't tract it either
+                if target_addr in tmp_exit_set:
+                    continue
+
+                tmp_exit_set.add(target_addr)
+
+                if new_exit.jumpkind == "Ijk_Call":
+                    # This is a call. Let's record it
+                    new_state = new_exit.state.copy_after()
+                    # Unconstrain those parameters
+                    # TODO: Support other archs as well
+                    if (12 + 16) in new_state.registers.mem:
+                        del new_state.registers.mem[12 + 16]
+                    if (16 + 16) in new_state.registers.mem:
+                        del new_state.registers.mem[16 + 16]
+                    if (20 + 16) in new_state.registers.mem:
+                        del new_state.registers.mem[20 + 16]
+                    remaining_exits.add((target_addr, target_addr, exit_addr, new_state))
+                    l.debug("Function calls: %d", len(self._call_map.nodes()))
+                elif new_exit.jumpkind == "Ijk_Boring" or \
+                        new_exit.jumpkind == "Ijk_Ret":
+                    new_state = new_exit.state.copy_after()
                     remaining_exits.add((current_function_addr, target_addr, \
-                                         new_exit.state))
+                                         exit_addr, new_state))
                 elif new_exit.jumpkind == "Ijk_NoDecode":
                     # That's something VEX cannot decode!
                     # We assume we ran into a deadend
@@ -301,8 +431,35 @@ class Scout(object):
                 elif new_exit.jumpkind.startswith("Ijk_Sig"):
                     # Should not go into that exit
                     pass
+                elif new_exit.jumpkind == "Ijk_TInval":
+                    # ppc32: isync
+                    pass
                 else:
                     raise Exception("NotImplemented")
+
+        # Post-processing: Map those calls that are not made by call/blr
+        # instructions to their targets in our map
+        for src, s in function_exits.items():
+            if src in self._call_map:
+                for target in s:
+                    if target in self._call_map:
+                        self._call_map.add_edge(src, target)
+
+        nodes = sorted(self._call_map.nodes())
+        for i in range(len(nodes) - 1):
+            if nodes[i] >= nodes[i + 1] - 4:
+                for dst in self._call_map.successors(nodes[i + 1]):
+                    self._call_map.add_edge(nodes[i], dst)
+                for src in self._call_map.predecessors(nodes[i + 1]):
+                    self._call_map.add_edge(src, nodes[i])
+                self._call_map.remove_node(nodes[i + 1])
+
+        import pickle
+        pickle.dump(self._call_map, open("call_map", "wb"))
+        pickle.dump(function_exits, open("function_exits", "wb"))
+        l.debug("Construction finished.")
+        import ipdb
+        ipdb.set_trace()
 
     def _dbg_output(self):
         ret = ""
