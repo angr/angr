7f60dea80cda591f26cabefa8570fe2c0fd5c4b3
Fish <fish@cs.ucsb.edu>
2014-02-26 03:16:27 -0800

    Added support for showing how many paths have reached targets.

--- surveyors/slicecutor.py
+++ surveyors/slicecutor.py
@@ -10,105 +10,116 @@ from angr import Surveyor, Path
 #
 from collections import defaultdict
 class HappyGraph(object):
-	def __init__(self, path = None, paths=None, strict=False):
-		if not strict:
-			self.jumps = defaultdict(lambda: False)
-		else:
-			self.jumps = { }
+    def __init__(self, path = None, paths=None, strict=False):
+        if not strict:
+            self.jumps = defaultdict(lambda: False)
+        else:
+            self.jumps = { }
 
-		if paths is None:
-			paths = [ ]
-		if path is not None:
-			paths.append(path)
+        if paths is None:
+            paths = [ ]
+        if path is not None:
+            paths.append(path)
 
-		for p in paths:
-			for i in range(len(p.addr_backtrace) - 1):
-				self.jumps[(p.addr_backtrace[i], p.addr_backtrace[i+1])] = True
+        for p in paths:
+            for i in range(len(p.addr_backtrace) - 1):
+                self.jumps[(p.addr_backtrace[i], p.addr_backtrace[i+1])] = True
 
-	def should_take_exit(self, src, dst): # pylint: disable=W0613,R0201,
-		return self.jumps[(src, dst)]
+    def should_take_exit(self, src, dst): # pylint: disable=W0613,R0201,
+        return self.jumps[(src, dst)]
 
-	def get_whitelisted_statements(self, addr): # pylint: disable=W0613,R0201,
-		return None
+    def get_whitelisted_statements(self, addr): # pylint: disable=W0613,R0201,
+        return None
 
-	def get_last_statement_index(self, addr): # pylint: disable=W0613,R0201,
-		return None
+    def get_last_statement_index(self, addr): # pylint: disable=W0613,R0201,
+        return None
 
 class Slicecutor(Surveyor):
-	'''The Slicecutor is a surveyor that executes provided code slices.'''
+    '''The Slicecutor is a surveyor that executes provided code slices.'''
 
-	def __init__(self, project, annotated_cfg, start=None, starts=None, max_concurrency=None, pickle_paths=None):
-		Surveyor.__init__(self, project, start=None, starts=[ ], max_concurrency=max_concurrency, pickle_paths=pickle_paths)
+    def __init__(self, project, annotated_cfg, start=None, starts=None, targets=None, max_concurrency=None, pickle_paths=None):
+        Surveyor.__init__(self, project, start=None, starts=[ ], max_concurrency=max_concurrency, pickle_paths=pickle_paths)
 
-		# the project we're slicing up!
-		self._project = project
+        # the project we're slicing up!
+        self._project = project
 
-		# the annotated cfg to determine what to execute
-		self._annotated_cfg = annotated_cfg
+        # the annotated cfg to determine what to execute
+        self._annotated_cfg = annotated_cfg
 
-		# these are paths that are taking exits that the annotated CFG does not
-		# know about
-		self.mysteries = [ ]
+        # these are paths that are taking exits that the annotated CFG does not
+        # know about
+        self.mysteries = [ ]
 
-		# these are paths that we cut due to the slicing
-		self.cut = [ ]
+        # these are paths that we cut due to the slicing
+        self.cut = [ ]
 
-		# create the starting paths
-		entries = [ ]
-		if start is not None: entries.append(start)
-		if starts is not None: entries.extend(starts)
-		if len(entries) == 0:
-			entries.append(project.initial_exit())
+        # those that have reached one of our targets
+        self.reached_targets = []
 
-		l.debug("%s starting up with %d exits", self, len(entries))
-		for e in entries:
-			p = self.tick_path_exit(Path(project=project), e)
-			print p, p.last_run
-			if p is not None:
-				self.active.append(p)
-		l.debug("... which created %d paths", len(self.active))
+        if targets is not None:
+            self._targets = targets
+        else:
+            self._targets = []
 
-	def tick_path_exit(self, p, e):
-		addr = e.concretize()
-		whitelist = self._annotated_cfg.get_whitelisted_statements(addr)
-		last_stmt = self._annotated_cfg.get_last_statement_index(addr)
-		return p.continue_through_exit(e, stmt_whitelist=whitelist, last_stmt=last_stmt)
+        # create the starting paths
+        entries = [ ]
+        if start is not None: entries.append(start)
+        if starts is not None: entries.extend(starts)
+        if len(entries) == 0:
+            entries.append(project.initial_exit())
 
-	def tick_path(self, path):
-		path_exits = path.flat_exits(reachable=True)
-		new_paths = [ ]
+        l.debug("%s starting up with %d exits", self, len(entries))
+        for e in entries:
+            p = self.tick_path_exit(Path(project=project), e)
+            print p, p.last_run
+            if p is not None:
+                self.active.append(p)
+        l.debug("... which created %d paths", len(self.active))
 
-		mystery = False
-		cut = False
+    def tick_path_exit(self, p, e):
+        addr = e.concretize()
+        whitelist = self._annotated_cfg.get_whitelisted_statements(addr)
+        last_stmt = self._annotated_cfg.get_last_statement_index(addr)
+        return p.continue_through_exit(e, stmt_whitelist=whitelist, last_stmt=last_stmt)
 
-		l.debug("%s ticking path %s, last run is %s", self, path, path.last_run)
+    def tick_path(self, path):
+        path_exits = path.flat_exits(reachable=True)
+        new_paths = [ ]
 
-		for e in path_exits:
-			dst_addr = e.concretize()
-			l.debug("... checking exit to 0x%x from %s", dst_addr, path.last_run)
-			try:
-				taken = self._annotated_cfg.should_take_exit(path.last_addr, dst_addr)
-			except Exception: # TODO: which exception?
-				l.debug("... annotated CFG did not know about it!")
-				mystery = True
-				continue
+        mystery = False
+        cut = False
 
-			if taken:
-				l.debug("... taking the exit.")
-				p = self.tick_path_exit(path, e)
-				if p: new_paths.append(p)
-				# the else case isn't here, because the path should set errored in this
-				# case and we'll catch it below
-			else:
-				l.debug("... not taking the exit.")
-				cut = True
+        l.debug("%s ticking path %s, last run is %s", self, path, path.last_run)
 
-		if mystery: self.mysteries.append(path)
-		if cut: self.cut.append(path)
-		return new_paths
+        for e in path_exits:
+            dst_addr = e.concretize()
+            l.debug("... checking exit to 0x%x from %s", dst_addr, path.last_run)
+            try:
+                taken = self._annotated_cfg.should_take_exit(path.last_addr, dst_addr)
+            except Exception: # TODO: which exception?
+                l.debug("... annotated CFG did not know about it!")
+                mystery = True
+                continue
 
-	def path_comparator(self, a, b):
-		return self._annotated_cfg.path_priority(a) - self._annotated_cfg.path_priority(b)
+            if taken:
+                l.debug("... taking the exit.")
+                p = self.tick_path_exit(path, e)
+                if p: new_paths.append(p)
+                # the else case isn't here, because the path should set errored in this
+                # case and we'll catch it below
+            else:
+                l.debug("... not taking the exit.")
+                cut = True
 
-	def __str__(self):
-		return "<Slicecutor with paths: %s, %d cut, %d mysteries>" % (Surveyor.__str__(self), len(self.cut), len(self.mysteries))
+        if mystery: self.mysteries.append(path)
+        if cut: self.cut.append(path)
+        if path.last_run is not None and \
+                path.last_run.addr in self._targets:
+            self.reached_targets.append(path)
+        return new_paths
+
+    def path_comparator(self, a, b):
+        return self._annotated_cfg.path_priority(a) - self._annotated_cfg.path_priority(b)
+
+    def __str__(self):
+        return "<Slicecutor with paths: %s, %d cut, %d mysteries, %d reached targets>" % (Surveyor.__str__(self), len(self.cut), len(self.mysteries), len(self.reached_targets))
