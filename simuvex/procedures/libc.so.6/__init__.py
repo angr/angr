4216b524531cbf41a8ccfd1f632bf28d3bb96b6f
Fish <fish@cs.ucsb.edu>
2014-05-06 21:39:14 -0700

    Added global var max_mem_per_variable.

--- simuvex/procedures/libc.so.6/__init__.py
+++ simuvex/procedures/libc.so.6/__init__.py
@@ -3,26 +3,28 @@ import simuvex
 max_mem_per_variable = 2 ** 16
 
 class SimStateLibc(simuvex.SimStatePlugin):
-	'''
-	This state plugin keeps track of various libc stuff:
+    '''
+    This state plugin keeps track of various libc stuff:
 
-		heap_location: the current location of the heap
-		max_str_symbolic_bytes: the maximum number of symbolic bytes
-			that a string can have (for the str* sections).
-	'''
+        heap_location: the current location of the heap
+        max_str_symbolic_bytes: the maximum number of symbolic bytes
+            that a string can have (for the str* sections).
+    '''
 
-	#__slots__ = [ 'heap_location', 'max_str_symbolic_bytes' ]
+    #__slots__ = [ 'heap_location', 'max_str_symbolic_bytes' ]
 
-	def __init__(self, heap_location=0xc0000000, max_str_symbolic_bytes = 16):
-		simuvex.SimStatePlugin.__init__(self)
-		self.heap_location = heap_location
-		self.max_str_symbolic_bytes = max_str_symbolic_bytes
+    def __init__(self, heap_location=0xc0000000, max_str_symbolic_bytes = 16):
+        simuvex.SimStatePlugin.__init__(self)
+        self.heap_location = heap_location
+        self.max_str_symbolic_bytes = max_str_symbolic_bytes
+        # TODO: Justify this setting!
+        self.max_mem_per_variable = 10000
 
-	def copy(self):
-		return SimStateLibc(self.heap_location, self.max_str_symbolic_bytes)
+    def copy(self):
+        return SimStateLibc(self.heap_location, self.max_str_symbolic_bytes)
 
-	def merge(self, others, merge_flag, flag_values):
-		self.heap_location = max(o.heap_location for o in others)
-		return [ ]
+    def merge(self, others, merge_flag, flag_values):
+        self.heap_location = max(o.heap_location for o in others)
+        return [ ]
 
 simuvex.SimStatePlugin.register_default('libc', SimStateLibc)
