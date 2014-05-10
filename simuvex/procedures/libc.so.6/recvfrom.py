6a1a1bbd980c20c1e6f2eb60224682520d8b1352
Fish <fish@cs.ucsb.edu>
2014-05-09 21:20:33 -0700

    Added two SimProcedures in libc.so.6.

---
+++ simuvex/procedures/libc.so.6/recvfrom.py
@@ -0,0 +1,23 @@
+import simuvex
+
+######################################
+# recvfrom
+######################################
+
+class recvfrom(simuvex.SimProcedure):
+    def __init__(self): # pylint: disable=W0231
+        # TODO: Symbolic fd
+        fd = self.get_arg_value(0)
+        sim_dst = self.get_arg_value(1)
+        plugin = self.state['posix']
+
+        # TODO: Now it's limiting UDP package to 40 bytes
+        # We need to better handling for this
+        length = 40
+
+        # TODO handle errors
+        data = plugin.read(fd.expr, length)
+        self.state.store_mem(sim_dst.expr, data)
+        self.add_refs(simuvex.SimMemWrite(self.addr, self.stmt_from, sim_dst, self.state.expr_value(data), length, [], [], [], []))
+
+        self.exit_return(simuvex.SimValue(length).expr)
