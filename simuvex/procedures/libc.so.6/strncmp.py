fc12890ee2e7efa8b3020b9555c3dd6f4ce5e5b3
Yan <yans@yancomm.net>
2014-05-12 04:13:35 -0700

    fixed some cases where refs might be lost

--- simuvex/procedures/libc.so.6/strncmp.py
+++ simuvex/procedures/libc.so.6/strncmp.py
@@ -33,6 +33,8 @@ class strncmp(simuvex.SimProcedure):
 			if (c_a_len < c_limit or c_b_len < c_limit) and c_a_len != c_b_len:
 				l.debug("lengths < limit and unmatched")
 				self.exit_return(se.BitVecVal(1, self.state.arch.bits))
+				self.add_refs(simuvex.SimMemRead(self.addr, self.stmt_from, self.state.expr_value(a_addr), self.state.mem_expr(a_addr, a_len), a_len))
+				self.add_refs(simuvex.SimMemRead(self.addr, self.stmt_from, self.state.expr_value(b_addr), self.state.mem_expr(b_addr), b_len))
 				return
 
 			concrete_run = True
@@ -49,6 +51,8 @@ class strncmp(simuvex.SimProcedure):
 		if maxlen == 0:
 			l.debug("returning equal for 0-length maximum strings")
 			self.exit_return(se.BitVecVal(0, self.state.arch.bits))
+			self.add_refs(simuvex.SimMemRead(self.addr, self.stmt_from, self.state.expr_value(a_addr), self.state.mem_expr(a_addr, a_len), a_len))
+			self.add_refs(simuvex.SimMemRead(self.addr, self.stmt_from, self.state.expr_value(b_addr), self.state.mem_expr(b_addr), b_len))
 			return
 
 		# wtf, z3
