d5d343ade1334c7a3ea69831391afc6d7582dd21
Fish <fish@cs.ucsb.edu>
2014-05-13 19:41:11 -0700

    .

--- pathprioritizer.py
+++ pathprioritizer.py
@@ -2,7 +2,7 @@ import logging
 
 import networkx
 
-l = logging.getLogger("angr.PathPrioritizer")
+l = logging.getLogger("angr.pathprioritizer")
 
 class PathPrioritizer(object):
     def __init__(self, cfg, target):
@@ -28,10 +28,15 @@ class PathPrioritizer(object):
         for k, v in bbl_dict.items():
             bbl_key_map[v] = k
 
+        # Reverse it
+        # As SimIRSB is not copiable, we have to do it by ourselves
+        reversed_graph = networkx.DiGraph()
+        for a, b in g.edges():
+            reversed_graph.add_edge(b, a)
         # Do a BFS from target, and save the length of shortest path to each
         # basic block
         shortest_path_length = networkx.single_source_shortest_path_length( \
-                                                            g, self._target)
+                                                    reversed_graph, self._target)
         for k, v in shortest_path_length.items():
             bbl_key = bbl_key_map[k]
             self._shortest_path_length_dict[bbl_key] = v
@@ -42,13 +47,13 @@ class PathPrioritizer(object):
         # Each tuple looks like (a, b), where b is the function address of a
         # basic block, and a is the IRSB addr where the function is called
         l.debug("Retrieving path priority of %s..." % path)
-        call_stack = path.call_stack
-        if len(call_stack) == 0:
-            l.debug("Call stack is empty")
-            return MAX_INT
+        call_stack = path.callstack
         # FIXME: For now we are only supporting level 2 context-sensitivity
         # But we shouldn't hard code this anyway
-        tpl = call_stack[-1] + (path.last_run.addr,)
+        if len(call_stack) == 0:
+            tpl = (None, None, path.last_run.addr)
+        else:
+            tpl = call_stack[-1] + (path.last_run.addr,)
         if tpl in self._shortest_path_length_dict:
             priority = self._shortest_path_length_dict[tpl]
             l.debug("The priority is %d", priority)
