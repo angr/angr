58eeadf482bd22c12305e208d7cf68917ff66741
Yan <yans@yancomm.net>
2014-05-14 04:26:43 -0700

    copy maximum strlen

--- simuvex/procedures/libc.so.6/__init__.py
+++ simuvex/procedures/libc.so.6/__init__.py
@@ -14,7 +14,7 @@ class SimStateLibc(simuvex.SimStatePlugin):
 
         # various thresholds
         self.heap_location = 0xc0000000
-        self.buf_symbolic_bytes = 48
+        self.buf_symbolic_bytes = 60
         self.max_symbolic_search = 16
         self.max_variable_size = 10000
         self.max_str_len = 128
@@ -35,7 +35,7 @@ class SimStateLibc(simuvex.SimStatePlugin):
         c.max_symbolic_search = self.max_symbolic_search
         c.max_variable_size = self.max_variable_size
         c.max_buffer_size = self.max_buffer_size
-
+        c.max_str_len = self.max_str_len
         c.strtok_heap = self.strtok_heap[:]
         c.simple_strtok = self.simple_strtok
         c.strtok_token_size = self.strtok_token_size
