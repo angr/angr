acda5af6e93d3f08de0d231fd733a1887b0835e0
Yan <yans@yancomm.net>
2014-06-20 15:23:48 -0700

    installable via pip

---
+++ setup.py
@@ -0,0 +1,3 @@
+from distutils.core import setup
+
+setup(name='simuvex', version='1.00', packages=['simuvex', 'simuvex.procedures', 'simuvex.procedures.stubs', 'simuvex.procedures.syscalls', 'simuvex.procedures.ld-uClibc___so___0', 'simuvex.procedures.libc_legacy', 'simuvex.procedures.libc___so___6'], package_dir={'simuvex': 'simuvex'}, )
--- simuvex/procedures/__init__.py
+++ simuvex/procedures/__init__.py
@@ -9,13 +9,12 @@ SimProcedures = defaultdict(dict)
 path = os.path.dirname(os.path.abspath(__file__))
 skip_dirs = []
 
-for libname in [f for f in os.listdir(path)
-				if f != "__init__.py" and f not in skip_dirs]:
+for libname in [f for f in os.listdir(path) if f != "__init__.py" and f not in skip_dirs]:
 	lib_path = os.path.join(path, libname)
+	libname = libname.replace("___", ".")
 	if not os.path.isdir(lib_path):
 		continue
-	for py in [f[: -3] for f in os.listdir(lib_path)
-			if f.endswith(".py")]:
+	for py in [f[: -3] for f in os.listdir(lib_path) if f.endswith(".py")]:
 		module_file_path = os.path.join(lib_path, py) + ".py"
 		with open(module_file_path) as module_file:
 			mod = imp.load_module(py, module_file, module_file_path,
