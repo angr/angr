c3bdce9ceb5408b98f8add4b40a56a563ee634d9
Yan <yans@yancomm.net>
2014-07-25 01:19:21 -0700

    fixed 8-space terrorism!

--- simuvex/procedures/syscalls/exit.py
+++ simuvex/procedures/syscalls/exit.py
@@ -5,10 +5,10 @@ import simuvex
 ######################################
 
 class exit(simuvex.SimProcedure):
-        def __init__(self, ret_expr = None):
-                # FIXME: code ref: Where is it the address of the current statment?
-                # self.add_refs(simuvex.SimCodeRef(self.addr, self.stmt_from, current_addr, (), ()))
+    def __init__(self, ret_expr = None):
+        # FIXME: code ref: Where is it the address of the current statment?
+        # self.add_refs(simuvex.SimCodeRef(self.addr, self.stmt_from, current_addr, (), ()))
 
-                # no exits from this block
-                return
+        # no exits from this block
+        return
 
