71f3733035709d499a2e3bb34df2d766cc650162
Yan <yans@yancomm.net>
2014-07-28 11:06:15 -0700

    fix some simprocs and reduce maximum malloc amount

--- simuvex/procedures/libc___so___6/realloc.py
+++ simuvex/procedures/libc___so___6/realloc.py
@@ -1,6 +1,9 @@
 import simuvex
 from simuvex.s_type import SimTypeLength, SimTypeTop
 
+import logging
+l = logging.getLogger("simuvex.procedures.libc.realloc")
+
 ######################################
 # realloc
 ######################################
@@ -11,22 +14,18 @@ class realloc(simuvex.SimProcedure):
 		ptr = self.arg(0)
 		size = self.arg(1)
 
-		if self.state.se.symbolic(size):
-			# TODO: find a better way
-			size = self.state.se.max(size)
-			if size > plugin.max_variable_size:
-				size = plugin.max_variable_size
-		else:
-			size = self.state.se.any(size)
+		size_int = min(self.state.se.max_int(size), plugin.max_variable_size)
+		l.debug("Size: %d", size_int)
+		self.state.add_constraints(size_int == size)
 
 		self.argument_types = { 0: self.ty_ptr(SimTypeTop()),
 				       			1: SimTypeLength(self.state.arch)}
 		self.return_type = self.ty_ptr(SimTypeTop(size))
 
 		addr = plugin.heap_location
-		v = self.state.mem_expr(ptr, size)
+		v = self.state.mem_expr(ptr, size_int)
 		self.state.store_mem(addr, v)
-		plugin.heap_location += size
+		plugin.heap_location += size_int
 
-		self.add_refs(simuvex.SimMemWrite(self.addr, self.stmt_from, addr, v, size, [], [], [], []))
+		self.add_refs(simuvex.SimMemWrite(self.addr, self.stmt_from, self.state.BVV(addr, self.state.arch.bits), v, size, [], [], [], []))
 		self.ret(addr)
