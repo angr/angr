e42fa1471f92099f783f7f474f6891040e5e76f3
Yan <yans@yancomm.net>
2014-07-30 15:23:01 -0700

    fixed putchar

--- simuvex/procedures/libc___so___6/putchar.py
+++ simuvex/procedures/libc___so___6/putchar.py
@@ -7,11 +7,9 @@ import symexec as se
 
 class putchar(simuvex.SimProcedure):
 	def __init__(self): # pylint: disable=W0231,
-		write = simuvex.SimProcedures['syscalls']['write']
-
-		string = self.get_arg_expr(0)
-		self.inline_call(write, se.BitVecVal(1, self.state.arch.bits), string, 1)
+		string = self.arg(0)
+		self.state['posix'].write(1, string[7:0], 1)
 		self.state['posix'].write(1, se.BitVecVal(0x0a, 8), 1)
 
 		# TODO: return values
-		self.exit_return()
+		self.ret()
