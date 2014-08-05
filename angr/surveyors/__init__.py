b9d515401d226a070c4359915c20950320cc6257
Yan <yans@yancomm.net>
2014-08-05 13:07:13 -0700

    some small stuff to help the web UI

--- angr/surveyors/__init__.py
+++ angr/surveyors/__init__.py
@@ -2,3 +2,5 @@ from .explorer import Explorer
 from .executor import Executor
 from .escaper import Escaper
 from .slicecutor import Slicecutor, HappyGraph
+
+all_surveyors = { 'Explorer': Explorer, 'Executor': Executor, 'Escaper': Escaper, 'Slicecutor': Slicecutor }
