44b1d111f05b8829858e5ee36e464209d28f8502
Yan <yans@yancomm.net>
2014-09-09 20:12:42 -0700

    SimMemory base class

---
+++ simuvex/plugins/memory.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import logging
+
+l = logging.getLogger("simuvex.s_memory")
+
+from ..s_state import SimStatePlugin
+class SimMemory(SimStatePlugin):
+	def __init__(self):
+		SimStatePlugin.__init__(self)
+
+	def store(self, addr, data, size, condition=None, fallback=None):
+		'''
+		Returns the address of bytes equal to 'what', starting from 'start'.
+		'''
+		raise NotImplementedError()
+
+	def load(self, addr, size, condition=None, fallback=None):
+		raise NotImplementedError()
+
+	def find(self, addr, what, max_search=None, max_symbolic_bytes=None, default=None):
+		'''
+		Returns the address of bytes equal to 'what', starting from 'start'.
+		'''
+		raise NotImplementedError()
