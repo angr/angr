d1a5679c80e2d4030714cdcbb7b735baea1c9ffa
Yan <yans@yancomm.net>
2014-09-26 13:38:26 -0700

    states create their own claripy instances now

--- simuvex/plugins/solver.py
+++ simuvex/plugins/solver.py
@@ -12,34 +12,141 @@ def unsat_catcher(f):
 	def wrapped_f(self, *args, **kwargs):
 		try:
 			return f(self, *args, **kwargs)
-		except self.UnsatError:
+		except claripy.UnsatError:
 			e_type, value, traceback = sys.exc_info()
 			raise SimUnsatError, ("Got an unsat result", e_type, value), traceback
 	return wrapped_f
 
-class SimSolver(SimStatePlugin): #pylint:disable=abstract-class-little-used
-	def __init__(self):
+import claripy
+class SimSolver(SimStatePlugin):
+	def __init__(self, solver=None, claripy=None): #pylint:disable=redefined-outer-name
+		l.debug("Creating SimSolverClaripy.")
 		SimStatePlugin.__init__(self)
 
-	# Passthroughs
-	def add(self, *constraints): raise NotImplementedError()
-	def satisfiable(self): raise NotImplementedError()
-	def check(self): raise NotImplementedError()
-	def downsize(self): raise NotImplementedError()
-	def solution(self, *args, **kwargs): raise NotImplementedError()
+		self._claripy = claripy
+		self._stored_solver = solver
 
-	def any_expr(self, e, extra_constraints=()): raise NotImplementedError()
-	def any_n_expr(self, e, n, extra_constraints=()): raise NotImplementedError()
-	def max_expr(self, *args, **kwargs): raise NotImplementedError()
-	def min_expr(self, *args, **kwargs): raise NotImplementedError()
+	def set_state(self, state):
+		SimStatePlugin.set_state(self, state)
 
-	def any_raw(self, e, extra_constraints=()): raise NotImplementedError()
-	def any_n_raw(self, e, n, extra_constraints=()): raise NotImplementedError()
-	def min_raw(self, e, extra_constraints=()): raise NotImplementedError()
-	def max_raw(self, e, extra_constraints=()): raise NotImplementedError()
+		if self._claripy is None:
+			if o.ABSTRACT_SOLVER in self.state.options:
+				backend_vsa = claripy.backends.BackendVSA()
+				backend_concrete = claripy.backends.BackendConcrete()
+				self._claripy = claripy.init_standalone(model_backends=[backend_concrete, backend_vsa])
+				claripy.set_claripy(self._claripy)
+			else:
+				self._claripy = claripy.set_claripy(claripy.ClaripyStandalone(parallel=o.PARALLEL_SOLVES in self.state.options))
+		for op in claripy.operations.backend_operations_all | { 'ite_cases', 'ite_dict', 'true', 'false' }:
+			setattr(self, op, getattr(self._claripy, op))
 
+	@property
+	def _solver(self):
+		if self._stored_solver is not None:
+			return self._stored_solver
+
+		if o.COMPOSITE_SOLVER in self.state.options:
+			self._stored_solver = self._claripy.composite_solver()
+		else:
+			self._stored_solver = self._claripy.solver()
+		return self._stored_solver
+
+	@property
+	def constraints(self):
+		return self._solver.constraints
+
+	#
+	# Various passthroughs
+	#
+
+	def add(self, *constraints): return self._solver.add(constraints)
+	def downsize(self): return self._solver.downsize()
+
+	@unsat_catcher
+	def satisfiable(self): return self._solver.satisfiable()
+	@unsat_catcher
+	def check(self): return self._solver.check()
+	@unsat_catcher
+	def solution(self, *args, **kwargs): return self._solver.solution(*args, **kwargs)
+
+
+	#
+	# These solver routines return claripy expressions
+	#
+
+	@unsat_catcher
+	def any_expr(self, e, extra_constraints=()):
+		return self._claripy.wrap(self._solver.eval(e, 1, extra_constraints=extra_constraints)[0])
+
+	def any_n_expr(self, e, n, extra_constraints=()):
+		try:
+			vals = self._solver.eval(e, n, extra_constraints=extra_constraints)
+			return [ self._claripy.wrap(v) for v in vals ]
+		except claripy.UnsatError:
+			return [ ]
+
+	@unsat_catcher
+	def max_expr(self, *args, **kwargs):
+		return self._claripy.wrap(self._solver.max(*args, **kwargs))
+	@unsat_catcher
+	def min_expr(self, *args, **kwargs):
+		return self._claripy.wrap(self._solver.min(*args, **kwargs))
+
+	#
+	# And these return raw results
+	#
+
+	@unsat_catcher
+	def any_raw(self, e, extra_constraints=()):
+		return self._solver.eval(e, 1, extra_constraints=extra_constraints)[0]
+
+	def any_n_raw(self, e, n, extra_constraints=()):
+		try:
+			return self._solver.eval(e, n, extra_constraints=extra_constraints)
+		except claripy.UnsatError:
+			return [ ]
+
+	@unsat_catcher
+	def min_raw(self, e, extra_constraints=()):
+		return self._solver.min(e, extra_constraints=extra_constraints)
+
+	@unsat_catcher
+	def max_raw(self, e, extra_constraints=()):
+		return self._solver.max(e, extra_constraints=extra_constraints)
+
+	def symbolic(self, e): # pylint:disable=R0201
+		if type(e) in (int, str, float, bool, long, claripy.BVV):
+			return False
+		return e.symbolic
+
+	def simplify(self, *args):
+		if len(args) == 0:
+			return self._solver.simplify()
+		elif type(args[0]) is claripy.E:
+			return self._claripy.simplify(args[0])
+		else:
+			return args[0]
+
+	def variables(self, e): #pylint:disable=no-self-use
+		return e.variables
+
+	#
+	# Branching stuff
+	#
+
+	def copy(self):
+		return SimSolver(self._solver.branch())
+
+	def merge(self, others, merge_flag, flag_values): # pylint: disable=W0613
+		#import ipdb; ipdb.set_trace()
+
+		self._stored_solver = self._solver.merge([ oc._solver for oc in others ], merge_flag, flag_values)
+		#import ipdb; ipdb.set_trace()
+		return [ ]
+
+	#
 	# Other stuff
-
+	#
 	def any_str(self, e, extra_constraints=()): return self.any_n_str(e, 1, extra_constraints=extra_constraints)[0]
 	def any_n_str_iter(self, e, n, extra_constraints=()):
 		for s in self.any_n_raw(e, n, extra_constraints=extra_constraints):
@@ -94,124 +201,6 @@ class SimSolver(SimStatePlugin): #pylint:disable=abstract-class-little-used
 		else:
 			return False
 
-
-import claripy
-class SimSolverClaripy(SimSolver):
-	def __init__(self, solver=None):
-		l.debug("Creating SimSolverClaripy.")
-		SimSolver.__init__(self)
-		self._stored_solver = solver
-		self.UnsatError = claripy.UnsatError
-
-	def set_state(self, state):
-		SimStatePlugin.set_state(self, state)
-		for op in claripy.operations.backend_operations_all | { 'ite_cases', 'ite_dict', 'true', 'false' }:
-			setattr(self, op, getattr(state._engine, op))
-
-	@property
-	def _solver(self):
-		if self._stored_solver is not None:
-			return self._stored_solver
-
-		if o.COMPOSITE_SOLVER in self.state.options:
-			self._stored_solver = self.state._engine.composite_solver()
-		else:
-			self._stored_solver = self.state._engine.solver()
-		return self._stored_solver
-
-	@property
-	def constraints(self):
-		return self._solver.constraints
-
-	#
-	# Various passthroughs
-	#
-
-	def add(self, *constraints): return self._solver.add(constraints)
-	def downsize(self): return self._solver.downsize()
-
-	@unsat_catcher
-	def satisfiable(self): return self._solver.satisfiable()
-	@unsat_catcher
-	def check(self): return self._solver.check()
-	@unsat_catcher
-	def solution(self, *args, **kwargs): return self._solver.solution(*args, **kwargs)
-
-
-	#
-	# These solver routines return claripy expressions
-	#
-
-	@unsat_catcher
-	def any_expr(self, e, extra_constraints=()):
-		return self.state._engine.wrap(self._solver.eval(e, 1, extra_constraints=extra_constraints)[0])
-
-	def any_n_expr(self, e, n, extra_constraints=()):
-		try:
-			vals = self._solver.eval(e, n, extra_constraints=extra_constraints)
-			return [ self.state._engine.wrap(v) for v in vals ]
-		except self.UnsatError:
-			return [ ]
-
-	@unsat_catcher
-	def max_expr(self, *args, **kwargs):
-		return self.state._engine.wrap(self._solver.max(*args, **kwargs))
-	@unsat_catcher
-	def min_expr(self, *args, **kwargs):
-		return self.state._engine.wrap(self._solver.min(*args, **kwargs))
-
-	#
-	# And these return raw results
-	#
-
-	@unsat_catcher
-	def any_raw(self, e, extra_constraints=()):
-		return self._solver.eval(e, 1, extra_constraints=extra_constraints)[0]
-
-	def any_n_raw(self, e, n, extra_constraints=()):
-		try:
-			return self._solver.eval(e, n, extra_constraints=extra_constraints)
-		except self.UnsatError:
-			return [ ]
-
-	@unsat_catcher
-	def min_raw(self, e, extra_constraints=()):
-		return self._solver.min(e, extra_constraints=extra_constraints)
-
-	@unsat_catcher
-	def max_raw(self, e, extra_constraints=()):
-		return self._solver.max(e, extra_constraints=extra_constraints)
-
-	def symbolic(self, e): # pylint:disable=R0201
-		if type(e) in (int, str, float, bool, long, claripy.BVV):
-			return False
-		return e.symbolic
-
-	def simplify(self, *args):
-		if len(args) == 0:
-			return self._solver.simplify()
-		elif type(args[0]) is claripy.E:
-			return self.state._engine.simplify(args[0])
-		else:
-			return args[0]
-
-	def variables(self, e): #pylint:disable=no-self-use
-		return e.variables
-
-	#
-	# Branching stuff
-	#
-
-	def copy(self):
-		return SimSolverClaripy(self._solver.branch())
-
-	def merge(self, others, merge_flag, flag_values): # pylint: disable=W0613
-		#import ipdb; ipdb.set_trace()
-
-		self._stored_solver = self._solver.merge([ oc._solver for oc in others ], merge_flag, flag_values)
-		#import ipdb; ipdb.set_trace()
-		return [ ]
-
-SimStatePlugin.register_default('solver_engine', SimSolverClaripy)
+SimStatePlugin.register_default('solver_engine', SimSolver)
 from .. import s_options as o
 from ..s_errors import SimValueError, SimUnsatError
