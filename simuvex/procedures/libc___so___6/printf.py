94492688a845067cdfedadf165f3889a5de7271b
Fish <fish@cs.ucsb.edu>
2014-10-14 19:30:41 -0700

    Removed unnecessary debug output.

--- simuvex/procedures/libc___so___6/printf.py
+++ simuvex/procedures/libc___so___6/printf.py
@@ -13,4 +13,4 @@ class printf(simuvex.SimProcedure):
         # This function returns
         # Add another exit to the retn_addr that is at the top of the stack now
         self.ret()
-        l.debug("Got return address for %s: 0x%08x.", __file__, self._exits[0].concretize())
+        # l.debug("Got return address for %s: 0x%08x.", __file__, self._exits[0].concretize())
