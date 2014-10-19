03bcdbf2ba82690926a1eea3714efac3ad6b01e5
Christophe <christophe@cs.ucsb.edu>
2014-10-18 18:59:38 -0700

    New "resolves" parameter to ReturnUnconstrained

--- simuvex/procedures/stubs/ReturnUnconstrained.py
+++ simuvex/procedures/stubs/ReturnUnconstrained.py
@@ -5,9 +5,10 @@ import simuvex
 ######################################
 
 class ReturnUnconstrained(simuvex.SimProcedure):
-    def __init__(self, name=None): # pylint: disable=W0231,
+    def __init__(self, name=None, resolves=None): # pylint: disable=W0231,
         self._name = name
         self.ret(self.state.BV("unconstrained_ret", self.state.arch.bits))
+        self.resolves = resolves
 
     def __repr__(self):
-        return 'ReturnUnconstrained[Pseudo %s]' % self._name
+        return 'ReturnUnconstrained[Pseudo %s - resolves %s]' % (self._name, self.resolves)
