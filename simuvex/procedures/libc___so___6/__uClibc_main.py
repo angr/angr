42a9d9de6279bc851448310dfedc57e4335d77ed
Yan <yans@yancomm.net>
2014-10-18 23:14:01 -0700

    fixed up uClibc_main

--- simuvex/procedures/libc___so___6/__uClibc_main.py
+++ simuvex/procedures/libc___so___6/__uClibc_main.py
@@ -1,12 +1,12 @@
 import simuvex
-from ..libc____so____6.__libc_start_main import __libc_start_main
+from ..libc___so___6.__libc_start_main import __libc_start_main as fucker
 
 ######################################
 # __uClibc_main
 ######################################
-
 class __uClibc_main(simuvex.SimProcedure):
     ADDS_EXITS = True
 
-    self.__init__ = __libc_start_main.__init__
-
+    # This is called "fucker" cause otherwise the double underscores cause
+    # python to name-mangle and everything gets fucked.
+    __init__ = fucker.__init__.__func__
