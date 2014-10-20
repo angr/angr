32ee5f3efb344104daba4fffa471ff2848e853a1
Fish <fish@cs.ucsb.edu>
2014-10-20 04:19:43 -0700

    variableseekr test is updated

--- tests/test_variableseekr.py
+++ tests/test_variableseekr.py
@@ -20,6 +20,7 @@ projects = {}
 projects['fauxwares'] = {}
 projects['cfg_1'] = {}
 projects['allcmps'] = {}
+projects['basic_buffer_overflows'] = {}
 
 def setup_x86():
     fauxware = projects['fauxwares']
@@ -29,10 +30,12 @@ def setup_amd64():
     fauxware = projects['fauxwares']
     cfg_1 = projects['cfg_1']
     all_cmps = projects['allcmps']
+    basic_buffer_overflows = projects['basic_buffer_overflows']
 
     fauxware['amd64'] = angr.Project(test_location + "/blob/x86_64/fauxware",  arch="AMD64")
     cfg_1['amd64'] = angr.Project(test_location + "/blob/x86_64/cfg_1", arch="AMD64")
     all_cmps['amd64'] = angr.Project(test_location + '/blob/x86_64/allcmps', arch="AMD64")
+    basic_buffer_overflows['amd64'] = angr.Project(test_location + '/blob/x86_64/basic_buffer_overflows')
 
 def setup_ppc32():
     fauxware = projects['fauxwares']
@@ -117,6 +120,31 @@ def test_allcmps(arch, starts):
                 else:
                     l.info("%s(%d),  referenced at %08x", var, var._size, var._inst_addr)
 
+def test_basic_buffer_overflows(arch, starts):
+    basic_buffer_overflows = projects['basic_buffer_overflows']
+    cfg = basic_buffer_overflows[arch].construct_cfg()
+    for start in starts:
+        basic_buffer_overflows[arch].construct_vfg(start=start)
+    vfg = basic_buffer_overflows[arch].vfg
+    variable_seekr = angr.VariableSeekr(basic_buffer_overflows[arch], cfg, vfg)
+
+    for start in starts:
+        variable_seekr.construct(func_start=start)
+        function_manager = cfg.function_manager
+        for func_addr, func in function_manager.functions.items():
+            l.info("Function %xh", func_addr)
+            variable_manager = variable_seekr.get_variable_manager(func_addr)
+            if variable_manager is None:
+                continue
+            # TODO: Check the result returned
+            l.info("Variables: ")
+            for var in variable_manager.variables:
+                if isinstance(var, angr.StackVariable):
+                    l.info(var.detail_str())
+                else:
+                    l.info("%s(%d),  referenced at %08x", var, var._size, var._inst_addr)
+
+
 if __name__ == "__main__":
     logging.getLogger('angr.cfg').setLevel(logging.DEBUG)
     logging.getLogger('angr.vfg').setLevel(logging.DEBUG)
@@ -124,5 +152,5 @@ if __name__ == "__main__":
     setup_amd64()
     l.info("LOADED")
     #test_fauxware('amd64', 0x40071d)
-    test_allcmps('amd64', (0x4005ff, 0x4004ec))
+    test_basic_buffer_overflows('amd64', (0x40068f, 0x40055c, 0x4005b6, 0x40063e))
     l.info("DONE")
