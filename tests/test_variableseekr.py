8eb9167508b99ea9a5b2db540b87b31c4dff866b
Fish <fish@cs.ucsb.edu>
2014-10-23 08:04:34 -0700

    Added a testcase for finding all uninitialized reads.

--- tests/test_variableseekr.py
+++ tests/test_variableseekr.py
@@ -4,6 +4,8 @@ import nose
 import logging
 l = logging.getLogger("angr.tests")
 
+from angr import AngrError
+
 try:
     # pylint: disable=W0611,F0401
     import standard_logging
@@ -21,6 +23,7 @@ projects['fauxwares'] = {}
 projects['cfg_1'] = {}
 projects['allcmps'] = {}
 projects['basic_buffer_overflows'] = {}
+projects['uninitialized_reads'] = {}
 
 def setup_x86():
     fauxware = projects['fauxwares']
@@ -31,11 +34,13 @@ def setup_amd64():
     cfg_1 = projects['cfg_1']
     all_cmps = projects['allcmps']
     basic_buffer_overflows = projects['basic_buffer_overflows']
+    uninitialized_reads = projects['uninitialized_reads']
 
     fauxware['amd64'] = angr.Project(test_location + "/blob/x86_64/fauxware",  arch="AMD64")
     cfg_1['amd64'] = angr.Project(test_location + "/blob/x86_64/cfg_1", arch="AMD64")
     all_cmps['amd64'] = angr.Project(test_location + '/blob/x86_64/allcmps', arch="AMD64")
     basic_buffer_overflows['amd64'] = angr.Project(test_location + '/blob/x86_64/basic_buffer_overflows')
+    uninitialized_reads['amd64'] = angr.Project(test_location + '/blob/x86_64/uninitialized_reads')
 
 def setup_ppc32():
     fauxware = projects['fauxwares']
@@ -144,15 +149,46 @@ def test_basic_buffer_overflows(arch, starts):
                 else:
                     l.info("%s(%d),  referenced at %08x", var, var._size, var._inst_addr)
 
+def test_uninitialized_reads(arch, starts):
+    uninitialized_reads = projects['uninitialized_reads']
+    cfg = uninitialized_reads[arch].construct_cfg()
+    for start in starts:
+        uninitialized_reads[arch].construct_vfg(start=start)
+    vfg = uninitialized_reads[arch].vfg
+    variable_seekr = angr.VariableSeekr(uninitialized_reads[arch], cfg, vfg)
+
+    for start in starts:
+        try:
+            variable_seekr.construct(func_start=start)
+        except AngrError:
+            l.info('AngrError...')
+            continue
+        function_manager = cfg.function_manager
+        for func_addr, func in function_manager.functions.items():
+            l.info("Function %xh", func_addr)
+            variable_manager = variable_seekr.get_variable_manager(func_addr)
+            if variable_manager is None:
+                continue
+            # TODO: Check the result returned
+            l.info("Variables: ")
+            for var in variable_manager.variables:
+                if isinstance(var, angr.StackVariable):
+                    l.info(var.detail_str())
+                else:
+                    l.info("%s(%d),  referenced at %08x", var, var._size, var._inst_addr)
+
+    import ipdb; ipdb.set_trace()
 
 if __name__ == "__main__":
     logging.getLogger('angr.cfg').setLevel(logging.DEBUG)
     logging.getLogger('angr.vfg').setLevel(logging.DEBUG)
     logging.getLogger('simuvex.plugins.symbolic_memory').setLevel(logging.INFO)
+    #logging.getLogger('simuvex.plugins.abstract_memory').setLevel(logging.DEBUG)
     logging.getLogger('claripy.claripy').setLevel(logging.ERROR)
     l.setLevel(logging.DEBUG)
     setup_amd64()
     l.info("LOADED")
     #test_fauxware('amd64', 0x40071d)
-    test_basic_buffer_overflows('amd64', (0x40068f, 0x40055c, 0x4005b6, 0x40063e))
+    #test_basic_buffer_overflows('amd64', (0x40068f, 0x40055c, 0x4005b6, 0x40063e))
+    test_uninitialized_reads('amd64', (0x40052c, 0x40056c))
     l.info("DONE")
