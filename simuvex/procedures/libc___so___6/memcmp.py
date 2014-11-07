ee713feec20a4b5689f6494d8afdf551289bcba6
Yan <yans@yancomm.net>
2014-11-07 15:21:27 -0800

    tabs to spaces :-(

--- simuvex/procedures/libc___so___6/memcmp.py
+++ simuvex/procedures/libc___so___6/memcmp.py
@@ -5,61 +5,61 @@ import logging
 l = logging.getLogger("simuvex.procedures.libc.memcmp")
 
 class memcmp(simuvex.SimProcedure):
-	def __init__(self): # pylint: disable=W0231,
-		s1_addr = self.arg(0)
-		s2_addr = self.arg(1)
-		n = self.arg(2)
+    def __init__(self): # pylint: disable=W0231,
+        s1_addr = self.arg(0)
+        s2_addr = self.arg(1)
+        n = self.arg(2)
 
-		# TODO: look into smarter types here
-		self.argument_types = {0: self.ty_ptr(SimTypeTop()),
-							   1: self.ty_ptr(SimTypeTop()),
-							   2: SimTypeLength(self.state.arch)}
-		self.return_type = SimTypeInt(32, True)
+        # TODO: look into smarter types here
+        self.argument_types = {0: self.ty_ptr(SimTypeTop()),
+                               1: self.ty_ptr(SimTypeTop()),
+                               2: SimTypeLength(self.state.arch)}
+        self.return_type = SimTypeInt(32, True)
 
-		max_memcmp_size = self.state['libc'].max_buffer_size
+        max_memcmp_size = self.state['libc'].max_buffer_size
 
-		definite_size = self.state.se.min_int(n)
-		conditional_s1_start = s1_addr + definite_size
-		conditional_s2_start = s2_addr + definite_size
-		if self.state.se.symbolic(n):
-			conditional_size = int(max(max_memcmp_size - definite_size, 0))
-		else:
-			conditional_size = 0
+        definite_size = self.state.se.min_int(n)
+        conditional_s1_start = s1_addr + definite_size
+        conditional_s2_start = s2_addr + definite_size
+        if self.state.se.symbolic(n):
+            conditional_size = int(max(max_memcmp_size - definite_size, 0))
+        else:
+            conditional_size = 0
 
-		l.debug("Definite size %s and conditional size: %s", definite_size, conditional_size)
+        l.debug("Definite size %s and conditional size: %s", definite_size, conditional_size)
 
-		if definite_size > 0:
-			s1_part = self.state.mem_expr(s1_addr, definite_size, endness='Iend_BE')
-			s2_part = self.state.mem_expr(s2_addr, definite_size, endness='Iend_BE')
-			cases = [ [s1_part == s2_part, self.state.BVV(0)], [self.state.se.ULT(s1_part, s2_part), self.state.BVV(-1)], [self.state.se.UGT(s1_part, s2_part), self.state.BVV(1) ] ]
-			definite_answer = self.state.se.ite_cases(cases, 2)
-			constraint = self.state.se.Or(*[c for c,_ in cases])
-			self.state.add_constraints(constraint)
+        if definite_size > 0:
+            s1_part = self.state.mem_expr(s1_addr, definite_size, endness='Iend_BE')
+            s2_part = self.state.mem_expr(s2_addr, definite_size, endness='Iend_BE')
+            cases = [ [s1_part == s2_part, self.state.BVV(0)], [self.state.se.ULT(s1_part, s2_part), self.state.BVV(-1)], [self.state.se.UGT(s1_part, s2_part), self.state.BVV(1) ] ]
+            definite_answer = self.state.se.ite_cases(cases, 2)
+            constraint = self.state.se.Or(*[c for c,_ in cases])
+            self.state.add_constraints(constraint)
 
-			l.debug("Created definite answer: %s", definite_answer)
-			l.debug("Created constraint: %s", constraint)
-			l.debug("... crom cases: %s", cases)
-		else:
-			definite_answer = self.state.BVV(0, self.state.arch.bits)
+            l.debug("Created definite answer: %s", definite_answer)
+            l.debug("Created constraint: %s", constraint)
+            l.debug("... crom cases: %s", cases)
+        else:
+            definite_answer = self.state.BVV(0, self.state.arch.bits)
 
-		if not self.state.se.symbolic(definite_answer) and self.state.se.any_int(definite_answer) != 0:
-			self.ret(definite_answer)
-			return
+        if not self.state.se.symbolic(definite_answer) and self.state.se.any_int(definite_answer) != 0:
+            self.ret(definite_answer)
+            return
 
-		if conditional_size > 0:
-			s1_all = self.state.mem_expr(conditional_s1_start, conditional_size, endness='Iend_BE')
-			s2_all = self.state.mem_expr(conditional_s2_start, conditional_size, endness='Iend_BE')
-			conditional_rets = { 0: definite_answer }
+        if conditional_size > 0:
+            s1_all = self.state.mem_expr(conditional_s1_start, conditional_size, endness='Iend_BE')
+            s2_all = self.state.mem_expr(conditional_s2_start, conditional_size, endness='Iend_BE')
+            conditional_rets = { 0: definite_answer }
 
-			for byte, bit in zip(range(conditional_size), range(conditional_size*8, 0, -8)):
-				s1_part = s1_all[conditional_size*8-1 : bit-8]
-				s2_part = s2_all[conditional_size*8-1 : bit-8]
-				cases = [ [s1_part == s2_part, self.state.BVV(0)], [self.state.se.ULT(s1_part, s2_part), self.state.BVV(-1)], [self.state.se.UGT(s1_part, s2_part), self.state.BVV(1) ] ]
-				conditional_rets[byte+1] = self.state.se.ite_cases(cases, 0)
-				self.state.add_constraints(self.state.se.Or(*[c for c,_ in cases]))
+            for byte, bit in zip(range(conditional_size), range(conditional_size*8, 0, -8)):
+                s1_part = s1_all[conditional_size*8-1 : bit-8]
+                s2_part = s2_all[conditional_size*8-1 : bit-8]
+                cases = [ [s1_part == s2_part, self.state.BVV(0)], [self.state.se.ULT(s1_part, s2_part), self.state.BVV(-1)], [self.state.se.UGT(s1_part, s2_part), self.state.BVV(1) ] ]
+                conditional_rets[byte+1] = self.state.se.ite_cases(cases, 0)
+                self.state.add_constraints(self.state.se.Or(*[c for c,_ in cases]))
 
-			ret_expr = self.state.se.ite_dict(n - definite_size, conditional_rets, 2)
-			self.state.add_constraints(self.state.se.Or(*[n-definite_size == c for c in conditional_rets.keys()]))
-			self.ret(ret_expr)
-		else:
-			self.ret(definite_answer)
+            ret_expr = self.state.se.ite_dict(n - definite_size, conditional_rets, 2)
+            self.state.add_constraints(self.state.se.Or(*[n-definite_size == c for c in conditional_rets.keys()]))
+            self.ret(ret_expr)
+        else:
+            self.ret(definite_answer)
--- simuvex/procedures/libc___so___6/sprintf.py
+++ simuvex/procedures/libc___so___6/sprintf.py
@@ -10,103 +10,103 @@ l = logging.getLogger("simuvex.procedures.sprintf")
 import math
 
 class sprintf(simuvex.SimProcedure):
-	def __init__(self): # pylint: disable=W0231,
-		dst_ptr = self.arg(0)
-		format_ptr = self.arg(1)
-		first_arg = self.arg(2)
+    def __init__(self): # pylint: disable=W0231,
+        dst_ptr = self.arg(0)
+        format_ptr = self.arg(1)
+        first_arg = self.arg(2)
 
-		strlen = simuvex.SimProcedures['libc.so.6']['strlen']
+        strlen = simuvex.SimProcedures['libc.so.6']['strlen']
 
-		l.debug("WTF")
+        l.debug("WTF")
 
-		format_len = self.inline_call(strlen, format_ptr).ret_expr
-		if self.state.se.symbolic(format_len):
-			raise Exception("ZOMG, symbolic format strings? Are you joking?")
+        format_len = self.inline_call(strlen, format_ptr).ret_expr
+        if self.state.se.symbolic(format_len):
+            raise Exception("ZOMG, symbolic format strings? Are you joking?")
 
-		format_expr = self.state.mem_expr(format_ptr, format_len, endness="Iend_BE")
-		if self.state.se.symbolic(format_expr):
-			raise Exception("ZOMG, symbolic format strings? Are you joking?")
-		format_str = self.state.se.any_str(format_expr)
+        format_expr = self.state.mem_expr(format_ptr, format_len, endness="Iend_BE")
+        if self.state.se.symbolic(format_expr):
+            raise Exception("ZOMG, symbolic format strings? Are you joking?")
+        format_str = self.state.se.any_str(format_expr)
 
-		# get the pieces
-		if format_str == "%d":
-			# our string
-			max_bits = 96
-			new_str = self.state.BV("sprintf_str", max_bits)
-			old_str = self.state.mem_expr(dst_ptr, max_bits/8, endness="Iend_BE")
+        # get the pieces
+        if format_str == "%d":
+            # our string
+            max_bits = 96
+            new_str = self.state.BV("sprintf_str", max_bits)
+            old_str = self.state.mem_expr(dst_ptr, max_bits/8, endness="Iend_BE")
 
-			l.debug("INTEGER")
-			digits = [ ]
-			for i in [ 10**i for i in range(0, 10) ]:
-				digit = ((first_arg / i) % 10 + 0x30)[7:0]
-				digits.append(digit)
-			digits.reverse()
-			digits.append(self.state.BVV(0, 8))
+            l.debug("INTEGER")
+            digits = [ ]
+            for i in [ 10**i for i in range(0, 10) ]:
+                digit = ((first_arg / i) % 10 + 0x30)[7:0]
+                digits.append(digit)
+            digits.reverse()
+            digits.append(self.state.BVV(0, 8))
 
-			# figure out how many will actually be written
-			num_constraints = [ ]
-			for i,j in [ (10**k/10*10, 10**(k+1)) for k in [ _ - 1 for _ in range(1, 11) ] ]:
-				offset = int(9 - math.floor(math.log(i+1, 10)))
-				actual_bytes = digits[offset:]
+            # figure out how many will actually be written
+            num_constraints = [ ]
+            for i,j in [ (10**k/10*10, 10**(k+1)) for k in [ _ - 1 for _ in range(1, 11) ] ]:
+                offset = int(9 - math.floor(math.log(i+1, 10)))
+                actual_bytes = digits[offset:]
 
-				if len(actual_bytes) > 1: digit_str = self.state.se.Concat(*actual_bytes)
-				else: digit_str = actual_bytes[0]
+                if len(actual_bytes) > 1: digit_str = self.state.se.Concat(*actual_bytes)
+                else: digit_str = actual_bytes[0]
 
-				bits_written = len(actual_bytes)*8
+                bits_written = len(actual_bytes)*8
 
-				num_constraints.append(self.state.se.And(
-												self.state.se.UGE(first_arg, self.state.se.BitVecVal(i, first_arg.size())),
-												self.state.se.ULE(first_arg, self.state.se.BitVecVal(j-1, first_arg.size())),
-												new_str[max_bits-1 : max_bits-bits_written] == digit_str,
-												new_str[max_bits-bits_written : 0] == old_str[max_bits-bits_written : 0]
-											))
+                num_constraints.append(self.state.se.And(
+                                                self.state.se.UGE(first_arg, self.state.se.BitVecVal(i, first_arg.size())),
+                                                self.state.se.ULE(first_arg, self.state.se.BitVecVal(j-1, first_arg.size())),
+                                                new_str[max_bits-1 : max_bits-bits_written] == digit_str,
+                                                new_str[max_bits-bits_written : 0] == old_str[max_bits-bits_written : 0]
+                                            ))
 
-			self.state.add_constraints(self.state.se.Or(*num_constraints))
-		elif format_str == "%c":
-			new_str = self.state.Concat(first_arg[7:0], self.state.se.BitVecVal(0, 8))
-		elif format_str == "%s=":
-			first_strlen = self.inline_call(strlen, first_arg)
-			if self.state.se.symbolic(first_strlen.ret_expr):
-				self.ret(self.state.BV("sprintf_fail", self.state.arch.bits))
-				return
+            self.state.add_constraints(self.state.se.Or(*num_constraints))
+        elif format_str == "%c":
+            new_str = self.state.Concat(first_arg[7:0], self.state.se.BitVecVal(0, 8))
+        elif format_str == "%s=":
+            first_strlen = self.inline_call(strlen, first_arg)
+            if self.state.se.symbolic(first_strlen.ret_expr):
+                self.ret(self.state.BV("sprintf_fail", self.state.arch.bits))
+                return
 
-			new_str = self.state.se.Concat(self.state.mem_expr(first_arg, self.state.se.any_int(first_strlen.ret_expr), endness='Iend_BE'), self.state.se.BitVecVal(0x3d00, 16))
-		elif format_str == "%%%ds %%%ds %%%ds":
-			if self.state.se.symbolic(first_arg) or self.state.se.symbolic(self.arg(3)) or self.state.se.symbolic(self.arg(4)):
-				l.debug("Symbolic args. Hackin' out.")
-				a = 5
-				b = 8192
-				c = 12
-			else:
-				a = self.state.se.any_int(first_arg)
-				b = self.state.se.any_int(self.arg(3))
-				c = self.state.se.any_int(self.arg(4))
+            new_str = self.state.se.Concat(self.state.mem_expr(first_arg, self.state.se.any_int(first_strlen.ret_expr), endness='Iend_BE'), self.state.se.BitVecVal(0x3d00, 16))
+        elif format_str == "%%%ds %%%ds %%%ds":
+            if self.state.se.symbolic(first_arg) or self.state.se.symbolic(self.arg(3)) or self.state.se.symbolic(self.arg(4)):
+                l.debug("Symbolic args. Hackin' out.")
+                a = 5
+                b = 8192
+                c = 12
+            else:
+                a = self.state.se.any_int(first_arg)
+                b = self.state.se.any_int(self.arg(3))
+                c = self.state.se.any_int(self.arg(4))
 
-			new_str = self.state.BVV(format_str % (a,b,c) + "\x00")
-		elif format_str == "Basic %s\r\n":
-			str_ptr = first_arg
-			str_len = self.inline_call(strlen, str_ptr).ret_expr
+            new_str = self.state.BVV(format_str % (a,b,c) + "\x00")
+        elif format_str == "Basic %s\r\n":
+            str_ptr = first_arg
+            str_len = self.inline_call(strlen, str_ptr).ret_expr
 
-			if not self.state.se.unique(str_len):
-				new_str = self.state.BVV("Basic POOP\r\n\x00")
-			else:
-				pieces = [ ]
-				pieces.append(self.state.BVV("Basic "))
-				if self.state.se.any_int(str_len.se.any) != 0:
-					pieces.append(self.state.mem_expr(str_ptr, self.state.se.any_int(str_len)))
-				pieces.append(self.state.BVV("\r\n\x00"))
+            if not self.state.se.unique(str_len):
+                new_str = self.state.BVV("Basic POOP\r\n\x00")
+            else:
+                pieces = [ ]
+                pieces.append(self.state.BVV("Basic "))
+                if self.state.se.any_int(str_len.se.any) != 0:
+                    pieces.append(self.state.mem_expr(str_ptr, self.state.se.any_int(str_len)))
+                pieces.append(self.state.BVV("\r\n\x00"))
 
-				new_str = self.state.se.Concat(*pieces)
-		elif format_str == '<HTML><HEAD><TITLE>%d %s</TITLE></HEAD>\n<BODY BGCOLOR="#cc9999"><H4>%d %s</H4>\n':
-			new_str = self.state.BVV("THIS IS THE START OF A (ERROR?) MESSAGE THAT'S RETURNED FROM SOMEWHERE\x00")
-		elif format_str == '%s\n</BODY></HTML>\n':
-			new_str = self.state.BVV("THE END OF AN HTML PAGE\x00")
-		else:
-			if simuvex.o.SYMBOLIC in self.state.options:
-				raise Exception("Unsupported format string: %r" % format_str)
-			new_str = self.state.BVV("\x00")
+                new_str = self.state.se.Concat(*pieces)
+        elif format_str == '<HTML><HEAD><TITLE>%d %s</TITLE></HEAD>\n<BODY BGCOLOR="#cc9999"><H4>%d %s</H4>\n':
+            new_str = self.state.BVV("THIS IS THE START OF A (ERROR?) MESSAGE THAT'S RETURNED FROM SOMEWHERE\x00")
+        elif format_str == '%s\n</BODY></HTML>\n':
+            new_str = self.state.BVV("THE END OF AN HTML PAGE\x00")
+        else:
+            if simuvex.o.SYMBOLIC in self.state.options:
+                raise Exception("Unsupported format string: %r" % format_str)
+            new_str = self.state.BVV("\x00")
 
-		self.state.store_mem(dst_ptr, new_str)
+        self.state.store_mem(dst_ptr, new_str)
 
-		# TODO: actual value
-		self.ret(self.state.BV("sprintf_ret", self.state.arch.bits))
+        # TODO: actual value
+        self.ret(self.state.BV("sprintf_ret", self.state.arch.bits))
