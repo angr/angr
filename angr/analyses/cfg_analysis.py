3756372e192ce6d4f4916f5e4aa740739fbc4067
Fish <fish@cs.ucsb.edu>
2014-11-11 17:11:00 -0800

    removed cfg_analysis.py

--- angr/analyses/cfg_analysis.py
+++
@@ -1,9 +0,0 @@
-from ..analysis import Analysis
-from ..cfg import CFG
-
-class CFGAnalysis(Analysis):
-    __analysis_name__ = 'CFG'
-
-    def __init__(self, context_sensitivity=1, start=None, avoid_runs=None):
-        self.cfg = CFG(project=self._p, context_sensitivity_level=context_sensitivity)
-        self.cfg.construct(self._p.main_binary, start=start, avoid_runs=avoid_runs)
