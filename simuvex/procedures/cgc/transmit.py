4bf056a37c869d9fe27ed2b019bbdee9162c39c5
Yan <yans@yancomm.net>
2014-12-02 17:04:57 -0800

    receive and transmit now create SimActions

--- simuvex/procedures/cgc/transmit.py
+++ simuvex/procedures/cgc/transmit.py
@@ -5,8 +5,12 @@ class transmit(simuvex.SimProcedure):
 
     def run(self, fd, buf, count, tx_bytes):
         if self.state.satisfiable(extra_constraints=[count != 0]):
+            pos = self.state.posix.get_file(fd)
             data = self.state.mem_expr(buf, count)
             self.state.posix.write(fd, data, count)
+
+            a = simuvex.SimActionData(self.state, 'file', 'write', fd=fd, addr=pos, size=count, data=data)
+            self.state.log._add_event(a)
 
         self.state.memory.store(tx_bytes, count, condition=tx_bytes != 0)
 
