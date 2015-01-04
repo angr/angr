a114d9aff0c86b2cd9091604b7a8f89d5bf232d2
Yan <yans@yancomm.net>
2015-01-04 12:40:35 -0700

    feature to provide a state when creating paths

--- angr/paths.py
+++ angr/paths.py
@@ -6,19 +6,19 @@ class PathGenerator(object):
     def __init__(self, project):
         self._project = project
 
-    def blank_path(self, *args, **kwargs):
+    def blank_path(self, state=None, *args, **kwargs):
         '''
         blank_point - Returns a start path, representing a clean start of symbolic execution.
         '''
-        s = self._project.state_generator.blank_state(*args, **kwargs)
+        s = self._project.state_generator.blank_state(*args, **kwargs) if state is None else state
         return Path(self._project, s)
 
-    def entry_point(self, *args, **kwargs):
+    def entry_point(self, state=None, *args, **kwargs):
         '''
         entry_point - Returns a path reflecting the processor when execution
                       reaches the binary's entry point.
         '''
-        s = self._project.state_generator.entry_point(*args, **kwargs)
+        s = self._project.state_generator.entry_point(*args, **kwargs) if state is None else state
         return Path(self._project, s)
 
 from .path import Path
