386f28e1c32393da04d7f6af55b0416e4c82c1f5
Fish <fish@cs.ucsb.edu>
2015-02-14 16:45:04 -0800

    Added a test case for SimCC.

---
+++ tests/src/simcc.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+
+/*
+ * Normal functions.
+ * i.e. all arguments are used.
+ */
+int arg1(int a1)
+{
+    printf("%d\n", a1);
+    return a1;
+}
+
+int arg2(int a1, int a2)
+{
+    printf("%d\n", a1 + a2);
+    return a2;
+}
+
+int arg3(int a1, int a2, int a3)
+{
+    printf("%d\n", a1 + a2 + a3);
+    return a3;
+}
+
+int arg4(int a1, int a2, int a3, int a4)
+{
+    printf("%d\n", a1 + a2 + a3 + a4);
+    return a4;
+}
+
+int arg5(int a1, int a2, int a3, int a4, int a5)
+{
+    printf("%d\n", a1 + a2 + a3 + a4 + a5);
+    return a5;
+}
+
+int arg6(int a1, int a2, int a3, int a4, int a5, int a6)
+{
+    printf("%d\n", a1 + a2 + a3 + a4 + a5 + a6);
+    return a6;
+}
+
+int arg7(int a1, int a2, int a3, int a4, int a5, int a6, int a7)
+{
+    printf("%d\n", a1 + a2 + a3 + a4 + a5 + a6 + a7);
+    return a7;
+}
+
+int arg8(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8)
+{
+    printf("%d\n", a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8);
+    return a8;
+}
+
+int arg9(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9)
+{
+    printf("%d\n", a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9);
+    return a9;
+}
+
+int main(int argc, char** argv)
+{
+    int a;
+    scanf("%d", &a);
+    arg1(a);
+    arg2(a, a);
+    arg3(a, a, a);
+    arg4(a, a, a, a);
+    arg5(a, a, a, a, a);
+    arg6(a, a, a, a, a, a);
+    arg7(a, a, a, a, a, a, a);
+    arg8(a, a, a, a, a, a, a, a);
+    arg9(a, a, a, a, a, a, a, a, a);
+}
---
+++ tests/test_simcc.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+import os
+import logging
+import time
+import pickle
+import sys
+
+l = logging.getLogger("angr.tests.test_simcc")
+
+import nose
+import pprint
+import networkx
+
+try:
+    import standard_logging
+    import angr_debug
+except ImportError:
+    pass
+
+import angr
+
+# Load the tests
+test_location = str(os.path.dirname(os.path.realpath(__file__)))
+
+def _test_simcc_x86_64():
+    binary_path = test_location + "/blob/x86_64/simcc"
+
+    p = angr.Project(binary_path)
+    cfg = p.analyses.CFG()
+
+    fm = cfg.function_manager
+
+    f_arg1 = fm.function(name='arg1')
+    nose.tools.assert_not_equal(f_arg1, None)
+    nose.tools.assert_equal(len(f_arg1.arguments), 1)
+    nose.tools.assert_equal(f_arg1.arguments[0].name, 'rdi')
+
+    f_arg7 = fm.function(name='arg7')
+    nose.tools.assert_not_equal(f_arg7, None)
+    nose.tools.assert_equal(len(f_arg7.arguments), 7)
+    nose.tools.assert_equal(f_arg7.arguments[1].name, 'rsi')
+
+    f_arg9 = fm.function(name='arg9')
+    nose.tools.assert_not_equal(f_arg9, None)
+    nose.tools.assert_equal(len(f_arg9.arguments), 9)
+    nose.tools.assert_equal(f_arg9.arguments[8].offset, 0x10 + 0x8 * 2)
+
+def test_all():
+    functions = globals()
+    all_functions = dict(filter((lambda (k, v): k.startswith('_test_')), functions.items()))
+    for f in sorted(all_functions.keys()):
+        all_functions[f]()
+
+if __name__ == "__main__":
+    logging.getLogger("angr.analyses.cfg").setLevel(logging.DEBUG)
+
+    if len(sys.argv) > 1:
+        globals()['_test_' + sys.argv[1]]()
+    else:
+        test_all()
