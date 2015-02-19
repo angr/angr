10756ad2443d1cc267754c2f8bedfe398d256674
Christophe <christophe@cs.ucsb.edu>
2015-02-18 19:03:23 -0800

    Fix test_explorer

--- tests/test_explorer.py
+++ tests/test_explorer.py
@@ -3,14 +3,12 @@ import nose
 
 def test_xpl():
     p = angr.Project("blob/x86_64/all")
-    b = angr.surveyors.Explorer(p, find=(0x600cd0,), num_find=4)
+    pltaddr = p.main_binary.get_plt_stub_addr("printf")
+
+    nose.tools.assert_equal(pltaddr, 0x400560)
     a = angr.surveyors.Explorer(p, find=(0x400560,), num_find=4)
-
     a.run()
-    b.run()
-
     nose.tools.assert_equal(len(a.found), 4)
-    nose.tools.assert_equal(len(b.found), 4)
 
 
 if __name__ == '__main__':
