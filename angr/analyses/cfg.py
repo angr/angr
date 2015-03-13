d7e5656d149ff8b9d4ae6f5787ce8dd870885e2d
Nilo Redini <n.redini@gmail.com>
2015-03-13 17:28:09 +0000

    added check for stack pointer

--- angr/analyses/cfg.py
+++ angr/analyses/cfg.py
@@ -659,9 +659,10 @@ class CFG(Analysis, CFGBase):
             new_call_stack.ret(exit_target)
 
             # Calculate the delta of stack pointer
-            delta = sp - old_sp + self._p.arch.call_sp_fix
-            # Set sp_delta of the function
-            self._function_manager.function(entry_wrapper.current_function_address).sp_delta = delta
+            if sp != None and old_sp != None:
+                delta = sp - old_sp + self._p.arch.call_sp_fix
+                # Set sp_delta of the function
+                self._function_manager.function(entry_wrapper.current_function_address).sp_delta = delta
 
             self._function_manager.return_from(
                 function_addr=entry_wrapper.current_function_address,
