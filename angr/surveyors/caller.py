79b25f539dd431d94233647f800c36b1e0c34f9e
Yan <yans@yancomm.net>
2015-03-26 00:36:22 -0700

    extra functionality to do some helpful solving and such

--- angr/surveyors/caller.py
+++ angr/surveyors/caller.py
@@ -2,7 +2,22 @@ from .explorer import Explorer
 import simuvex
 
 class Caller(Explorer):
+    '''
+    Caller is a surveyor that executes functions to see what they do.
+    '''
+
     def __init__(self, project, addr, args=(), start=None, num_find=None, **kwargs):
+        '''
+        Creates a Caller.
+
+        @arg project: the project
+        @arg addr: the address to start calling at
+        @arg args: a tuple of arguments. Any members that are None will be replaced with
+         arg     symbolic expressions with a length of the architecture's bitwidth
+        @arg start: a path (or set of paths) to start from
+        @arg num_find: find at least this many returns from the function
+        '''
+
         self._fake_return_addr = project.entry
         self._cc = simuvex.DefaultCC[project.arch.name](project.arch)
 
@@ -25,9 +40,53 @@ class Caller(Explorer):
 
         super(Caller, self).__init__(project, find=self._fake_return_addr, start=start_paths, num_find=num_find, **kwargs)
 
-    def iter_returns(self, runs=None):
+    def map_se(self, func, *args, **kwargs):
+        '''
+        Maps the state.se."func" function for all the return address states. This is a generator.
+
+        @arg func: the function name, used as getattr(p.state.se, func). Normally any_n_int or any_n_str
+
+        @kwarg runs: the maximum number of runs to execute
+        @kwarg solutions: check only returns with this value as a possible solution
+        @kwarg sort: sort the result before yielding it
+
+        Other *args and **kwargs are passed to the called state.se.* function.
+
+        @yields (r, func_return) for each state.
+        '''
+
+        runs = kwargs.pop('runs', None)
+        solution = kwargs.pop('solution', None)
+        extra_constraints = kwargs.pop('extra_constraints', ())
+        sort = kwargs.pop('sort', True)
+        for r,p in self.iter_returns(runs=runs, solution=solution):
+            v = getattr(p.state.se, func)(*args, extra_constraints=extra_constraints + (r==solution,), **kwargs)
+            yield r, sorted(v) if sort else v
+
+    def map_func(self, func, runs=None, solution=None):
+        '''
+        Calls func(return_value, args_tuple, path) for each function return. This is a generator.
+
+        @arg func: the function to call
+        @kwarg runs: the maximum number of runs to execute
+        @kwarg solutions: check only returns with this value as a possible solution
+
+        @yields the return values of func
+        '''
+        for r,p in self.iter_returns(runs=runs, solution=solution):
+            yield func(r, self.symbolic_args, p)
+
+    def iter_returns(self, runs=None, solution=None):
+        '''
+        Yields (return_value, path) for every return. This is a generator.
+
+        @kwarg runs: the maximum number of runs to execute
+        @kwarg solutions: check only returns with this value as a possible solution
+        '''
         for p in self.iter_found(runs=runs):
-            r = self._cc.get_return_expr(p.state)
+            r = p.state.se.simplify(self._cc.get_return_expr(p.state))
+            if solution is not None and not p.state.se.solution(r, solution):
+                continue
             yield (r, p)
     __iter__ = iter_returns
 
