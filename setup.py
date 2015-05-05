4ce3dfe1cbfa660384f6a14139c11b7f3f352861
Yan <yans@yancomm.net>
2015-05-04 19:06:58 -0700

    updated setup

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from distutils.core import setup
 
-setup(name='simuvex', version='1.00', packages=['simuvex', 'simuvex.procedures', 'simuvex.procedures.stubs', 'simuvex.procedures.syscalls', 'simuvex.procedures.ld-uClibc___so___0', 'simuvex.procedures.libc___so___6'], package_dir={'simuvex': 'simuvex'}, )
+setup(name='simuvex', version='1.00', packages=['simuvex', 'simuvex.plugins', 'simuvex.storage', 'simuvex.vex', 'simuvex.procedures', 'simuvex.procedures.cgc', 'simuvex.procedures.ld-linux-x86-64___so___2', 'simuvex.procedures.testing', 'simuvex.procedures.stubs', 'simuvex.procedures.syscalls', 'simuvex.procedures.ld-uClibc___so___0', 'simuvex.procedures.libc___so___6'])
