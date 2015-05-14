350efcc165faf91cbf1ae73ff7d9e1be7d8b475b
John Grosen <jmg@johngrosen.com>
2015-05-13 19:37:52 -0700

    structs re-implemented!

---
+++ simuvex/plugins/view.py
@@ -0,0 +1,122 @@
+import claripy
+from .plugin import SimStatePlugin
+
+class SimRegNameView(SimStatePlugin):
+    def __init__(self):
+        super(SimRegNameView, self).__init__()
+
+    def __getattr__(self, k):
+        try:
+            return self.state.reg_expr(self.state.arch.registers[k][0])
+        except KeyError:
+            return getattr(super(SimRegNameView, self), k)
+
+    def __setattr__(self, k, v):
+        if k == 'state':
+            return object.__setattr__(self, k, v)
+
+        try:
+            return self.state.store_reg(self.state.arch.registers[k][0], v)
+        except KeyError:
+            raise AttributeError(k)
+
+    def __dir__(self):
+        return self.state.arch.registers.keys()
+
+    def copy(self):
+        return SimRegNameView()
+
+    def merge(self, others, merge_flag, flag_values):
+        return False, [ ]
+
+    def widen(self, others, merge_flag, flag_values):
+        return False, [ ]
+
+class SimMemView(SimStatePlugin):
+    def __init__(self, ty=None, addr=None, state=None):
+        super(SimMemView, self).__init__()
+        self._type = ty
+        self._addr = addr
+        if state is not None:
+            self.set_state(state)
+
+    def set_state(self, state):
+        super(SimMemView, self).set_state(state)
+
+    def _deeper(self, **kwargs):
+        if 'ty' not in kwargs:
+            kwargs['ty'] = self._type
+        if 'addr' not in kwargs:
+            kwargs['addr'] = self._addr
+        if 'state' not in kwargs:
+            kwargs['state'] = self.state
+        return SimMemView(**kwargs)
+
+    def __getitem__(self, k):
+        if isinstance(k, slice):
+            if k.step is not None:
+                raise ValueError("Slices with strides are not supported")
+            elif k.start is None:
+                raise ValueError("Must specify start index")
+            elif k.stop is not None:
+                raise ValueError("Slices with stop index are not supported")
+            else:
+                return self._deeper(addr=k.start)
+        else:
+            raise KeyError(k)
+
+    types = {}
+    state = None
+
+    def __repr__(self):
+        value = '<unresolvable>' if not self.resolvable else self.resolved
+        if isinstance(self._addr, claripy.BV) and not self._addr.symbolic:
+            addr = format(self._addr.model.value, '#x')
+        else:
+            addr = repr(self._addr)
+        return '<{} {} at {}>'.format(self._type.name,
+                                      value,
+                                      addr)
+
+    def __dir__(self):
+        return self._type._refine_dir() if self._type else SimMemView.types.keys()
+
+    def __getattr__(self, k):
+        if k in ('resolvable', 'resolved'):
+            return object.__getattr__(self, k)
+        return self._type._refine(self, k) if self._type else self._deeper(ty=SimMemView.types[k](self.state.arch))
+
+    def __setattr__(self, k, v):
+        if k in ('state', '_addr', '_type'):
+            return object.__setattr__(self, k, v)
+        self.__getattr__(k).store(v)
+
+    def copy(self):
+        return SimMemView()
+
+    def merge(self, others, merge_flag, flag_values):
+        return False, [ ]
+
+    def widen(self, others, merge_flag, flag_values):
+        return False, [ ]
+
+    @property
+    def resolvable(self):
+        return self._type is not None and self._addr is not None
+
+    @property
+    def resolved(self):
+        if not self.resolvable:
+            raise ValueError("Trying to resolve value without type and addr defined")
+        return self._type.extract(self.state, self._addr)
+
+    def store(self, value):
+        if self._addr is None:
+            raise ValueError("Trying to store to location without specifying address")
+        if self._type is None:
+            raise ValueError("Trying to store to location without specifying type")
+
+        self._type.store(self.state, self._addr, value)
+
+from ..s_type import ALL_TYPES
+SimMemView.types = ALL_TYPES # identity purposefully here
