4fcd23a752fda4ad393b9b062d5f3976293d51ad
Yan <yans@yancomm.net>
2015-05-24 16:00:44 -0700

    moved cgc analysis into shellphish-crs

--- angr/analyses/__init__.py
+++ angr/analyses/__init__.py
@@ -4,7 +4,6 @@ from .sleakslice import Sleakslice
 from .cdg import CDG
 from .ddg import DDG
 from .vfg import VFG
-from .cgc import CGC
 from .boyscout import BoyScout
 from .girlscout import GirlScout
 from .buffer_overlap import BufferOverlap
