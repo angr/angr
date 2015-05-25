6b93d907fbb2a2a1216eb793433ba1542b040861
Yan <yans@yancomm.net>
2015-05-25 01:38:47 -0700

    fixed *lots* of issue with hierarchies

--- angr/path_heirarchy.py
+++ angr/path_heirarchy.py
@@ -9,6 +9,7 @@ class PathHeirarchy(object):
         self._successors = { }
         self._good = set()
         self._path_mapping = weakref.WeakValueDictionary()
+        #self._path_mapping = { }
 
     def _lineage(self, se):
         lineage = [ ]
@@ -19,20 +20,33 @@ class PathHeirarchy(object):
             lineage.append(cur)
 
         lineage.reverse()
+
         return lineage
 
     def _all_successors(self, se):
         todo = [ se ]
+        successors = [ ]
 
         while len(todo) != 0:
             cur = todo.pop()
             if cur in self._successors:
-                todo.extend(self._successors[cur])
-            yield cur
+                todo += self._successors[cur]
+            successors.append(cur)
+
+        return successors
+
+    def _is_reachable(self, se):
+        if se in self._good:
+            return True
+        else:
+            s = se._solver.satisfiable()
+            if s:
+                self._good.add(se)
+            return s
 
     def _find_root_unreachable(self, se):
         lineage = self._lineage(se)
-        if len(lineage) == 0 or lineage[-1].reachable:
+        if len(lineage) == 0 or self._is_reachable(lineage[-1]):
             return se
 
         good = max([0] + [ i for i,s in enumerate(lineage) if s in self._good ])
@@ -43,41 +57,46 @@ class PathHeirarchy(object):
             cur = (bad+good)/2
 
             if cur == good or cur == bad:
-                if lineage[bad].satisfiable():
-                    self._good.add(lineage[bad])
+                if self._is_reachable(lineage[bad]):
                     bad += 1
 
                 root = lineage[bad]
                 l.debug("... returning %d (%s)", bad, root)
                 return root
-            elif lineage[cur].satisfiable():
+            elif self._is_reachable(lineage[cur]):
                 l.debug("... %d is reachable", cur)
-                self._good.add(lineage[cur])
                 good = cur
             else:
                 l.debug("... %d is unreachable", bad)
                 bad = cur
 
     def _prune(self, se):
-        for c in self._all_successors(se):
+        to_prune = self._all_successors(se)
+        to_prune.append(se)
+
+        for c in to_prune:
             l.debug("... pruning %s", c)
 
             try:
                 p = self._path_mapping[c]
+                l.debug("... still there: %s", p)
                 if not p._error:
                     p.error = PathUnreachableError('ancestral path %s is unreachable' % p)
                     p._reachable = False
             except KeyError:
-                pass
+                l.debug("... gc'ed: path of %s", c)
 
+        for c in to_prune:
             self._remove(c)
-        self._remove(se)
+
+        return to_prune
 
     def _remove(self, se):
         if se in self._parents:
             del self._parents[se]
         if se in self._successors:
             del self._successors[se]
+        self._good.discard(se)
 
     def unreachable(self, p):
         se = p.state.se
@@ -87,13 +106,24 @@ class PathHeirarchy(object):
         l.debug("... root is %s", root)
         self._prune(root)
 
+        #paths = [ ]
+        #for se in pruned:
+        #   try:
+        #       paths.append(self._path_mapping[se])
+        #   except KeyError:
+        #       pass
+        #return paths
+
     def add_successors(self, p, successors):
         l.debug("Adding %d successors for %s", len(successors), p)
+
         if p.state.se not in self._successors:
             self._successors[p.state.se] = [ ]
+        self._successors[p.state.se] += [ s.state.se for s in successors ]
+        self._path_mapping[p.state.se] = p
 
-        self._successors[p.state.se].extend(successors)
         for s in successors:
-            self._parents[s] = p.state.se
+            self._parents[s.state.se] = p.state.se
+            self._path_mapping[s.state.se] = s
 
 from .errors import PathUnreachableError
