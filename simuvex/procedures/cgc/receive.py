cf029f7b815b6969490592a0287e2f9b400e3301
Fish <fish@cs.ucsb.edu>
2015-05-31 16:53:39 -0700

    special handler in cgc.syscalls.receive for receive

--- simuvex/procedures/cgc/receive.py
+++ simuvex/procedures/cgc/receive.py
@@ -1,9 +1,27 @@
 import simuvex
+from itertools import count
+
+fastpath_data_counter = count()
 
 class receive(simuvex.SimProcedure):
     #pylint:disable=arguments-differ
 
     def run(self, fd, buf, count, rx_bytes):
+
+        if self.state.mode == 'fastpath':
+            # Special case for CFG generation
+            if not self.state.se.symbolic(count):
+                actual_size = count
+                data = self.state.se.Unconstrained(
+                    'receive_data_%d' % fastpath_data_counter.next(),
+                    self.state.se.exactly_int(actual_size) * 8
+                )
+                self.state.store_mem(buf, data)
+            else:
+                actual_size = self.state.se.Unconstrained('receive_length', self.state.arch.bits)
+            self.state.store_mem(rx_bytes, actual_size, endness='Iend_LE')
+
+            return self.state.se.BVV(0, self.state.arch.bits)
 
         if ABSTRACT_MEMORY in self.state.options:
             actual_size = count
