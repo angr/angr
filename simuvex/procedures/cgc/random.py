59d35afec40de00a9932742b584ff9ddd0595247
Fish <fish@cs.ucsb.edu>
2015-05-31 17:33:44 -0700

    added special case for fastpath mode in cgc.syscalls.random

--- simuvex/procedures/cgc/random.py
+++ simuvex/procedures/cgc/random.py
@@ -7,6 +7,22 @@ class random(simuvex.SimProcedure):
     #pylint:disable=arguments-differ
 
     def run(self, buf, count, rnd_bytes):
+        if self.state.mode == 'fastpath':
+            # Special case for CFG
+            if (not self.state.se.symbolic(count) and
+                    not self.state.se.symbolic(buf) and
+                    not self.state.se.symbolic(rnd_bytes)):
+                if self.state.cgc.addr_invalid(rnd_bytes) or self.state.cgc.addr_invalid(buf):
+                    return self.state.cgc.EFAULT
+
+                max_count = self.state.se.exactly_int(count)
+                random_num = self.state.se.Unconstrained('random_%d' % rand_count.next(), max_count * 8)
+                self.state.store_mem(buf, random_num, size=count)
+                self.state.store_mem(rnd_bytes, count, endness='Iend_LE')
+
+            # We always return something in fastpath mode
+            return self.state.se.BVV(0, self.state.arch.bits)
+
         # return code
         r = self.state.se.ite_cases((
                 (self.state.cgc.addr_invalid(buf), self.state.cgc.EFAULT),
