6673cc80998b051c8cb2538aa19578a1cd257889
Christophe <christophe@cs.ucsb.edu>
2015-06-24 12:14:23 -0700

    Test for user input tracking

--- setup.py
+++ setup.py
@@ -1,3 +1,8 @@
 from distutils.core import setup
 
-setup(name='angr', version='1.00', packages=['angr', 'angr.surveyors', 'angr.tests'], package_dir={'angr': 'angr'}, )
+setup(
+    name='angr',
+    version='1.00',
+    packages=['angr', 'angr.surveyors', 'angr.analyses'],
+    install_requires=[i.strip() for i in open('requirements.txt').readlines() if 'git' not in i]
+)
