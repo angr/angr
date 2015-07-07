d03a36ac29397737002d41c99d6221787c8dad07
Yan <yans@yancomm.net>
2015-07-06 20:50:06 -0700

    changing llsc to always succeed, since we only support single-threaded stuff for now and it'll always succeed in a single thread

--- simuvex/vex/statements/llsc.py
+++ simuvex/vex/statements/llsc.py
@@ -12,7 +12,7 @@ l = logging.getLogger('simuvex.vex.statements.llsc')
 
 class SimIRStmt_LLSC(SimIRStmt):
     def _execute(self):
-        l.warning("LLSC is handled soundly but imprecisely.")
+        #l.warning("LLSC is handled soundly but imprecisely.")
         addr = self._translate_expr(self.stmt.addr)
 
         if self.stmt.storedata is None:
@@ -22,20 +22,24 @@ class SimIRStmt_LLSC(SimIRStmt):
             self.state.scratch.store_tmp(self.stmt.result, data)
         else:
             # it's a store-conditional
-            result = self.state.se.Unconstrained('llcd_result', 1)
+            #result = self.state.se.Unconstrained('llcd_result', 1)
 
-            new_data = self._translate_expr(self.stmt.storedata)
-            old_data = self.state.memory.load(addr.expr, new_data.size_bytes(), endness=self.stmt.endness)
+            #new_data = self._translate_expr(self.stmt.storedata)
+            #old_data = self.state.memory.load(addr.expr, new_data.size_bytes(), endness=self.stmt.endness)
 
-            store_data = self.state.se.If(result == 1, new_data.expr, old_data)
+            #store_data = self.state.se.If(result == 1, new_data.expr, old_data)
+
+            # for single-threaded programs, an SC will never fail. For now, we just assume it succeeded.
+            store_data = self._translate_expr(self.stmt.storedata)
+            result = self.state.se.BVV(1, 1)
 
             # the action
             if o.MEMORY_REFS in self.state.options:
-                data_ao = SimActionObject(store_data, reg_deps=new_data.reg_deps(), tmp_deps=new_data.tmp_deps())
+                data_ao = SimActionObject(store_data, reg_deps=store_data.reg_deps(), tmp_deps=store_data.tmp_deps())
                 addr_ao = SimActionObject(addr.expr, reg_deps=addr.reg_deps(), tmp_deps=addr.tmp_deps())
-                guard_ao = SimActionObject(result == 1)
-                size_ao = SimActionObject(store_data.length)
-                a = SimActionData(self.state, self.state.memory.id, SimActionData.WRITE, addr=addr_ao, data=data_ao, condition=guard_ao, size=size_ao)
+                #guard_ao = SimActionObject(result == 1)
+                size_ao = SimActionObject(store_data.expr.length)
+                a = SimActionData(self.state, self.state.memory.id, SimActionData.WRITE, addr=addr_ao, data=data_ao, size=size_ao)
                 self.actions.append(a)
             else:
                 a = None
