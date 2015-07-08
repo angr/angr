f9b8ee72091d2f7af3c25b0a6921bea08957773e
Yan <yans@yancomm.net>
2015-07-07 19:16:10 -0700

    added a quick performance test

---
+++ tests/manual_performance.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import time
+#import nose
+import logging
+l = logging.getLogger("angr_tests.counter")
+l.setLevel(logging.INFO)
+
+try:
+    # pylint: disable=W0611,F0401
+    import standard_logging
+    import angr_debug
+except ImportError:
+    pass
+
+
+addresses_counter = {
+    'armel': None,
+    'armhf': None,  # addr+1 to force thumb
+    'i386': None,
+    'mips': None,
+    'mipsel': None,
+    'ppc': None,
+    'ppc64': None,
+    'x86_64': None
+}
+
+import angr
+
+import os
+location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../binaries/tests'))
+
+def run_counter(arch):
+    p = angr.Project(location + '/' + arch + '/counter')
+
+    pg = p.path_group()
+
+    start = time.time()
+    pg.step(n=1000)
+    end = time.time()
+
+    l.info("Time passed: %f seconds", end-start)
+
+def test_counter():
+    for arch in addresses_counter:
+        yield run_counter, arch
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        for func, march in test_counter():
+            print 'testing ' + march
+            func(march)
+    else:
+        run_counter(sys.argv[1])
