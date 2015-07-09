b3abcfae2cae789dc1b1a84cbc5736cdd4120bb7
John Grosen <jmgrosen@gmail.com>
2015-07-08 17:43:50 -0700

    all mappings should be SimTypes

--- simuvex/s_type.py
+++ simuvex/s_type.py
@@ -332,10 +332,10 @@ _C_TYPE_TO_SIMTYPE = {
     ('uint32_t',): lambda _: SimTypeInt(32, False),
     ('int64_t',): lambda _: SimTypeInt(64, True),
     ('uint64_t',): lambda _: SimTypeInt(64, False),
-    ('ptrdiff_t',): lambda arch: (arch.bits, False),
-    ('size_t',): lambda arch: (arch.bits, False),
-    ('ssize_t',): lambda arch: (arch.bits, True),
-    ('uintptr_t',) : lambda arch: SimTypeInt(arch.bits, False)
+    ('ptrdiff_t',): lambda arch: SimTypeInt(arch.bits, False),
+    ('size_t',): lambda arch: SimTypeInt(arch.bits, False),
+    ('ssize_t',): lambda arch: SimTypeInt(arch.bits, True),
+    ('uintptr_t',) : lambda arch: SimTypeInt(arch.bits, False),
 }
 
 def _decl_to_type(decl):
