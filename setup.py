f828da72e781d4f774cbd29ed466a9bd24196cd8
Yan <yans@yancomm.net>
2015-08-11 01:35:56 -0700

    some setup updates

--- setup.py
+++ setup.py
@@ -17,11 +17,11 @@ setup(
         'claripy',
         'simuvex',
         'cle',
+        'mulpyplexer',
+        'cooldict',
+        'ana'
     ],
     dependency_links=[
-        'git+https://github.com/zardus/mulpyplexer#egg=mulpyplexer-0.1',
-        'git+https://github.com/zardus/cooldict#egg=cooldict-0.1',
-        'git+https://github.com/zardus/ana#egg=ana-0.1',
         'git+https://github.com/angr/archinfo.git#egg=archinfo-4.0',
         'git+https://github.com/angr/pyvex.git#egg=pyvex-4.0',
         'git+https://github.com/angr/claripy.git#egg=claripy-0.1',
