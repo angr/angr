8b932d2441af0d44b1df9b19878a506640556abf
Yan <yans@yancomm.net>
2015-08-16 21:07:35 -0700

    dpkg-fix for dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.00',
     packages=['simuvex', 'simuvex.plugins', 'simuvex.storage', 'simuvex.vex', 'simuvex.vex.statements', 'simuvex.vex.expressions', 'simuvex.procedures', 'simuvex.procedures.cgc', 'simuvex.procedures.ld-linux-x86-64___so___2', 'simuvex.procedures.testing', 'simuvex.procedures.stubs', 'simuvex.procedures.syscalls', 'simuvex.procedures.ld-uClibc___so___0', 'simuvex.procedures.libc___so___6'],
     install_requires=[
-        'dpkt',
+        'dpkt-fix',
         'pyvex',
         'archinfo',
         'claripy',
