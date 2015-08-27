a3864aff2733b8e7ce1cf02e3f1a1057596b5082
Yan <yans@yancomm.net>
2015-08-26 23:17:43 -0700

    tick setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='angr',
-    version='0.8.0',
+    version='0.8.1',
     description='The next-generation binary analysis platform from UC Santa Barbara\'s Seclab!',
     packages=['angr', 'angr.surveyors', 'angr.analyses'],
     install_requires=[
