ae44fb1d43d02d2ea28168d8e335f1749b6089a6
Riyad Parvez <riyadparvez@users.noreply.github.com>
2015-09-10 13:19:47 -0400

    Implemented pc_actions_LOGIC_CondLE
    According to the discussion at: https://github.com/angr/angr/issues/10

--- simuvex/vex/ccall.py
+++ simuvex/vex/ccall.py
@@ -760,6 +760,19 @@ def pc_actions_LOGIC_CondZ(state, arg_l, arg_r, cc_ndep):
 
     return r
 
+def pc_actions_LOGIC_CondLE(state, arg_l, arg_r, cc_ndep):
+    se = state.se
+
+    result = (arg_l <= arg_r)
+    if se.is_true(result):
+        r = se.BVV(1, 1)
+    elif se.is_false(result):
+        r = se.BVV(0, 1)
+    else:
+        r = state.se.If(result, se.BitVecVal(1, 1), se.BitVecVal(0, 1))
+
+    return r
+    
 def pc_actions_LOGIC_CondNZ(state, arg_l, arg_r, cc_ndep):
     se = state.se
 
