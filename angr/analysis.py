bf47150b870a4ba4d44f19d7ba21aa1541bdf5d9
Andrew Dutcher <andrew@andrewdutcher.com>
2015-09-14 10:10:03 -0700

    Option 2 for making analyses less weird

--- angr/analysis.py
+++ angr/analysis.py
@@ -1,6 +1,5 @@
 import sys
 import contextlib
-import functools
 
 RESULT_ERROR = "An error occured"
 RESULT_NONE = "No result"
@@ -25,51 +24,21 @@ class AnalysisLogEntry(object):
 
 class AnalysisMeta(type):
     """
-    This metaclass is Yan being too clever with analysis creation, and will probably
-    come back to bite us in the ass. Basically, it replaces the Analysis' __init__
-    with the base __init__, which yanks the project and the fail_fast setting out
-    from the arguments. This lets us avoid the three worse alternatives:
-
-        1. override __new__, which doesn't work because you still can't pull the
-           args out
-        2. make the analyses implement something other than __init__, which will
-           reduce pylint's effectiveness in regards to the defined-outside-init stuff
-        3. make the analyses handle the project and fail_fast args and pass them on
-           to Analysis.__init__
+    Whenever Analaysis is subclassed, make sure we know about it.
     """
 
     def __new__(mcs, name, bases, d):
-        if name == 'Analysis':
-            d['__init__'] = d['__core_init__']
-        else:
-            d['__analysis_init__'] = d['__init__']
-            del d['__init__']
-
         t = type.__new__(mcs, name, bases, d)
         if name != 'Analysis':
-            registered_analyses[d.get('__analysis_name__', name)] = t
+            chosen_name = d.get('__analysis_name__', name)
+            registered_analyses[chosen_name] = (t.__module__, name)
         return t
 
-
-# function that produces unpredictable results that should appease pylint's
-# static analysis and stop giving us those awful errors!!!!
-
-def dummy_func(*args, **kwargs):
-    return args + list(kwargs)
 
 class Analyses(object):
     """
     This class contains functions for all the registered and runnable analyses,
     """
-
-    def _analysis(self, analysis, *args, **kwargs):
-        fail_fast = kwargs.pop('fail_fast', False)
-
-        # Call __init__ of chosen analysis
-        a = analysis(self._p, fail_fast, *args, **kwargs)
-
-        return a
-
     def __init__(self, p):
         """
         Creates an Analyses object
@@ -78,25 +47,33 @@ class Analyses(object):
         @param analysis_results: the result cache
         """
         self._p = p
+        self._registered_analyses = {}
+        self.reload_analyses()
 
-        # Appease pylint's static analysis
-        self.CFG = dummy_func
-        self.VFG = dummy_func
-        self.Veritesting = dummy_func
-        self.DDG = dummy_func
-        self.CDG = dummy_func
-        self.BackwardSlice = dummy_func
-        self.BoyScout = dummy_func
-        self.GirlScout = dummy_func
+    def reload_analyses(self):
+        for analysis_name, (module_name, key) in registered_analyses.iteritems():
+            module = reload(sys.modules[module_name])
+            analysis = getattr(module, key)
+            self._registered_analyses[analysis_name] = self._specialize_analysis(analysis)
 
-        for analysis_name,analysis in registered_analyses.items():
-            partial = functools.partial(self._analysis, analysis)
-            partial.__doc__ = analysis_name + ' analysis'
-            if analysis.__doc__:
-                partial.__doc__ += analysis.__doc__
-            if analysis.__analysis_init__.__doc__:
-                partial.__doc__ += analysis.__analysis_init__.__doc__
-            setattr(self, analysis_name, partial)
+    def _specialize_analysis(self, analysis):
+        @staticmethod
+        def __new__(cls, *args, **kwargs): # pylint: disable=unused-argument
+            fail_fast = kwargs.pop('fail_fast', False)
+
+            oself = object.__new__(cls)
+            oself.named_errors = {}
+            oself.errors = []
+            oself.log = []
+
+            oself._fail_fast = fail_fast
+            oself._p = self._p
+
+            oself.result = RESULT_NONE
+            return oself
+
+        analysis.__new__ = __new__
+        return analysis
 
     def __getstate__(self):
         return self._p
@@ -104,26 +81,23 @@ class Analyses(object):
     def __setstate__(self, s):
         self.__init__(s)
 
+    def __getattr__(self, k):
+        r = super(Analyses, self).__getattribute__('_registered_analyses')
+        if k == '_registered_analyses':
+            return r
+        if k in r:
+            return r[k]
+        return super(Analyses, self).__getattribute__(k)
+
+    def __dir__(self):
+        return dir(Analyses) + self._registered_analyses.keys()
+
 
 registered_analyses = {}
 
 class Analysis(object):
+    # pylint: disable=no-member
     __metaclass__ = AnalysisMeta
-
-    def __core_init__(self, project, fail_fast, *args, **kwargs):
-        # pylint:disable=attribute-defined-outside-init
-        self.named_errors = {}
-        self.errors = []
-        self.log = []
-
-        self._fail_fast = fail_fast
-        self._p = project
-
-        self.result = RESULT_NONE
-
-        if kwargs.pop('do_analysis', True):
-            self.__analysis_init__(*args, **kwargs)  # pylint:disable=no-member
-
     def post_load(self):
         pass
 
@@ -153,5 +127,5 @@ class Analysis(object):
     def _checkpoint(self):
         pass
 
-    def copy(self):
-        return self.__class__(self._p, self._fail_fast, do_analysis=False)
+    def __repr__(self):
+        return '<%s Analysis Result at %#x>' % id(self)
