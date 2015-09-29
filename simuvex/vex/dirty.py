1ab0629e55e21c35fce9d7c935358661a4b8a545
Andrew Dutcher <andrew@andrewdutcher.com>
2015-09-29 13:20:04 -0700

    Implement x86 FNINIT and FINIT x87 dirty helper

--- simuvex/vex/dirty.py
+++ simuvex/vex/dirty.py
@@ -251,3 +251,10 @@ def x86g_dirtyhelper_LGDT_LIDT(state, addr, op):
         pass
 
     return None, [ ]
+
+def x86g_dirtyhelper_FINIT(state, bbptr):
+    state.regs.fpu_tags = 0
+    state.regs.fpround = 0
+    state.regs.fc3210 = 0x0300
+    state.regs.ftop = 0
+    return None, [ ]
