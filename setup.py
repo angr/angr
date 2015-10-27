c2400bad2d8a02e23758d8bbf2cfc9180e54d971
Yan <yans@yancomm.net>
2015-10-27 16:44:46 -0700

    bintrees added as a dependency

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     url='https://github.com/angr/simuvex',
     packages=['simuvex', 'simuvex.plugins', 'simuvex.storage', 'simuvex.vex', 'simuvex.vex.statements', 'simuvex.vex.expressions', 'simuvex.procedures', 'simuvex.procedures.cgc', 'simuvex.procedures.ld-linux-x86-64___so___2', 'simuvex.procedures.testing', 'simuvex.procedures.stubs', 'simuvex.procedures.syscalls', 'simuvex.procedures.ld-uClibc___so___0', 'simuvex.procedures.libc___so___6'],
     install_requires=[
+        'bintrees',
         'dpkt-fix',
         'pyvex',
         'archinfo',
