c44fb58f0ad55d2fc542061acdafa1b02460e788
Yan <yans@yancomm.net>
2015-11-01 00:29:28 -0700

    SimSolver now pulls from the frontend before trying to pull from claripy.all_operations

--- simuvex/plugins/solver.py
+++ simuvex/plugins/solver.py
@@ -148,7 +148,10 @@ class SimSolver(SimStatePlugin):
         return self._solver.downsize()
 
     def __getattr__(self, a):
-        f = getattr(claripy._all_operations, a)
+        try:
+            f = getattr(self._solver, a)
+        except AttributeError:
+            f = getattr(claripy._all_operations, a)
         if hasattr(f, '__call__'):
             ff = functools.partial(ast_stripping_op, f, the_solver=self)
             ff.__doc__ = f.__doc__
