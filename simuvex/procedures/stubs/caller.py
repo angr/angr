aa5bc77e78e82fbe63acf2fd8f6764a420f2e4e8
Fish <fish@cs.ucsb.edu>
2015-11-08 17:23:36 -0800

    Make sure Caller does not return

--- simuvex/procedures/stubs/caller.py
+++ simuvex/procedures/stubs/caller.py
@@ -11,6 +11,8 @@ class Caller(simuvex.SimProcedure):
     Caller stub. Creates a Ijk_Call exit to the specified function
     """
 
+    NO_RET = True
+
     def run(self, target_addr=None):
         self.call(target_addr, [ ], self.after_call)
 
