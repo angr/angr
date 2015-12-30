e39dc34e5aa44f45078afe3f27940aab55614e66
Fish <fish@cs.ucsb.edu>
2015-12-30 00:57:59 -0800

    Move CFGNode to a separate file.

--- angr/analyses/cfg.py
+++ angr/analyses/cfg.py
@@ -10,116 +10,13 @@ from archinfo import ArchARM
 
 import angr
 from ..entry_wrapper import EntryWrapper
-from .cfg_base import CFGBase
 from ..analysis import Analysis, register_analysis
-from ..errors import AngrCFGError, AngrError, AngrBackwardSlicingError
+from ..errors import AngrCFGError, AngrError
+from ..path import make_path
+from .cfg_node import CFGNode
+from .cfg_base import CFGBase
 
 l = logging.getLogger(name="angr.analyses.cfg")
-
-class CFGNode(object):
-    '''
-    This class stands for each single node in CFG.
-    '''
-    def __init__(self, callstack_key, addr, size, cfg, input_state=None, simprocedure_name=None, syscall_name=None,
-                 looping_times=0, no_ret=False, is_syscall=False, syscall=None, simrun=None, function_address=None,
-                 final_states=None):
-        '''
-        Note: simprocedure_name is not used to recreate the SimProcedure object. It's only there for better
-        __repr__.
-        '''
-
-        self.callstack_key = callstack_key
-        self.addr = addr
-        self.input_state = input_state
-        self.simprocedure_name = simprocedure_name
-        self.syscall_name = syscall_name
-        self.size = size
-        self.looping_times = looping_times
-        self.no_ret = no_ret
-        self.is_syscall = is_syscall
-        self.syscall = syscall
-        self._cfg = cfg
-        self.function_address = function_address
-        self.name = simprocedure_name or cfg.project.loader.find_symbol_name(addr)
-        if function_address and self.name is None:
-            self.name = cfg.project.loader.find_symbol_name(function_address)
-            if self.name is not None:
-                self.name = "%s+0x%x" % (self.name, (addr - function_address))
-
-        # If this CFG contains an Ijk_Call, `return_target` stores the returning site.
-        # Note: this is regardless of whether the call returns or not. You should always check the `no_ret` property if
-        # you are using `return_target` to do some serious stuff.
-        self.return_target = None
-
-        self.instruction_addrs = [ ]
-        if not self.is_simprocedure and simrun is not None:
-            # This is a SimIRSB
-            # Grab all instruction addresses out!
-            irsb = simrun.irsb
-            self.instruction_addrs = [ s.addr for s in irsb.statements if type(s) is pyvex.IRStmt.IMark ]  # pylint:disable=unidiomatic-typecheck
-
-        self.final_states = [ ] if final_states is None else final_states
-
-    @property
-    def successors(self):
-        return self._cfg.get_successors(self)
-
-    @property
-    def predecessors(self):
-        return self._cfg.get_predecessors(self)
-
-    @property
-    def is_simprocedure(self):
-        return self.simprocedure_name is not None
-
-
-    def downsize(self):
-        """
-        Drop saved states.
-        :return: None
-        """
-
-        self.input_state = None
-        self.final_states = [ ]
-
-    def copy(self):
-        c = CFGNode(self.callstack_key,
-                    self.addr,
-                    self.size,
-                    self._cfg,
-                    self.input_state,
-                    self.simprocedure_name,
-                    self.looping_times,
-                    self.no_ret,
-                    self.is_syscall,
-                    self.syscall,
-                    self.function_address,
-                    final_states=self.final_states[ :: ])
-        c.instruction_addrs = self.instruction_addrs[ :: ]
-        return c
-
-    def __repr__(self):
-        if self.name is not None:
-            s = "<CFGNode %s (0x%x) [%d]>" % (self.name, self.addr, self.looping_times)
-        else:
-            s = "<CFGNode 0x%x (%d) [%d]>" % (self.addr, self.size, self.looping_times)
-
-        return s
-
-    def __eq__(self, other):
-        if isinstance(other, simuvex.SimIRSB) or isinstance(other, simuvex.SimProcedure):
-            raise ValueError("You do not want to be comparing a SimRun to a CFGNode.")
-        if not isinstance(other, CFGNode):
-            return False
-        return (self.callstack_key == other.callstack_key and
-                self.addr == other.addr and
-                self.size == other.size and
-                self.looping_times == other.looping_times and
-                self.simprocedure_name == other.simprocedure_name
-                )
-
-    def __hash__(self):
-        return hash((self.callstack_key, self.addr, self.looping_times, self.simprocedure_name))
 
 class PendingExit(object):
     def __init__(self, returning_source, state, bbl_stack, call_stack):
