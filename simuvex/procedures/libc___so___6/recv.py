0daf68b10c1db29669a7b280de24e771e22de3e1
thebarbershopper <cld251@gmail.com>
2016-01-08 17:39:28 +0000

    Quick fix for the API mismatch between recv and posix.read

--- simuvex/procedures/libc___so___6/recv.py
+++ simuvex/procedures/libc___so___6/recv.py
@@ -8,6 +8,5 @@ class recv(simuvex.SimProcedure):
     #pylint:disable=arguments-differ
 
     def run(self, fd, dst, length):
-        data = self.state.posix.read(fd, self.state.se.any_int(length))
-        self.state.memory.store(dst, data)
-        return length
+        bytes_recvd = self.state.posix.read(fd, dst, self.state.se.any_int(length))
+        return bytes_recvd
