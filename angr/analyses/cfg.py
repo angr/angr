dc08f0727af1b5c69e80df0184468707f17e2789
Fish <fish@cs.ucsb.edu>
2016-01-29 11:56:25 -0500

    Convert some of the error output in CFG to debug output.
    ... to avoid user panicking :-)

--- angr/analyses/cfg.py
+++ angr/analyses/cfg.py
@@ -814,34 +814,38 @@ class CFG(Analysis, CFGBase):
         except simuvex.SimIRSBError:
             # It's a tragedy that we came across some instructions that VEX
             # does not support. I'll create a terminating stub there
-            l.error("SimIRSBError occurred(%s). Creating a PathTerminator.", ex)
+            l.debug("Caught a SimIRSBError during CFG recovery. Creating a PathTerminator.", exc_info=True)
             error_occurred = True
             sim_run = \
                 simuvex.procedures.SimProcedures["stubs"]["PathTerminator"](
                     state, addr=addr)
+
         except claripy.ClaripyError:
-            l.error("ClaripyError: ", exc_info=True)
+            l.debug("Caught a ClaripyError during CFG recovery. Don't panic, this is usually expected.", exc_info=True)
             error_occurred = True
             # Generate a PathTerminator to terminate the current path
             sim_run = \
                 simuvex.procedures.SimProcedures["stubs"]["PathTerminator"](
                     state, addr=addr)
+
         except simuvex.SimError:
-            l.error("SimError: ", exc_info=True)
+            l.debug("Caught a SimError when during CFG recovery. Don't panic, this is usually expected.", exc_info=True)
 
             error_occurred = True
             # Generate a PathTerminator to terminate the current path
             sim_run = \
                 simuvex.procedures.SimProcedures["stubs"]["PathTerminator"](
                     state, addr=addr)
-        except AngrError as ex:
+
+        except AngrError:
             section = self.project.loader.main_bin.find_section_containing(addr)
             if section is None:
                 sec_name = 'No section'
             else:
                 sec_name = section.name
-            l.error("AngrError %s when creating SimRun at 0x%x (%s)",
-                    ex, addr, sec_name)
+            # AngrError shouldn't really happen though
+            l.error("Caught an AngrError during CFG recovery at %#x (%s)",
+                    addr, sec_name, exc_info=True)
             # We might be on a wrong branch, and is likely to encounter the
             # "No bytes in memory xxx" exception
             # Just ignore it
