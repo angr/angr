149a8091333766068cac445db770ea73055d8647
Andrew Dutcher <andrew@andrewdutcher.com>
2016-02-05 19:23:55 -0800

    Make the userhook take the length arg b/c why not

--- simuvex/procedures/stubs/UserHook.py
+++ simuvex/procedures/stubs/UserHook.py
@@ -4,7 +4,7 @@ class UserHook(simuvex.SimProcedure):
     NO_RET = True
 
     # pylint: disable=arguments-differ
-    def run(self, user_func=None, user_kwargs=None, default_return_addr=None):
+    def run(self, user_func=None, user_kwargs=None, default_return_addr=None, length=None):
         result = user_func(self.state, **user_kwargs)
         if result is None:
             self.add_successor(self.state, default_return_addr, self.state.se.true, 'Ijk_NoHook')
