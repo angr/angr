f8cda63974f81a103f78fbdc2cb77e7e9f281ef3
Fish <fish@cs.ucsb.edu>
2016-02-20 11:24:08 -0500

    fix fgetc so that it uses FILE instead of fd

--- simuvex/procedures/libc___so___6/fgetc.py
+++ simuvex/procedures/libc___so___6/fgetc.py
@@ -1,6 +1,8 @@
 import simuvex
-from simuvex.s_type import SimTypeFd, SimTypeInt
-from claripy import BVV
+from simuvex.s_type import SimTypeInt
+
+from . import _IO_FILE
+
 ######################################
 # fgetc
 ######################################
@@ -9,9 +11,12 @@ from claripy import BVV
 class fgetc(simuvex.SimProcedure):
     # pylint:disable=arguments-differ
 
-    def run(self, fd):
-        self.argument_types = {0: SimTypeFd()}
+    def run(self, file_ptr):
         self.return_type = SimTypeInt(32, True)
+
+        fd_offset = _IO_FILE[self.state.arch.name]['fd']
+        fd = self.state.mem[file_ptr + fd_offset : ].int.resolved
+
         data = self.inline_call(
             simuvex.SimProcedures['libc.so.6']['_IO_getc'], fd).ret_expr
         return data
