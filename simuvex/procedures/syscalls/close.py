58509e6c368451356be37bd5bc5577ae6cb4be8a
Owlz <whootandahalf@gmail.com>
2016-03-03 22:44:36 -0500

    Added checking of input type

--- simuvex/procedures/syscalls/close.py
+++ simuvex/procedures/syscalls/close.py
@@ -8,6 +8,9 @@ class close(simuvex.SimProcedure):
     #pylint:disable=arguments-differ
 
     def run(self, fd):
+        if type(fd) == simuvex.s_action_object.SimActionObject:
+            fd = self.state.se.any_int(fd)
+        
         # Return error if file is already closed
         if self.state.posix.files[fd].closed == True:
             v = self.state.se.BVV(-1, self.state.arch.bits)
