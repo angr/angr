18ed162a89175bd6abaf9635689a6088b0d71258
Yan <yans@yancomm.net>
2016-03-04 00:40:10 -0800

    add a bunch of posix constants for use elsewhere

--- simuvex/plugins/posix.py
+++ simuvex/plugins/posix.py
@@ -18,6 +18,47 @@ Stat = namedtuple('Stat', ('st_dev', 'st_ino', 'st_nlink', 'st_mode', 'st_uid',
 class SimStateSystem(SimStatePlugin):
     #__slots__ = [ 'maximum_symbolic_syscalls', 'files', 'max_length' ]
 
+    # some posix constants
+    SIG_BLOCK=0
+    SIG_UNBLOCK=1
+    SIG_SETMASK=2
+
+    EPERM      =     1 # /* Operation not permitted */
+    ENOENT     =     2 # /* No such file or directory */
+    ESRCH      =     3 # /* No such process */
+    EINTR      =     4 # /* Interrupted system call */
+    EIO        =     5 # /* I/O error */
+    ENXIO      =     6 # /* No such device or address */
+    E2BIG      =     7 # /* Argument list too long */
+    ENOEXEC    =     8 # /* Exec format error */
+    EBADF      =     9 # /* Bad file number */
+    ECHILD     =    10 # /* No child processes */
+    EAGAIN     =    11 # /* Try again */
+    ENOMEM     =    12 # /* Out of memory */
+    EACCES     =    13 # /* Permission denied */
+    EFAULT     =    14 # /* Bad address */
+    ENOTBLK    =    15 # /* Block device required */
+    EBUSY      =    16 # /* Device or resource busy */
+    EEXIST     =    17 # /* File exists */
+    EXDEV      =    18 # /* Cross-device link */
+    ENODEV     =    19 # /* No such device */
+    ENOTDIR    =    20 # /* Not a directory */
+    EISDIR     =    21 # /* Is a directory */
+    EINVAL     =    22 # /* Invalid argument */
+    ENFILE     =    23 # /* File table overflow */
+    EMFILE     =    24 # /* Too many open files */
+    ENOTTY     =    25 # /* Not a typewriter */
+    ETXTBSY    =    26 # /* Text file busy */
+    EFBIG      =    27 # /* File too large */
+    ENOSPC     =    28 # /* No space left on device */
+    ESPIPE     =    29 # /* Illegal seek */
+    EROFS      =    30 # /* Read-only file system */
+    EMLINK     =    31 # /* Too many links */
+    EPIPE      =    32 # /* Broken pipe */
+    EDOM       =    33 # /* Math argument out of domain of func */
+    ERANGE     =    34 # /* Math result not representable */
+
+
     def __init__(self, initialize=True, files=None, concrete_fs=False, chroot=None, sockets=None,
             pcap_backer=None, inetd=False, argv=None, argc=None, environ=None, auxv=None, tls_modules=None,
             fs=None, queued_syscall_returns=None):
