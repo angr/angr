e2c2862be699ae7f68d571a36b5e54ac763c183b
Fish <fish@cs.ucsb.edu>
2016-03-09 01:33:33 -0500

    Modify function.py to adapt to the renaming from unresolved_indirect_jumps to _unresolved_indirect_jumps

--- angr/knowledge/function.py
+++ angr/knowledge/function.py
@@ -109,7 +109,7 @@ class Function(object):
     @property
     def has_unresolved_jumps(self):
         for addr in self.block_addrs:
-            if addr in self._function_manager._kb.unresolved_indirect_jumps:
+            if addr in self._function_manager._kb._unresolved_indirect_jumps:
                 b = self._function_manager.project.factory.block(addr)
                 if b.vex.jumpkind == 'Ijk_Boring':
                     return True
@@ -118,7 +118,7 @@ class Function(object):
     @property
     def has_unresolved_calls(self):
         for addr in self.block_addrs:
-            if addr in self._function_manager._kb.unresolved_indirect_jumps:
+            if addr in self._function_manager._kb._unresolved_indirect_jumps:
                 b = self._function_manager.project.factory.block(addr)
                 if b.vex.jumpkind == 'Ijk_Call':
                     return True
