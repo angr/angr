7a381361088cf7c0cb9078b133a70b00c38fbde3
Yan <yans@yancomm.net>
2016-03-15 13:17:09 -0700

    add knowledge to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='angr',
     version='4.6.3.15',
     description='The next-generation binary analysis platform from UC Santa Barbara\'s Seclab!',
-    packages=['angr', 'angr.surveyors', 'angr.analyses'],
+    packages=['angr', 'angr.surveyors', 'angr.analyses', 'angr.knowledge'],
     install_requires=[
         'capstone',
         'networkx',
