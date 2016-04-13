722f60358fa8307f96567bf8da575fbd8e9e5878
Adrian Herrera <adrian.herrera02@gmail.com>
2016-04-13 23:23:35 +1000

    Modified naming scheme for CFG nodes if addr - function_address results in a negative number

--- angr/analyses/cfg_node.py
+++ angr/analyses/cfg_node.py
@@ -41,7 +41,9 @@ class CFGNode(object):
         if function_address and self.name is None:
             self.name = cfg.project.loader.find_symbol_name(function_address)
             if self.name is not None:
-                self.name = "%s+0x%x" % (self.name, (addr - function_address))
+                offset = addr - function_address
+                direction = "+" if offset >= 0 else "-"
+                self.name = "%s%s0x%x" % (self.name, direction, abs(offset))
 
         # If this CFG contains an Ijk_Call, `return_target` stores the returning site.
         # Note: this is regardless of whether the call returns or not. You should always check the `no_ret` property if
