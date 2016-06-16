012cd096d54deee2130b6d2b4fd7d8841dd3184b
Yan <yans@yancomm.net>
2016-06-16 03:44:39 -0700

    add exploration techniques to setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='angr',
     version='4.6.6.15',
     description='The next-generation binary analysis platform from UC Santa Barbara\'s Seclab!',
-    packages=['angr', 'angr.surveyors', 'angr.analyses', 'angr.knowledge'],
+    packages=['angr', 'angr.surveyors', 'angr.analyses', 'angr.knowledge', 'angr.exploration_techniques'],
     install_requires=[
         'capstone',
         'networkx',
