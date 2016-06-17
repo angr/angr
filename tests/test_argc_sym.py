4d7246695840c63eb90ad2ecd56eef22316fdd95
Yan <yans@yancomm.net>
2016-06-16 18:39:26 -0700

    rewrite tests to make them deterministic

--- tests/test_argc_sym.py
+++ tests/test_argc_sym.py
@@ -1,5 +1,5 @@
-import nose
-import angr, claripy
+import angr
+import claripy
 
 import logging
 l = logging.getLogger("angr_tests")
@@ -7,32 +7,22 @@ l = logging.getLogger("angr_tests")
 import os
 test_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../binaries/tests'))
 
+def _verify_results(pg, sargc, length=400):
+    argcs = pg.mp_found.state.se.any_int(sargc)
+    strs = pg.mp_found.state.se.any_str(pg.mp_found.state.memory.load(pg.mp_found.state.regs.sp, length))
+
+    for a,s in zip(argcs.mp_items, strs.mp_items):
+        assert a in (0,1,2)
+        assert "Good man" in s if a == 1 else "Very Good man" if a == 2 else True
+
 def test_mips():
     arger_mips = angr.Project(test_location + "/mips/argc_symbol")
     r_addr = [0x400720, 0x40076c, 0x4007bc]
 
     sargc = claripy.BVS('argc', 32)
     s = arger_mips.factory.path(args = [claripy.BVS('arg_0', 40*8), claripy.BVS('arg_1', 40*8), claripy.BVS('arg_2', 40*8)], env ={"HOME": "/home/angr"}, argc=sargc)
-    xpl = arger_mips.surveyors.Explorer(find=r_addr, num_find=100, start=s)
-    xpl.run()
-
-    nose.tools.assert_equals(len(xpl.found), 3)
-
-    found = xpl.found[0]
-    argc = found.state.se.any_int(sargc)
-    nose.tools.assert_equals(argc, 0)
-
-    found = xpl.found[1]
-    argc = found.state.se.any_int(sargc)
-    conc = found.state.se.any_str(found.state.memory.load(found.state.registers.load('sp'), 400))
-    nose.tools.assert_equals("Good man" in conc, True)
-    nose.tools.assert_equals(argc, 1)
-
-    found = xpl.found[2]
-    argc = found.state.se.any_int(sargc)
-    conc = found.state.se.any_str(found.state.memory.load(found.state.registers.load('sp'), 400))
-    nose.tools.assert_equals("Very Good man" in conc, True)
-    nose.tools.assert_equals(argc, 2)
+    pg = arger_mips.factory.path_group(s).explore(find=r_addr, num_find=100)
+    _verify_results(pg, sargc)
 
 def test_mipsel():
     arger_mipsel = angr.Project(test_location + "/mipsel/argc_symbol")
@@ -40,26 +30,8 @@ def test_mipsel():
 
     sargc = claripy.BVS('argc', 32)
     s = arger_mipsel.factory.path(args = [claripy.BVS('arg_0', 40*8), claripy.BVS('arg_1', 40*8), claripy.BVS('arg_2', 40*8)], env ={"HOME": "/home/angr"}, argc=sargc)
-    xpl = arger_mipsel.surveyors.Explorer(find=r_addr, num_find=100, start=s)
-    xpl.run()
-
-    nose.tools.assert_equals(len(xpl.found), 3)
-
-    found = xpl.found[0]
-    argc = found.state.se.any_int(sargc)
-    nose.tools.assert_equals(argc, 0)
-
-    found = xpl.found[1]
-    argc = found.state.se.any_int(sargc)
-    conc = found.state.se.any_str(found.state.memory.load(found.state.registers.load('sp'), 400))
-    nose.tools.assert_equals("Good man" in conc, True)
-    nose.tools.assert_equals(argc, 1)
-
-    found = xpl.found[2]
-    argc = found.state.se.any_int(sargc)
-    conc = found.state.se.any_str(found.state.memory.load(found.state.registers.load('sp'), 400))
-    nose.tools.assert_equals("Very Good man" in conc, True)
-    nose.tools.assert_equals(argc, 2)
+    pg = arger_mipsel.factory.path_group(s).explore(find=r_addr, num_find=100)
+    _verify_results(pg, sargc)
 
 def test_i386():
     arger_i386 = angr.Project(test_location + "/i386/argc_symbol")
@@ -67,26 +39,8 @@ def test_i386():
 
     sargc = claripy.BVS('argc', 32)
     s = arger_i386.factory.path(args = [claripy.BVS('arg_0', 40*8), claripy.BVS('arg_1', 40*8), claripy.BVS('arg_2', 40*8)], env ={"HOME": "/home/angr"}, argc=sargc)
-    xpl = arger_i386.surveyors.Explorer(find=r_addr, num_find=100, start=s)
-    xpl.run()
-
-    nose.tools.assert_equals(len(xpl.found), 3)
-
-    found = xpl.found[0]
-    argc = found.state.se.any_int(sargc)
-    nose.tools.assert_equals(argc, 0)
-
-    found = xpl.found[1]
-    argc = found.state.se.any_int(sargc)
-    conc = found.state.se.any_str(found.state.memory.load(found.state.registers.load('sp'), 400))
-    nose.tools.assert_equals("Good man" in conc, True)
-    nose.tools.assert_equals(argc, 1)
-
-    found = xpl.found[2]
-    argc = found.state.se.any_int(sargc)
-    conc = found.state.se.any_str(found.state.memory.load(found.state.registers.load('sp'), 400))
-    nose.tools.assert_equals("Very Good man" in conc, True)
-    nose.tools.assert_equals(argc, 2)
+    pg = arger_i386.factory.path_group(s).explore(find=r_addr, num_find=100)
+    _verify_results(pg, sargc)
 
 def test_amd64():
     arger_amd64 = angr.Project(test_location + "/x86_64/argc_symbol", load_options={'auto_load_libs': False})
@@ -94,26 +48,8 @@ def test_amd64():
 
     sargc = claripy.BVS('argc', 64)
     s = arger_amd64.factory.path(args = [claripy.BVS('arg_0', 40*8), claripy.BVS('arg_1', 40*8), claripy.BVS('arg_2', 40*8)], env ={"HOME": "/home/angr"}, argc=sargc)
-    xpl = arger_amd64.surveyors.Explorer(find=r_addr, num_find=100, start=s)
-    xpl.run()
-
-    nose.tools.assert_equals(len(xpl.found), 3)
-
-    found = xpl.found[0]
-    argc = found.state.se.any_int(sargc)
-    nose.tools.assert_equals(argc, 0)
-
-    found = xpl.found[1]
-    argc = found.state.se.any_int(sargc)
-    conc = found.state.se.any_str(found.state.memory.load(found.state.registers.load('sp'), 800))
-    nose.tools.assert_equals("Good man" in conc, True)
-    nose.tools.assert_equals(argc, 1)
-
-    found = xpl.found[2]
-    argc = found.state.se.any_int(sargc)
-    conc = found.state.se.any_str(found.state.memory.load(found.state.registers.load('sp'), 800))
-    nose.tools.assert_equals("Very Good man" in conc, True)
-    nose.tools.assert_equals(argc, 2)
+    pg = arger_amd64.factory.path_group(s).explore(find=r_addr, num_find=100)
+    _verify_results(pg, sargc, length=800)
 
 def test_arm():
     arger_arm = angr.Project(test_location + "/armel/argc_symbol")
@@ -121,26 +57,8 @@ def test_arm():
 
     sargc = claripy.BVS('argc', 32)
     s = arger_arm.factory.path(args = [claripy.BVS('arg_0', 40*8), claripy.BVS('arg_1', 40*8), claripy.BVS('arg_2', 40*8)], env ={"HOME": "/home/angr"}, argc=sargc)
-    xpl = arger_arm.surveyors.Explorer(find=r_addr, num_find=100, start=s)
-    xpl.run()
-
-    nose.tools.assert_equals(len(xpl.found), 3)
-
-    found = xpl.found[0]
-    argc = found.state.se.any_int(sargc)
-    nose.tools.assert_equals(argc, 0)
-
-    found = xpl.found[1]
-    argc = found.state.se.any_int(sargc)
-    conc = found.state.se.any_str(found.state.memory.load(found.state.registers.load('sp'), 400))
-    nose.tools.assert_equals("Good man" in conc, True)
-    nose.tools.assert_equals(argc, 1)
-
-    found = xpl.found[2]
-    argc = found.state.se.any_int(sargc)
-    conc = found.state.se.any_str(found.state.memory.load(found.state.registers.load('sp'), 400))
-    nose.tools.assert_equals("Very Good man" in conc, True)
-    nose.tools.assert_equals(argc, 2)
+    pg = arger_arm.factory.path_group(s).explore(find=r_addr, num_find=100)
+    _verify_results(pg, sargc)
 
 def test_ppc32():
     arger_ppc32 = angr.Project(test_location + "/ppc/argc_symbol")
@@ -148,27 +66,8 @@ def test_ppc32():
 
     sargc = claripy.BVS('argc', 32)
     s = arger_ppc32.factory.path(args = [claripy.BVS('arg_0', 40*8), claripy.BVS('arg_1', 40*8), claripy.BVS('arg_2', 40*8)], env ={"HOME": "/home/angr"}, argc=sargc)
-    xpl = arger_ppc32.surveyors.Explorer(find=r_addr, num_find=100, start=s)
-    xpl.run()
-
-    nose.tools.assert_equals(len(xpl.found), 3)
-
-    found = xpl.found[0]
-    argc = found.state.se.any_int(sargc)
-    conc = found.state.se.any_str(found.state.memory.load(found.state.registers.load('sp'), 400))
-    nose.tools.assert_equals(argc, 0)
-
-    found = xpl.found[1]
-    argc = found.state.se.any_int(sargc)
-    conc = found.state.se.any_str(found.state.memory.load(found.state.registers.load('sp'), 400))
-    nose.tools.assert_equals("Good man" in conc, True)
-    nose.tools.assert_equals(argc, 1)
-
-    found = xpl.found[2]
-    argc = found.state.se.any_int(sargc)
-    conc = found.state.se.any_str(found.state.memory.load(found.state.registers.load('sp'), 400))
-    nose.tools.assert_equals("Very Good man" in conc, True)
-    nose.tools.assert_equals(argc, 2)
+    pg = arger_ppc32.factory.path_group(s).explore(find=r_addr, num_find=100)
+    _verify_results(pg, sargc)
 
 if __name__ == "__main__":
     test_mips()
