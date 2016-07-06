0220d798a4159810f630bf2fe844d74982376215
Andrew Dutcher <andrew@andrewdutcher.com>
2016-07-06 03:12:59 -0700

    Don't assume list order in test (pypy fix)

--- tests/test_symbolic.py
+++ tests/test_symbolic.py
@@ -32,7 +32,7 @@ def test_concretization_strategies():
     s.add_constraints(x <= 3)
 
     ss = s.copy()
-    nose.tools.assert_equal(ss.se.any_n_str(ss.memory.load(x, 1), 10), ['B', 'C', 'D'])
+    nose.tools.assert_equal(set(ss.se.any_n_str(ss.memory.load(x, 1), 10), {'B', 'C', 'D'})
 
     ss = s.copy()
     x = s.se.BVS('x', s.arch.bits)
