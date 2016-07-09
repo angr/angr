9a3301d70f51ca2e5c25f9c966bceab3b1c1ab78
Yan <yans@yancomm.net>
2016-07-09 01:16:31 -0700

    fix up explosion test

--- tests/manual_explosion.py
+++ tests/manual_explosion.py
@@ -2,8 +2,16 @@ import angr
 import resource
 import time
 import simuvex
+
+import os
+
+
+b = angr.Project(os.path.join(
+    os.path.dirname(__file__),
+    "../../binaries-private/cgc_scored_event_2/cgc/0b32aa01_01"
+))
+
 start = time.time()
-b = angr.Project("/home/salls/Projects/angr/binaries-private/cgc_scored_event_2/cgc/0b32aa01_01")
 #s = b.factory.blank_state(add_options={"COMPOSITE_SOLVER"})
 s = b.factory.blank_state(add_options={"COMPOSITE_SOLVER"}, remove_options={"LAZY_SOLVES"})
 pg = b.factory.path_group(s)
@@ -14,5 +22,3 @@ print "MB:", resource.getrusage(resource.RUSAGE_SELF).ru_maxrss/1024
 print "time:", end-start
 #assert len(pg.active) == 1538
 #assert len(pg.deadended) == 27
-
-
