55465022777c970490b4189ea345ec1a273ffc20
Nick Stephens <nick.d.stephens@gmail.com>
2016-07-10 18:20:46 -0700

    Better identification of strcmp, now testcase to prevent misidentification when testing a strcasecmp

--- identifier/functions/strcmp.py
+++ identifier/functions/strcmp.py
@@ -45,8 +45,9 @@ class strcmp(Func):
         if s is None or s.se.any_int(s.regs.eax) != 0:
             return False
 
+        # should return true for strcmp, false for memcpy
         bufa = "asdfa\x00sfdadfsa"
-        bufb = "asdfb\x00sadfsadf"
+        bufb = "asdfa\x00sadfsadf"
         test_input = [bufa, bufb]
         test_output = [bufa, bufb]
         test = TestData(test_input, test_output, return_val, max_steps)
@@ -55,6 +56,7 @@ class strcmp(Func):
             return False
         outval1 = s.se.any_int(s.regs.eax)
 
+        # should fail
         bufa = "asdfc\x00as"
         bufb = "asdfb\x0011232"
         test_input = [bufa, bufb]
@@ -65,7 +67,18 @@ class strcmp(Func):
             return False
         outval2 = s.se.any_int(s.regs.eax)
 
-        if outval1 == 0 or outval2 == 0:
+        # should prevent us from misidentifying strcasecmp
+        bufa = "ASDFC\x00"
+        bufb = "asdfc\x00"
+        test_input = [bufa, bufb]
+        test_output = [bufa, bufb]
+        test = TestData(test_input, test_output, return_val, max_steps)
+        s = runner.get_out_state(func, test)
+        if s is None:
             return False
+        outval3 = s.se.any_int(s.regs.eax)
 
-        return True
+        if outval1 == 0 and outval2 != 0 and outval3 != 0:
+            return True
+
+        return False
