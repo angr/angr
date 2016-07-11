16b9cb68de5e3fc9ff220cf4a6b09336750f9595
Nick Stephens <nick.d.stephens@gmail.com>
2016-07-10 18:54:45 -0700

    Add comparison function identification testcase

--- tests/test_identifier.py
+++ tests/test_identifier.py
@@ -8,10 +8,11 @@ bin_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..
 import logging
 logging.getLogger("identifier").setLevel("DEBUG")
 
+# smoketest
 def test_palindrome():
-    '''
+    """
     Test identification of functions in palindrome.
-    '''
+    """
 
     p = angr.Project(os.path.join(bin_location, "cgc_scored_event_1/cgc/0b32aa01_01"))
     idfer = identifier.Identifier(p)
@@ -21,6 +22,23 @@ def test_palindrome():
         seen[addr] = symbol
 
     nose.tools.assert_equals(seen[134513765], "receive_n4")
+
+def test_comparison_identification():
+    """
+    Test identification of common comparison functions
+    """
+
+    true_symbols = {0x804a0f0: 'strcmp', 0x8048e60: 'memcmp', 0x8049f40: 'strcasecmp'}
+
+    p = angr.Project(os.path.join(bin_location, "tests/i386/identifiable"))
+    idfer = identifier.Identifier(p)
+
+    seen = dict()
+    for addr, symbol in idfer.run():
+        seen[addr] = symbol
+
+    for addr, symbol in true_symbols.items():
+        nose.tools.assert_equal(true_symbols[addr], seen[addr])
 
 def run_all():
     functions = globals()
