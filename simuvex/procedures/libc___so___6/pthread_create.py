cbe1c30f2baa6209c230bfa8ac1075389cda44c8
Andrew Dutcher <andrew@andrewdutcher.com>
2016-07-12 21:58:58 -0700

    add static exits to pthread_create

--- simuvex/procedures/libc___so___6/pthread_create.py
+++ simuvex/procedures/libc___so___6/pthread_create.py
@@ -1,4 +1,5 @@
 import simuvex
+import pyvex
 
 ######################################
 # pthread_create
@@ -21,3 +22,30 @@ class pthread_create(simuvex.SimProcedure):
 
         self.add_successor(new_state, code_addr, new_state.se.true, jumpkind='Ijk_Call')
         return self.state.se.BVV(0, self.state.arch.bits)
+
+    @classmethod
+    def static_exits(cls, arch, blocks):
+        # Execute those blocks with a blank state, and then dump the arguments
+        blank_state = simuvex.SimState(arch=arch, mode="fastpath")
+
+        # Execute each block
+        state = blank_state
+        for b in blocks:
+            # state.regs.ip = next(iter(stmt for stmt in b.statements if isinstance(stmt, pyvex.IRStmt.IMark))).addr
+            irsb = simuvex.SimIRSB(state, b,
+                        addr=next(iter(stmt for stmt in b.statements if isinstance(stmt, pyvex.IRStmt.IMark))).addr)
+            if irsb.successors:
+                state = irsb.successors[0]
+            else:
+                break
+
+        cc = simuvex.DefaultCC[arch.name](arch)
+        callfunc = cc.arg(state, 2)
+        retaddr = state.memory.load(state.regs.sp, arch.bytes)
+
+        all_exits = [
+            (callfunc, 'Ijk_Call'),
+            (retaddr, 'Ijk_Ret')
+        ]
+
+        return all_exits
