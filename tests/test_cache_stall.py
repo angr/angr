de6c376031179caed3861e7bfd483a8a7013da24
Chris Salls <salls@cs.ucsb.edu>
2016-07-13 23:57:50 -0700

    do something dirty to use zen during the trace of test_cache_stall

--- tests/test_cache_stall.py
+++ tests/test_cache_stall.py
@@ -1,4 +1,5 @@
 import os
+import rex.trace_additions
 import nose
 import tracer
 
@@ -18,6 +19,7 @@ def test_cache_stall():
 
     # test a valid palindrome
     t = tracer.Tracer(os.path.join(bin_location, "cgc_trials/CROMU_00071"), "0c0c492a53acacacacacacacacacacacacac000100800a0b690e0aef6503697d660a0059e20afc0a0a332f7d66660a0059e20afc0a0a332f7fffffff16fb1616162516161616161616166a7dffffff7b0e0a0a6603697d660a0059e21c".decode('hex'))
+    rex.trace_additions.ZenPlugin.prep_tracer(t)
     crash_path, crash_state = t.run()
 
     nose.tools.assert_not_equal(crash_path, None)
@@ -25,6 +27,7 @@ def test_cache_stall():
 
     # load it again
     t = tracer.Tracer(os.path.join(bin_location, "cgc_trials/CROMU_00071"), "0c0c492a53acacacacacacacacacacacacac000100800a0b690e0aef6503697d660a0059e20afc0a0a332f7d66660a0059e20afc0a0a332f7fffffff16fb1616162516161616161616166a7dffffff7b0e0a0a6603697d660a0059e21c".decode('hex'))
+    rex.trace_additions.ZenPlugin.prep_tracer(t)
     crash_path, crash_state = t.run()
 
     nose.tools.assert_not_equal(crash_path, None)
