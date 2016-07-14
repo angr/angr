3f883cfa7070ea8632c52c83447aae1b4415404e
Andrew Dutcher <andrew@andrewdutcher.com>
2016-07-14 10:17:17 -0700

    Also test unicorn self-modifying code

--- tests/test_self_modifying_code.py
+++ tests/test_self_modifying_code.py
@@ -13,5 +13,13 @@ def test_self_modifying_code():
     retval = pg.one_deadended.state.regs.ebx
     nose.tools.assert_true(claripy.is_true(retval == 65))
 
+    pgu = p.factory.path_group(p.factory.entry_state(add_options={o.STRICT_PAGE_ACCESS} | o.unicorn))
+    pgu.step(until=lambda lpg: len(lpg.active) != 1)
+    retval = pgu.one_deadended.state.regs.ebx
+    nose.tools.assert_true(claripy.is_true(retval == 65))
+
+    nose.tools.assert_true(pg.one_deadended.addr_trace.hardcopy == pgu.one_deadended.addr_trace.hardcopy)
+
+
 if __name__ == '__main__':
     test_self_modifying_code()
