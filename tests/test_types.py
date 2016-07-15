634f1ca629ed161f32d5e9380ca26bda73ddb163
Andrew Dutcher <andrew@andrewdutcher.com>
2016-07-15 04:34:52 -0700

    update the test case

--- tests/test_types.py
+++ tests/test_types.py
@@ -4,13 +4,13 @@ import simuvex
 import claripy
 
 def test_type_annotation():
-    struct_ty = simuvex.s_type_backend.SimStructAbstract('mystruct')
-    ptr = claripy.BVS('ptr', 32).annotate(simuvex.s_type_backend.TypeAnnotation(simuvex.s_type.SimTypePointer(struct_ty)))
+    my_ty = simuvex.s_type.SimTypeTop()
+    ptr = claripy.BVS('ptr', 32).annotate(simuvex.s_type_backend.TypeAnnotation(simuvex.s_type.SimTypePointer(my_ty, label=[])))
     ptroffset = ptr + 4
 
     bt = simuvex.s_type_backend.TypeBackend()
     tv = bt.convert(ptroffset)
-    nose.tools.assert_equal(tv.ty.pts_to.label, 'mystruct')
+    nose.tools.assert_is(tv.ty.pts_to, my_ty)
     nose.tools.assert_true(claripy.is_true(tv.ty.offset == 4))
 
 if __name__ == '__main__':
