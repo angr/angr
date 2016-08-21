ba5446b8a3270e072e86a9544d22918f902955bb
Yan <yans@yancomm.net>
2016-08-20 21:57:16 -0700

    Closes #1 - should properly pip install outside of an editable install

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name='tracer', version='0.1', description="Symbolically trace concrete inputs.",
-    packages=['tracer'],
+    packages=['tracer', 'tracer.cachemanager', 'tracer.simprocedures'],
     install_requires=[ 'shellphish-qemu' ],
 )
