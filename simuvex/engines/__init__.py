adbdfd5cd54b54c8d75faa259c60bddff11ea3c1
Yan <yans@yancomm.net>
2016-10-05 18:52:00 -0700

    move SimRun's capabilities into SimEngine, create SimEngineRequest

---
+++ simuvex/engines/__init__.py
@@ -0,0 +1,2 @@
+from .request import SimEngineRequest
+from .engine import SimEngine
--- simuvex/s_engine.py
+++
@@ -1,8 +0,0 @@
-class SimEngine(object):
-    """
-    How to actually execute stuff.
-    Abstracts over VEX, LLVM, and hopefully more in the future.
-    """
-
-    def make_sim_run(self, state, addr):
-        pass
