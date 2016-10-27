92fa0699fc1673b76e0878dcce86bd9fd006c0c9
Fish <fish@cs.ucsb.edu>
2016-10-27 00:37:09 -0700

    Typo fix in an error message.

--- angr/factory.py
+++ angr/factory.py
@@ -145,7 +145,7 @@ class AngrObjectFactory(object):
             raise AngrExitError("Cannot create run following jumpkind %s" % jumpkind)
 
         if jumpkind == "Ijk_NoDecode" and not self._project.is_hooked(addr):
-            raise AngrExitError("IR decoding error at #%x. You can hook this instruction with a python replacement "
+            raise AngrExitError("IR decoding error at %#x. You can hook this instruction with a python replacement "
                                 "using project.hook(%#x, your_function, length=length_of_instruction)." % (addr, addr))
 
         elif self._project.is_hooked(addr) and jumpkind != 'Ijk_NoHook':
