8d3d8ba974c4725e9e42b6d9467b5c8ac47025e2
Emilio Coppa <ercoppa@gmail.com>
2016-11-16 16:39:11 +0100

    adding a i386 test for OF computed by ADC

---
+++ tests/test_adc.py
@@ -0,0 +1,28 @@
+import angr
+import sys
+import nose
+import simuvex
+import os
+
+test_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../binaries/tests')) 
+
+def test_adc_i386():
+	proj = angr.Project(test_location + "/i386/test_adc", load_options={'auto_load_libs':False})
+
+	start = 0x804840b
+	end = 0x804842e
+
+	state = proj.factory.blank_state(addr=start, remove_options={simuvex.o.LAZY_SOLVES,}, add_options={simuvex.o.SYMBOLIC_WRITE_ADDRESSES})
+
+	pg = proj.factory.path_group(state, veritesting=False)
+	pg.explore(find=end)
+
+	found_state = pg.found[0].state
+	result = found_state.se.any_int(found_state.regs.eax)
+	nose.tools.assert_equal(result, 0x1)
+
+def test_all():
+    test_adc_i386()
+
+if __name__ == "__main__":
+    test_all()
