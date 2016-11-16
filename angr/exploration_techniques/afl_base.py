d3e45b181adb4136b60c3810404e4fdf0cb8f90a
Lukas Dresel <lukas.dresel@fau.de>
2016-11-15 20:33:28 -0800

    made functions static

--- angr/exploration_techniques/afl_base.py
+++ angr/exploration_techniques/afl_base.py
@@ -1,8 +1,5 @@
-from collections import defaultdict
-
-import simuvex
-
 from . import ExplorationTechnique
+
 
 class AFLBase(ExplorationTechnique):
     """
@@ -10,14 +7,17 @@ class AFLBase(ExplorationTechnique):
 
     It takes care of the transition tracking in the state so that the actual analysis can access this information.
     """
+
     def __init__(self):
         super(AFLBase, self).__init__()
 
-    def _find_unique_transition_path(self, transition_to_path_mapping):
+    @staticmethod
+    def _find_unique_transition_path(transition_to_path_mapping):
         shortest_list = min(transition_to_path_mapping.values(), key=len)
         return shortest_list[0] if len(shortest_list) == 1 else None
 
-    def _find_most_covering_path(self, transition_to_path_mapping):
+    @staticmethod
+    def _find_most_covering_path(transition_to_path_mapping):
         path_to_transition_mapping = {}
         for t, path_list in transition_to_path_mapping.items():
             for path in path_list:
@@ -27,8 +27,6 @@ class AFLBase(ExplorationTechnique):
         return most_covering_path
 
     def _approximate_best_path_set_cover(self, transition_to_covering_paths_mapping):
-        cover_paths = set()
-
         mapping = transition_to_covering_paths_mapping.copy()
 
         while len(mapping) > 0:
@@ -42,7 +40,6 @@ class AFLBase(ExplorationTechnique):
             mapping = {t: p_list for t, p_list in mapping.items() if new_path not in p_list}
 
             yield new_path
-
 
     def step(self, pg, stash, **kwargs):
 
@@ -59,4 +56,3 @@ class AFLBase(ExplorationTechnique):
                 path.state.transition_tracker.register_transition(transition)
 
         return pg
-    
