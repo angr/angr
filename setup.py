22d868e26eeab2b0edd96ecc4502e4ef9a240bb0
Andrew Dutcher <andrew@andrewdutcher.com>
2016-12-03 19:04:57 -0800

    use setuptools

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+    from setuptools import find_packages
+    packages = find_packages()
+except ImportError:
+    from distutils.core import setup
+    import os
+    packages = [x.strip('./').replace('/','.') for x in os.popen('find -name "__init__.py" | xargs -n1 dirname').read().strip().split('\n')]
 
 setup(
     name='angr',
     version='5.6.12.3',
     description='The next-generation binary analysis platform from UC Santa Barbara\'s Seclab!',
-    packages=['angr', 'angr.surveyors', 'angr.analyses', 'angr.knowledge', 'angr.exploration_techniques'],
+    packages=packages,
     install_requires=[
         'capstone',
         'networkx',
