5c8c3c58274e8930f406d994b817352901340fca
Andrew Dutcher <andrew@andrewdutcher.com>
2016-12-17 13:45:03 -0800

    the self-modifying stuff binary has moved

--- tests/test_self_modifying_code.py
+++ tests/test_self_modifying_code.py
@@ -7,7 +7,7 @@ import os
 test_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../binaries/tests'))
 
 def test_self_modifying_code():
-    p = angr.Project(os.path.join(test_location, 'i386/stuff'))
+    p = angr.Project(os.path.join(test_location, 'cgc/stuff'))
     pg = p.factory.path_group(p.factory.entry_state(add_options={o.STRICT_PAGE_ACCESS}))
     pg.step(until=lambda lpg: len(lpg.active) != 1)
     retval = pg.one_deadended.state.regs.ebx
