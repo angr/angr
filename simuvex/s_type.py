8eea4adc82b53c9ef5e510e0855639586fedfe59
Owlz <Owlz@users.noreply.github.com>
2017-01-01 21:56:31 -0600

    getitem update
    Get Item has the same problem of skipping items.

--- simuvex/s_type.py
+++ simuvex/s_type.py
@@ -368,7 +368,7 @@ class SimTypeFixedSizeArray(SimType):
     _can_refine_int = True
 
     def _refine(self, view, k):
-        return view._deeper(addr=view._addr + k * self.elem_type.size, ty=self.elem_type)
+        return view._deeper(addr=view._addr + k * (self.elem_type.size/8), ty=self.elem_type)
 
     def extract(self, state, addr, concrete=False):
         return [self.elem_type.extract(state, addr + i*(self.elem_type.size/8), concrete) for i in xrange(self.length)]
