d7bba99d6631070d3e576d0ae5ae0e80bcaa2112
Maxime Meignan <meignanmaxime@hotmail.fr>
2017-01-28 13:54:30 +0100

    add ptrace in syscall table in SimLinux

--- angr/simos.py
+++ angr/simos.py
@@ -514,6 +514,7 @@ class SimLinux(SimOS):
             14: ('sigprocmask', 'sigprocmask'),
             39: ('getpid', 'getpid'),
             60: ('exit', 'exit'),
+            101: ('ptrace', 'ptrace'),
             158: ('arch_prctl','arch_prctl'),
             186: ('gettid', 'gettid'),
             231: ('exit_group', 'exit'),  # really exit_group, but close enough
@@ -525,6 +526,7 @@ class SimLinux(SimOS):
             4: ('write', 'write'),
             5: ('open', 'open'),
             6: ('close', 'close'),
+            26: ('ptrace', 'ptrace'),
             45: ('brk', 'brk'),
             252: ('exit_group', 'exit'),  # really exit_group, but close enough
         },
