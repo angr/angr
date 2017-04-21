039d2f6379d77e3736c44a20cd5df1eef504eef3
Fish <fish@cs.ucsb.edu>
2017-04-21 01:27:12 -0700

    KeyedRegion: support storing overlapping variables.

--- angr/analyses/variable_recovery/keyed_region.py
+++ angr/analyses/variable_recovery/keyed_region.py
@@ -7,6 +7,55 @@ from bintrees import AVLTree
 l = logging.getLogger('angr.analyses.variable_recovery.keyed_region')
 
 
+class LocationAndVariable(object):
+    def __init__(self, start, variable):
+        self.start = start
+        self.variable = variable
+
+    def __eq__(self, other):
+        assert type(other) is LocationAndVariable
+
+        return self.variable == other.variable and self.start == other.start
+
+
+class RegionObject(object):
+    """
+    Represents one or more objects occupying one or more bytes in KeyedRegion.
+    """
+    def __init__(self, start, size, objects=None):
+        self.start = start
+        self.size = size
+        self.objects = set() if objects is None else objects
+
+    @property
+    def end(self):
+        return self.start + self.size
+
+    @property
+    def variables(self):
+        vars = set()
+        for obj in self.objects:
+            vars.add(obj.variable)
+        return vars
+
+    def includes(self, offset):
+        return self.start <= offset < self.start + self.size
+
+    def split(self, split_at):
+        assert self.includes(split_at)
+        a = RegionObject(self.start, split_at - self.start, self.objects.copy())
+        b = RegionObject(split_at, self.start + self.size - split_at, self.objects.copy())
+
+        return a, b
+
+    def add_object(self, obj):
+        self.objects.add(obj)
+
+    def set_object(self, obj):
+        self.objects.clear()
+        self.objects.add(obj)
+
+
 class KeyedRegion(object):
     """
     KeyedRegion keeps a mapping between stack offsets and all variables covering that offset. It assumes no variable in
@@ -15,7 +64,7 @@ class KeyedRegion(object):
     Registers and function frames can all be viewed as a keyed region.
     """
     def __init__(self, tree=None):
-        self._tree = AVLTree() if tree is None else tree
+        self._storage = AVLTree() if tree is None else tree  # type: AVLTree
 
     def __contains__(self, offset):
         """
@@ -25,160 +74,206 @@ class KeyedRegion(object):
         :return:
         """
 
-        try: base_offset = self._tree.floor_key(offset)
+        try: base_offset, item = self._storage.floor_item(offset)
         except KeyError: return False
 
-        variables = self._tree[base_offset]
-
-        v = variables[0]
-        var_size = 1 if v.size is None else v.size
-        if base_offset <= offset < base_offset + var_size:
+        if item.includes(offset):
             return True
         return False
 
     def __len__(self):
-        return len(self._tree)
+        return len(self._storage)
 
     def __iter__(self):
-        for offset in self._tree.keys():
-            for item in self._tree[offset]:
-                yield item
+        for _, item in self._storage.items():
+            yield item
 
     def copy(self):
-        if not self._tree:
+        if not self._storage:
             return KeyedRegion()
         else:
-            return KeyedRegion(tree=self._tree.copy())
+            return KeyedRegion(tree=self._storage.copy())
 
     def merge(self, other):
-        for offset in set(self._tree.keys()) | set(other._tree.keys()):
-            if offset in self._tree:
-                if offset in other._tree:
-                    for var in other._tree[offset]:
-                        self.add_variable(offset, var)
-                        # print "adding variable %s during merge" % var
-            else:
-                self._tree[offset] = other._tree[offset]
+        """
+        Merge another KeyedRegion into this KeyedRegion.
 
-    def add_variable(self, offset, variable):
+        :param KeyedRegion other: The other instance to merge with.
+        :return: None
+        """
+
+        # TODO: is the current solution not optimal enough?
+        for _, item in other._storage.iter_items():  # type: RegionObject
+            for loc_and_var in item.objects:
+                self.__store(loc_and_var, overwrite=False)
+
+    def add_variable(self, start, variable):
         """
         Add a variable to this region at the given offset.
 
-        :param int offset:
+        :param int start:
         :param SimVariable variable:
         :return: None
         """
 
-        if offset not in self._tree:
-            if self._is_overlapping(offset, variable):
-                l.debug('Variable %s overlaps with an existing variable. Skip.', variable)
-                return
+        self._store(start, variable, overwrite=False)
 
-            self._tree[offset] = [variable]
-
-        else:
-            if variable in self._tree[offset]:
-                return
-
-            if variable.size != self._tree[offset][0].size:
-                l.debug('Adding a new variable to stack with a different size as the previous item. Skip.')
-                return
-
-            self._tree[offset].append(variable)
-
-    def set_variable(self, offset, variable):
+    def set_variable(self, start, variable):
         """
         Add a variable to this region at the given offset, and remove all other variables that are fully covered by 
         this variable.
 
-        :param int offset:
+        :param int start:
         :param SimVariable variable:
         :return: None
         """
 
-        if offset not in self._tree:
-            # TODO: check for overlaps
+        self._store(start, variable, overwrite=True)
 
-            self._tree[offset] = [ variable ]
-
-        else:
-            # TODO: check for overlaps
-
-            self._tree[offset] = [ variable ]
-
-    def get_base_offset(self, offset):
+    def get_base_addr(self, addr):
         """
         Get the base offset (the key we are using to index variables covering the given offset) of a specific offset.
 
-        :param int offset:
+        :param int addr:
         :return:
         :rtype:  int or None
         """
 
-        try: base_offset = self._tree.floor_key(offset)
+        try: base_addr, item = self._storage.floor_item(addr)
         except KeyError: return None
 
-        v = self._tree[base_offset][0]
-        var_size = 1 if v.size is None else v.size
-
-        if base_offset <= offset < base_offset + var_size:
-            return base_offset
+        if item.includes(addr):
+            return base_addr
         else:
             return None
 
-    def get_variables_by_offset(self, offset):
+    def get_variables_by_offset(self, start):
         """
-        Find variables that cover the given stack offset.
+        Find variables covering the given region offset.
 
-        :param int offset:
+        :param int start:
         :return: A list of stack variables.
-        :rtype:  list
+        :rtype:  set
         """
 
-        try: base_offset = self._tree.floor_key(offset)
+        try: base_addr = self._storage.floor_key(start)
         except KeyError: return [ ]
 
-        variables = self._tree[base_offset]
+        item = self._storage[base_addr]  # type: RegionObject
 
-        v = variables[0]
-        var_size = 1 if v.size is None else v.size
-
-        if base_offset <= offset < base_offset + var_size:
-            return variables
-        return [ ]
+        return item.variables
 
 
     #
     # Private methods
     #
 
-    def _is_overlapping(self, offset, variable):
+    def _store(self, start, variable, overwrite=False):
+        """
+        Store a variable into the storage.
+
+        :param int start: The beginning address of the variable.
+        :param variable: The variable to store.
+        :param bool overwrite: Whether existing variables should be overwritten or not.
+        :return: None
+        """
+
+        loc_and_var = LocationAndVariable(start, variable)
+        self.__store(loc_and_var, overwrite=overwrite)
+
+    def __store(self, loc_and_var, overwrite=False):
+        """
+        Store a variable into the storage.
+
+        :param LocationAndVariable loc_and_var: The descriptor describing start address and the variable.
+        :param bool overwrite: Whether existing variables should be overwritten or not.
+        :return: None
+        """
+
+        start = loc_and_var.start
+        variable = loc_and_var.variable
+        variable_size = variable.size if variable.size is not None else 1
+        end = start + variable_size
+
+        # region items in the middle
+        overlapping_items = list(self._storage.item_slice(start, end + 1))
+
+        # is there a region item that begins before the start and overlaps with this variable?
+        try:
+            floor_key, floor_item = self._storage.floor_item(start)  # type: RegionObject
+            if floor_item.includes(start):
+                # insert it into the beginning
+                overlapping_items.insert(0, (floor_key, floor_item))
+        except KeyError:
+            # no there isn't
+            pass
+
+        # scan through the entire list of region items, split existing regions and insert new regions as needed
+        to_update = { start: RegionObject(start, variable_size, { loc_and_var }) }
+        last_end = start
+
+        for _, item in overlapping_items:  # type: RegionObject
+            if item.start < start:
+                # we need to break this item into two
+                a, b = item.split(start)
+                if overwrite:
+                    b.set_object(loc_and_var)
+                else:
+                    b.add_object(loc_and_var)
+                to_update[a.start] = a
+                to_update[b.start] = b
+                last_end = b.end
+            elif item.start > last_end:
+                # there is a gap between the last item and the current item
+                # fill in the gap
+                new_item = RegionObject(last_end, item.start - last_end, { loc_and_var })
+                to_update[new_item.start] = new_item
+                last_end = new_item.end
+            elif item.end > end:
+                # we need to split this item into two
+                a, b = item.split(end)
+                if overwrite:
+                    a.set_object(loc_and_var)
+                else:
+                    a.add_object(loc_and_var)
+                to_update[a.start] = a
+                to_update[b.start] = b
+                last_end = b.end
+            else:
+                if overwrite:
+                    item.set_object(loc_and_var)
+                else:
+                    item.add_object(loc_and_var)
+
+        self._storage.update(to_update)
+
+    def _is_overlapping(self, start, variable):
 
         if variable.size is not None:
             # make sure this variable does not overlap with any other variable
-            end = offset + variable.size
+            end = start + variable.size
             try:
-                prev_offset = self._tree.floor_key(end - 1)
+                prev_offset = self._storage.floor_key(end - 1)
             except KeyError:
                 prev_offset = None
 
             if prev_offset is not None:
-                if offset <= prev_offset < end:
+                if start <= prev_offset < end:
                     return True
-                prev_item = self._tree[prev_offset][0]
+                prev_item = self._storage[prev_offset][0]
                 prev_item_size = prev_item.size if prev_item.size is not None else 1
-                if offset < prev_offset + prev_item_size < end:
+                if start < prev_offset + prev_item_size < end:
                     return True
         else:
             try:
-                prev_offset = self._tree.floor_key(offset)
+                prev_offset = self._storage.floor_key(start)
             except KeyError:
                 prev_offset = None
 
             if prev_offset is not None:
-                prev_item = self._tree[prev_offset][0]
+                prev_item = self._storage[prev_offset][0]
                 prev_item_size = prev_item.size if prev_item.size is not None else 1
-                if prev_offset <= offset < prev_offset + prev_item_size:
+                if prev_offset <= start < prev_offset + prev_item_size:
                     return True
 
         return False
