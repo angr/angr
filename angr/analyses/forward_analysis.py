4961b8b4925e60e9271f8198a048b74fef2d9a88
Fish <fish@cs.ucsb.edu>
2017-04-25 20:09:30 -0700

    Add a visitor for call graphs.

--- angr/analyses/forward_analysis.py
+++ angr/analyses/forward_analysis.py
@@ -199,6 +199,49 @@ class FunctionGraphVisitor(GraphVisitor):
         return sorted_nodes
 
 
+class CallGraphVisitor(GraphVisitor):
+    def __init__(self, callgraph):
+        """
+
+        :param networkx.DiGraph callgraph:
+        """
+
+        super(CallGraphVisitor, self).__init__()
+
+        self.callgraph = callgraph
+
+        self.reset()
+
+    def startpoints(self):
+
+        # TODO: make sure all connected components are covered
+
+        start_nodes = [node for node in self.callgraph.nodes() if self.callgraph.in_degree(node) == 0]
+
+        if not start_nodes:
+            # randomly pick one
+            start_nodes = [ self.callgraph.nodes()[0] ]
+
+        return start_nodes
+
+    def successors(self, node):
+
+        return self.callgraph.successors(node)
+
+    def predecessors(self, node):
+
+        return self.callgraph.predecessors(node)
+
+    def sort_nodes(self, nodes=None):
+
+        sorted_nodes = CFGUtils.quasi_topological_sort_nodes(self.callgraph)
+
+        if nodes is not None:
+            sorted_nodes = [ n for n in sorted_nodes if n in set(nodes) ]
+
+        return sorted_nodes
+
+
 #
 # Job info
 #
