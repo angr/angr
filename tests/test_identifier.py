44288dfe15b0e43484d9cfe95e94b5c12470fcca
Akash Trehan <akash.trehan123@gmail.com>
2017-06-15 01:42:44 -0700

    Update tests for identifier

--- tests/test_identifier.py
+++ tests/test_identifier.py
@@ -1,6 +1,5 @@
 import angr
 import nose
-import identifier
 
 import os
 bin_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../binaries'))
@@ -16,7 +15,7 @@ def test_comparison_identification():
     true_symbols = {0x804a3d0: 'strncmp', 0x804a0f0: 'strcmp', 0x8048e60: 'memcmp', 0x8049f40: 'strcasecmp'}
 
     p = angr.Project(os.path.join(bin_location, "tests/i386/identifiable"))
-    idfer = identifier.Identifier(p, require_predecessors=False)
+    idfer = p.analyses.Identifier(require_predecessors=False)
 
     seen = dict()
     for addr, symbol in idfer.run():
@@ -38,3 +37,4 @@ if __name__ == "__main__":
         globals()['test_' + sys.argv[1]]()
     else:
         run_all()
+
