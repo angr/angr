61acbb6162d70bfd05f00637dd812232227f7880
Andrew Dutcher <andrew@andrewdutcher.com>
2017-07-02 01:37:23 -0700

    EVEN HARDER

--- angr/analyses/cfg/cfg_fast.py
+++ angr/analyses/cfg/cfg_fast.py
@@ -1790,7 +1790,7 @@ class CFGFast(ForwardAnalysis, CFGBase):    # pylint: disable=abstract-method
             # Fix the target_addr for syscalls
             tmp_state = self.project.factory.blank_state(mode="fastpath", addr=cfg_node.addr)
             succ = self.project.factory.successors(tmp_state).flat_successors[0]
-            _, syscall_addr, _, _ = self.project._simos.syscall_info(state)
+            _, syscall_addr, _, _ = self.project._simos.syscall_info(succ)
             target_addr = syscall_addr
 
         new_function_addr = target_addr
