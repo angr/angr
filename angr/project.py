0c8b9ffa8e19627fc5984fb6787782742debfae0
Andrew Dutcher <andrewrdutcher@gmail.com>
2017-07-07 18:16:30 -0700

    I said I was gonna fuck it up and I fucked it up

--- angr/project.py
+++ angr/project.py
@@ -276,10 +276,11 @@ class Project(object):
 
                 elif self._should_use_sim_procedures:
                     for lib in libs:
+                        # TODO: hey uh what the HECK is this logic
                         simfuncs = SIM_PROCEDURES[lib]
                         if func.name in simfuncs:
-                            l.info("Providing %s from %s with SimProcedure", func.name, lib)
-                            pending_hooks[func.name] = Hook(simfuncs[func.name])
+                            l.info("Providing %s for %s with SimProcedure", func.name, func.owner_object.provides)
+                            pending_hooks[func.name] = simfuncs[func.name]()
                             already_resolved.add(func.name)
                             break
                     else: # we could not find a simprocedure for this function
@@ -302,11 +303,11 @@ class Project(object):
             # Don't touch weakly bound symbols, they are allowed to go unresolved
             if func.is_weak:
                 continue
-            l.info("[U] %s", func.name)
+            l.info("Providing %s for %s with default stub", func.name, func.owner_object.provides)
             procedure = SIM_PROCEDURES['stubs']['NoReturnUnconstrained']
             if func.name not in procedure.use_cases:
                 procedure = SIM_PROCEDURES['stubs']['ReturnUnconstrained']
-            pending_hooks[func.name] = Hook(procedure, resolves=func.name)
+            pending_hooks[func.name] = procedure(resolves=func.name)
 
         self.hook_symbol_batch(pending_hooks)
 
@@ -351,35 +352,24 @@ class Project(object):
         """
 
         if hook is None:
+            # if we haven't been passed a thing to hook with, assume we're being used as a decorator
             return self._hook_decorator(addr, length=length, kwargs=kwargs)
 
         l.debug('hooking %#x with %s', addr, hook)
 
         if self.is_hooked(addr):
-            l.warning("Address is already hooked [hook(%#x, %s)]", addr, hook)
+            l.warning("Address is already hooked [hook(%#x, %s)]. Not re-hooking.", addr, hook)
             return
 
-        if not isinstance(hook, Hook):
-            if kwargs is None: kwargs = {}
+        if isinstance(hook, type):
+            if once("hook_instance_warning"):
+                l.critical("Hooking with a SimProcedure instance is deprecated! Please hook with an instance.")
+            hook = hook(**kwargs)
 
-            if isinstance(hook, type) and issubclass(hook, SimProcedure):
-                hook = Hook(hook, **kwargs)
-            elif callable(hook):
-                hook = Hook(procedures.stubs.UserHook.UserHook,
-                            user_func=hook,
-                            length=length)
-            else:
-                raise AngrError("%s is not a valid object to execute in a hook", hook)
+        if callable(hook):
+            hook = procedures.stubs.UserHook.UserHook(user_func=hook, length=length, **kwargs)
 
         self._sim_procedures[addr] = hook
-
-        # set up a continuation if necessary
-        cont = hook.make_continuation()
-        if cont is not None:
-            cont_addr = self._extern_obj.anon_allocation()
-            self.hook(cont_addr, cont)
-            hook.set_continuation_addr(cont_addr)
-            cont.set_continuation_addr(cont_addr)
 
     def is_hooked(self, addr):
         """
@@ -389,52 +379,6 @@ class Project(object):
         :returns:    True if addr is hooked, False otherwise.
         """
         return addr in self._sim_procedures
-
-    def is_symbol_hooked(self, symbol_name):
-        """
-        Check if a symbol is already hooked.
-
-        :param str symbol_name: Name of the symbol.
-        :return: True if the symbol can be resolved and is hooked, False otherwise.
-        :rtype: bool
-        """
-
-        ident = self._symbol_name_to_ident(symbol_name)
-
-        # TODO: this method does not follow the SimOS.prepare_function_symbol() path. We should fix it later.
-
-        if not self._extern_obj.contains_identifier(ident):
-            return False
-
-        return True
-
-    def hooked_symbol_addr(self, symbol_name):
-        """
-        Check if a symbol is hooked or not, and if it is hooked, return the address of the symbol.
-
-        :param str symbol_name: Name of the symbol.
-        :return: Address of the symbol if it is hooked, None otherwise.
-        :rtype: int or None
-        """
-
-        if not self.is_symbol_hooked(symbol_name):
-            return None
-
-        ident = self._symbol_name_to_ident(symbol_name)
-
-        return self._extern_obj.get_pseudo_addr_for_symbol(ident)
-
-    def unhook(self, addr):
-        """
-        Remove a hook.
-
-        :param addr:    The address of the hook.
-        """
-        if not self.is_hooked(addr):
-            l.warning("Address %#x not hooked", addr)
-            return
-
-        del self._sim_procedures[addr]
 
     def hooked_by(self, addr):
         """
@@ -451,6 +395,18 @@ class Project(object):
 
         return self._sim_procedures[addr]
 
+    def unhook(self, addr):
+        """
+        Remove a hook.
+
+        :param addr:    The address of the hook.
+        """
+        if not self.is_hooked(addr):
+            l.warning("Address %#x not hooked", addr)
+            return
+
+        del self._sim_procedures[addr]
+
     def hook_symbol(self, symbol_name, obj, kwargs=None):
         """
         Resolve a dependency in a binary. Uses the "externs object" (project._extern_obj) to
@@ -460,55 +416,84 @@ class Project(object):
         :param symbol_name: The name of the dependency to resolve.
         :param obj:         The thing with which to satisfy the dependency. May be a python integer
                             or anything that may be passed to `project.hook()`.
-        :param length:      If you provide a function for the hook, this is the number of bytes
-                            that will be skipped by executing the hook by default.
         :param kwargs:      If you provide a SimProcedure for the hook, these are the keyword
                             arguments that will be passed to the procedure's `run` method
                             eventually.
         :returns:           The address of the new symbol.
         :rtype:             int
         """
-        if kwargs is None: kwargs = {}
-        ident = self._symbol_name_to_ident(symbol_name, kwargs)
+        if type(obj) in (int, long):
+            # this is pretty intensely sketchy
+            l.info("Instructing the loader to re-point symbol %s at address %#x", symbol_name, obj)
+            self.loader.provide_symbol(self._extern_obj, symbol_name, AT.from_mva(obj, self._extern_obj))
+            return obj
 
-        if not isinstance(obj, (int, long)):
-            pseudo_addr = self._simos.prepare_function_symbol(ident)
+        sym = self.loader.find_symbol(symbol_name)
 
-            if self.is_hooked(pseudo_addr):
-                l.warning("Re-hooking symbol " + symbol_name)
-                self.unhook(pseudo_addr)
-
-            self.hook(pseudo_addr, obj, kwargs=kwargs)
+        if sym is None:
+            addr = self._simos.prepare_function_symbol(symbol_name)
+            l.info("Providing extern symbol for unresolved %s at #%x", symbol_name, addr)
+            self.loader.provide_symbol(self._extern_obj, symbol_name, AT.from_MVA(addr, self._extern_obj))
         else:
-            # This is pretty intensely sketchy
-            pseudo_addr = obj
+            addr = sym.rebased_addr
 
-        self.loader.provide_symbol(self._extern_obj, symbol_name, AT.from_mva(pseudo_addr, self._extern_obj).to_lva())
+            if self.is_hooked(addr):
+                l.warning("Re-hooking symbol %s", symbol_name)
+                self.unhook(addr)
 
-        return pseudo_addr
+        self.hook(addr, obj, kwargs=kwargs)
+        return addr
 
     def hook_symbol_batch(self, hooks):
         """
         Hook many symbols at once.
 
-        :param dict provisions:     A mapping from symbol name to hook
+        :param dict hooks:     A mapping from symbol name to hook
         """
 
         provisions = {}
 
         for name, obj in hooks.iteritems():
-            ident = self._symbol_name_to_ident(name, None)
+            sym = self.loader.find_symbol(name)
+            if sym is None:
+                pseudo_addr = self._simos.prepare_function_symbol(name)
+                pseudo_vaddr = pseudo_addr - self._extern_obj.rebase_addr
+                l.info("Providing extern symbol for unresolved %s at #%x", name, pseudo_addr)
+                self.hook(pseudo_addr, obj)
+                provisions[name] = (AT.from_mva(pseudo_addr, self._extern_obj), 0, None)
+            else:
+                addr = sym.rebased_addr
+                if self.is_hooked(addr):
+                    l.warning("Re-hooking symbol %s", name)
+                    self.unhook(addr)
+                self.hook(addr, obj)
 
-            pseudo_addr = self._simos.prepare_function_symbol(ident)
+        if provisions:
+            self.loader.provide_symbol_batch(self._extern_obj, provisions)
 
-            if self.is_hooked(pseudo_addr):
-                l.warning("Re-hooking symbol " + name)
-                self.unhook(pseudo_addr)
+    def is_symbol_hooked(self, symbol_name):
+        """
+        Check if a symbol is already hooked.
 
-            self.hook(pseudo_addr, obj)
-            provisions[name] = (AT.from_mva(pseudo_addr, self._extern_obj).to_lva(), 0, None)
+        :param str symbol_name: Name of the symbol.
+        :return: True if the symbol can be resolved and is hooked, False otherwise.
+        :rtype: bool
+        """
+        # TODO: this method does not follow the SimOS.prepare_function_symbol() path. We should fix it later.
+        sym = self.loader.find_symbol(symbol_name)
+        return self.is_hooked(sym.rebased_addr)
 
-        self.loader.provide_symbol_batch(self._extern_obj, provisions)
+
+    def hooked_symbol_addr(self, symbol_name):
+        """
+        Check if a symbol is hooked or not, and if it is hooked, return the address of the symbol.
+
+        :param str symbol_name: Name of the symbol.
+        :return: Address of the symbol if it is hooked, None otherwise.
+        :rtype: int or None
+        """
+        sym = self.loader.find_symbol(symbol_name)
+        return self.unhook(sym.rebased_addr)
 
     #
     # A convenience API (in the style of triton and manticore) for symbolic execution.
@@ -565,22 +550,6 @@ class Project(object):
 
         return hook_decorator
 
-    @staticmethod
-    def _symbol_name_to_ident(symbol_name, kwargs=None):
-        """
-        Convert a symbol name to an identifier that are used by hooking.
-
-        :param str symbol_name: Name of the symbol.
-        :param dict kwargs: Any additional keyword arguments.
-        :return: An identifier.
-        :rtype: str
-        """
-        ident = 'symbol hook: ' + symbol_name
-        if kwargs and 'resolves' in kwargs:
-            ident += '.' + kwargs['resolves']
-
-        return ident
-
     #
     # Pickling
     #
@@ -598,108 +567,6 @@ class Project(object):
         self.analyses = Analyses(self)
         self.surveyors = Surveyors(self)
 
-
-class Hook(object):
-    """
-    An object describing an action to be taken at a given address instead of executing binary code.
-    An instance of this class may be passed to `angr.Project.hook` along with the address at which
-    to hook.
-
-    More specifically, a hook is a wrapper for a SimProcedure, an object that contains a lot
-    of logic for how to mutate a state in common ways. The SimProcedure base class is subclassed
-    to produce a SimProcedure that may be used for hooking. If the SimProcedure class is too heavy
-    for your use case, there is a class method `wrap` on this class that can be used to wrap a
-    simple function into a SimProcedure, and then further into a `Hook` directly.
-
-    This class is a bit of a hack to deal with the fact that SimProcedures need to hold state but
-    having them do so makes them not thread-safe.
-    """
-    def __init__(self, procedure, cc=None, **kwargs):
-        """
-        :param procedure:   The class of the procedure to use
-        :param cc:          The calling convention to use
-        :param kwargs:      Any additional keyword arguments will be eventually passed to the
-                            `run` method of the procedure on its execution, via the `sim_kwargs`
-                            SimProcedure constructor parameter.
-        """
-        self.procedure = procedure
-        self.cc = cc
-        self.kwargs = kwargs
-        self.is_continuation = False
-        self._continuation_addr = None
-
-    def instantiate(self, *args, **kwargs):
-        kwargs['sim_kwargs'] = self.kwargs
-        kwargs['is_continuation'] = self.is_continuation
-        kwargs['continuation_addr'] = self._continuation_addr
-        if self.cc: kwargs['cc'] = self.cc
-        return self.procedure(*args, **kwargs)
-    instantiate.__doc__ = SimProcedure.__init__.__doc__
-
-    def __repr__(self):
-        try:
-            resolves_str = ' (resolves %s)' % self.kwargs['resolves']
-        except KeyError:
-            resolves_str = ''
-
-        if len(self.kwargs) == 0:
-            args_str = ''
-        elif len(self.kwargs) == 1:
-            args_str = ' (1 arg)'
-        else:
-            args_str = ' (%d args)' % len(self.kwargs)
-
-        cont_str = ' (continuation)' if self.is_continuation else ''
-
-        return '<Hook for %s%s%s%s>' % (self.name, resolves_str, args_str, cont_str)
-
-    def make_continuation(self):
-        if self.is_continuation or not self.procedure.IS_FUNCTION or self._continuation_addr is not None:
-            return None
-        hook = Hook(self.procedure, **self.kwargs)
-        hook.is_continuation = True
-        return hook
-
-    def set_continuation_addr(self, addr):
-        self._continuation_addr = addr
-
-    @property
-    def name(self):
-        return self.procedure.__name__
-
-    @classmethod
-    def wrap(cls, length=0):
-        """
-        Call this function to return a decorator you can use to turn a function into a Hook. This
-        specific kind of hook is called a "User Hook".
-
-        For example:
-
-        >>> @Hook.wrap(length=5)
-        >>> def set_rax(state):
-        >>>     state.regs.rax = 1
-        >>>
-        >>> project.hook(0x1234, set_rax)
-
-        The function may modify the state in any way it sees fit. In order to produce successors
-        from a user hook, you have two options.
-
-            1. Return nothing. In this case, execution will resume at `length` bytes after the hook
-               address.
-            2. Return a list of successor states. Each of the states should have the following
-               attributes set:
-
-                - `state.history.last_guard`: a symbolic boolean describing the condition necessary for
-                  this successor to be taken. A shortcut to the symbolic `True` value is
-                  `state.se.true`.
-                - `state.history.jumpkind`: The type of the jump to be taken, as a VEX enum string.
-                  This will usually be `Ijk_Boring`, which signifies an ordinary jump or branch.
-        """
-        def inner(function):
-            return Hook(procedures.stubs.UserHook.UserHook,
-                        user_func=function,
-                        length=length)
-        return inner
 
 from .errors import AngrError
 from .factory import AngrObjectFactory
