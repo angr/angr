329be2f3c5a602e5045c604b572d0f6d45fb56ec
Fish <fish@cs.ucsb.edu>
2017-07-17 10:56:15 -0700

    SimDeclarations.

--- angr/declarations/libc.py
+++ angr/declarations/libc.py
@@ -1,6 +1,9 @@
 
 from .sim_declarations import SimDeclarations
 
-libc = SimDeclarations('libc')
+libc = SimDeclarations(
+    'libc',
+    alternative_names=['libc.so.6'],
+)
 
 libc.add_c_decl('int strcmp (const char * str1, const char * str2);')
