4e95ee9d8853f07fad8ce9279f8466c1a8ad7c53
Andrew Dutcher <andrewrdutcher@gmail.com>
2017-07-18 14:08:33 -0700

    Upgrades to statichooker

--- angr/analyses/static_hooker.py
+++ angr/analyses/static_hooker.py
@@ -11,23 +11,30 @@ l = logging.getLogger("angr.analyses.static_hooker")
 class StaticHooker(Analysis):
     """
     This analysis works on statically linked binaries - it finds the library functions statically
-    linked into the binary and hooks them with the appropraite simprocedures.
+    linked into the binary and hooks them with the appropriate simprocedures.
 
-    Right now it only works on libc functions and unstripped binaries, but hey! There's room to
-    grow!
+    Right now it only works on unstripped binaries, but hey! There's room to grow!
     """
 
-    def __init__(self):
+    def __init__(self, library, binary=None):
         self.results = {}
-        libc = SIM_LIBRARIES['libc.so.6']
+        try:
+            lib = SIM_LIBRARIES[library]
+        except KeyError:
+            raise AngrValueError("No such library %s" % library)
 
-        if self.project.loader.main_bin.linking == 'dynamic':
-            raise AngrValueError('StaticHooker only works on static binaries!')
+        if binary is None:
+            binary = self.project.loader.main_bin
 
-        for func in self.project.loader.main_bin._symbol_cache.values():
-            if not func.is_function: continue
-            if libc.has_implementation(func.name):
-                proc = libc.get(func.name, self.project.arch)
+        for func in binary._symbol_cache.values():
+            if not func.is_function:
+                continue
+
+            if self.project.is_hooked(func.rebased_addr):
+                l.debug("Skipping %s at %#x, already hooked", func.name, func.rebased_addr)
+
+            if lib.has_implementation(func.name):
+                proc = lib.get(func.name, self.project.arch)
                 self.project.hook(func.rebased_addr, proc)
                 l.info("Hooked %s at %#x", func.name, func.rebased_addr)
                 self.results[func.rebased_addr] = proc
