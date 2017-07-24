29013afa48061b8d9acbb17ccdf9dc8bfe2c1693
Fish <fish@cs.ucsb.edu>
2017-07-23 18:14:23 -0700

    Region Identifier, Structurer, and Clinic.

---
+++ tests/test_regionidentifier.py
@@ -0,0 +1,22 @@
+
+import os
+
+import nose.tools
+
+import angr
+
+
+def test_smoketest():
+
+    p = angr.Project(os.path.join('..', '..', 'binaries', 'tests', 'x86_64', 'all'), auto_load_libs=False)
+    cfg = p.analyses.CFG(normalize=True)
+
+    main_func = cfg.kb.functions['main']
+
+    st = p.analyses.RegionIdentifier(main_func)
+
+    import ipdb; ipdb.set_trace()
+
+
+if __name__ == "__main__":
+    test_smoketest()
