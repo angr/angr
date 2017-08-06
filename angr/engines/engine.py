d8363f3e82e92d3d5c0bc740de8f92ca5cfafc10
Andrew Dutcher <andrew@andrewdutcher.com>
2017-08-05 17:14:48 -0700

    Roll back the attempt at fixing track_action_history

--- angr/engines/engine.py
+++ angr/engines/engine.py
@@ -55,8 +55,8 @@ class SimEngine(object):
         # downsizing
         new_state.inspect.downsize()
         # if not TRACK, clear actions on OLD state
-        if o.TRACK_ACTION_HISTORY not in old_state.options:
-            old_state.history.recent_events = []
+        #if o.TRACK_ACTION_HISTORY not in old_state.options:
+        #    old_state.history.recent_events = []
 
 
         return successors
