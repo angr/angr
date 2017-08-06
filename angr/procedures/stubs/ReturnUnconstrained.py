3e78e73f35faa9c2ea413c81c74bdc7fbc0145c1
Lukas Dresel <lukas.dresel@cs.ucsb.edu>
2017-08-06 01:32:29 -0700

    Fixed the variables created by the ReturnUnconstrained simprocedure

--- angr/procedures/stubs/ReturnUnconstrained.py
+++ angr/procedures/stubs/ReturnUnconstrained.py
@@ -5,14 +5,13 @@ import angr
 ######################################
 
 class ReturnUnconstrained(angr.SimProcedure):
-    def run(self, resolves=None): #pylint:disable=arguments-differ
+    def run(self): #pylint:disable=arguments-differ
         #pylint:disable=attribute-defined-outside-init
-        self.resolves = resolves
 
         if self.successors is not None:
             self.successors.artifacts['resolves'] = resolves
 
-        o = self.state.se.Unconstrained("unconstrained_ret_%s" % self.resolves, self.state.arch.bits)
+        o = self.state.se.Unconstrained("unconstrained_ret_%s" % self.display_name, self.state.arch.bits)
         #if 'unconstrained_ret_9_64' in o.variables:
         #   __import__('ipdb').set_trace()
         return o
