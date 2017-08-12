5caf6362102c84a486971fcfa9b02ccc88bd74e2
Yan <yans@yancomm.net>
2017-08-12 10:43:51 -0700

    change path references to state references in spiller

--- angr/exploration_techniques/spiller.py
+++ angr/exploration_techniques/spiller.py
@@ -1,24 +1,25 @@
 import logging
 
 l = logging.getLogger("angr.exploration_techniques.spiller")
+l.setLevel('DEBUG')
 
 import ana
 from . import ExplorationTechnique
 
-class SpilledPath(ana.Storable):
-    def __init__(self, path):
-        self.path = path
+class SpilledState(ana.Storable):
+    def __init__(self, state):
+        self.state = state
 
     def _ana_getstate(self):
-        return (self.path,)
+        return (self.state,)
 
     def _ana_setstate(self, s):
-        self.path = s[0]
+        self.state = s[0]
 
 class Spiller(ExplorationTechnique):
     """
-    Automatically spill paths out. It can spill out paths to a different stash, spill
-    them out to ANA, or first do the former and then (after enough paths) the latter.
+    Automatically spill states out. It can spill out states to a different stash, spill
+    them out to ANA, or first do the former and then (after enough states) the latter.
     """
 
     def __init__(
@@ -30,11 +31,11 @@ class Spiller(ExplorationTechnique):
         """
         Initializes the spiller.
 
-        @param max: the number of paths that are *not* spilled
-        @param src_stash: the stash from which to spill paths (default: active)
-        @param staging_stash: the stash *to* which to spill paths (default: "spill_stage")
-        @param staging_max: the number of paths that can be in the staging stash before things get spilled to ANA (default: None. If staging_stash is set, then this means unlimited, and ANA will not be used).
-        @param priority_key: a function that takes a path and returns its numberical priority (MAX_INT is lowest priority). By default, self.path_priority will be used, which prioritizes by object ID.
+        @param max: the number of states that are *not* spilled
+        @param src_stash: the stash from which to spill states (default: active)
+        @param staging_stash: the stash *to* which to spill states (default: "spill_stage")
+        @param staging_max: the number of states that can be in the staging stash before things get spilled to ANA (default: None. If staging_stash is set, then this means unlimited, and ANA will not be used).
+        @param priority_key: a function that takes a state and returns its numberical priority (MAX_INT is lowest priority). By default, self.state_priority will be used, which prioritizes by object ID.
         """
         super(Spiller, self).__init__()
         self.max = max
@@ -50,69 +51,71 @@ class Spiller(ExplorationTechnique):
         self.pickle_callback = pickle_callback
 
         # tracking of pickled stuff
-        self._pickled_paths = [ ]
+        self._pickled_states = [ ]
         self._ever_pickled = 0
         self._ever_unpickled = 0
 
     def _unpickle(self, n):
-        self._pickled_paths.sort()
-        unpickled = [ SpilledPath.ana_load(pid).path for _,pid in self._pickled_paths[:n] ]
-        self._pickled_paths[:n] = [ ]
+        self._pickled_states.sort()
+        unpickled = [ SpilledState.ana_load(pid).state for _,pid in self._pickled_states[:n] ]
+        self._pickled_states[:n] = [ ]
         self._ever_unpickled += len(unpickled)
         if self.unpickle_callback:
             map(self.unpickle_callback, unpickled)
         return unpickled
 
-    def _get_priority(self, path):
-        return (self.priority_key or self.path_priority)(path)
+    def _get_priority(self, state):
+        return (self.priority_key or self.state_priority)(state)
 
-    def _pickle(self, paths):
+    def _pickle(self, states):
         if self.pickle_callback:
-            map(self.pickle_callback, paths)
-        wrappers = [ SpilledPath(path) for path in paths ]
-        self._ever_pickled += len(paths)
+            map(self.pickle_callback, states)
+        wrappers = [ SpilledState(state) for state in states ]
+        self._ever_pickled += len(states)
         for w in wrappers:
             w.make_uuid()
-        self._pickled_paths += [ (self._get_priority(w.path), w.ana_store()) for w in wrappers ]
+        self._pickled_states += [ (self._get_priority(w.state), w.ana_store()) for w in wrappers ]
 
     def step(self, pg, stash, **kwargs):
         pg = pg.step(stash=stash, **kwargs)
 
-        paths = pg.stashes[self.src_stash]
-        staged_paths = pg.stashes.setdefault(self.staging_stash, [ ]) if self.staging_stash else [ ]
+        l.debug("STASH STATUS: active: %d, staging: %d", len(pg.stashes[self.src_stash]), len(pg.stashes[self.staging_stash]))
 
-        if len(paths) < self.min:
-            missing = (self.max + self.min) / 2 - len(paths)
-            l.debug("Too few paths in stash %s.", self.src_stash)
+        states = pg.stashes[self.src_stash]
+        staged_states = pg.stashes.setdefault(self.staging_stash, [ ]) if self.staging_stash else [ ]
+
+        if len(states) < self.min:
+            missing = (self.max + self.min) / 2 - len(states)
+            l.debug("Too few states (%d/%d) in stash %s.", len(states), self.min, self.src_stash)
             if self.staging_stash:
-                l.debug("... retrieving paths from staging stash (%s)", self.staging_stash)
-                staged_paths.sort(key=self.priority_key or self.path_priority)
-                paths += staged_paths[:missing]
-                staged_paths[:missing] = [ ]
+                l.debug("... retrieving states from staging stash (%s)", self.staging_stash)
+                staged_states.sort(key=self.priority_key or self.state_priority)
+                states += staged_states[:missing]
+                staged_states[:missing] = [ ]
             else:
-                l.debug("... staging stash disabled; unpickling paths")
-                paths += self._unpickle(missing)
+                l.debug("... staging stash disabled; unpickling states")
+                states += self._unpickle(missing)
 
-        if len(paths) > self.max:
-            l.debug("Too many paths in stash %s", self.src_stash)
-            paths.sort(key=self.priority_key or self.path_priority)
-            staged_paths += paths[self.max:]
-            paths[self.max:] = [ ]
+        if len(states) > self.max:
+            l.debug("Too many states (%d/%d) in stash %s", len(states), self.max, self.src_stash)
+            states.sort(key=self.priority_key or self.state_priority)
+            staged_states += states[self.max:]
+            states[self.max:] = [ ]
 
-        # if we have too few staged paths, unpickle up to halfway between max and min
-        if len(staged_paths) < self.staging_min:
-            l.debug("Too few paths in staging stash (%s)", self.staging_stash)
-            staged_paths += self._unpickle((self.staging_min + self.staging_max) / 2 - len(staged_paths))
+        # if we have too few staged states, unpickle up to halfway between max and min
+        if len(staged_states) < self.staging_min:
+            l.debug("Too few states in staging stash (%s)", self.staging_stash)
+            staged_states += self._unpickle((self.staging_min + self.staging_max) / 2 - len(staged_states))
 
-        if len(staged_paths) > self.staging_max:
-            l.debug("Too many paths in staging stash (%s)", self.staging_stash)
-            self._pickle(staged_paths[self.staging_max:])
-            staged_paths[self.staging_max:] = [ ]
+        if len(staged_states) > self.staging_max:
+            l.debug("Too many states in staging stash (%s)", self.staging_stash)
+            self._pickle(staged_states[self.staging_max:])
+            staged_states[self.staging_max:] = [ ]
 
-        pg.stashes[self.src_stash] = paths
-        pg.stashes[self.staging_stash] = staged_paths
+        pg.stashes[self.src_stash] = states
+        pg.stashes[self.staging_stash] = staged_states
         return pg
 
     @staticmethod
-    def path_priority(path):
-        return id(path)
+    def state_priority(state):
+        return id(state)
