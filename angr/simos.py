290109687732199c30cb915813f0e0c1c29b83ff
Andrew Dutcher <andrewrdutcher@gmail.com>
2017-08-11 17:47:12 -0700

    Refactor syscall support into a SimUserland SimOS

--- angr/simos.py
+++ angr/simos.py
@@ -52,66 +52,7 @@ class SimOS(object):
         self.arch = project.arch
         self.project = project
         self.name = name
-        self.continue_addr = None
         self.return_deadend = None
-        self.syscall_library = None
-        self.kernel_base = None
-
-    def syscall(self, state, allow_unsupported=True):
-        """
-        Given a state, return the procedure corresponding to the current syscall.
-        This procedure will have .syscall_number, .display_name, and .addr set.
-
-        :param state:               The state to get the syscall number from
-        :param allow_unsupported    Whether to return a "dummy" sycall instead of raising an unsupported exception
-        """
-        if state.os_name in SYSCALL_CC[state.arch.name]:
-            cc = SYSCALL_CC[state.arch.name][state.os_name](state.arch)
-        else:
-            # Use the default syscall calling convention - it may bring problems
-            cc = SYSCALL_CC[state.arch.name]['default'](state.arch)
-
-        sym_num = cc.syscall_num(state)
-        possible = state.se.any_n_int(sym_num, 2)
-
-        if len(possible) == 0:
-            raise AngrUnsupportedSyscallError("The program state is not satisfiable")
-        elif len(possible) == 1:
-            num = possible[0]
-        elif allow_unsupported:
-            num = self.syscall_library.maximum_syscall_number(self.arch.name) + 1 if self.syscall_library else 0
-        else:
-            raise AngrUnsupportedSyscallError("Got a symbolic syscall number")
-
-        proc = self.syscall_from_number(num, allow_unsupported=allow_unsupported)
-        proc.cc = cc
-        return proc
-
-    def is_syscall_addr(self, addr):
-        if self.kernel_base is None:
-            return False
-        addr -= self.kernel_base
-        return 0 <= addr < 0x4000 # TODO: make this number come from somewhere
-
-    def syscall_from_addr(self, addr, allow_unsupported=True):
-        number = addr - self.kernel_base
-        return self.syscall_from_number(number, allow_unsupported=allow_unsupported)
-
-    def syscall_from_number(self, number, allow_unsupported=True):
-        if not allow_unsupported and not self.syscall_library:
-            raise AngrUnsupportedSyscallError("%s does not have a library of syscalls implemented", self.name)
-
-        addr = number + self.kernel_base
-
-        if self.syscall_library is None:
-            proc = P['stubs']['syscall']()
-        elif not allow_unsupported and not self.syscall_library.has_implementation(number, self.arch):
-            raise AngrUnsupportedSyscallError("No implementation for syscall %d" % number)
-        else:
-            proc = self.syscall_library.get(number, self.arch)
-
-        proc.addr = addr
-        return proc
 
     def configure_project(self):
         """
@@ -134,7 +75,6 @@ class SimOS(object):
             except AngrCallableError:
                 l.error("Resolver at %#x failed to resolve!", resolver_addr)
                 return None
-
 
             return val._model_concrete.value
 
@@ -199,7 +139,7 @@ class SimOS(object):
             highest_reg_offset, reg_size = max(state.arch.registers.values())
             for i in range(0, highest_reg_offset + reg_size, state.arch.bytes):
                 state.registers.store(i, state.se.BVV(0, state.arch.bits))
-        if hasattr(state.regs,"sp"):
+        if state.arch.sp_offset is not None:
             state.regs.sp = stack_end
 
         if initial_prefix is not None:
@@ -307,20 +247,106 @@ class SimOS(object):
         return basic_addr, basic_addr
 
 
-class SimLinux(SimOS):
+class SimUserland(SimOS):
+    """
+    This is a base class for any SimOS that wants to support syscalls.
+
+    It uses the CLE kernel object to provide addresses for syscalls. Syscalls will be emulated as a jump to one of these
+    addresses, where a SimProcedure from the syscall library provided at construction time will be executed.
+    """
+    def __init__(self, project, syscall_library=None, **kwargs):
+        super(SimUserland, self).__init__(project, **kwargs)
+        self.syscall_library = syscall_library
+        self.kernel_base = None
+
+    def configure_project(self):
+        super(SimUserland, self).configure_project()
+        self.kernel_base = self.project.loader.kernel_object.mapped_base
+
+    def syscall(self, state, allow_unsupported=True):
+        """
+        Given a state, return the procedure corresponding to the current syscall.
+        This procedure will have .syscall_number, .display_name, and .addr set.
+
+        :param state:               The state to get the syscall number from
+        :param allow_unsupported    Whether to return a "dummy" sycall instead of raising an unsupported exception
+        """
+        if state.os_name in SYSCALL_CC[state.arch.name]:
+            cc = SYSCALL_CC[state.arch.name][state.os_name](state.arch)
+        else:
+            # Use the default syscall calling convention - it may bring problems
+            cc = SYSCALL_CC[state.arch.name]['default'](state.arch)
+
+        sym_num = cc.syscall_num(state)
+        possible = state.se.any_n_int(sym_num, 2)
+
+        if len(possible) == 0:
+            raise AngrUnsupportedSyscallError("The program state is not satisfiable")
+        elif len(possible) == 1:
+            num = possible[0]
+        elif allow_unsupported:
+            num = self.syscall_library.maximum_syscall_number(self.arch.name) + 1 if self.syscall_library else 0
+        else:
+            raise AngrUnsupportedSyscallError("Got a symbolic syscall number")
+
+        proc = self.syscall_from_number(num, allow_unsupported=allow_unsupported)
+        proc.cc = cc
+        return proc
+
+    def is_syscall_addr(self, addr):
+        """
+        Return whether or not the given address corresponds to a syscall.
+        """
+        if self.kernel_base is None:
+            return False
+        addr -= self.kernel_base
+        return 0 <= addr < 0x4000 # TODO: make this number come from somewhere
+
+    def syscall_from_addr(self, addr, allow_unsupported=True):
+        """
+        Get a syscall SimProcedure from an address.
+
+        :param addr: The address to convert to a syscall SimProcedure
+        :param allow_unsupported: Whether to return a dummy procedure for an unsupported syscall instead of raising an
+                                  exception.
+        :return: The SimProcedure for the syscall, or None if the address is not a syscall address.
+        """
+        if not self.is_syscall_addr(addr):
+            return None
+
+        number = addr - self.kernel_base
+        return self.syscall_from_number(number, allow_unsupported=allow_unsupported)
+
+    def syscall_from_number(self, number, allow_unsupported=True):
+        if not allow_unsupported and not self.syscall_library:
+            raise AngrUnsupportedSyscallError("%s does not have a library of syscalls implemented", self.name)
+
+        addr = number + self.kernel_base
+
+        if self.syscall_library is None:
+            proc = P['stubs']['syscall']()
+        elif not allow_unsupported and not self.syscall_library.has_implementation(number, self.arch):
+            raise AngrUnsupportedSyscallError("No implementation for syscall %d" % number)
+        else:
+            proc = self.syscall_library.get(number, self.arch)
+
+        proc.addr = addr
+        return proc
+
+
+class SimLinux(SimUserland):
     """
     OS-specific configuration for \\*nix-y OSes.
     """
 
-    def __init__(self, *args, **kwargs):
-        super(SimLinux, self).__init__(*args, name="Linux", **kwargs)
+    def __init__(self, project, **kwargs):
+        super(SimLinux, self).__init__(project, syscall_library=L['linux'], name="Linux", **kwargs)
 
         self._loader_addr = None
         self._loader_lock_addr = None
         self._loader_unlock_addr = None
+        self._error_catch_tsd_addr = None
         self._vsyscall_addr = None
-        self.syscall_library = L['linux']
-        self.kernel_base = self.project.loader.kernel_object.mapped_base
 
     def configure_project(self):
         super(SimLinux, self).configure_project()
@@ -562,15 +588,12 @@ class SimLinux(SimOS):
             return basic_addr, basic_addr
 
 
-class SimCGC(SimOS):
+class SimCGC(SimUserland):
     """
     Environment configuration for the CGC DECREE platform
     """
-    def __init__(self, *args, **kwargs):
-        super(SimCGC, self).__init__(*args, name="CGC", **kwargs)
-
-        self.syscall_library = L['cgcabi']
-        self.kernel_base = self.project.loader.kernel_object.mapped_base
+    def __init__(self, project, **kwargs):
+        super(SimCGC, self).__init__(project, syscall_library=L['cgcabi'], name="CGC", **kwargs)
 
     def state_blank(self, fs=None, **kwargs):
         s = super(SimCGC, self).state_blank(**kwargs)  # pylint:disable=invalid-name
@@ -688,10 +711,9 @@ class SimCGC(SimOS):
 
 
 class SimWindows(SimOS):
-    def __init__(self, project):
-        super(SimWindows, self).__init__(project)
-        self.kernel_base = self.project.loader.kernel_object.mapped_base
-
+    """
+    Environemnt for the Windows Win32 subsystem. Does not support syscalls currently.
+    """
     def configure_project(self):
         super(SimWindows, self).configure_project()
 
