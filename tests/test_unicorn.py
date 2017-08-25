da203cb3c11e28c4247e8452fc8c1e82384a0315
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2017-08-25 15:16:26 +0200

    unicorn: add tests for breakpoints, explorer and single stepping

--- tests/test_unicorn.py
+++ tests/test_unicorn.py
@@ -231,6 +231,88 @@ def test_concrete_transmits():
 
     nose.tools.assert_equal(pg_unicorn.one_active.posix.dumps(1), '1) Add number to the array\n2) Add random number to the array\n3) Sum numbers\n4) Exit\nRandomness added\n1) Add number to the array\n2) Add random number to the array\n3) Sum numbers\n4) Exit\n  Index: \n1) Add number to the array\n2) Add random number to the array\n3) Sum numbers\n4) Exit\n')
 
+def test_inspect():
+    p = angr.Project(os.path.join(test_location, 'binaries/tests/i386/uc_stop'))
+
+    def main_state(argc, add_options=so.unicorn):
+        main_addr = p.loader.find_symbol("main").rebased_addr
+        return p.factory.call_state(main_addr, argc, [], add_options=add_options)
+
+    # test breaking on specific addresses
+    s_break_addr = main_state(1)
+    addr0 = 0x08048454 # at the beginning of a basic block, at end of stop_normal function
+    addr1 = 0x080484c7 # this is at the beginning of main, in the middle of a basic block
+    addr2 = 0x0804843e # another non-bb address, at the start of stop_normal
+    addr3 = 0x08048457 # address of a block that should not get hit (stop_symbolc function)
+    addr4 = 0x0804850b # another address that shouldn't get hit, near end of main
+    hits = { addr0 : 0, addr1: 0, addr2: 0, addr3: 0, addr4: 0 }
+
+    def create_addr_action(addr):
+        def action(state):
+            hits[addr] += 1
+        return action
+
+    for addr in [addr0, addr1, addr2]:
+        s_break_addr.inspect.b("instruction", instruction=addr, action=create_addr_action(addr))
+
+    pg_instruction = p.factory.simgr(s_break_addr)
+    pg_instruction.run()
+    nose.tools.assert_equal(hits[addr0], 1)
+    nose.tools.assert_equal(hits[addr1], 1)
+    nose.tools.assert_equal(hits[addr2], 1)
+    nose.tools.assert_equal(hits[addr3], 0)
+    nose.tools.assert_equal(hits[addr4], 0)
+
+    # test breaking on every instruction
+    def collect_trace(options):
+        s_break_every = main_state(1, add_options=options)
+        trace = []
+        def action_every(state):
+            trace.append(state.addr)
+        s_break_every.inspect.b("instruction", action=action_every)
+        pg_break_every = p.factory.simgr(s_break_every)
+        pg_break_every.run()
+    nose.tools.assert_equal(collect_trace(so.unicorn), collect_trace(set()))
+
+def test_explore():
+    p = angr.Project(os.path.join(test_location, 'binaries/tests/i386/uc_stop'))
+
+    def main_state(argc, add_options=so.unicorn):
+        main_addr = p.loader.find_symbol("main").rebased_addr
+        return p.factory.call_state(main_addr, argc, [], add_options=add_options)
+
+    addr = 0x08048454
+    s_explore = main_state(1)
+    pg_explore_find = p.factory.simgr(s_explore)
+    pg_explore_find.explore(find=addr)
+    nose.tools.assert_equal(len(pg_explore_find.found), 1)
+    nose.tools.assert_equal(pg_explore_find.found[0].addr, addr)
+
+    pg_explore_avoid = p.factory.simgr(s_explore)
+    pg_explore_avoid.explore(avoid=addr)
+    nose.tools.assert_equal(len(pg_explore_avoid.avoid), 1)
+    nose.tools.assert_equal(pg_explore_avoid.avoid[0].addr, addr)
+
+
+def test_single_step():
+    p = angr.Project(os.path.join(test_location, 'binaries/tests/i386/uc_stop'))
+
+    def main_state(argc, add_options=so.unicorn):
+        main_addr = p.loader.find_symbol("main").rebased_addr
+        return p.factory.call_state(main_addr, argc, [], add_options=add_options)
+
+    s_main = main_state(1)
+
+    step1 = s_main.block().instruction_addrs[1]
+    successors1 = s_main.step(num_inst=1).successors
+    nose.tools.assert_equal(len(successors1), 1)
+    nose.tools.assert_equal(successors1[0].addr, step1)
+
+    step5 = s_main.block().instruction_addrs[5]
+    successors2 = successors1[0].step(num_inst=4).successors
+    nose.tools.assert_equal(len(successors2), 1)
+    nose.tools.assert_equal(successors2[0].addr, step5)
+
 if __name__ == '__main__':
     #import logging
     #logging.getLogger('angr.state_plugins.unicorn_engine').setLevel('DEBUG')
