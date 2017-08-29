d83c88b912b8db0542b862306ca48245118afbc7
SonTuanVu <sontuan.vu119@gmail.com>
2017-08-29 21:30:46 +0200

    Add shortcut to flags access for AARCH64

--- angr/analyses/congruency_check.py
+++ angr/analyses/congruency_check.py
@@ -327,8 +327,8 @@ class CongruencyCheck(Analysis):
                 n_flags = sr.regs.eflags.canonicalize(var_map=n_map, counter=n_counter)[-1]
                 u_flags = sl.regs.eflags.canonicalize(var_map=u_map, counter=u_counter)[-1]
             else:
-                n_flags = engines.vex.ccall._get_flags(sr)[0].canonicalize(var_map=n_map, counter=n_counter)[-1]
-                u_flags = engines.vex.ccall._get_flags(sl)[0].canonicalize(var_map=u_map, counter=u_counter)[-1]
+                n_flags = sr.regs.flags.canonicalize(var_map=n_map, counter=n_counter)[-1]
+                u_flags = sl.regs.flags.canonicalize(var_map=u_map, counter=u_counter)[-1]
             if n_flags is not u_flags and sl.se.simplify(n_flags) is not sr.se.simplify(u_flags):
                 self._report_incongruency("Different flags!")
                 return False
