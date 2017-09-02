1c616f45eabf63298e3f6ebfde7b946f867db6ae
schieb <schieb@users.noreply.github.com>
2017-09-02 16:28:13 -0700

    Favor simprocedures defined in msvcr over libc

--- angr/procedures/definitions/msvcr.py
+++ angr/procedures/definitions/msvcr.py
@@ -4,9 +4,8 @@ from .. import SIM_PROCEDURES as P
 
 libc = SimLibrary()
 libc.set_library_names('msvcr100.dll', 'msvcr110.dll')
-libc.add_all_from_dict(P['msvcr'])
+libc.add_all_from_dict(P['libc'])
+libc.add_all_from_dict(P['msvcr']) # overwrite any that are also defined in libc
 libc.set_non_returning('_exit', 'abort', 'exit', '_invoke_watson')
 
 libc.add_alias('_initterm', '_initterm_e')
-
-libc.add_all_from_dict(P['libc'])
