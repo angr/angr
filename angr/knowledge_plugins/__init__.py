02d795106a749da32050893a1672fd0c65854f90
Danse Macabre <danse616@gmail.com>
2017-09-08 20:11:04 +0300

    TransitionsPlugin: first blood

--- angr/knowledge_plugins/__init__.py
+++ angr/knowledge_plugins/__init__.py
@@ -6,4 +6,5 @@ from .data import Data
 from .indirect_jumps import IndirectJumps
 from .labels import LabelsPlugin
 from .basic_blocks import BasicBlocksPlugin
+from .transitions import TransitionsPlugin
 from .plugin import KnowledgeBasePlugin
