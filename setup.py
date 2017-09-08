65df21ad45c2d5d3d08d0cd8b668f890e604c758
Eric Gustafson <edg@cs.ucsb.edu>
2017-09-07 18:44:15 -0700

    Add dep on pygit

--- setup.py
+++ setup.py
@@ -116,7 +116,8 @@ setup(
         'claripy>=7.0.0.0rc1',
         'cle>=7.0.0.0rc1',
         'pyvex>=7.0.0.0rc1',
-        'pkg_resources'
+        'pkg_resources',
+        'pygit'
     ],
     setup_requires=['unicorn', 'pyvex'],
     cmdclass=cmdclass,
