620bd392d80d9ce338b038146510ef399a4b121c
Danse Macabre <danse616@gmail.com>
2017-09-10 21:47:55 +0300

    TransitionsPlugin: this should go to KB views

--- angr/knowledge_plugins/__init__.py
+++ angr/knowledge_plugins/__init__.py
@@ -6,5 +6,4 @@ from .data import Data
 from .indirect_jumps import IndirectJumps
 from .labels import LabelsPlugin
 from .basic_blocks import BasicBlocksPlugin
-from .transitions import TransitionsPlugin
 from .plugin import KnowledgeBasePlugin
