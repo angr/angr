be71c0e1ec158948bd60228bad83762be3349080
Danse Macabre <danse616@gmail.com>
2017-09-25 21:00:23 +0300

    BasicBlocksPlugin: update tests

--- tests/test_kb_bbl.py
+++ tests/test_kb_bbl.py
@@ -1,4 +1,5 @@
 import functools
+import random
 
 import nose
 import angr
@@ -11,6 +12,24 @@ import os
 location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../binaries/tests'))
 
 
+def test_layout():
+    p = angr.Project(location + "/cgc/stuff", auto_load_libs=False)
+    cfg = p.analyses.CFGFast()
+
+    basic_blocks = p.kb.basic_blocks
+    for node in cfg.nodes():
+        if node.size > 0:
+            basic_blocks.add_block(node.addr, node.size, node.thumb)
+
+    for node in cfg.nodes():
+        if node.size > 0:
+            addr = node.addr + random.randint(0, node.size - 1)  # wow
+            this_block = basic_blocks.get_block(addr, normalize=False)
+            nose.tools.assert_equal(this_block.addr, node.addr)
+            nose.tools.assert_equal(this_block.size, node.size)
+            nose.tools.assert_equal(this_block.thumb, node.thumb)
+
+
 def test_trim():
     p = angr.Project(location + "/cgc/stuff", auto_load_libs=False)
     cfg = p.analyses.CFGFast()
@@ -20,7 +39,7 @@ def test_trim():
         if node.size > 0:
             basic_blocks.add_block(node.addr, node.size, node.thumb)
             if node.addr == 0x80480dc:
-                this_block = basic_blocks.get_block(0x80480d3)
+                this_block = basic_blocks.get_block(0x80480d3, normalize=True)
                 nose.tools.assert_equal(this_block.size, 9)
 
 
@@ -42,7 +61,7 @@ def test_handle():
                                    overlap_mode='handle', overlap_handler=_ovarlap_handler,
                                    node_addr=node.addr)
             if node.addr == 0x80480dc:
-                this_block = basic_blocks.get_block(0x80480d3)
+                this_block = basic_blocks.get_block(0x80480d3, normalize=True)
                 nose.tools.assert_equal(this_block.size, 9)
 
 
@@ -63,6 +82,7 @@ def test_raise():
 
 
 if __name__ == '__main__':
+    test_layout()
     test_trim()
     test_handle()
     test_raise()
