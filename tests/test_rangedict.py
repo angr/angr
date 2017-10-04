3916036e3630270c3ac78ed7dfd76f9d7f9ad6a1
Danse Macabre <danse616@gmail.com>
2017-10-04 17:23:43 +0300

    RangeDict: fix test

--- tests/test_rangedict.py
+++ tests/test_rangedict.py
@@ -1,29 +1,29 @@
 import nose
-import rangedict
+from angr.misc.rangedict import RangeDict
 
 
 def test_items():
-    rdict = rangedict.RangeDict()
+    rdict = RangeDict()
     rdict[0:10] = 'a'
     rdict[5:15] = 'b'
     nose.tools.assert_equal(len(rdict), 2)
     nose.tools.assert_equal(rdict[0:5], ['a'])
     nose.tools.assert_equal(rdict[5:15], ['b'])
 
-    rdict = rangedict.RangeDict()
+    rdict = RangeDict()
     rdict[0:15] = 'a'
     rdict[-5:10] = 'b'
     nose.tools.assert_equal(len(rdict), 2)
     nose.tools.assert_equal(rdict[-5:10], ['b'])
     nose.tools.assert_equal(rdict[10:15], ['a'])
 
-    rdict = rangedict.RangeDict()
+    rdict = RangeDict()
     rdict[0:10] = 'a'
     rdict[-5:15] = 'b'
     nose.tools.assert_equal(len(rdict), 1)
     nose.tools.assert_equal(rdict[-5:15], ['b'])
 
-    rdict = rangedict.RangeDict()
+    rdict = RangeDict()
     rdict[-5:15] = 'a'
     rdict[0:10] = 'b'
     nose.tools.assert_equal(len(rdict), 3)
@@ -31,14 +31,14 @@ def test_items():
     nose.tools.assert_equal(rdict[0:10], ['b'])
     nose.tools.assert_equal(rdict[10:15], ['a'])
 
-    rdict = rangedict.RangeDict()
+    rdict = RangeDict()
     rdict[0:5] = 'a'
     rdict[10:15] = 'a'
     rdict[5:10] = 'a'
     nose.tools.assert_equal(len(rdict), 1)
     nose.tools.assert_equal(rdict[0:15], ['a'])
 
-    rdict = rangedict.RangeDict()
+    rdict = RangeDict()
     rdict[0:5] = 'b'
     rdict[10:15] = 'a'
     rdict[5:10] = 'a'
@@ -46,7 +46,7 @@ def test_items():
     nose.tools.assert_equal(rdict[0:5], ['b'])
     nose.tools.assert_equal(rdict[5:15], ['a'])
 
-    rdict = rangedict.RangeDict()
+    rdict = RangeDict()
     rdict[0:5] = 'b'
     rdict[5:10] = 'a'
     rdict[3:8] = 'c'
