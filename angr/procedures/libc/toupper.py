e445d59b25a2bb4d216bbb01555c3fa9f91a976f
fabros <14915211+fabros@users.noreply.github.com>
2017-10-08 00:06:30 +0200

    Add toupper simproc.

---
+++ angr/procedures/libc/toupper.py
@@ -0,0 +1,22 @@
+import angr
+from angr.sim_type import SimTypeInt
+
+import logging
+l = logging.getLogger("angr.procedures.libc.toupper")
+
+
+class toupper(angr.SimProcedure):
+    def run(self, c):
+        self.argument_types = {0: SimTypeInt(self.state.arch, True)}
+        self.return_type = SimTypeInt(self.state.arch, True)
+
+        if not self.state.solver.symbolic(c):
+            try:
+                ret_expr = chr(self.state.solver.eval(c)).upper()
+            except ValueError:  # not in range(256)
+                ret_expr = c
+            return ret_expr
+        else:
+            return self.state.solver.If(
+                self.state.solver.And(c >= 97, c <= 122),  # a - z
+                c - 32, c)
