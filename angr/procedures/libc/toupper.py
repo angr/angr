154492caea2c7a8488b14f9b4287447670643d92
fabros <14915211+fabros@users.noreply.github.com>
2017-10-08 01:12:45 +0200

    Simplify.

--- angr/procedures/libc/toupper.py
+++ angr/procedures/libc/toupper.py
@@ -10,13 +10,6 @@ class toupper(angr.SimProcedure):
         self.argument_types = {0: SimTypeInt(self.state.arch, True)}
         self.return_type = SimTypeInt(self.state.arch, True)
 
-        if not self.state.solver.symbolic(c):
-            try:
-                ret_expr = ord(chr(self.state.solver.eval(c)).upper())
-            except ValueError:  # not in range(256)
-                ret_expr = c
-            return ret_expr
-        else:
-            return self.state.solver.If(
-                self.state.solver.And(c >= 97, c <= 122),  # a - z
-                c - 32, c)
+        return self.state.solver.If(
+            self.state.solver.And(c >= 97, c <= 122),  # a - z
+            c - 32, c)
