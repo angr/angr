c1b3d46eb57e1e61af3793a02a12974d8d0e6ffa
XiaoyuHe <1422930734@qq.com>
2017-10-29 08:33:09 -0700

    add test for looplimiter

---
+++ tests/test_looplimiter.py
@@ -0,0 +1,18 @@
+import nose
+import angr
+
+import os
+import pdb
+location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../binaries/tests'))
+
+def test_looplimiter():
+    p = angr.Project(os.path.join(location, "x86_64/test_loops"))
+    pg = p.factory.simgr()
+    tech = angr.exploration_techniques.LoopLimiter(count = 5)
+    pg.use_technique(tech) 
+    pg.explore()
+    #pdb.set_trace()
+    nose.tools.assert_equal(len(pg.stashes['spinning']), 1)
+
+if __name__ == '__main__':
+    test_looplimiter()
