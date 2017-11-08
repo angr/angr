a68dd7a21e569f0c34fc4ee02a4d480e4b99d9cc
tyb0807 <sontuan.vu119@gmail.com>
2017-11-08 16:39:50 +0100

    Caching Project as well

--- angr/exploration_techniques/cacher.py
+++ angr/exploration_techniques/cacher.py
@@ -1,5 +1,4 @@
 import os
-import pickle
 import hashlib
 import logging
 
@@ -14,11 +13,11 @@ class Cacher(ExplorationTechnique):
     An exploration technique that caches states during symbolic execution.
     """
 
-    def __init__(self, when=None, dump_cache=True, load_cache=True, cache_file=None, dump_func=None, load_func=None):
+    def __init__(self, when=None, dump_cache=True, load_cache=True, container=None, dump_func=None, load_func=None):
         """
         :param dump_cache: Whether to dump data to cache.
         :param load_cache: Whether to load data from cache.
-        :param cache_file: File name format string to cache data.
+        :param container : Data container.
         :param when      : If provided, should be a function that takes a SimulationManager and returns
                            a Boolean, or the address of the state to be cached.
         :param dump_func : If provided, should be a function that defines how Cacher should cache the
@@ -31,65 +30,84 @@ class Cacher(ExplorationTechnique):
         self._dump_cond = self._condition_to_lambda(when)
         self._dump_cache = dump_cache
         self._load_cache = load_cache
-        self._cache_file = cache_file
+        self._container = container
         self._dump_func = self._dump_stash if dump_func is None else dump_func
         self._load_func = self._load_stash if load_func is None else load_func
 
+        self._container_picklable = None
+        try:
+            import pickle
+            pickle.loads(container)
+            self._container_picklable = True
+        except (pickle.UnpicklingError, TypeError):
+            self._container_picklable = False
+
     def setup(self, simgr):
-        self.project = simgr._project
-        binary = self.project.filename
+        binary = simgr._project.filename
         binhash = hashlib.md5(open(binary).read()).hexdigest()
 
-        if self._cache_file is None:
-            self._cache_file = os.path.join("/tmp", "%s-%s.cache" % (os.path.basename(binary), binhash))
-        else:
+        # By default, we dump data to a file in under /tmp/.
+        if self._container is None:
+            self._container = os.path.join("/tmp", "%s-%s.cache" % (os.path.basename(binary), binhash))
+
+        # Container is the file name.
+        elif isinstance(self._container, str) and not self._container_picklable:
             try:
-                self._cache_file = self._cache_file % {'name': os.path.basename(binary), 'binhash': binhash, 'addr': '%(addr)s'}
+                self._container = self._container % {'name': os.path.basename(binary), 'binhash': binhash, 'addr': '%(addr)s'}
             except KeyError:
                 l.error("Only the following cache keys are accepted: 'name', 'binhash' and 'addr'.")
                 raise
 
-        if self._load_cache and os.path.exists(self._cache_file):
-            l.warning("Loading from %s...", self._cache_file)
+        if (self._load_cache
+           and isinstance(self._container, str)
+           and not self._container_picklable
+           and os.path.exists(self._container)):
+            l.warning("Uncaching from %s...", self._container)
+            self._load_func(self._container, simgr)
 
-            with open(self._cache_file) as f:
-                self._load_func(f, simgr)
+        self.project = simgr._project
 
     def step(self, simgr, stash, **kwargs):
         # We cache if any of the states in 'stash' satisfies the condition.
         for s in simgr.stashes[stash]:
             if self._dump_cache and self._dump_cond(s):
-                self._cache_file = self._cache_file % {'addr': hex(s.addr)[:-1]}
-                if os.path.exists(self._cache_file):
-                    continue
-                l.warning("Caching to %s...", self._cache_file)
+                if isinstance(self._container, str):
+                    self._container = self._container % {'addr': hex(s.addr)[:-1]}
 
-                f = open(self._cache_file, 'wb')
-                try:
-                    self._dump_func(f, simgr, stash)
-                finally:
-                    if f:
-                        f.close()
+                if not self._container_picklable and os.path.exists(self._container):
+                    continue
+
+                l.warning("Caching to %s...", self._container)
+
+                self._dump_func(self._container, simgr, stash)
 
         return simgr.step(stash=stash, **kwargs)
 
     @staticmethod
-    def _load_stash(f, simgr):
-        stash = pickle.load(f)
+    def _load_stash(container, simgr):
+        project = simgr._project
+        cached_project = project.load_function(container)
+        cached_project.analyses = project.analyses
+        cached_project.surveyors = project.surveyors
+        cached_project.store_function = project.store_function
+        cached_project.load_function = project.load_function
+
+        stash = cached_project.storage['cached_states']
         for s in stash:
-            s.project = simgr._project
+            s.project = cached_project
+
         simgr.stashes['active'] = stash
+        simgr._project = cached_project
 
     @staticmethod
-    def _dump_stash(f, simgr, stash):
-        # Do not pickle project
+    def _dump_stash(container, simgr, stash):
         for s in simgr.stashes[stash]:
             s.project = None
             s.history.trim()
-        try:
-            pickle.dump(simgr.stashes[stash], f, pickle.HIGHEST_PROTOCOL)
-        except RuntimeError as e: # maximum recursion depth can be reached here
-            l.error("Unable to cache, '%s' during pickling", e.message)
-        finally:
-            for s in simgr.stashes[stash]:
-                s.project = simgr._project
+
+        project = simgr._project
+        project.storage['cached_states'] = simgr.stashes[stash]
+        project.store_function(container)
+
+        for s in simgr.stashes[stash]:
+            s.project = project
