192a027e2e23fc9d66728846986a24fc81d88f16
Danse Macabre <danse616@gmail.com>
2017-12-13 21:04:44 +0300

    PluginHub: enable lazy plugin initialization

--- angr/misc/plugins.py
+++ angr/misc/plugins.py
@@ -7,38 +7,63 @@ class PluginHub(object):
     """
 
     def __init__(self):
+        super(PluginHub, self).__init__()
         self._plugins = {}
+        self._default_plugins = {}
 
     def __getstate__(self):
-        return {'_plugins': self._plugins}
+        return {'_plugins': self._plugins,
+                '_default_plugins': self._default_plugins}
 
     def __setstate__(self, state):
         self._plugins = {}
         for name, plugin in state['_plugins'].items():
             self.register_plugin(name, plugin)
+        self._default_plugins = state['_default_plugins']
 
     def __getattr__(self, name):
-        raise NoPlugin("No such plugin", name)
+        return self.get_plugin(name)
 
     #
     #   ...
     #
 
     def get_plugin(self, name):
-        return self._plugins[name]
+        if name in self._plugins:
+            return self._plugins[name]
+        elif name in self._default_plugins:
+            plugin_cls = self._default_plugins[name]
+            return self.register_plugin(name, plugin_cls())
+        else:
+            raise NoPlugin("No such plugin: %s", name)
 
     def has_plugin(self, name):
         return name in self._plugins
 
     def register_plugin(self, name, plugin):
+        if self.has_plugin(name):
+            self.release_plugin(name)
         self._plugins[name] = plugin
         self.__dict__[name] = plugin
         return plugin
 
     def release_plugin(self, name):
-        if name in self._plugins:
-            del self._plugins[name]
-            del self.__dict__[name]
+        del self._plugins[name]
+        del self.__dict__[name]
+
+    #
+    #   ...
+    #
+
+    def has_default(self, name):
+        return name in self._default_plugins
+
+    def register_default(self, name, plugin_cls):
+        self._default_plugins[name] = plugin_cls
+
+    def release_default(self, name):
+        if name in self._default_plugins:
+            del self._default_plugins[name]
 
 
 class PluginPreset(object):
