b2d8f21664d64c88546b426a6d4d9731508888bc
Fish <fish@cs.ucsb.edu>
2018-01-01 11:17:07 +0800

    CFGFast: Remove duplicated code for fast pointer loading.

--- angr/analyses/cfg/cfg_fast.py
+++ angr/analyses/cfg/cfg_fast.py
@@ -2218,24 +2218,9 @@ class CFGFast(ForwardAnalysis, CFGBase):    # pylint: disable=abstract-method
                 if data_type == 'pointer-array':
                     # make sure all pointers are identified
                     pointer_size = self.project.arch.bits / 8
-                    buf = self._fast_memory_load(data_addr)
-
-                    # TODO: this part of code is duplicated in _guess_data_type()
-                    # TODO: remove the duplication
-                    if self.project.arch.memory_endness == 'Iend_LE':
-                        fmt = "<"
-                    else:
-                        fmt = ">"
-                    if pointer_size == 8:
-                        fmt += "Q"
-                    elif pointer_size == 4:
-                        fmt += "I"
-                    else:
-                        raise AngrCFGError("Pointer size of %d is not supported" % pointer_size)
 
                     for j in xrange(0, data_size, pointer_size):
-                        ptr_str = self._ffi.unpack(self._ffi.cast('char*', buf + j), pointer_size)
-                        ptr = struct.unpack(fmt, ptr_str)[0]  # type:int
+                        ptr = self._fast_memory_load_pointer(data_addr + j)
 
                         # is this pointer coming from the current binary?
                         obj = self.project.loader.find_object_containing(ptr)
@@ -2296,32 +2281,11 @@ class CFGFast(ForwardAnalysis, CFGBase):    # pylint: disable=abstract-method
                 # IRSB is owned by plt!
                 return "GOT PLT Entry", pointer_size
 
-        # try to decode it as a pointer array
-        buf = self._fast_memory_load(data_addr)
-        if buf is None:
-            # The data address does not exist in static regions
-            return None, None
-
-        if self.project.arch.memory_endness == 'Iend_LE':
-            fmt = "<"
-        else:
-            fmt = ">"
-        if pointer_size == 8:
-            fmt += "Q"
-        elif pointer_size == 4:
-            fmt += "I"
-        else:
-            raise AngrCFGError("Pointer size of %d is not supported" % pointer_size)
-
         pointers_count = 0
 
         max_pointer_array_size = min(512 * pointer_size, max_size)
         for i in xrange(0, max_pointer_array_size, pointer_size):
-            ptr_str = self._ffi.unpack(self._ffi.cast('char*', buf + i), pointer_size)
-            if len(ptr_str) != pointer_size:
-                break
-
-            ptr = struct.unpack(fmt, ptr_str)[0]  # type:int
+            ptr = self._fast_memory_load_pointer(data_addr + i)
 
             if ptr is not None:
                 #if self._seg_list.is_occupied(ptr) and self._seg_list.occupied_by_sort(ptr) == 'code':
