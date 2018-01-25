19587af82a5fc158ee82a26dc6bc7f964bc88113
Jin <54jin.huang@gmail.com>
2018-01-25 09:23:57 +0800

    change the debug info of block transition
    Remove preoconstraints unsat should not regard as "transition has already been encountered".

--- angr/exploration_techniques/driller_core.py
+++ angr/exploration_techniques/driller_core.py
@@ -74,7 +74,8 @@ class DrillerCore(ExplorationTechnique):
 
                     else:
                         l.debug("State at %#x is not satisfiable.", transition[1])
-
+                elif self._has_false(state):
+                    l.debug("Remove preconstraints also unsat")
                 else:
                     l.debug("%#x -> %#x transition has already been encountered.", transition[0], transition[1])
 
