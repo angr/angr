fe33d12ab2a672e8869245641a561b1c88b9caa4
Fish <fish@cs.ucsb.edu>
2018-03-19 07:37:58 -0700

    Use nose.tools to make it more "testy".

--- tests/test_regression_memcmp_definite_size.py
+++ tests/test_regression_memcmp_definite_size.py
@@ -1,5 +1,7 @@
 import os
 
+import nose.tools
+
 import angr
 import claripy as cp
 
@@ -11,20 +13,20 @@ def regression_test_memcmp_strlen_simprocedure_interaction():
     # logging.getLogger('angr.manager').setLevel(logging.DEBUG)
 
     bin_path = os.path.join(test_location, 'i386', 'cpp_regression_test_ch25')
-    p = angr.Project(bin_path)
+    p = angr.Project(bin_path, auto_load_libs=True)  # this binary requires the loading of libstdc++.so.6
     argv1 = cp.Concat(*[cp.BVS('argv%d' % i, 8) for i in range(48)])
 
     state = p.factory.full_init_state(args=[bin_path, argv1],
-            add_options=angr.sim_options.unicorn,
-            remove_options={angr.sim_options.LAZY_SOLVES})
+                                      add_options=angr.sim_options.unicorn
+                                      )
 
     sm = p.factory.simgr(state)
     x = sm.explore(find=0x8048b9b, num_find=3)
 
-    print x
+    nose.tools.assert_equal(len(x.found), 1)
     for s in x.found:
-        solution = s.state.se.eval_one(argv1, cast_to=str)
-        print solution
+        solution = s.state.se.eval_one(argv1, cast_to=str).strip("\x00")
+        nose.tools.assert_equal(solution, "Here_you_have_to_understand_a_little_C++_stuffs")
 
 
 if __name__ == '__main__':
