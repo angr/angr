585b3e7e77047ae23579b4415ba3a05ca189b45a
Fish <ltfish@users.noreply.github.com>
2018-03-19 13:21:04 -0700

    Fix test case regression_memcmp_definite_size. It now works under Windows. (#902)
    * Remove the reference to simuvex from test_regression_memcmp_definite_size.
    
    * Use nose.tools to make it more "testy".

--- tests/test_regression_memcmp_definite_size.py
+++ tests/test_regression_memcmp_definite_size.py
@@ -1,30 +1,33 @@
 import os
 
-import angr
-import simuvex
-import claripy as cp
-import logging
+import nose.tools
 
-test_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../binaries/tests'))
+import angr
+import claripy as cp
+
+test_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'binaries', 'tests'))
+
 
 def regression_test_memcmp_strlen_simprocedure_interaction():
-    logging.getLogger('angr.manager').setLevel(logging.DEBUG)
+    # import logging
+    # logging.getLogger('angr.manager').setLevel(logging.DEBUG)
 
-    bin_path = os.path.join(test_location, 'i386/cpp_regression_test_ch25')
-    p = angr.Project(bin_path)
+    bin_path = os.path.join(test_location, 'i386', 'cpp_regression_test_ch25')
+    p = angr.Project(bin_path, auto_load_libs=True)  # this binary requires the loading of libstdc++.so.6
     argv1 = cp.Concat(*[cp.BVS('argv%d' % i, 8) for i in range(48)])
 
     state = p.factory.full_init_state(args=[bin_path, argv1],
-            add_options=simuvex.o.unicorn,
-            remove_options={simuvex.o.LAZY_SOLVES})
+                                      add_options=angr.sim_options.unicorn
+                                      )
 
     sm = p.factory.simgr(state)
     x = sm.explore(find=0x8048b9b, num_find=3)
 
-    print x
+    nose.tools.assert_equal(len(x.found), 1)
     for s in x.found:
-        solution = s.state.se.eval_one(argv1, cast_to=str)
-        print solution
+        solution = s.state.se.eval_one(argv1, cast_to=str).strip("\x00")
+        nose.tools.assert_equal(solution, "Here_you_have_to_understand_a_little_C++_stuffs")
+
 
 if __name__ == '__main__':
     regression_test_memcmp_strlen_simprocedure_interaction()
