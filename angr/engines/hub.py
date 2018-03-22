44548f68eb36c97d843038e0180d53c12c99d4ed
tyb0807 <sontuan.vu119@gmail.com>
2018-03-22 13:18:58 +0100

    engine.successors(): test if assembling succeeds

--- angr/engines/hub.py
+++ angr/engines/hub.py
@@ -1,5 +1,5 @@
 from ..misc.plugins import PluginHub, PluginPreset
-from ..errors import AngrExitError, SimEngineError
+from ..errors import AngrExitError, AngrAssemblyError, SimEngineError
 
 class EngineHub(PluginHub):
     def __init__(self, project):
@@ -25,10 +25,14 @@ class EngineHub(PluginHub):
             raise SimEngineError("You cannot provide both 'insn_bytes' and 'insn_text'!")
         insn_text = kwargs.get('insn_text', None)
         if insn_text is not None:
-            kwargs['insn_bytes'] = self.project.arch.asm(insn_text,
-                                                         addr=kwargs.get('addr', 0),
-                                                         as_bytes=True,
-                                                         thumb=kwargs.get('thumb', False))
+            insn_bytes = self.project.arch.asm(insn_text,
+                                               addr=kwargs.get('addr', 0),
+                                               as_bytes=True,
+                                               thumb=kwargs.get('thumb', False))
+            if insn_bytes is None:
+                raise AngrAssemblyError("Assembling failed. Please make sure keystone is installed, and the assembly"
+                                        " string is correct.")
+            kwargs['insn_bytes'] = insn_bytes
 
         if addr is not None or jumpkind is not None:
             state = state.copy()
