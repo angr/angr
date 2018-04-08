2456c64bed0d520d032a4602ac6ade164e25da6b
schieb <schieb@users.noreply.github.com>
2018-04-08 16:06:38 -0700

    Add wchar and case-sensitivity support for string-related SimProcedures

--- angr/procedures/libc/strcmp.py
+++ angr/procedures/libc/strcmp.py
@@ -7,16 +7,16 @@ l = logging.getLogger("angr.procedures.libc.strcmp")
 class strcmp(angr.SimProcedure):
     #pylint:disable=arguments-differ
 
-    def run(self, a_addr, b_addr):
+    def run(self, a_addr, b_addr, wchar=False, ignore_case=False):
         self.argument_types = {0: self.ty_ptr(SimTypeString()),
                        1: self.ty_ptr(SimTypeString())}
         self.return_type = SimTypeInt(32, True)
 
         strlen = angr.SIM_PROCEDURES['libc']['strlen']
 
-        a_strlen = self.inline_call(strlen, a_addr)
-        b_strlen = self.inline_call(strlen, b_addr)
+        a_strlen = self.inline_call(strlen, a_addr, wchar=wchar)
+        b_strlen = self.inline_call(strlen, b_addr, wchar=wchar)
         maxlen = self.state.se.BVV(max(a_strlen.max_null_index, b_strlen.max_null_index), self.state.arch.bits)
 
-        strncmp = self.inline_call(angr.SIM_PROCEDURES['libc']['strncmp'], a_addr, b_addr, maxlen, a_len=a_strlen, b_len=b_strlen)
+        strncmp = self.inline_call(angr.SIM_PROCEDURES['libc']['strncmp'], a_addr, b_addr, maxlen, a_len=a_strlen, b_len=b_strlen, wchar=wchar, ignore_case=ignore_case)
         return strncmp.ret_expr
---
+++ angr/procedures/win32/strings.py
@@ -0,0 +1,20 @@
+import angr
+from angr.sim_type import SimTypeString, SimTypeInt
+
+class lstrcmpiW(angr.SimProcedure):
+    def run(self, lpString1, lpString2):
+        self.argument_types = {0: self.ty_ptr(SimTypeString()),
+                               1: self.ty_ptr(SimTypeString())}
+        self.return_type = SimTypeInt(signed=True)
+
+        strcmp = angr.SIM_PROCEDURES['libc']['strcmp']
+        return self.inline_call(strcmp, lpString1, lpString2, wchar=True, ignore_case=True).ret_expr
+
+class lstrcmpW(angr.SimProcedure):
+    def run(self, lpString1, lpString2):
+        self.argument_types = {0: self.ty_ptr(SimTypeString()),
+                               1: self.ty_ptr(SimTypeString())}
+        self.return_type = SimTypeInt(signed=True)
+
+        strcmp = angr.SIM_PROCEDURES['libc']['strcmp']
+        return self.inline_call(strcmp, lpString1, lpString2, wchar=True).ret_expr
