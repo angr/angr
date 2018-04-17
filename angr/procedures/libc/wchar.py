73d92b01b80ea760812e654129ffc4bbe08c9bf4
schieb <schieb@users.noreply.github.com>
2018-04-16 19:20:15 -0700

    test wchar

--- angr/procedures/libc/wchar.py
+++ angr/procedures/libc/wchar.py
@@ -1,18 +1,7 @@
 import angr
 from angr.sim_type import SimTypeString, SimTypeInt
 
-class lstrcmpiW(angr.SimProcedure):
-    #pylint:disable=arguments-differ
-    def run(self, lpString1, lpString2):
-        self.argument_types = {0: self.ty_ptr(SimTypeString()),
-                               1: self.ty_ptr(SimTypeString())}
-        self.return_type = SimTypeInt(signed=True)
-
-        strcmp = angr.SIM_PROCEDURES['libc']['strcmp']
-        return self.inline_call(strcmp, lpString1, lpString2, wchar=True, ignore_case=True).ret_expr
-
-class lstrcmpW(angr.SimProcedure):
-    #pylint:disable=arguments-differ
+class wcscmp(angr.SimProcedure):
     def run(self, lpString1, lpString2):
         self.argument_types = {0: self.ty_ptr(SimTypeString()),
                                1: self.ty_ptr(SimTypeString())}
@@ -20,3 +9,12 @@ class lstrcmpW(angr.SimProcedure):
 
         strcmp = angr.SIM_PROCEDURES['libc']['strcmp']
         return self.inline_call(strcmp, lpString1, lpString2, wchar=True).ret_expr
+
+class wcscasecmp(angr.SimProcedure):
+    def run(self, lpString1, lpString2):
+        self.argument_types = {0: self.ty_ptr(SimTypeString()),
+                               1: self.ty_ptr(SimTypeString())}
+        self.return_type = SimTypeInt(signed=True)
+
+        strcmp = angr.SIM_PROCEDURES['libc']['strcmp']
+        return self.inline_call(strcmp, lpString1, lpString2, wchar=True, ignore_case=True).ret_expr
--- tests/test_strcasecmp.py
+++ tests/test_strcasecmp.py
@@ -12,14 +12,17 @@ def test_i386():
     arg1 = claripy.BVS('arg1', 20*8)
     s = p.factory.entry_state(args=("test_strcasecmp", arg1))
     sm = p.factory.simulation_manager(s)
-    sm.explore(find=lambda s: "Welcome" in s.posix.dumps(1))
+    sm.explore()
+
+    sm.move('deadended', 'found', filter_func=lambda s: "Welcome" in s.posix.dumps(1))
 
     nose.tools.assert_equal(len(sm.found), 1)
-
+    
     f = sm.found[0]
     sol = f.solver.eval(arg1, cast_to=str)
     nose.tools.assert_in('\x00', sol)
     nose.tools.assert_equal(sol[:sol.index('\x00')].lower(), 'letmein')
+    nose.tools.assert_in('wchar works', f.posix.dumps(1))
 
 if __name__ == "__main__":
     test_i386()
