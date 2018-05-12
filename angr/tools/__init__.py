d1b3297f91363236ebd617b5123dce4458d1e5ba
Maksim Malyutin <m.malyutin@embedi.com>
2018-05-12 22:03:07 +0300

    Add FastMem as an example of a tool

--- angr/tools/__init__.py
+++ angr/tools/__init__.py
@@ -3,7 +3,9 @@ from .tool import Tool, ToolHub, ToolSet
 
 # Default tools
 from .factory import AngrObjectFactory
+from .fastmem import FastMemory
 
 default_tools = ToolSet()
 default_tools.add_default_plugin('factory', AngrObjectFactory)
+default_tools.add_default_plugin('fastmem', FastMemory)
 ToolHub.register_preset('default', default_tools)
