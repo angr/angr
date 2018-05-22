76f7d92352f9e1493436a525457fa609ae38e25a
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2018-05-21 17:56:02 -0700

    Add SimProcedure for String.concat

---
+++ angr/procedures/java_lang/string_concat.py
@@ -0,0 +1,20 @@
+from ..java import JavaSimProcedure
+import  logging
+
+import claripy
+
+l = logging.getLogger('angr.procedures.java.string.concat')
+
+class StringConcat(JavaSimProcedure):
+
+    __provides__ = (
+        ("java.lang.String", "concat(java.lang.String)"),
+    )
+
+    def run(self, str_1, str_2):
+        l.debug("Called SimProcedure java.string.concat with args: %s (%r), %s (%r)", str_1, str_1, str_2, str_2)
+        str_1 = str_1 if "String" in str_1.__class__.__name__ else self.state.memory.load(str_1)
+        str_2 = str_2 if "String" in str_2.__class__.__name__ else self.state.memory.load(str_2)
+        import ipdb; ipdb.set_trace()
+        result = claripy.StrConcat(str_1, str_2)
+        return result
