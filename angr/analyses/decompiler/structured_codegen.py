0a8435b47587bb7ea8d97abb1c45099272d6415c
Fish <fish@cs.ucsb.edu>
2018-05-29 18:54:57 +0800

    StructuredCodegen: Implement C codegen in a better way.

--- angr/analyses/decompiler/structured_codegen.py
+++ angr/analyses/decompiler/structured_codegen.py
@@ -8,6 +8,196 @@ from .structurer import SequenceNode, CodeNode, ConditionNode, ConditionalBreakN
 INDENT_DELTA = 4
 
 
+class CConstruct(object):
+    """
+    Represents a program construct in C.
+    """
+
+    def __init__(self):
+        pass
+
+    def c_repr(self, indent=0):
+        raise NotImplementedError()
+
+
+class CFunction(CConstruct):
+    """
+    Represents a function in C.
+    """
+    def __init__(self, name, statements):
+
+        super(CFunction, self).__init__()
+
+        self.name = name
+        self.statements = statements
+
+
+class CStatement(CConstruct):
+    """
+    Represents a statement in C.
+    """
+    def __init__(self):
+
+        super(CStatement, self).__init__()
+
+    def indent_str(self, indent=0):
+        return " " * indent
+
+
+class CStatements(CStatement):
+    """
+    Represents a sequence of statements in C.
+    """
+    def __init__(self, statements):
+
+        super(CStatements, self).__init__()
+
+        self.statements = statements
+
+    def c_repr(self, indent=0):
+
+        return "\n".join([ stmt.c_repr(indent=indent) for stmt in self.statements ])
+
+
+class CAILBlock(CStatement):
+    """
+    Represents a block of AIL statements.
+    """
+    def __init__(self, block):
+
+        super(CAILBlock, self).__init__()
+
+        self.block = block
+
+    def c_repr(self, indent=0):
+
+        lines = [ ]
+
+        r = str(self.block)
+
+        indent_str = self.indent_str(indent=indent)
+        for l in r.split("\n"):
+            lines.append(indent_str + l)
+
+        return "\n".join(lines)
+
+
+class CLoop(CStatement):
+    """
+    Represents a loop in C.
+    """
+    def __init__(self):
+
+        super(CLoop, self).__init__()
+
+
+class CWhileLoop(CLoop):
+    """
+    Represents a while loop in C.
+    """
+    def __init__(self, condition, body):
+
+        super(CWhileLoop, self).__init__()
+
+        self.condition = condition
+        self.body = body
+
+    def c_repr(self, indent=0):
+
+        indent_str = self.indent_str(indent=indent)
+
+        lines = [ ]
+
+        if self.condition is None:
+            # while(true)
+            lines.append(indent_str + 'while(true)')
+            lines.append(indent_str + '{')
+            lines.append(indent_str + self.body.c_repr(indent=indent + INDENT_DELTA))
+            lines.append(indent_str + '}')
+        else:
+            # while(cond)
+            lines.append(indent_str + 'while(%s)' % repr(self.condition))
+            lines.append(indent_str + '{')
+            lines.append(indent_str + self.body.c_repr(indent=indent + INDENT_DELTA))
+            lines.append(indent_str + '}')
+
+        return "\n".join(lines)
+
+
+class CIfElse(CStatement):
+    """
+    Represents an if-else construct in C.
+    """
+    def __init__(self, condition, true_node=None, false_node=None):
+
+        super(CIfElse, self).__init__()
+
+        self.condition = condition
+        self.true_node = true_node
+        self.false_node = false_node
+
+        if self.true_node is None and self.false_node is None:
+            raise ValueError("'true_node' and 'false_node' cannot be both unspecified.")
+
+    def c_repr(self, indent=0):
+
+        indent_str = self.indent_str(indent=indent)
+
+        lines = [
+            indent_str + "if (%s)" % self.condition,
+            indent_str + "{",
+        ]
+
+        lines.append(self.true_node.c_repr(indent=indent + INDENT_DELTA))
+        lines.append(indent_str + "}")
+
+        if self.false_node is not None:
+            lines += [
+                indent_str + 'else',
+                indent_str + '{',
+            ]
+            lines.append(self.false_node.c_repr(indent=indent + INDENT_DELTA))
+            lines.append(indent_str + "}")
+
+        return "\n".join(lines)
+
+
+class CIfBreak(CStatement):
+    """
+    Represents an if-break statement in C.
+    """
+    def __init__(self, condition):
+
+        super(CIfBreak, self).__init__()
+
+        self.condition = condition
+
+    def c_repr(self, indent=0):
+
+        indent_str = self.indent_str(indent=indent)
+
+        lines = [
+            indent_str + "if (%s)" % self.condition,
+            indent_str + "{",
+            indent_str + self.indent_str(indent=INDENT_DELTA) + "break;",
+            indent_str + "}",
+        ]
+
+        return "\n".join(lines)
+
+
+class CAssignment(CStatement):
+    """
+    a = b
+    """
+    def __init__(self, lhs, rhs):
+
+        super(CAssignment, self).__init__()
+
+        self.lhs = lhs
+        self.rhs = rhs
+
+
 class StructuredCodeGenerator(Analysis):
     def __init__(self, sequence):
         self._sequence = sequence
@@ -18,136 +208,95 @@ class StructuredCodeGenerator(Analysis):
 
     def _analyze(self):
 
-        text = "\n".join(self._handle(self._sequence))
+        obj = self._handle(self._sequence)
+        text = obj.c_repr()
 
         self.text = text
-
-    @staticmethod
-    def indent(indent_count):
-        return " " * indent_count
 
     #
     # Handlers
     #
 
-    def _handle(self, node, indent=0):
+    def _handle(self, node):
 
         if type(node) is SequenceNode:
-            return self._handle_Sequence(node, indent)
+            return self._handle_Sequence(node)
         elif type(node) is CodeNode:
-            return self._handle_Code(node, indent)
+            return self._handle_Code(node)
         elif type(node) is LoopNode:
-            return self._handle_Loop(node, indent)
+            return self._handle_Loop(node)
         elif type(node) is ConditionNode:
-            return self._handle_Condition(node, indent)
+            return self._handle_Condition(node)
         elif type(node) is ConditionalBreakNode:
-            return self._handle_ConditionalBreak(node, indent)
+            return self._handle_ConditionalBreak(node)
         elif type(node) is MultiNode:
-            return self._handle_MultiNode(node, indent)
+            return self._handle_MultiNode(node)
         elif type(node) is Block:
-            return self._handle_AILBlock(node, indent)
+            return self._handle_AILBlock(node)
         else:
             raise Exception("Node type %s is not supported yet." % type(node))
 
-    def _handle_Code(self, code, indent):
+    def _handle_Code(self, code):
 
-        return self._handle(code.node, indent=indent)
+        return self._handle(code.node)
 
-    def _handle_Sequence(self, seq, indent):
+    def _handle_Sequence(self, seq):
 
         lines = [ ]
 
         for node in seq.nodes:
-            lines.extend(self._handle(node, indent=indent))
+            lines.append(self._handle(node))
 
-        return lines
+        return CStatements(lines) if len(lines) > 1 else lines[0]
 
-    def _handle_Loop(self, loop_node, indent):
+    def _handle_Loop(self, loop_node):
 
         lines = [ ]
 
         if loop_node.sort == 'while':
-            if loop_node.condition == None:
-                # while(true)
-                lines.append('while(true)')
-                lines.append('{')
-                lines.extend(self._handle(loop_node.sequence_node, indent=INDENT_DELTA))
-                lines.append('}')
-            else:
-                # while(cond)
-                lines.append('while(%s)' % repr(loop_node.condition))
-                lines.append('{')
-                lines.extend(self._handle(loop_node.sequence_node, indent=INDENT_DELTA))
-                lines.append('}')
+            return CWhileLoop(loop_node.condition,
+                              self._handle(loop_node.sequence_node)
+                              )
         elif loop_node.sort == 'do-while':
+            # TODO: FIXME
+
             if loop_node.condition == None:
                 raise NotImplementedError()
             else:
                 lines.append('do')
                 lines.append('{')
-                lines.extend(self._handle(loop_node.sequence_node, indent=INDENT_DELTA))
+                lines.extend(self._handle(loop_node.sequence_node))
                 lines.append('} while(%s);' % repr(loop_node.condition))
+
+            return lines
+
         else:
             raise NotImplementedError()
 
-        new_lines = [ self.indent(indent) + l for l in lines ]
+    def _handle_Condition(self, condition_node):
 
-        return new_lines
+        code = CIfElse(condition_node.condition,
+                       true_node=self._handle(condition_node.true_node) if condition_node.true_node else None,
+                       false_node=self._handle(condition_node.false_node) if condition_node.false_node else None,
+                       )
+        return code
 
-    def _handle_Condition(self, condition_node, indent):
+    def _handle_ConditionalBreak(self, node):
 
-        indent_str = self.indent(indent)
+        return CIfBreak(node.condition)
 
-        lines = [
-            "if (%s)" % condition_node.condition,
-            "{",
-        ]
-
-        lines.extend(self._handle(condition_node.true_node, indent=INDENT_DELTA))
-        lines.append("}")
-
-        if condition_node.false_node is not None:
-            lines += [
-                'else',
-                '{',
-            ]
-            lines.extend(self._handle(condition_node.false_node, indent=INDENT_DELTA))
-            lines.append("}")
-
-        new_lines = [ indent_str + l for l in lines ]
-
-        return new_lines
-
-    def _handle_ConditionalBreak(self, node, indent):
-
-        lines = [
-            self.indent(indent) + "if (" + str(node.condition) + ")",
-            self.indent(indent + INDENT_DELTA) + "break;",
-            self.indent(indent) + "}",
-        ]
-
-        return lines
-
-    def _handle_MultiNode(self, node, indent):
+    def _handle_MultiNode(self, node):
 
         lines = [ ]
 
         for n in node.nodes:
-            r = str(n)
-            for l in r.split("\n"):
-                lines.append(self.indent(indent) + l)
+            r = self._handle(n)
+            lines.append(r)
 
-        return lines
+        return CStatements(lines) if len(lines) > 1 else lines[0]
 
-    def _handle_AILBlock(self, node, indent):
+    def _handle_AILBlock(self, node):
 
-        lines = [ ]
-
-        r = str(node)
-
-        for l in r.split("\n"):
-            lines.append(self.indent(indent) + l)
-
-        return lines
+        return CStatements([ CAILBlock(node) ])
 
 register_analysis(StructuredCodeGenerator, 'StructuredCodeGenerator')
