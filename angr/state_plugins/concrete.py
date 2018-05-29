3e99e8805698c6002c1436efc82ba43fc28750cc
degrigis <degrigis@ucsb.edu>
2018-05-29 16:50:07 -0700

    refactored code, added check on auto_load_libs
    Signed-off-by: degrigis <degrigis@ucsb.edu>

--- angr/state_plugins/concrete.py
+++ angr/state_plugins/concrete.py
@@ -3,17 +3,16 @@ from angr_targets.segment_registers import *
 import struct
 import logging
 from archinfo import ArchX86, ArchAMD64
+
 l = logging.getLogger("state_plugin.concrete")
-l.setLevel(logging.DEBUG)
+# l.setLevel(logging.DEBUG)
 
 
 class Concrete(SimStatePlugin):
     def __init__(self, segment_registers_already_init=False):
-         self.segment_registers_already_init = segment_registers_already_init
-
+        self.segment_registers_already_init = segment_registers_already_init
 
     def copy(self, _memo):
-        print("Copying Concrete Plugin")
         conc = Concrete(segment_registers_already_init=True)
         return conc
 
@@ -26,18 +25,17 @@ class Concrete(SimStatePlugin):
     def set_state(self, state):
         SimStatePlugin.set_state(self, state)
 
-
     def sync(self):
         """
-                Handling the switch between the concrete execution and Angr.
-                This method takes care of:
-                1- Synchronize registers
-                2- Substitute the CLEMemory backer of the self.state with a ConcreteCLEMemory object
-                   that redirects the read inside the concrete process.
-                3- Flush all the pages loaded until now.
+        Handling the switch between the concrete execution and angr.
+        This method takes care of:
+        1- Synchronize registers
+        2- Substitute the CLEMemory backer of the self.state with a ConcreteCLEMemory object
+           that redirects the read inside the concrete process.
+        3- Flush all the pages loaded until now.
 
-                :return:
-                """
+        :return:
+        """
 
         l.debug("Sync the state with the concrete memory inside the Concrete plugin")
 
@@ -52,18 +50,17 @@ class Concrete(SimStatePlugin):
         regs_blacklist = ['fs', 'gs']
         l.info("Synchronizing general purpose registers")
 
-        # TODO create a better list containing only the register useful for syncronization
         for reg_key, reg_name in self.state.arch.register_names.items():
             if reg_name not in regs_blacklist:
                 try:
                     reg_value = self.target.read_register(reg_name)
-                    #self.state.registers.store(reg_name, self.state.se.BVV(reg_value, self.state.arch.bits))
                     setattr(self.state.regs, reg_name, reg_value)
-                    l.debug("Register: %s value: %x "%(reg_name,self.state.se.eval(getattr(self.state.regs,reg_name),cast_to=int)))
+                    l.debug("Register: %s value: %x " % (reg_name,
+                                                         self.state.se.eval(getattr(self.state.regs, reg_name),
+                                                                            cast_to=int)))
                 except ValueError as exc:
-                    l.debug("Can't set register %s reason: %s, if this register is not used this message can be ignored"%( reg_name, exc))
-                    #pass
-
+                    l.debug("Can't set register %s reason: %s, if this register is not used "
+                            "this message can be ignored" % (reg_name, exc))
 
         # Initialize the segment register value if not already initialized
         if not self.segment_registers_already_init:
@@ -97,22 +94,28 @@ class Concrete(SimStatePlugin):
                 whitelist.append((gdt_addr, gdt_addr+gdt_size))
             self.segment_registers_already_init = True
 
-        # Synchronize the imported functions addresses (.got, IAT) in the concrete process with ones used in the SimProcedures dictionary
-        #if self.state.project._should_use_sim_procedures and not self.state.project.loader.main_object.pic:
+        # Synchronize the imported functions addresses (.got, IAT) in the
+        # concrete process with ones used in the SimProcedures dictionary
+
+        # if self.state.project._should_use_sim_procedures and not self.state.project.loader.main_object.pic:
         if self.state.project._should_use_sim_procedures:
             l.info("Restoring SimProc using concrete memory")
             for reloc in self.state.project.loader.main_object.relocs:
-                if reloc.symbol is not None: # consider only reloc with a symbol
+                if reloc.symbol is not None:  # consider only reloc with a symbol
+                    l.debug("Trying to re-hook SimProc %s" % reloc.symbol.name)
+                    l.debug("reloc.rebased_addr: %s " % hex(reloc.rebased_addr))
+
                     func_address = self.target.read_memory(reloc.rebased_addr, self.state.project.arch.bits / 8)
                     func_address = struct.unpack(self.state.project.arch.struct_fmt(), func_address)[0]
-                    l.debug("Re-hooking SimProc " + reloc.symbol.name + " with address " + hex(func_address))
                     self.state.project.rehook_symbol(func_address, reloc.symbol.name)
         else:
             l.warn("SimProc not restored, you are going to simulate also the code of external libraries!")
 
-        # flush the angr memory in order to synchronize them with the content of the concrete process memory when a read/write to the page is performed
+        # flush the angr memory in order to synchronize them with the content of the
+        # concrete process memory when a read/write to the page is performed
         self.state.memory.flush_pages(whitelist)
-        l.info("Exiting SimEngineConcrete: simulated address %x concrete address %x "%(self.state.addr, self.target.read_register("pc")))
+        l.info("Exiting SimEngineConcrete: simulated address %x concrete address %x "
+               % (self.state.addr, self.target.read_register("pc")))
 
 
 from .. import sim_options as options
