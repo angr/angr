3a5b560f365b75f716fcbad30a9d6e5870883fe4
Audrey Dutcher <audrey@rhelmot.io>
2018-06-08 15:53:55 -0700

    finish pinning sortedcontainers

--- setup.py
+++ setup.py
@@ -100,7 +100,7 @@ setup(
     packages=packages,
     install_requires=[
         'ana',
-        'sortedcontainers',
+        'sortedcontainers<2',
         'cachetools',
         'capstone>=3.0.5rc2',
         'cooldict',
