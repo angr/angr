6e561fa232f3a002befabadbd72cca68cfefb536
Audrey Dutcher <audrey@rhelmot.io>
2018-06-11 13:49:45 -0700

    Can't use & or | operators with new state options stuff

--- angr/state_plugins/solver.py
+++ angr/state_plugins/solver.py
@@ -273,7 +273,7 @@ class SimSolver(SimStatePlugin):
             self._stored_solver = claripy.SolverCacheless(track=track)
         elif o.SYMBOLIC in self.state.options and o.COMPOSITE_SOLVER in self.state.options:
             self._stored_solver = claripy.SolverComposite(track=track)
-        elif o.SYMBOLIC in self.state.options and o.approximation & self.state.options:
+        elif o.SYMBOLIC in self.state.options and any(opt in self.state.options for opt in o.approximation):
             self._stored_solver = claripy.SolverHybrid(track=track)
         elif o.SYMBOLIC in self.state.options:
             self._stored_solver = claripy.Solver(track=track)
