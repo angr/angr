2ee61b05a6a5232e12a1c952e533d0238a92c9d3
Artem Shypotilov <a.shypotilov@samsung.com>
2018-06-18 03:20:20 -0600

    Missing mode parameter when calling open from opendir

--- angr/procedures/posix/opendir.py
+++ angr/procedures/posix/opendir.py
@@ -3,6 +3,6 @@ from .open import open
 
 class opendir(angr.SimProcedure):
     def run(self, fname):
-        p_open = self.inline_call(open, fname, 0o200000) # O_DIRECTORY
+        p_open = self.inline_call(open, fname, 0o200000, 0) # O_DIRECTORY
         # using the same hack we used to use for fopen etc... using the fd as a pointer
         return p_open.ret_expr
