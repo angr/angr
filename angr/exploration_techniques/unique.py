6888d17d9f9723923fda03d667add20081d9e156
Audrey Dutcher <audrey@rhelmot.io>
2018-06-20 15:08:32 -0700

    Don't crash while running the unique exploration technique to exhaustion; closes #1068

--- angr/exploration_techniques/unique.py
+++ angr/exploration_techniques/unique.py
@@ -65,11 +65,12 @@ class UniqueSearch(ExplorationTechnique):
                 update_average(state_a, similarity)
         self.num_deadended = len(simgr.deadended)
 
-        unique_state = min(self.uniqueness.items(), key=lambda e: e[1])[0]
-        del self.uniqueness[unique_state]
+        if self.uniqueness:
+            unique_state = min(self.uniqueness.items(), key=lambda e: e[1])[0]
+            del self.uniqueness[unique_state]
 
-        simgr.move(from_stash=self.deferred_stash, to_stash=stash,
-                   filter_func=lambda s: s is unique_state)
+            simgr.move(from_stash=self.deferred_stash, to_stash=stash,
+                       filter_func=lambda s: s is unique_state)
 
         return simgr
 
