angr/angr

commit d27e3eaa8a3dd7614bdd9b6c08d842af570621f3

Pound identifier into submission until it works without crashing on fp values

angr/analyses/identifier/identify.py
@@ -97,10 +97,12 @@ class Identifier(Analysis):
             try:
                 func_info = self.find_stack_vars_x86(f)
                 self.func_info[f] = func_info
-            except (SimEngineError, SimMemoryError) as ex:
-                l.debug("angr translation error: %s", ex.message)
+            except (SimEngineError, SimMemoryError) as e:
+                l.debug("angr translation error: %s", e.message)
             except IdentifierException as e:
-                l.debug("Identifier Exception: %s", e.message)
+                l.debug("Identifier error: %s", e.message)
+            except SimError as e:
+                l.debug("Simulation error: %s", e.message)
 
     def _too_large(self):
         if len(self._cfg.functions) > 400:
@@ -312,36 +314,40 @@ class Identifier(Analysis):
             s.regs.bp = s.regs.sp + func_info.bp_sp_diff
         s.regs.ip = addr_trace[0]
         addr_trace = addr_trace[1:]
+        simgr = self.project.factory.simulation_manager(s, save_unconstrained=True)
         while len(addr_trace) > 0:
-            succ = self.project.factory.successors(s)
+            simgr.stashes['unconstrained'] = []
+            simgr.step()
             stepped = False
-            for ss in succ.flat_successors:
+            for ss in simgr.active:
                 # todo could write symbolic data to pointers passed to functions
                 if ss.history.jumpkind == "Ijk_Call":
                     ss.regs.eax = ss.se.BVS("unconstrained_ret_%#x" % ss.addr, ss.arch.bits)
                     ss.regs.ip = ss.stack_pop()
                     ss.history.jumpkind = "Ijk_Ret"
                 if ss.addr == addr_trace[0]:
-                    s = ss
+                    simgr.stashes['active'] = [ss]
                     stepped = True
+                    break
             if not stepped:
-                if len(succ.unconstrained_successors) > 0:
-                    s = succ.unconstrained_successors[0]
+                if len(simgr.unconstrained) > 0:
+                    s = simgr.unconstrained[0]
                     if s.history.jumpkind == "Ijk_Call":
                         s.regs.eax = s.se.BVS("unconstrained_ret", s.arch.bits)
                         s.regs.ip = s.stack_pop()
                         s.history.jumpkind = "Ijk_Ret"
                     s.regs.ip = addr_trace[0]
+                    simgr.stashes['active'] = [s]
                     stepped = True
             if not stepped:
                 raise IdentifierException("could not get call args")
             addr_trace = addr_trace[1:]
 
         # step one last time to the call
-        succ = self.project.factory.successors(s)
-        if len(succ.flat_successors) == 0:
+        simgr.step()
+        if len(simgr.active) == 0:
             IdentifierException("Didn't succeed call")
-        return succ.flat_successors[0]
+        return simgr.active[0]
 
     def get_call_args(self, func, callsite):
         if isinstance(func, (int, long)):
@@ -571,13 +577,20 @@ class Identifier(Analysis):
             addr = block.addr
             if addr in preamble_addrs:
                 addr = end_preamble
-            if self.project.factory.block(addr).vex.jumpkind == "Ijk_Ret":
-                main_state.ip = addr
-                for a in self.project.factory.successors(main_state).all_successors[0].history.recent_actions:
-                    if a.type == "reg" and a.action == "write":
-                        if self.get_reg_name(self.project.arch, a.offset) == self._sp_reg:
+            irsb = self.project.factory.block(addr).vex
+            if irsb.jumpkind == "Ijk_Ret":
+                cur_addr = None
+                found_end = False
+                for stmt in irsb.statements:
+                    if stmt.tag == 'Ist_Imark':
+                        cur_addr = stmt.addr
+                        if found_end:
+                            all_end_addrs.add(cur_addr)
+                    elif not found_end and stmt.tag == 'Ist_Put':
+                        if stmt.offset == self.project.arch.sp_offset:
+                            found_end = True
                             ends.add(a.ins_addr)
-                            all_end_addrs.update(set(self.project.factory.block(a.ins_addr).instruction_addrs))
+                            all_end_addrs.add(cur_addr)
 
         bp_sp_diff = None
         if bp_based:
@@ -607,7 +620,10 @@ class Identifier(Analysis):
             if self._no_sp_or_bp(bl):
                 continue
             main_state.ip = addr
-            succ = self.project.factory.successors(main_state, num_inst=1).all_successors[0]
+            try:
+                succ = self.project.factory.successors(main_state, num_inst=1).all_successors[0]
+            except SimError:
+                continue
 
             written_regs = set()
             # we can get stack variables via memory actions

angr/analyses/identifier/runner.py
@@ -7,7 +7,7 @@ import claripy
 
 from ...sim_type import SimTypeFunction, SimTypeInt
 from ... import sim_options as so
-from ... import SIM_PROCEDURES
+from ... import SIM_LIBRARIES
 from ... import BP_BEFORE, BP_AFTER
 from ...storage.file import SimFile
 from ...errors import AngrCallableMultistateError, AngrCallableError, AngrError, SimError
@@ -112,8 +112,8 @@ class Runner(object):
     def setup_state(self, function, test_data, initial_state=None, concrete_rand=False):
         # FIXME fdwait should do something concrete...
         # FixedInReceive and FixedOutReceive always are applied
-        SIM_PROCEDURES['cgc']['transmit'] = self.FixedOutTransmit
-        SIM_PROCEDURES['cgc']['receive'] = self.FixedInReceive
+        SIM_LIBRARIES['cgcabi'].add('transmit', self.FixedOutTransmit)
+        SIM_LIBRARIES['cgcabi'].add('receive', self.FixedInReceive)
 
         fs = {'/dev/stdin': SimFile(
             "/dev/stdin", "r",

commit 9b2dd0dedec953fb8c46919a95f1b8b0449427df

Fix bug introduced in d27e3eaa8a3dd7614bdd9b6c08d842af570621f3; closes #1079

angr/analyses/identifier/identify.py
@@ -588,7 +588,7 @@ class Identifier(Analysis):
                     elif not found_end and stmt.tag == 'Ist_Put':
                         if stmt.offset == self.project.arch.sp_offset:
                             found_end = True
-                            ends.add(a.ins_addr)
+                            ends.add(cur_addr)
                             all_end_addrs.add(cur_addr)
 
         bp_sp_diff = None