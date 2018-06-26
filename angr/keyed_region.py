angr/angr

commit 13f510604cd598e6ac200714fd60abd541a4ca58

Transition to sortedcontainers over bintrees; closes 947 (#962)

angr/keyed_region.py
@@ -1,7 +1,5 @@
-
 import logging
-
-from bintrees import AVLTree
+from sortedcontainers import SortedDict
 
 
 l = logging.getLogger("angr.knowledge.keyed_region")
@@ -86,7 +84,18 @@ class KeyedRegion(object):
     Registers and function frames can all be viewed as a keyed region.
     """
     def __init__(self, tree=None):
-        self._storage = AVLTree() if tree is None else tree  # type: AVLTree
+        self._storage = SortedDict() if tree is None else tree
+
+    def _get_container(self, offset):
+        try:
+            base_offset = next(self._storage.irange(maximum=offset, reverse=True))
+        except StopIteration:
+            return offset, None
+        else:
+            container = self._storage[base_offset]
+            if container.includes(offset):
+                return base_offset, container
+            return offset, None
 
     def __contains__(self, offset):
         """
@@ -96,25 +105,19 @@ class KeyedRegion(object):
         :return:
         """
 
-        try: base_offset, item = self._storage.floor_item(offset)  #pylint:disable=unused-variable
-        except KeyError: return False
-
-        if item.includes(offset):
-            return True
-        return False
+        return self._get_container(offset)[1] is not None
 
     def __len__(self):
         return len(self._storage)
 
     def __iter__(self):
-        for _, item in self._storage.items():
-            yield item
+        return self._storage.itervalues()
 
     def __eq__(self, other):
         if set(self._storage.keys()) != set(other._storage.keys()):
             return False
 
-        for k, v in self._storage.iter_items():
+        for k, v in self._storage.iteritems():
             if v != other._storage[k]:
                 return False
 
@@ -125,7 +128,7 @@ class KeyedRegion(object):
             return KeyedRegion()
 
         kr = KeyedRegion()
-        for key, ro in self._storage.iter_items():
+        for key, ro in self._storage.iteritems():
             kr._storage[key] = ro.copy()
         return kr
 
@@ -138,7 +141,7 @@ class KeyedRegion(object):
         """
 
         # TODO: is the current solution not optimal enough?
-        for _, item in other._storage.iter_items():  # type: RegionObject
+        for _, item in other._storage.iteritems():  # type: RegionObject
             for loc_and_var in item.objects:
                 self.__store(loc_and_var, overwrite=False, make_phi_func=make_phi_func)
 
@@ -194,13 +197,11 @@ class KeyedRegion(object):
         :rtype:  int or None
         """
 
-        try: base_addr, item = self._storage.floor_item(addr)
-        except KeyError: return None
-
-        if item.includes(addr):
+        base_addr, container = self._get_container(addr)
+        if container is None:
+            return None
+        else:
             return base_addr
-
-        return None
 
     def get_variables_by_offset(self, start):
         """
@@ -211,14 +212,11 @@ class KeyedRegion(object):
         :rtype:  set
         """
 
-        try: base_addr = self._storage.floor_key(start)
-        except KeyError: return [ ]
-
-        item = self._storage[base_addr]  # type: RegionObject
-        if item.includes(start):
-            return item.variables
-        return [ ]
-
+        base_addr, container = self._get_container(start)
+        if container is None:
+            return []
+        else:
+            return container.variables
 
     #
     # Private methods
@@ -252,25 +250,20 @@ class KeyedRegion(object):
         end = start + variable_size
 
         # region items in the middle
-        overlapping_items = list(self._storage.item_slice(start, end))
+        overlapping_items = list(self._storage.irange(start, end-1))
 
         # is there a region item that begins before the start and overlaps with this variable?
-        try:
-            floor_key, floor_item = self._storage.floor_item(start)  # type: RegionObject
-            if floor_item.includes(start):
-                item = (floor_key, floor_item)
-                if item not in overlapping_items:
-                    # insert it into the beginningq
-                    overlapping_items.insert(0, (floor_key, floor_item))
-        except KeyError:
-            # no there isn't
-            pass
+        floor_key, floor_item = self._get_container(start)
+        if floor_item is not None and floor_key not in overlapping_items:
+                # insert it into the beginningq
+                overlapping_items.insert(0, (floor_key, self._storage[floor_key]))
 
         # scan through the entire list of region items, split existing regions and insert new regions as needed
         to_update = { start: RegionObject(start, variable_size, { loc_and_var }) }
         last_end = start
 
-        for _, item in overlapping_items:  # type: RegionObject
+        for floor_key in overlapping_items:
+            item = self._storage[floor_key]
             if item.start < start:
                 # we need to break this item into two
                 a, b = item.split(start)
@@ -312,8 +305,8 @@ class KeyedRegion(object):
             # make sure this variable does not overlap with any other variable
             end = start + variable.size
             try:
-                prev_offset = self._storage.floor_key(end - 1)
-            except KeyError:
+                prev_offset = next(self._storage.irange(maximum=end-1, reverse=True))
+            except StopIteration:
                 prev_offset = None
 
             if prev_offset is not None:
@@ -325,8 +318,8 @@ class KeyedRegion(object):
                     return True
         else:
             try:
-                prev_offset = self._storage.floor_key(start)
-            except KeyError:
+                prev_offset = next(self._storage.irange(maximum=start, reverse=True))
+            except StopIteration:
                 prev_offset = None
 
             if prev_offset is not None:

angr/analyses/cfg/cfb.py
@@ -1,10 +1,8 @@
-
 import logging
-
-from bintrees import AVLTree
 import cffi
-
 import cle
+
+from sortedcontainers import SortedDict
 
 from ..analysis import Analysis
 
@@ -67,7 +65,7 @@ class CFBlanket(Analysis):
     A Control-Flow Blanket is a representation for storing all instructions, data entries, and bytes of a full program.
     """
     def __init__(self, cfg=None):
-        self._blanket = AVLTree()
+        self._blanket = SortedDict()
 
         self._ffi = cffi.FFI()
 
@@ -78,63 +76,51 @@ class CFBlanket(Analysis):
             for func in self.kb.functions.values():
                 self.add_function(func)
 
+    def floor_addr(self, addr):
+        try:
+            return next(self._blanket.irange(maximum=addr, reverse=True))
+        except StopIteration:
+            raise KeyError(addr)
+
     def floor_item(self, addr):
-        return self._blanket.floor_item(addr)
+        key = self.floor_addr(addr)
+        return key, self._blanket[key]
 
     def floor_items(self, addr=None):
-
         if addr is None:
-            addr = self._blanket.min_key()
+            start_addr = None
         else:
             try:
-                addr = self.floor_addr(addr)
-            except KeyError:
-                try:
-                    addr = self.ceiling_addr(addr)
-                except KeyError:
-                    # Nothing to yield
-                    raise StopIteration
+                start_addr = next(self._blanket.irange(maximum=addr, reverse=True))
+            except StopIteration:
+                start_addr = addr
 
-        while True:
-            try:
-                item = self._blanket[addr]
-                yield (addr, item)
-                item_size = item.size if item.size > 0 else 1  # pylint: disable=no-member
-                addr = self.ceiling_addr(addr + item_size)
-            except KeyError:
-                break
-
-    def floor_addr(self, addr):
-        return self._blanket.floor_key(addr)
-
-    def ceiling_item(self, addr):
-        return self._blanket.ceiling_item(addr)
+        for key in self._blanket.irange(minimum=start_addr):
+            yield key, self._blanket[key]
 
     def ceiling_addr(self, addr):
-        return self._blanket.ceiling_key(addr)
+        try:
+            return next(self._blanket.irange(minimum=addr, reverse=False))
+        except StopIteration:
+            raise KeyError(addr)
+
+    def ceiling_item(self, addr):
+        key = self.ceiling_addr(addr)
+        return key, self._blanket[key]
 
     def __getitem__(self, addr):
         return self._blanket[addr]
 
     def add_obj(self, addr, obj):
         """
-
-
-        :param addr:
-        :param obj:
-        :return:
+        Adds an object `obj` to the blanket at the specified address `addr`
         """
-
         self._blanket[addr] = obj
 
     def add_function(self, func):
         """
-        Add a function and all blocks of this function to the blanket.
-
-        :param angr.Function func: The function to add.
-        :return:
+        Add a function `func` and all blocks of this function to the blanket.
         """
-
         for block in func.blocks:
             self.add_obj(block.addr, block)
 

angr/analyses/cfg/cfg_fast.py
@@ -5,7 +5,7 @@ import re
 import string
 from collections import defaultdict
 
-from bintrees import AVLTree
+from sortedcontainers import SortedDict
 
 import claripy
 import cle
@@ -763,10 +763,8 @@ class CFGFast(ForwardAnalysis, CFGBase):    # pylint: disable=abstract-method
         # sort the regions
         regions = sorted(regions, key=lambda x: x[0])
         self._regions_size = sum((b - a) for a, b in regions)
-        # initial self._regions as an AVL tree
-        self._regions = AVLTree()
-        for start_, end_ in regions:
-            self._regions.insert(start_, end_)
+        # initial self._regions as a sorted dict
+        self._regions = SortedDict(regions)
 
         self._pickle_intermediate_results = pickle_intermediate_results
         self._indirect_jump_target_limit = indirect_jump_target_limit
@@ -801,8 +799,8 @@ class CFGFast(ForwardAnalysis, CFGBase):    # pylint: disable=abstract-method
         self._data_type_guessing_handlers = [ ] if data_type_guessing_handlers is None else data_type_guessing_handlers
 
         l.debug("CFG recovery covers %d regions:", len(self._regions))
-        for start_addr, end_addr in self._regions.iter_items():
-            l.debug("... %#x - %#x", start_addr, end_addr)
+        for start_addr in self._regions:
+            l.debug("... %#x - %#x", start_addr, self._regions[start_addr])
 
         # A mapping between address and the actual data in memory
         self._memory_data = { }
@@ -935,10 +933,11 @@ class CFGFast(ForwardAnalysis, CFGBase):    # pylint: disable=abstract-method
         """
 
         try:
-            start_addr, end_addr = self._regions.floor_item(address)
-            return start_addr <= address < end_addr
+            start_addr = next(self._regions.irange(maximum=address, reverse=True))
         except KeyError:
             return False
+        else:
+            return address < self._regions[start_addr]
 
     def _get_min_addr(self):
         """
@@ -952,7 +951,7 @@ class CFGFast(ForwardAnalysis, CFGBase):    # pylint: disable=abstract-method
             l.error("self._regions is empty or not properly set.")
             return None
 
-        return self._regions.min_key()
+        return next(self._regions.irange())
 
     def _next_address_in_regions(self, address):
         """
@@ -963,12 +962,11 @@ class CFGFast(ForwardAnalysis, CFGBase):    # pylint: disable=abstract-method
         :rtype:             int
         """
 
+        if self._inside_regions(address):
+            return address
+
         try:
-            start_addr, end_addr = self._regions.floor_item(address)
-            if start_addr <= address < end_addr:
-                return address
-            else:
-                return self._regions.ceiling_key(address)
+            return next(self._regions.irange(minimum=address, reverse=True))
         except KeyError:
             return None
 
@@ -1005,7 +1003,7 @@ class CFGFast(ForwardAnalysis, CFGBase):    # pylint: disable=abstract-method
 
         # Make sure curr_addr exists in binary
         accepted = False
-        for start, end in self._regions.iter_items():
+        for start, end in self._regions.iteritems():
             if start <= curr_addr < end:
                 # accept
                 accepted = True

angr/knowledge_plugins/functions/function_manager.py
@@ -1,7 +1,6 @@
 import logging
 import collections
-
-import bintrees
+from sortedcontainers import SortedDict
 import networkx
 
 from ...errors import SimEngineError
@@ -12,39 +11,37 @@ from .function import Function
 l = logging.getLogger("angr.knowledge.function_manager")
 
 
-class FunctionDict(dict):
+class FunctionDict(SortedDict):
     """
     FunctionDict is a dict where the keys are function starting addresses and
     map to the associated :class:`Function`.
     """
     def __init__(self, backref, *args, **kwargs):
         self._backref = backref
-        self._avltree = bintrees.AVLTree()
         super(FunctionDict, self).__init__(*args, **kwargs)
 
-    def __missing__(self, key):
-        if isinstance(key, (int, long)):
-            addr = key
-        else:
-            raise ValueError("FunctionDict.__missing__ only supports int as key type")
+    def __getitem__(self, addr):
+        try:
+            return super(FunctionDict, self).__getitem__(addr)
+        except KeyError:
+            if not isinstance(addr, (int, long)):
+                raise TypeError("FunctionDict only supports int as key type")
 
-        t = Function(self._backref, addr)
-        self[addr] = t
-        return t
-
-    def __setitem__(self, addr, func):
-        self._avltree[addr] = func
-        super(FunctionDict, self).__setitem__(addr, func)
-
-    def __delitem__(self, addr):
-        del self._avltree[addr]
-        super(FunctionDict, self).__delitem__(addr)
+            t = Function(self._backref, addr)
+            self[addr] = t
+            return t
 
     def floor_addr(self, addr):
-        return self._avltree.floor_key(addr)
+        try:
+            return next(self.irange(maximum=addr, reverse=True))
+        except StopIteration:
+            raise KeyError(addr)
 
     def ceiling_addr(self, addr):
-        return self._avltree.ceiling_key(addr)
+        try:
+            return next(self.irange(minimum=addr, reverse=False))
+        except StopIteration:
+            raise KeyError(addr)
 
 
 class FunctionManager(KnowledgeBasePlugin, collections.Mapping):

angr/storage/memory.py
@@ -1,13 +1,15 @@
 import logging
-
-l = logging.getLogger("angr.storage.memory")
-
 import claripy
+from sortedcontainers import SortedDict
+
 from ..state_plugins.plugin import SimStatePlugin
 from ..engines.vex.ccall import _get_flags
 
+l = logging.getLogger("angr.storage.memory")
+
 stn_map = { 'st%d' % n: n for n in xrange(8) }
 tag_map = { 'tag%d' % n: n for n in xrange(8) }
+
 
 class AddressWrapper(object):
     """
@@ -79,8 +81,8 @@ class RegionMap(object):
         """
         self.is_stack = is_stack
 
-        # An AVLTree, which maps stack addresses to region IDs
-        self._address_to_region_id = AVLTree()
+        # A sorted list, which maps stack addresses to region IDs
+        self._address_to_region_id = SortedDict()
         # A dict, which maps region IDs to memory address ranges
         self._region_id_to_address = { }
 
@@ -102,7 +104,7 @@ class RegionMap(object):
         if not self.is_stack:
             raise SimRegionMapError('Calling "stack_base" on a non-stack region map.')
 
-        return self._address_to_region_id.max_key()
+        return next(self._address_to_region_id.irange(reverse=True))
 
     @property
     def region_ids(self):
@@ -117,10 +119,7 @@ class RegionMap(object):
         r = RegionMap(is_stack=self.is_stack)
 
         # A shallow copy should be enough, since we never modify any RegionDescriptor object in-place
-        if len(self._address_to_region_id) > 0:
-            # TODO: There is a bug in bintrees 2.0.2 that prevents us from copying a non-empty AVLTree object
-            # TODO: Consider submit a pull request
-            r._address_to_region_id = self._address_to_region_id.copy()
+        r._address_to_region_id = self._address_to_region_id.copy()
         r._region_id_to_address = self._region_id_to_address.copy()
 
         return r
@@ -143,13 +142,13 @@ class RegionMap(object):
             # Remove all stack regions that are lower than the one to add
             while True:
                 try:
-                    addr = self._address_to_region_id.floor_key(absolute_address)
+                    addr = next(self._address_to_region_id.irange(maximum=absolute_address, reverse=True))
                     descriptor = self._address_to_region_id[addr]
                     # Remove this mapping
                     del self._address_to_region_id[addr]
                     # Remove this region ID from the other mapping
                     del self._region_id_to_address[descriptor.region_id]
-                except KeyError:
+                except StopIteration:
                     break
 
         else:
@@ -217,13 +216,13 @@ class RegionMap(object):
         if target_region_id is None:
             if self.is_stack:
                 # Get the base address of the stack frame it belongs to
-                base_address = self._address_to_region_id.ceiling_key(absolute_address)
+                base_address = next(self._address_to_region_id.irange(minimum=absolute_address, reverse=False))
 
             else:
                 try:
-                    base_address = self._address_to_region_id.floor_key(absolute_address)
+                    base_address = next(self._address_to_region_id.irange(maximum=absolute_address, reverse=True))
 
-                except KeyError:
+                except StopIteration:
                     # Not found. It belongs to the global region then.
                     return 'global', absolute_address, None
 
@@ -879,7 +878,6 @@ class SimMemory(SimStatePlugin):
                       disable_actions=False):
         raise NotImplementedError()
 
-from bintrees import AVLTree
 from .. import sim_options as o
 from ..state_plugins.sim_action import SimActionData
 from ..state_plugins.sim_action_object import SimActionObject, _raw_ast

angr/storage/paged_memory.py
@@ -1,8 +1,8 @@
-import bintrees
 import cooldict
 import claripy
 import cffi
 import cle
+from sortedcontainers import SortedDict
 
 from ..errors import SimMemoryError, SimSegfaultError
 from .. import sim_options as options
@@ -127,84 +127,82 @@ class BasePage(object):
 
 class TreePage(BasePage):
     """
-    Page object, implemented with a bintree.
+    Page object, implemented with a sorted dict. Who knows what's underneath!
     """
 
     def __init__(self, *args, **kwargs):
         storage = kwargs.pop("storage", None)
         super(TreePage, self).__init__(*args, **kwargs)
-        self._storage = bintrees.AVLTree() if storage is None else storage
+        self._storage = SortedDict() if storage is None else storage
 
     def keys(self):
         if len(self._storage) == 0:
             return set()
         else:
-            return set.union(*(set(range(*self._resolve_range(mo))) for mo in self._storage.values()))
+            return set.union(*(set(range(*self._resolve_range(mo))) for mo in self._storage.itervalues()))
 
     def replace_mo(self, state, old_mo, new_mo):
         start, end = self._resolve_range(old_mo)
-        possible_items = list(self._storage.item_slice(start, end))
-        for a,v in possible_items:
-            if v is old_mo:
+        for key in self._storage.irange(start, end-1):
+            val = self._storage[key]
+            if val is old_mo:
                 #assert new_mo.includes(a)
-                self._storage[a] = new_mo
+                self._storage[key] = new_mo
 
     def store_overwrite(self, state, new_mo, start, end):
-        # get a list of items that we will overwrite
-        current_items = list(self._storage.item_slice(start, end + 1))
-
+        # iterate over each item we might overwrite
+        # track our mutations separately since we're in the process of iterating
+        deletes = []
         updates = { start: new_mo }
 
-        # remove the items we are overwriting
-        if not current_items:
-            # make sure we aren't overwriting an entire item that starts before
-            # the write range and extends past the end of it
-            try:
-                _, floor_value = self._storage.floor_item(start)
-                if floor_value.includes(end):
-                    updates[end] = floor_value
-            except KeyError:
-                pass
-        else:
-            # make sure we're not overwriting an entire item that starts inside
-            # the write range and extends past the end of it
-            if end < self._page_addr + self._page_size and current_items[-1][1].includes(end):
-                updates[end] = current_items[-1][1]
+        for key in self._storage.irange(maximum=end-1, reverse=True):
+            old_mo = self._storage[key]
 
-            # remove existing items
-            del self._storage[start:end]
+            # make sure we aren't overwriting all of an item that overlaps the end boundary
+            if end < self._page_addr + self._page_size and end not in updates and old_mo.includes(end):
+                updates[end] = old_mo
+
+            # we can't set a minimum on the range because we need to do the above for
+            # the first object before start too
+            if key < start:
+                break
+
+            # delete any key that falls within the range
+            deletes.append(key)
 
         #assert all(m.includes(i) for i,m in updates.items())
 
-        # store the new stuff
+        # perform mutations
+        for key in deletes:
+            del self._storage[key]
+
         self._storage.update(updates)
 
     def store_underwrite(self, state, new_mo, start, end):
-        # first, get the current items
-        current_items = list(self._storage.item_slice(start, end + 1))
-
-        # go through them backwards and fill in the gaps
+        # track the point that we need to write up to
         last_missing = end - 1
-        updates = { }
-        for _,mo in reversed(current_items):
-            if not mo.includes(last_missing) and not mo.base > last_missing:
-                # this mo does not cover up to the end; we need to fill it in
-                updates[mo.last_addr+1] = new_mo
+        # track also updates since we can't update while iterating
+        updates = {}
+
+        for key in self._storage.irange(maximum=end-1, reverse=True):
+            mo = self._storage[key]
+
+            # if the mo stops
+            if mo.base <= last_missing and not mo.includes(last_missing):
+                updates[max(mo.last_addr+1, start)] = new_mo
             last_missing = mo.base - 1
 
-        # if the beginning is missing, fill it in and make sure we're not
-        # overwriting something we shouldn't be
+            # we can't set a minimum on the range because we need to do the above for
+            # the first object before start too
+            if last_missing < start:
+                break
+
+        # if there are no memory objects <= start, we won't have filled start yet
         if last_missing >= start:
-            try:
-                _, floor_value = self._storage.floor_item(start)
-                if not floor_value.includes(last_missing):
-                    updates[max(floor_value.last_addr+1, start)] = new_mo
-            except KeyError:
-                updates[start] = new_mo
+            updates[start] = new_mo
 
         #assert all(m.includes(i) for i,m in updates.items())
 
-        # apply it
         self._storage.update(updates)
 
     def load_mo(self, state, page_idx):
@@ -216,11 +214,11 @@ class TreePage(BasePage):
         """
 
         try:
-            mo = self._storage.floor_item(page_idx)[1]
-        except KeyError:
-            mo = None
-
-        return mo
+            key = next(self._storage.irange(maximum=page_idx, reverse=True))
+        except StopIteration:
+            return None
+        else:
+            return self._storage[key]
 
     def load_slice(self, state, start, end):
         """
@@ -230,18 +228,19 @@ class TreePage(BasePage):
         :param end: the end address (non-inclusive)
         :returns: tuples of (starting_addr, memory_object)
         """
-        items = list(self._storage.item_slice(start, end))
-        if not items or items[0][0] != start:
+        keys = list(self._storage.irange(start, end-1))
+        if not keys or keys[0] != start:
             try:
-                _, floor_mo = self._storage.floor_item(start)
-                if floor_mo.includes(start):
-                    items.insert(0, (start, floor_mo))
-            except KeyError:
+                key = next(self._storage.irange(maximum=start, reverse=True))
+            except StopIteration:
                 pass
-        return items
+            else:
+                if self._storage[key].includes(start):
+                    items.insert(0, key)
+        return [(key, self._storage[key]) for key in keys]
 
     def _copy_args(self):
-        return { 'storage': bintrees.AVLTree(self._storage) }
+        return { 'storage': self._storage.copy() }
 
 class ListPage(BasePage):
     """

setup.py
@@ -100,7 +100,7 @@ setup(
     packages=packages,
     install_requires=[
         'ana',
-        'bintrees',
+        'sortedcontainers',
         'cachetools',
         'capstone>=3.0.5rc2',
         'cooldict',

commit cdcd0f35f458dfb4ce7622258ebccb9dc5833067

Fix bug in keyed_region introduced in #962

angr/keyed_region.py
@@ -299,8 +299,8 @@ class KeyedRegion(object):
         # is there a region item that begins before the start and overlaps with this variable?
         floor_key, floor_item = self._get_container(start)
         if floor_item is not None and floor_key not in overlapping_items:
-                # insert it into the beginningq
-                overlapping_items.insert(0, (floor_key, self._storage[floor_key]))
+                # insert it into the beginning
+                overlapping_items.insert(0, floor_key)
 
         # scan through the entire list of region items, split existing regions and insert new regions as needed
         to_update = {start: RegionObject(start, object_size, {stored_object})}