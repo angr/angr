68ed66ee8323bea94d2866d6d51a30d421f3464f
tyb0807 <sontuan.vu119@gmail.com>
2018-07-11 17:24:11 +0200

    LoopSeer refactor

--- angr/exploration_techniques/loop_seer.py
+++ angr/exploration_techniques/loop_seer.py
@@ -2,6 +2,7 @@ import logging
 
 from . import ExplorationTechnique
 from ..analyses.loopfinder import Loop
+from ..errors import AngrExplorationTechniqueError
 from ..knowledge_base import KnowledgeBase
 from ..knowledge_plugins.functions import Function
 
@@ -16,11 +17,12 @@ class LoopSeer(ExplorationTechnique):
     free to add something else).
     """
 
-    def __init__(self, cfg=None, functions=None, loops=None, bound=None, bound_reached=None, discard_stash='spinning'):
+    def __init__(self, cfg=None, functions=None, loops=None, use_header=False, bound=None, bound_reached=None, discard_stash='spinning'):
         """
         :param cfg:             Normalized CFG is required.
         :param functions:       Function(s) containing the loop(s) to be analyzed.
         :param loops:           Loop(s) to be analyzed.
+        :param use_header:      Whether to use header based trip counter to compare with the bound limit.
         :param bound:           Limit the number of iteration a loop may be executed.
         :param bound_reached:   If provided, should be a function that takes a SimulationManager and returns
                                 a SimulationManager. Will be called when loop execution reach the given bound.
@@ -34,9 +36,9 @@ class LoopSeer(ExplorationTechnique):
         self.bound = bound
         self.bound_reached = bound_reached
         self.discard_stash = discard_stash
+        self.use_header = use_header
 
         self.loops = {}
-
         if type(loops) is Loop:
             loops = [loops]
 
@@ -46,7 +48,7 @@ class LoopSeer(ExplorationTechnique):
                     self.loops[loop.entry_edges[0][0].addr] = loop
 
         elif loops is not None:
-            raise TypeError('What type of loop is it?')
+            raise AngrExplorationTechniqueError("Invalid type for 'loops' parameter!")
 
     def setup(self, simgr):
         if self.cfg is None:
@@ -56,36 +58,20 @@ class LoopSeer(ExplorationTechnique):
             l.warning("LoopSeer uses normalized CFG. Recomputing the CFG...")
             self.cfg.normalize()
 
-        if type(self.functions) is str:
-            func = [self.cfg.kb.functions.function(name=self.functions)]
+        func = []
+        if type(self.functions) in (str, int, Function):
+            func.extend(self._get_function(self.functions))
 
-        elif type(self.functions) is int:
-            func = [self.cfg.kb.functions.function(addr=self.functions)]
-
-        elif type(self.functions) is Function:
-            func = [self.functions]
-
-        elif type(self.functions) in (list, tuple):
-            func = []
+        elif type(self.functions) in (list, tuple) and all(type(f) in (str, int, Function) for f in self.functions):
             for f in self.functions:
-                if type(f) is str:
-                    func.append(self.cfg.kb.functions.function(name=f))
+                func.extend(self._get_function(f))
 
-                elif type(f) is int:
-                    func.append(self.cfg.kb.functions.function(addr=f))
+        elif self.functions is not None:
+            raise AngrExplorationTechniqueError("Invalid type for 'functions' parameter!")
 
-                elif type(f) is Function:
-                    func.append(f)
+        func = None if not func else func
 
-                else:
-                    raise TypeError("What type of function is it?")
-        elif self.functions is None:
-            func = None
-
-        else:
-            raise TypeError("What type of function is it?")
-
-        if not self.loops or func is not None:
+        if not self.loops:
             loop_finder = self.project.analyses.LoopFinder(kb=self.cfg.kb, normalize=True, functions=func)
 
             for loop in loop_finder.loops:
@@ -94,7 +80,7 @@ class LoopSeer(ExplorationTechnique):
                     self.loops[entry.addr] = loop
 
     def step(self, simgr, stash='active', **kwargs):
-        kwargs['successor_func'] = self.normalized_step
+        kwargs['successor_func'] = self._normalized_step
 
         for state in simgr.stashes[stash]:
             # Processing a currently running loop
@@ -103,37 +89,40 @@ class LoopSeer(ExplorationTechnique):
                 header = loop.entry.addr
 
                 if state.addr == header:
-                    state.loop_data.trip_counts[state.addr][-1] += 1
+                    continue_addrs = [e[0].addr for e in loop.continue_edges]
+                    if state.history.addr in continue_addrs:
+                        counts = state.loop_data.back_edge_trip_counts[header][-1] if not self.use_header else \
+                                 state.loop_data.header_trip_counts[header][-1]
+                        state.loop_data.back_edge_trip_counts[state.addr][-1] += 1
+                    #   print loop
+                    #   print hex(state.addr)
+                    #   print hex(state.history.addr)
+                    state.loop_data.header_trip_counts[state.addr][-1] += 1
 
                 elif state.addr in state.loop_data.current_loop[-1][1]:
-                    # This is for unoptimized while/for loops.
-                    #
-                    # 0x10812: movs r3, #0          -> this block dominates the loop
-                    # 0x10814: str  r3, [r7, #20]
-                    # 0x10816: b    0x10868
-                    # 0x10818: movs r3, #0          -> the real loop body starts here
-                    # ...
-                    # 0x10868: ldr  r3, [r7, #20]   -> the loop header is executed the first time without executing the loop body
-                    # 0x1086a: cmp  r3, #3
-                    # 0x1086c: ble  0x10818
-
-                    back_edge_src = loop.continue_edges[0][0].addr
-                    back_edge_dst = loop.continue_edges[0][1].addr
-                    block = self.project.factory.block(back_edge_src)
-                    if back_edge_src != back_edge_dst and back_edge_dst in block.instruction_addrs:
-                        state.loop_data.trip_counts[header][-1] -= 1
-
+                    for src, dst in loop.continue_edges:
+                        src_block = self.project.factory.block(src.addr)
+                        dst_block = self.project.factory.block(dst.addr)
+                        src_insns = len(src_block.instruction_addrs)
+                        dst_insns = len(dst_block.instruction_addrs)
+                    #   print hex(header), 'src'
+                    #   src_block.pp()
+                    #   print 'dst'
+                    #   dst_block.pp()
                     state.loop_data.current_loop.pop()
 
                 if self.bound is not None:
-                    if state.loop_data.trip_counts[header][-1] > self.bound:
+                    counts = state.loop_data.back_edge_trip_counts[header][-1] if not self.use_header else \
+                             state.loop_data.header_trip_counts[header][-1]
+                    if counts > self.bound:
                         if self.bound_reached is not None:
                             simgr = self.bound_reached(simgr)
                         else:
                             simgr.stashes[stash].remove(state)
                             simgr.stashes[self.discard_stash].append(state)
 
-                l.debug("%s trip counts %s", state, state.loop_data.trip_counts)
+                l.debug("%s back edge based trip counts %s", state, state.loop_data.back_edge_trip_counts)
+                l.debug("%s header based trip counts %s", state, state.loop_data.header_trip_counts)
 
             # Loop entry detected. This test is put here because in case of
             # nested loops, we want to handle the outer loop before proceeding
@@ -143,13 +132,30 @@ class LoopSeer(ExplorationTechnique):
                 header = loop.entry.addr
                 exits = [e[1].addr for e in loop.break_edges]
 
-                state.loop_data.trip_counts[header].append(0)
+                state.loop_data.back_edge_trip_counts[header].append(0)
+                state.loop_data.header_trip_counts[header].append(0)
                 state.loop_data.current_loop.append((loop, exits))
 
         simgr.step(stash=stash, **kwargs)
 
         return simgr
 
-    def normalized_step(self, state):
+    def _normalized_step(self, state):
         node = self.cfg.get_any_node(state.addr)
         return state.step(num_inst=len(node.instruction_addrs) if node is not None else None)
+
+    def _get_function(self, func):
+        if type(func) is str:
+            f = self.cfg.kb.functions.function(name=func)
+            if f is None:
+                l.warning("Function '%s' doesn't exist in the CFG. Skipping...", func)
+
+        elif type(func) is int:
+            f = self.cfg.kb.functions.function(addr=func)
+            if f is None:
+                l.warning("Function at 0x%x doesn't exist in the CFG. Skipping...", func)
+
+        elif type(func) is Function:
+            f = func
+
+        return [] if f is None else [f]
