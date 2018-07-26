0abd67e3f14dc3699178fbcfea945272421971f9
mor619dx <mor619dx@gmail.com>
2018-07-26 19:58:10 +0300

    Fixed vsnprintf and snprintf able to get buffer size 0 (#1154)
    * Update vsnprintf.py
    
    * Update snprintf.py

--- angr/procedures/libc/vsnprintf.py
+++ angr/procedures/libc/vsnprintf.py
@@ -10,7 +10,10 @@ class vsnprintf(angr.SimProcedure):
     def run(self, str_ptr, size, fmt, ap): #pylint:disable=unused-argument
         # This function returns
         # Add another exit to the retn_addr that is at the top of the stack now
-
+        
+        if self.state.se.eval(size) == 0:
+            return size
+        
         self.state.memory.store(str_ptr, "\x00")
 
         return size - 1
