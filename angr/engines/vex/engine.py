85f4cc145667121154ef585db285215f20a382a4
Eric Gustafson <edg@cs.ucsb.edu>
2018-08-18 17:01:37 -0700

    Don't execute junk IRSBs with no actual end due to nodecodes

--- angr/engines/vex/engine.py
+++ angr/engines/vex/engine.py
@@ -223,7 +223,7 @@ class SimEngineVEX(SimEngine):
 
         # if we've told the block to truncate before it ends, it will definitely have a default
         # exit barring errors
-        has_default_exit = last_stmt == 'default' or num_stmts <= last_stmt
+        has_default_exit = (last_stmt == 'default' or num_stmts <= last_stmt) and irsb.next is not None
 
         # This option makes us only execute the last four instructions
         if o.SUPER_FASTPATH in state.options:
