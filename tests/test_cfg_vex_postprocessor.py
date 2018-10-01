5140f1e6c9068603c3719293bc0e651714337981
Fish <fish@cs.ucsb.edu>
2018-10-01 12:05:40 -0700

    Fix the test case for issue #1172.

--- tests/test_cfg_vex_postprocessor.py
+++ tests/test_cfg_vex_postprocessor.py
@@ -1,18 +1,18 @@
 
 import os
 
-import nose.tools
-
 import angr
+
+test_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'binaries', 'tests'))
 
 
 def test_issue_1172():
 
-    path = os.path.join("..", "..", "binaries", "tests", "x86_64", "cfg_issue_1172")
+    path = os.path.join(test_location, "x86_64", "cfg_issue_1172")
     p = angr.Project(path, auto_load_libs=False)
 
     # it should not crash
-    cfg = p.analyses.CFG()
+    _ = p.analyses.CFG()
 
 
 if __name__ == "__main__":
