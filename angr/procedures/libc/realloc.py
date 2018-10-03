57e296d03a3da4d8d23a669aea46d05d1aaf55dd
Audrey Dutcher <audrey@rhelmot.io>
2018-10-02 17:39:10 -0700

    Fix several simprocedures; begin paradigm of linux importing procedures from posix

--- angr/procedures/libc/realloc.py
+++ angr/procedures/libc/realloc.py
@@ -12,15 +12,14 @@ class realloc(angr.SimProcedure):
     #pylint:disable=arguments-differ
 
     def run(self, ptr, size):
-        self.state.add_constraints(size <= self.state.libc.max_variable_size)
-        size_int = self.state.solver.max_int(size)
-
-        l.debug("Size: %d", size_int)
-        self.state.add_constraints(size_int == size)
-
-        self.argument_types = { 0: self.ty_ptr(SimTypeTop()),
-                                1: SimTypeLength(self.state.arch) }
-        self.return_type = self.ty_ptr(SimTypeTop(size))
+        if size.symbolic:
+            try:
+                size_int = self.state.solver.max(size, extra_constraints=(size < self.state.libc.max_variable_size,))
+            except angr.errors.SimSolverError:
+                size_int = self.state.solver.min(size)
+            self.state.add_constraints(size_int == size)
+        else:
+            size_int = self.state.solver.eval(size)
 
         addr = self.state.libc.heap_location
 
