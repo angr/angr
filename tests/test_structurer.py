78bd203fafef2af38aaa24ab3a0e194a02d1d9d5
Eric Gustafson <edg@cs.ucsb.edu>
2018-10-17 22:08:32 -0700

    Smoke test using real ARM firmware! No skipping ARM day.

--- tests/test_structurer.py
+++ tests/test_structurer.py
@@ -25,6 +25,23 @@ def test_smoketest():
     st = p.analyses.Structurer(ri.region)  # pylint:disable=unused-variable
 
 
+def test_smoketest_cm3_firmware(binname):
+
+    p = angr.Project(os.path.join(test_location, 'armel', 'i2c_master_read-nucleol152re.elf'), auto_load_libs=False)
+    cfg = p.analyses.CFG(normalize=True)
+
+    main_func = cfg.kb.functions['main']
+
+    # convert function blocks to AIL blocks
+    clinic = p.analyses.Clinic(main_func)
+
+    # recover regions
+    ri = p.analyses.RegionIdentifier(main_func, graph=clinic.graph)
+
+    # structure it
+    st = p.analyses.Structurer(ri.region)  # pylint:disable=unused-variable
+
+
 def test_simple():
 
     p = angr.Project(os.path.join(test_location, 'x86_64', 'all'), auto_load_libs=False)
