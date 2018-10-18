bf9340932bd51810883309a7044b982d03a528de
Eric Gustafson <edg@cs.ucsb.edu>
2018-10-17 21:35:13 -0700

    If you have no CC you have no SP adjustment. Not because CCA fails, but there's not always even a CC in the first place

--- angr/analyses/variable_recovery/variable_recovery_fast.py
+++ angr/analyses/variable_recovery/variable_recovery_fast.py
@@ -50,7 +50,8 @@ class ProcessorState(object):
         self.sp_adjustment = max(self.sp_adjustment, other.sp_adjustment)
         if other.bp_as_base is True:
             self.bp_as_base = True
-        self.bp = max(self.bp, other.bp)
+        if other.bp is not None:
+            self.bp = max(self.bp, other.bp)
         return self
 
     def __eq__(self, other):
@@ -627,9 +628,10 @@ class VariableRecoveryFast(ForwardAnalysis, Analysis):  #pylint:disable=abstract
         # readjusting sp at the end for blocks that end in a call
         if block.addr in self._node_to_cc:
             cc = self._node_to_cc[block.addr]
-            state.processor_state.sp_adjustment += cc.sp_delta
-            state.processor_state.sp_adjusted = True
-            l.debug('Adjusting stack pointer at end of block %#x with offset %+#x.', block.addr, state.processor_state.sp_adjustment)
+            if cc is not None:
+                state.processor_state.sp_adjustment += cc.sp_delta
+                state.processor_state.sp_adjusted = True
+                l.debug('Adjusting stack pointer at end of block %#x with offset %+#x.', block.addr, state.processor_state.sp_adjustment)
 
     def _make_phi_node(self, block_addr, *variables):
 
