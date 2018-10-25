80e349338eb2aeeaaf3c145f1b89debe1079c81f
Fish <fish@cs.ucsb.edu>
2018-10-25 12:01:49 -0700

    Call set_state() on socket pairs.

--- angr/state_plugins/posix.py
+++ angr/state_plugins/posix.py
@@ -208,6 +208,14 @@ class SimSystemPosix(SimStatePlugin):
         self.stdout.set_state(state)
         self.stderr.set_state(state)
 
+        if self.socket_queue:
+            for sock_pair in self.socket_queue:
+                if not sock_pair:
+                    continue
+                sock_pair[0].set_state(state)
+                sock_pair[1].set_state(state)
+
+
     def _pick_fd(self):
         for fd in range(0, 8192):
             if fd not in self.fd:
