337906c362d0d5ac577a06fd94b0dd55c3e9da93
Yan <yans@yancomm.net>
2018-10-30 03:28:56 -0700

    allow setting brk for good measure

--- tests/test_state_customization.py
+++ tests/test_state_customization.py
@@ -20,5 +20,17 @@ def test_stack_end():
 			s = p.factory.full_init_state(stack_end=n)
 			assert s.solver.eval_upto(s.regs.sp + offset == n, 2) == [ True ]
 
+def test_brk():
+	for fn in glob.glob(os.path.join(test_location, "*", "fauxware")):
+		p = angr.Project(fn, auto_load_libs=False)
+
+		# different stack ends
+		for n in [ 0x1337000, 0xbaaaaa00, 0x100, 0xffffff00, 0x13371337000, 0xbaaaaaaa0000, 0xffffffffffffff00 ]:
+			if n.bit_length() > p.arch.bits:
+				continue
+			s = p.factory.full_init_state(brk=n)
+			assert s.solver.eval_upto(s.posix.brk == n, 2) == [ True ]
+
 if __name__ == '__main__':
 	test_stack_end()
+	test_brk()
