9c43a3c22eaec471fdb3e7be97accc2bb7279771
Fish <fish@cs.ucsb.edu>
2018-11-02 10:18:00 -0700

    Add a simple implementation for access().

---
+++ angr/procedures/libc/access.py
@@ -0,0 +1,15 @@
+import angr
+
+######################################
+# access
+######################################
+
+
+class access(angr.SimProcedure):
+    #pylint:disable=arguments-differ
+
+    def run(self, path, mode):
+
+        ret = self.state.solver.BVS('access', self.arch.bits)
+        self.state.add_constraints(self.state.solver.Or(ret == 0, ret == -1))
+        return ret
