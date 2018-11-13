bdf9319abe64bfebfdab2b38b726400e17bf773c
LR.X <xiaolr2003@foxmail.com>
2018-11-13 10:40:39 -0500

    Use Exception

--- angr/analyses/cfg/cfg_base.py
+++ angr/analyses/cfg/cfg_base.py
@@ -1710,7 +1710,7 @@ class CFGBase(Analysis):
                 try:
                     if block.vex.jumpkind not in ('Ijk_Boring', 'Ijk_InvalICache'):
                         continue
-                except:
+                except SimEngineError:
                     continue
                 # Skip alignment blocks
                 if self._is_noop_block(self.project.arch, block):
