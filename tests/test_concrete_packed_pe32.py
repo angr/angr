fabebdc729b9ecee69c788a32e3f7f8b2f7ccbed
degrigis <degrigis@gmail.com>
2018-11-14 15:45:55 -0800

    removed also the code to test the thing manually
    Signed-off-by: degrigis <degrigis@gmail.com>

--- tests/test_concrete_packed_pe32.py
+++ tests/test_concrete_packed_pe32.py
@@ -130,15 +130,3 @@ def solv_concrete_engine_windows_x86(p, entry_state):
 
     print("[4]Malware execution ends, the configuration value is: " + hex(new_symbolic_state.solver.eval(arg0,
                                                                                                          cast_to=int)))
-
-
-# MANUAL TEST
-"""
-setup_x86()
-print("test_concrete_engine_windows_x86_no_simprocedures")
-avatar_gdb = AvatarGDBConcreteTarget(avatar2.archs.x86.X86, GDB_SERVER_IP, GDB_SERVER_PORT)
-p = angr.Project(binary_x86, concrete_target=avatar_gdb, use_sim_procedures=False,
-                 page_size=0x1000)
-entry_state = p.factory.entry_state()
-solv_concrete_engine_windows_x86(p, entry_state)
-"""
