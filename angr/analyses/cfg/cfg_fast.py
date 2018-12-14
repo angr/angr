d513e52744287309e69aff2d5bcadc5dc864d9d6
Fish <fish@cs.ucsb.edu>
2018-12-14 12:41:59 -0700

    CFGFast: Skip jobs that are outside predefined regions.

--- angr/analyses/cfg/cfg_fast.py
+++ angr/analyses/cfg/cfg_fast.py
@@ -16,7 +16,7 @@ from .memory_data import MemoryData
 from .cfg_arch_options import CFGArchOptions
 from .cfg_base import CFGBase
 from .cfg_node import CFGNode
-from ..forward_analysis import ForwardAnalysis
+from ..forward_analysis import ForwardAnalysis, AngrSkipJobNotice
 from ... import sim_options as o
 from ...errors import (AngrCFGError, SimEngineError, SimMemoryError, SimTranslationError, SimValueError,
                        AngrUnsupportedSyscallError
@@ -1396,6 +1396,10 @@ class CFGFast(ForwardAnalysis, CFGBase):    # pylint: disable=abstract-method
 
         # a new entry is picked. Deregister it
         self._deregister_analysis_job(job.func_addr, job)
+
+        if not self._inside_regions(job.addr):
+            # it's outside permitted regions. skip.
+            raise AngrSkipJobNotice()
 
         # Do not calculate progress if the user doesn't care about the progress at all
         if self._show_progressbar or self._progress_callback:
