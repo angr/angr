4372233609a95dd7118986f277360449c95d478f
Audrey Dutcher <audrey@rhelmot.io>
2018-12-18 20:58:57 -0800

    Don't return explicitly sized values for stat. closes #1004

--- angr/procedures/linux_kernel/fstat.py
+++ angr/procedures/linux_kernel/fstat.py
@@ -6,7 +6,7 @@ class fstat(angr.SimProcedure):
         stat = self.state.posix.fstat(fd)
         # TODO: make arch-neutral
         self._store_amd64(stat_buf, stat)
-        return self.state.solver.BVV(0, 64) # success
+        return 0
 
     def _store_amd64(self, stat_buf, stat):
         store = lambda offset, val: self.state.memory.store(stat_buf + offset, val, endness='Iend_LE')
