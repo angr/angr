094dc311a250096daf0ea10362f82ffd4fe54cfc
Andrea Continella <andreacontinella@gmail.com>
2018-12-19 17:31:30 -0800

    SimSootExpr_Phi considers pred block

--- angr/engines/soot/expressions/phi.py
+++ angr/engines/soot/expressions/phi.py
@@ -8,18 +8,6 @@ l = logging.getLogger('angr.engines.soot.expressions.phi')
 
 class SimSootExpr_Phi(SimSootExpr):
     def _execute(self):
-        locals_option = [self._translate_value(v) for v in self.expr.values]
-        values = []
-        for local in locals_option:
-            value = self.state.memory.load(local, none_if_missing=True)
-            if value is not None:
-                values.append(value)
-
-        if len(values) == 0:
-            l.warning("Couldn't find a value of Phi expression in memory.")
-            return
-
-        if len(values) > 2:
-            l.warning("Found multiple values of Phi expression in memory.")
-
-        self.expr = values[-1]
+        local = [self._translate_value(v) for v, idx in self.expr.values if idx == self.state.scratch.source.block_idx][0]
+        value = self.state.memory.load(local, none_if_missing=True)
+        self.expr = value
