80d8400085f4fbb972d8e6ba06918b3ae8016442
Fish <fish@cs.ucsb.edu>
2019-01-09 09:07:41 -0700

    UserHook: Fix the issue of skipping adjacent hooks.
    If a hook follows a previous one immediately, it would be skipped, which
    was an incorrect behavior. This commit fixes the issue and adds two test
    cases to make sure hooks work as expected.
    
    Thanks @graux-pierre for reporting this issue.

--- angr/procedures/stubs/UserHook.py
+++ angr/procedures/stubs/UserHook.py
@@ -7,7 +7,8 @@ class UserHook(angr.SimProcedure):
     def run(self, user_func=None, length=None):
         result = user_func(self.state)
         if result is None:
-            self.successors.add_successor(self.state, self.state.addr+length, self.state.solver.true, 'Ijk_NoHook')
+            jumpkind = 'Ijk_NoHook' if length == 0 else 'Ijk_Boring'
+            self.successors.add_successor(self.state, self.state.addr+length, self.state.solver.true, jumpkind)
         else:
             for state in result:
                 self.successors.add_successor(state, state.addr, state.scratch.guard, state.history.jumpkind)
