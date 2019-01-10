9cb56923ba57fec79a8efd4919315910f82f5da1
Andrea Continella <andreacontinella@gmail.com>
2019-01-09 18:55:37 -0800

    Add heuristic when Phi expr fails

--- angr/engines/soot/expressions/phi.py
+++ angr/engines/soot/expressions/phi.py
@@ -8,6 +8,16 @@ l = logging.getLogger('angr.engines.soot.expressions.phi')
 
 class SimSootExpr_Phi(SimSootExpr):
     def _execute(self):
-        local = [self._translate_value(v) for v, idx in self.expr.values if idx == self.state.scratch.source.block_idx][0]
-        value = self.state.memory.load(local, none_if_missing=True)
-        self.expr = value
+        try:
+            local = [self._translate_value(v) for v, idx in self.expr.values if idx == self.state.scratch.source.block_idx][0]
+            value = self.state.memory.load(local, none_if_missing=True)
+            self.expr = value
+        except IndexError:
+            # TODO is there a better way to do this?
+            local_options = [self._translate_value(v) for v, idx in self.expr.values[::-1]]
+            for local in local_options:
+                value = self.state.memory.load(local, none_if_missing=True)
+                if value is not None:
+                    self.expr = value
+                    return
+
