d037693fcb4bf4e32985559fa68443ea0491690c
Fish <fish@cs.ucsb.edu>
2019-01-12 10:26:10 -0700

    Correct the name of ld-linux.so on x86_64. Close issue #1398.

--- angr/procedures/definitions/linux_loader.py
+++ angr/procedures/definitions/linux_loader.py
@@ -2,5 +2,5 @@ from . import SimLibrary
 from .. import SIM_PROCEDURES as P
 
 lib = SimLibrary()
-lib.set_library_names('ld.so', 'ld-linux.so', 'ld.so.2', 'ld-linux.so.2', 'ld-linux-x86_64.so.2')
+lib.set_library_names('ld.so', 'ld-linux.so', 'ld.so.2', 'ld-linux.so.2', 'ld-linux-x86-64.so.2')
 lib.add_all_from_dict(P['linux_loader'])
