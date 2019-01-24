5b8808c87ffaac5af8dd636a3056eb0db69d12bd
Sergiusz Bazanski <q3k@q3k.org>
2019-01-24 02:59:21 +0100

    Angr: add CPython licensing information

--- angr/utils/weakref.py
+++ angr/utils/weakref.py
@@ -9,6 +9,11 @@ for https://bugs.python.org/issue35615 . It can be removed, and callers can
 revert to using the distribution library, once this fix lands in CPython stable
 releases.
 
+Copyright (c) 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
+2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019 Python Software Foundation.
+
+See angr/utils/weakref.LICENSE for licensing conditions of this file.
+
 """
 
 # Naming convention: Variables named "wr" are weak reference objects;
