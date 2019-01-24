f28b5686cda94b39918e038f171b77284ec67841
Fish <fish@cs.ucsb.edu>
2019-01-24 00:05:31 -0500

    Codegen: Do not crash if stmt.args is None.

--- angr/analyses/decompiler/structured_codegen.py
+++ angr/analyses/decompiler/structured_codegen.py
@@ -989,7 +989,7 @@ class StructuredCodeGenerator(Analysis):
             target_func = None
 
         args = [ ]
-        if target_func is not None and target_func.prototype is not None:
+        if target_func is not None and target_func.prototype is not None and stmt.args is not None:
             for i, arg in enumerate(stmt.args):
                 if i < len(target_func.prototype.args):
                     type_ = target_func.prototype.args[i].with_arch(self.project.arch)
