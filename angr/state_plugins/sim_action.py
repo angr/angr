3b0b643cc30fbf272a9ce909001251e9536511ad
bannsec <7632864+bannsec@users.noreply.github.com>
2019-01-31 14:54:20 -0500

    Adding incrementing ID to SimAction (#1421)
    * Adding incrementing ID to SimAction
    
    * Starting count at 0

--- angr/state_plugins/sim_action.py
+++ angr/state_plugins/sim_action.py
@@ -17,6 +17,7 @@ class SimAction(SimEvent):
     TMP = 'tmp'
     REG = 'reg'
     MEM = 'mem'
+    _MAX_ACTION_ID = -1
 
     def __init__(self, state, region_type):
         """
@@ -26,6 +27,8 @@ class SimAction(SimEvent):
         """
         SimEvent.__init__(self, state, 'action')
         self.type = region_type
+        SimAction._MAX_ACTION_ID += 1
+        self._action_id = SimAction._MAX_ACTION_ID
 
     def __repr__(self):
         if self.sim_procedure is not None:
