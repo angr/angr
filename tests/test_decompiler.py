09692739b420e97595cbeeb39e72b1781423ba78
Fish <fish@cs.ucsb.edu>
2019-01-31 01:58:58 -0700

    Decompiler: Add two more test cases.

--- tests/test_decompiler.py
+++ tests/test_decompiler.py
@@ -22,6 +22,37 @@ def test_decompiling_all_x86_64():
             print("Failed to decompile function %s." % repr(f))
 
 
+def test_decompiling_babypwn_i386():
+    bin_path = os.path.join(test_location, "i386", "decompiler", "codegate2017_babypwn")
+    p = angr.Project(bin_path, auto_load_libs=False)
+
+    cfg = p.analyses.CFG(normalize=True, collect_data_references=True)
+    for f in cfg.functions.values():
+        if f.is_simprocedure:
+            print("Skipping SimProcedure %s." % repr(f))
+            continue
+        if f.addr not in (0x8048a71, 0x8048c6b):
+            continue
+        dec = p.analyses.Decompiler(f, cfg=cfg)
+        if dec.codegen is not None:
+            print(dec.codegen.text)
+        else:
+            print("Failed to decompile function %s." % repr(f))
+
+
+def test_decompiling_loop_x86_64():
+    bin_path = os.path.join(test_location, "x86_64", "decompiler", "loop")
+    p = angr.Project(bin_path, auto_load_libs=False)
+
+    cfg = p.analyses.CFG(normalize=True, collect_data_references=True)
+    f = cfg.functions['loop']
+    dec = p.analyses.Decompiler(f, cfg=cfg)
+    if dec.codegen is not None:
+        print(dec.codegen.text)
+    else:
+        print("Failed to decompile function %s." % repr(f))
+
+
 def test_decompiling_all_i386():
     bin_path = os.path.join(test_location, "i386", "all")
     p = angr.Project(bin_path, auto_load_libs=False)
