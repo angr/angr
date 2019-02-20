96fb2548760c782d5c8e70352576bad3cffd79d0
bannsec <whootandahalf@gmail.com>
2019-02-20 14:19:53 +0000

    Verbose debug for symbolic reg too

--- angr/state_plugins/symbolic_memory.py
+++ angr/state_plugins/symbolic_memory.py
@@ -471,7 +471,7 @@ class SimSymbolicMemory(SimMemory): #pylint:disable=abstract-method
                     l.warning("Filling memory at %#x with %d unconstrained bytes referenced from %#x (%s)", addr, num_bytes, self.state.solver.eval(self.state.ip), self.state.project.loader.describe_addr(self.state.solver.eval(self.state.ip)))
                 else:
                     reg_str = self.state.arch.translate_register_name(addr, size=num_bytes)
-                    l.warning("Filling register %s with %d unconstrained bytes", reg_str, num_bytes)
+                    l.warning("Filling register %s with %d unconstrained bytes referenced from %#x (%s)", reg_str, num_bytes, self.state.solver.eval(self.state.ip), self.state.project.loader.describe_addr(self.state.solver.eval(self.state.ip)))
 
         if self.category == 'reg' and self.state.arch.register_endness == 'Iend_LE':
             all_missing = [ a.reversed for a in all_missing ]
