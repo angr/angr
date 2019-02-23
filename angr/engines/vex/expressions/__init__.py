5a762e7b64d39618c00f0917f9d437b25b097d20
Fish <fish@cs.ucsb.edu>
2019-02-23 06:47:49 -0800

    Make translate_expr() less expensive.

--- angr/engines/vex/expressions/__init__.py
+++ angr/engines/vex/expressions/__init__.py
@@ -1,15 +1,12 @@
 def translate_expr(expr, state):
-    expr_name = 'SimIRExpr_' + type(expr).__name__.split('IRExpr')[-1].split('.')[-1]
-    g = globals()
+    expr_class = EXPR_CLASSES.get(type(expr), None)
+    if expr_class is None:
+        if o.BYPASS_UNSUPPORTED_IREXPR not in state.options:
+            raise UnsupportedIRExprError("Unsupported expression type %s" % (type(expr)))
+        else:
+            expr_class = SimIRExpr_Unsupported
 
-    if expr_name not in g and o.BYPASS_UNSUPPORTED_IREXPR not in state.options:
-        raise UnsupportedIRExprError("Unsupported expression type %s" % (type(expr)))
-    elif expr_name not in g:
-        expr_class = SimIRExpr_Unsupported
-    else:
-        expr_class = g[expr_name]
-
-    l.debug("Processing expression %s", expr_name)
+    # l.debug("Processing expression %s", expr_name)
     e = expr_class(expr, state)
     e.process()
     return e
@@ -19,6 +16,8 @@ from .... import sim_options as o
 
 import logging
 l = logging.getLogger(name=__name__)
+
+import pyvex
 
 from .base import SimIRExpr
 
@@ -33,3 +32,19 @@ from .ccall import SimIRExpr_CCall
 from .ite import SimIRExpr_ITE
 from .geti import SimIRExpr_GetI
 from .unsupported import SimIRExpr_Unsupported
+
+EXPR_CLASSES = {
+    pyvex.expr.GSPTR: SimIRExpr_GSPTR,
+    pyvex.expr.VECRET: SimIRExpr_VECRET,
+    pyvex.expr.Const: SimIRExpr_Const,
+    pyvex.expr.RdTmp: SimIRExpr_RdTmp,
+    pyvex.expr.Get: SimIRExpr_Get,
+    pyvex.expr.Load: SimIRExpr_Load,
+    pyvex.expr.Unop: SimIRExpr_Unop,
+    pyvex.expr.Binop: SimIRExpr_Binop,
+    pyvex.expr.Triop: SimIRExpr_Triop,
+    pyvex.expr.Qop: SimIRExpr_Qop,
+    pyvex.expr.CCall: SimIRExpr_CCall,
+    pyvex.expr.ITE: SimIRExpr_ITE,
+    pyvex.expr.GetI:SimIRExpr_GetI,
+}
