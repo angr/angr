5b8643dfd5c6ada45bb57621093ad2e59501372b
Matt Borgerson <contact@mborgerson.com>
2019-04-09 12:22:04 -0700

    Handle multinodes

--- angr/engines/ail/nodes/multi.py
+++ angr/engines/ail/nodes/multi.py
@@ -1,10 +1,13 @@
 
 def handle_MultiNode(engine, state, node):
     """
+    Simply push the childnen of the multinode onto the execution stack.
 
     :param engine:
     :param state:
     :param node:
     :return:
     """
-    raise NotImplementedError()
+    for child in reversed(node.nodes):
+        state.ailexecstack.push(child)
+    return False
