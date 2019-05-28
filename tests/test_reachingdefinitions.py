18cb337bcc002946f6de12d0b95cf4576e4fcc5f
Pamplemousse <xav.maso@gmail.com>
2019-05-28 09:32:18 +0200

    Cleanup `test_reachingdefinitions.py`
    ... re-enable test. It does not do anything, but it does not fail.

--- tests/test_reachingdefinitions.py
+++ tests/test_reachingdefinitions.py
@@ -1,6 +1,5 @@
-
-import os
 import logging
+import os
 
 import angr
 
@@ -8,33 +7,28 @@ import angr
 l = logging.getLogger('test_reachingdefinitions')
 
 
-test_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                 '..', '..', 'binaries', 'tests'
-                                 )
-                    )
+TESTS_LOCATION = str(os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    '..', '..', 'binaries', 'tests'
+))
 
 
 def run_reaching_definition_analysis(project, func, groundtruth):  # pylint:disable=unused-argument
-
-    # Create a temporary KnowledgeBase instance
     tmp_kb = angr.KnowledgeBase(project)
 
-    _ = project.analyses.ReachingDefinitions(func, kb=tmp_kb)
+    _ = project.analyses.ReachingDefinitions(func, init_func=True, kb=tmp_kb)
 
 
-# def test_reaching_definition_analysis():
-def disabled_reaching_definition_analysis():
+def test_reaching_definition_analysis():
 
     # FIXME: Rewrite test cases for ReachingDefinitionAnalysis
 
-    binary_path = os.path.join(test_location, 'x86_64', 'all')
+    binary_path = os.path.join(TESTS_LOCATION, 'x86_64', 'all')
     project = angr.Project(binary_path, load_options={'auto_load_libs': False})
-    cfg = project.analyses.CFG()
+    cfg = project.analyses.CFGFast()
 
     groundtruth = {
-        'main': {
-
-        },
+        'main': {},
     }
 
     for func_name, truth in groundtruth.items():
@@ -42,13 +36,16 @@ def disabled_reaching_definition_analysis():
 
 
 def main():
-    g = globals()
-    for func_name, func in g.items():
-        if func_name.startswith('test_') and hasattr(func, '__call__'):
-            print(func_name)
-            for testfunc_and_args in func():
-                testfunc, args = testfunc_and_args[0], testfunc_and_args[1:]
-                testfunc(*args)
+    test_functions = list(filter(
+        lambda f: f[0].startswith('test_') and hasattr(f[1], '__call__'),
+        globals().items()
+    ))
+
+    for func_name, func in test_functions:
+        print(func_name)
+        for testfunc_and_args in func():
+            testfunc, args = testfunc_and_args[0], testfunc_and_args[1:]
+            testfunc(*args)
 
 
 if __name__ == '__main__':
