e81c75341e48ebd5854851e9e70ce5ed52714301
Pamplemousse <xav.maso@gmail.com>
2019-05-28 13:27:32 +0200

    Write tests for `reachingdefinitions.py`
    /!\ Just capture the current implementation behavior on a couple of
    binaries:
      * ran the binaries, then copy pasted the computed `observed_results`
      in files located under `tests/reachingdefinitions_results/`
      * wrote tests checking that a ReachingDefinition analysis will produce
      the exact output

--- tests/test_reachingdefinitions.py
+++ tests/test_reachingdefinitions.py
@@ -1,6 +1,8 @@
 import logging
 import os
 
+import nose
+
 import angr
 
 
@@ -13,26 +15,34 @@ TESTS_LOCATION = str(os.path.join(
 ))
 
 
-def run_reaching_definition_analysis(project, func, groundtruth):  # pylint:disable=unused-argument
+def run_reaching_definition_analysis(project, func, result_path):
     tmp_kb = angr.KnowledgeBase(project)
+    rd = project.analyses.ReachingDefinitions(func, init_func=True, kb=tmp_kb, observe_all=True)
 
-    _ = project.analyses.ReachingDefinitions(func, init_func=True, kb=tmp_kb)
+    result = "%s\n" % rd.observed_results
+
+    with open(result_path, 'r') as result_file:
+        expected_result = result_file.read()
+
+    nose.tools.assert_equals(result, expected_result)
 
 
 def test_reaching_definition_analysis():
+    def _binary_path(binary_name):
+        return os.path.join(TESTS_LOCATION, 'x86_64', binary_name)
+    def _result_path(binary_name):
+        return 'tests/reachingdefinitions_results/x86_64/' + binary_name + '.txt'
 
-    # FIXME: Rewrite test cases for ReachingDefinitionAnalysis
+    binaries_and_results = list(map(
+        lambda binary: (_binary_path(binary), _result_path(binary)),
+        ['all', 'fauxware', 'loop']
+    ))
 
-    binary_path = os.path.join(TESTS_LOCATION, 'x86_64', 'all')
-    project = angr.Project(binary_path, load_options={'auto_load_libs': False})
-    cfg = project.analyses.CFGFast()
+    for binary, result_path in binaries_and_results:
+        project = angr.Project(binary, load_options={'auto_load_libs': False})
+        cfg = project.analyses.CFGFast()
 
-    groundtruth = {
-        'main': {},
-    }
-
-    for func_name, truth in groundtruth.items():
-        yield run_reaching_definition_analysis, project, cfg.kb.functions[func_name], truth
+        yield run_reaching_definition_analysis, project, cfg.kb.functions['main'], result_path
 
 
 def main():
