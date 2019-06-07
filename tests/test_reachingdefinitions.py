c97439d1a7e13fb7fab34d42e7615e2d36a896e8
Pamplemousse <Pamplemousse@users.noreply.github.com>
2019-06-07 18:05:49 +0200

    Tests for Reachingdefinitions analysis (#1594)
    * Cleanup `test_reachingdefinitions.py`
    
    ... re-enable test. It does not do anything, but it does not fail.
    
    * Write tests for `reachingdefinitions.py`
    
    /!\ Just capture the current implementation behavior on a couple of
    binaries:
      * ran the binaries, then copy pasted the computed `observed_results`
      in files located under `tests/reachingdefinitions_results/`
      * wrote tests checking that a ReachingDefinition analysis will produce
      the exact output
    
    * Cleaner ReachingDefinitions test
    
      * do not rely on `__repr__`
      * updated the files containing results with pickled data instead of
      strings
      * pickled the current results using the following in the test file in
      place of the `load`
    ```
    with open(result_path, 'wb') as f:
        pickle.dump(result, f, protocol=pickle.HIGHEST_PROTOCOL)
    ```
    
    * Compute more thoughtfully the test files path
    
    * Update the extracted data
    
    As implementation surely have recently changed recently, results were
    outdated.
    
    * Order the results to make it more deterministic
    
    ... accross Python versions.

--- tests/test_reachingdefinitions.py
+++ tests/test_reachingdefinitions.py
@@ -1,6 +1,8 @@
-
-import os
 import logging
+import pickle
+import os
+
+import nose
 
 import angr
 
@@ -8,47 +10,68 @@ import angr
 l = logging.getLogger('test_reachingdefinitions')
 
 
-test_location = str(os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                                 '..', '..', 'binaries', 'tests'
-                                 )
-                    )
+TESTS_LOCATION = str(os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    '..', '..', 'binaries', 'tests'
+))
 
 
-def run_reaching_definition_analysis(project, func, groundtruth):  # pylint:disable=unused-argument
-
-    # Create a temporary KnowledgeBase instance
+def run_reaching_definition_analysis(project, func, result_path):
     tmp_kb = angr.KnowledgeBase(project)
+    rd = project.analyses.ReachingDefinitions(func, init_func=True, kb=tmp_kb, observe_all=True)
 
-    _ = project.analyses.ReachingDefinitions(func, kb=tmp_kb)
+    unsorted_result = map(
+        lambda x: {'key': x[0],\
+                   'register_definitions': x[1].register_definitions,\
+                   'stack_definitions': x[1].stack_definitions,\
+                   'memory_definitions': x[1].memory_definitions},
+        rd.observed_results.items()
+    )
+    result = list(sorted(
+        unsorted_result,
+        key=lambda x: x['key']
+    ))
+
+    with open(result_path, 'rb') as f:
+        expected_result = pickle.load(f)
+
+    nose.tools.assert_list_equal(result, expected_result)
 
 
-# def test_reaching_definition_analysis():
-def disabled_reaching_definition_analysis():
+def test_reaching_definition_analysis():
+    def _binary_path(binary_name):
+        return os.path.join(TESTS_LOCATION, 'x86_64', binary_name)
+    def _result_path(binary_name):
+        return os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            'reachingdefinitions_results',
+            'x86_64',
+            binary_name + '.pickle'
+        )
 
-    # FIXME: Rewrite test cases for ReachingDefinitionAnalysis
+    binaries_and_results = list(map(
+        lambda binary: (_binary_path(binary), _result_path(binary)),
+        ['all', 'fauxware', 'loop']
+    ))
 
-    binary_path = os.path.join(test_location, 'x86_64', 'all')
-    project = angr.Project(binary_path, load_options={'auto_load_libs': False})
-    cfg = project.analyses.CFG()
+    for binary, result_path in binaries_and_results:
+        project = angr.Project(binary, load_options={'auto_load_libs': False})
+        cfg = project.analyses.CFGFast()
 
-    groundtruth = {
-        'main': {
-
-        },
-    }
-
-    for func_name, truth in groundtruth.items():
-        yield run_reaching_definition_analysis, project, cfg.kb.functions[func_name], truth
+        yield run_reaching_definition_analysis, project, cfg.kb.functions['main'], result_path
 
 
 def main():
-    g = globals()
-    for func_name, func in g.items():
-        if func_name.startswith('test_') and hasattr(func, '__call__'):
-            print(func_name)
-            for testfunc_and_args in func():
-                testfunc, args = testfunc_and_args[0], testfunc_and_args[1:]
-                testfunc(*args)
+    test_functions = list(filter(
+        lambda f: f[0].startswith('test_') and hasattr(f[1], '__call__'),
+        globals().items()
+    ))
+
+    for func_name, func in test_functions:
+        print(func_name)
+        for testfunc_and_args in func():
+            testfunc, args = testfunc_and_args[0], testfunc_and_args[1:]
+            testfunc(*args)
 
 
 if __name__ == '__main__':
