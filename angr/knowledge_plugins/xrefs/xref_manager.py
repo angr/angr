a717da433df76125777ca33f8e636fb5ed944bff
Eric Gustafson <edg@cs.ucsb.edu>
2019-07-01 20:03:40 -0700

    Add ability to get xrefs to a region

--- angr/knowledge_plugins/xrefs/xref_manager.py
+++ angr/knowledge_plugins/xrefs/xref_manager.py
@@ -55,6 +55,15 @@ class XRefManager(KnowledgeBasePlugin, Serializable):
     def get_xrefs_by_dst(self, dst):
         return self.xrefs_by_dst.get(dst, set())
 
+    def get_xrefs_by_dst_region(self, start, end):
+        refs = []
+        for x, ref in self.xrefs_by_dst.items():
+            if not isinstance(x, int):
+                continue
+            if start <= x < end:
+                refs.append(ref)
+        return refs
+
     @classmethod
     def _get_cmsg(cls):
         return xrefs_pb2.XRefs()
