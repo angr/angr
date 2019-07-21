0f5321b2b494d0bfa4535b04bfdb12ac13af1c6a
Arvind <dnivra@users.noreply.github.com>
2019-07-21 13:09:06 -0700

    Moved StackBaseOffset expression handler to separate file to mirror VEX engine tree closely

--- angr/engines/ail/expressions/__init__.py
+++ angr/engines/ail/expressions/__init__.py
@@ -1,28 +1,6 @@
 import ailment
 
-def SimIRExpr_StackBaseOffset(engine, state, expr):
-	import ipdb; ipdb.set_trace()
-
-    # ix = engine.handle_expression(state, expr.ix)
-    # size_in_bits = get_type_size(expr.descr.elemTy)
-    # size = size_in_bits // state.arch.byte_width
-
-    # array_base = expr.descr.base
-    # array_index = (ix + expr.bias) % expr.descr.nElems
-    # offset = array_base + array_index*size
-
-    # # get it!
-    # result = state.registers.load(offset, size)
-
-    # if expr.descr.elemTy.startswith('Ity_F'):
-    #     result = result.raw_to_fp()
-
-    # # finish it and save the register references
-    # if o.TRACK_REGISTER_ACTIONS in state.options:
-    #     r = SimActionData(state, state.registers.id, SimActionData.READ, addr=offset, size=size_in_bits, data=result)
-    #     state.history.add_action(r)
-
-    # return result
+from .sbo import SimIRExpr_StackBaseOffset
 
 EXPR_CLASSES = {
 	# ailment.Expr.Atom:              SimIRExpr_Atom,
