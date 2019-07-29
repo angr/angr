62e6173f75214a42c70b4faef222d417de3fcfc3
Audrey Dutcher <audrey@rhelmot.io>
2019-07-29 13:36:36 -0700

    Expose the used endness for siminspect. see #1687

--- angr/state_plugins/inspect.py
+++ angr/state_plugins/inspect.py
@@ -36,24 +36,28 @@ inspect_attributes = {
     'mem_read_expr',
     'mem_read_length',
     'mem_read_condition',
+    'mem_read_endness',
 
     # mem_write
     'mem_write_address',
     'mem_write_expr',
     'mem_write_length',
     'mem_write_condition',
+    'mem_write_endness',
 
     # reg_read
     'reg_read_offset',
     'reg_read_expr',
     'reg_read_length',
     'reg_read_condition',
+    'reg_read_endness',
 
     # reg_write
     'reg_write_offset',
     'reg_write_expr',
     'reg_write_length',
     'reg_write_condition',
+    'reg_write_endness',
 
     # tmp_read
     'tmp_read_num',
