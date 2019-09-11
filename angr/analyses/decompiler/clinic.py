6e8260e162f09037b731958ce394da26ce7c7afa
Pamplemousse <Pamplemousse@users.noreply.github.com>
2019-09-11 01:08:14 -0700

    Refactor `ReachingDefinitionAnalysis` (#1715)
    * ReachingDefinitions: refactoring
    
      * don't need an extra private attribute
    
    * ReachingDefinitions: refactor `ReachingDefinitions`
    
    Constructor took `Block` *AND* `Function` as parameter, but could never
    analyse both.
    Use an enum-like kind-of logic: a single parameter, that can be either a
    `Block` or a `Function` and have separated behavior based on which it
    is.
    
    Motivation: cleaner code, and easier to add "structure" to analyse (such
    as slices...).

--- angr/analyses/decompiler/clinic.py
+++ angr/analyses/decompiler/clinic.py
@@ -186,7 +186,7 @@ class Clinic(Analysis):
         """
 
         # Computing reaching definitions
-        rd = self.project.analyses.ReachingDefinitions(func=self.function, func_graph=self.graph, observe_all=True)
+        rd = self.project.analyses.ReachingDefinitions(subject=self.function, func_graph=self.graph, observe_all=True)
 
         simp = self.project.analyses.AILSimplifier(self.function, func_graph=self.graph, reaching_definitions=rd)
 
@@ -218,7 +218,7 @@ class Clinic(Analysis):
         """
 
         # Computing reaching definitions
-        rd = self.project.analyses.ReachingDefinitions(func=self.function, func_graph=self.graph, observe_all=True)
+        rd = self.project.analyses.ReachingDefinitions(subject=self.function, func_graph=self.graph, observe_all=True)
 
         for key in self._blocks:
             block = self._blocks[key]
