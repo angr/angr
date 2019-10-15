826e0cec55aba9b6f94f10df9dd472e1275e4d8e
Pamplemousse <xav.maso@gmail.com>
2019-10-15 12:46:42 -0700

    ReachingDefinitionsAnalysis: extract data model
    300+ lines not related to the logic of the analysis could be moved into
    their own file.

--- angr/analyses/reaching_definitions/__init__.py
+++ angr/analyses/reaching_definitions/__init__.py
@@ -1,4 +1,4 @@
-
-from .reaching_definitions import ReachingDefinitionAnalysis, LiveDefinitions
+from .reaching_definitions import ReachingDefinitionAnalysis
+from .live_definitions import LiveDefinitions
 from .def_use import DefUseAnalysis, DefUseState
 from .constants import OP_AFTER, OP_BEFORE
