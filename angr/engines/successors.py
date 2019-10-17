7e7777c39549fae3b82fb57a70e4fdac7c68f1ad
Yan <yans@yancomm.net>
2019-10-16 17:36:35 -0700

    re-inserting Lukas-hated change

--- angr/engines/successors.py
+++ angr/engines/successors.py
@@ -208,7 +208,10 @@ class SimSuccessors:
 
             state._inspect('call', BP_AFTER)
         else:
-            while state.solver.is_true(state.regs._sp > state.callstack.top.stack_ptr):
+            while True:
+                cur_sp = state.solver.max(state.regs._sp) if state.has_plugin('symbolizer') else state.regs._sp
+                if not state.solver.is_true(cur_sp > state.callstack.top.stack_ptr):
+                    break
                 state._inspect('return', BP_BEFORE, function_address=state.callstack.top.func_addr)
                 state.callstack.pop()
                 state._inspect('return', BP_AFTER)
