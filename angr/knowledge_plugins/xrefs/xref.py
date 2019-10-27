378664acef860eee11cab6be6023d35d413c88be
Alexeyan <Alexeyan@users.noreply.github.com>
2019-10-27 09:31:18 +0100

    Fix format printing for non-address targets (#1765)
    * Fix format printing for non-address targets

--- angr/knowledge_plugins/xrefs/xref.py
+++ angr/knowledge_plugins/xrefs/xref.py
@@ -34,9 +34,9 @@ class XRef(Serializable):
 
     def __repr__(self):
         return "<XRef %s: %s->%s>" % (
-                XRefType.to_string(self.type),
+                self.type_string,
                 "%#x" % self.ins_addr if self.ins_addr is not None else "%#x[%d]" % (self.block_addr, self.stmt_idx),
-                "%#x" % (self.dst if self.dst is not None else self.memory_data.addr)
+                "%s" % self.dst if self.dst is not None else "%#x" % (self.memory_data.addr)
         )
 
     def __eq__(self, other):
