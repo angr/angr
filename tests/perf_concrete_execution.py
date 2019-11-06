c48383d3550095cd5578b018a7abb4e512d906b6
Audrey Dutcher <audrey@rhelmot.io>
2019-11-05 18:05:42 -0800

    supercharge perf_concrete_execution

--- tests/perf_concrete_execution.py
+++ tests/perf_concrete_execution.py
@@ -14,17 +14,21 @@ if hasattr(claripy, "set_debug"):
 
 test_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'binaries', 'tests')
 
+class SkinnyEngine(angr.engines.SimEngineFailure, angr.engines.SimEngineSyscall, angr.engines.HooksMixin, angr.engines.HeavyVEXMixin):
+    pass
+
 
 def test_tight_loop(arch):
     b = angr.Project(os.path.join(test_location, arch, "perf_tight_loops"), auto_load_libs=False)
     state = b.factory.full_init_state(plugins={'registers': angr.state_plugins.SimLightRegisters()},
                                       remove_options={angr.sim_options.COPY_STATES})
     simgr = b.factory.simgr(state)
+    engine = SkinnyEngine(b)
 
     # logging.getLogger('angr.sim_manager').setLevel(logging.INFO)
 
     start = time.time()
-    simgr.explore()
+    simgr.explore(engine=engine)
     elapsed = time.time() - start
 
     print("Elapsed %f sec" % elapsed)
