c9ce2748f9c0685e9de75b012b8bfd6bf28c861f
Lukas Dresel <Lukas-Dresel@users.noreply.github.com>
2019-12-17 14:16:24 -0800

    added simprocedures for  getu/gid functions

---
+++ angr/procedures/libc/getegid.py
@@ -0,0 +1,13 @@
+import angr
+from angr.sim_type import SimTypeInt
+
+######################################
+# getegid
+######################################
+
+
+class getegid(angr.SimProcedure):
+    # pylint: disable=arguments-differ
+    def run(self):
+        self.return_type = SimTypeInt(32, True)
+        return 1000
---
+++ angr/procedures/libc/getuid.py
@@ -0,0 +1,13 @@
+import angr
+from angr.sim_type import SimTypeInt
+
+######################################
+# getuid
+######################################
+
+
+class getuid(angr.SimProcedure):
+    # pylint: disable=arguments-differ
+    def run(self):
+        self.return_type = SimTypeInt(32, True)
+        return 1000
