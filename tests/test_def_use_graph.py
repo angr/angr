357675a14a4f6642395eda121ef321f7318e2070
Pamplemousse <xav.maso@gmail.com>
2019-12-30 10:45:15 -0700

    RDA: Improve `DefUseGraph`
      * `add_edge` should allow labels
      * add a `transitive_closure` method to get the subgraph containing
      all the paths to a definition (ancestors and edges, with their labels)

--- tests/test_def_use_graph.py
+++ tests/test_def_use_graph.py
@@ -49,10 +49,12 @@ def test_refuses_to_add_edge_between_non_definition_nodes():
 @mock.patch.object(networkx.DiGraph, 'add_edge')
 def test_delegate_add_edge_to_the_underlying_graph_object(digraph_add_edge_mock):
     use = (_a_mock_definition(), _a_mock_definition())
-    def_use_graph = DefUseGraph()
-    def_use_graph.add_edge(*use)
+    labels = { 'attribute1': 'value1', 'attribute2': 'value2' }
 
-    digraph_add_edge_mock.assert_called_once_with(*use)
+    def_use_graph = DefUseGraph()
+    def_use_graph.add_edge(*use, **labels)
+
+    digraph_add_edge_mock.assert_called_once_with(*use, **labels)
 
 
 def test_top_predecessors():
@@ -98,3 +100,44 @@ def test_top_predecessors_should_not_contain_duplicates():
     result = def_use_graph.top_predecessors(D)
 
     nose.tools.assert_list_equal(result, [A])
+
+
+def test_transitive_closure_of_a_node():
+    def_use_graph = DefUseGraph()
+
+    # A -> B, B -> D, C -> D
+    A = _a_mock_definition()
+    B = _a_mock_definition()
+    C = _a_mock_definition()
+    D = _a_mock_definition()
+    uses = [
+        (A, B),
+        (B, D),
+        (C, D),
+    ]
+
+    for use in uses:
+        def_use_graph.add_edge(*use)
+
+    result = def_use_graph.transitive_closure(D)
+    result_nodes = set(result.nodes)
+    result_edges = set(result.edges)
+
+    nose.tools.assert_set_equal(result_nodes, {D, B, C, A})
+    nose.tools.assert_set_equal(result_edges, {(B, D), (C, D), (A, B)})
+
+
+def test_transitive_closure_of_a_node_should_copy_labels_from_original_graph():
+    def_use_graph = DefUseGraph()
+
+    # A -> B
+    A = _a_mock_definition()
+    B = _a_mock_definition()
+    uses = [(A, B)]
+
+    for use in uses:
+        def_use_graph.add_edge(*use, label='some data')
+
+    result = def_use_graph.transitive_closure(B).get_edge_data(A, B)['label']
+
+    nose.tools.assert_equals(result, 'some data')
