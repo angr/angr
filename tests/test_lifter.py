a5ecd3f726e230c74c66f853fde4a45d20538a4b
Fish <fish@cs.ucsb.edu>
2020-01-07 11:59:06 -0700

    Add test cases for X86 and AMD64.

--- tests/test_lifter.py
+++ tests/test_lifter.py
@@ -17,5 +17,56 @@ def test_strict_block_ends_cbz():
     p.factory.default_engine.default_strict_block_end = True
     assert p.factory.block(1).instructions == 2
 
+
+def test_strict_block_ends_with_size_x86():
+    # cmovnz  cx, dx
+    # pop     eax
+    # jecxz   short loc_4010D7  ; the first block should end here
+    # sub     edi, 2
+    # loop    loc_4010c9
+    # nop
+    # nop
+
+
+    b = b"\x66\x0f\x45\xca\x58\xe3\x07\x83\xef\x02\xe2\xf4\x90\x90"
+    p = angr.load_shellcode(b, 'x86', load_address=0x4010c9)
+
+    # jecxz
+    assert p.factory.block(0x4010c9, size=len(b), strict_block_end=False).instructions == 7
+    assert p.factory.block(0x4010c9, strict_block_end=True).instructions == 3
+    assert p.factory.block(0x4010c9, size=len(b), strict_block_end=True).instructions == 3
+
+    # loop
+    assert p.factory.block(0x4010d0, strict_block_end=False).instructions == 4
+    assert p.factory.block(0x4010d0, strict_block_end=True).instructions == 2
+    assert p.factory.block(0x4010d0, size=7, strict_block_end=True).instructions == 2
+
+
+def test_strict_block_ends_with_size_amd64():
+    # cmovnz  cx, dx
+    # pop     rax
+    # jrcxz   short loc_4010D7  ; the first block should end here
+    # sub     edi, 2
+    # loop    loc_4010c9
+    # nop
+    # nop
+
+
+    b = b"\x66\x0f\x45\xca\x58\xe3\x07\x83\xef\x02\xe2\xf4\x90\x90"
+    p = angr.load_shellcode(b, 'amd64', load_address=0x4010c9)
+
+    # jrcxz
+    assert p.factory.block(0x4010c9, size=len(b), strict_block_end=False).instructions == 7
+    assert p.factory.block(0x4010c9, strict_block_end=True).instructions == 3
+    assert p.factory.block(0x4010c9, size=len(b), strict_block_end=True).instructions == 3
+
+    # loop
+    assert p.factory.block(0x4010d0, strict_block_end=False).instructions == 4
+    assert p.factory.block(0x4010d0, strict_block_end=True).instructions == 2
+    assert p.factory.block(0x4010d0, size=7, strict_block_end=True).instructions == 2
+
+
 if __name__ == '__main__':
     test_strict_block_ends_cbz()
+    test_strict_block_ends_with_size_x86()
+    test_strict_block_ends_with_size_amd64()
