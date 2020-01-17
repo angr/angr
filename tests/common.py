49ffba64ff8619c90e6dcf3c3f3a65060c6952e8
Eric Gustafson <edg@cs.ucsb.edu>
2020-01-16 17:52:15 -0800

    Add tests using real firmware (aka The Great ARM CFG Challenge) (#1668)
    * Tests for real firmware
    
    * Mark test as manual for now
    
    * Make executable

--- tests/common.py
+++ tests/common.py
@@ -9,6 +9,8 @@ except ImportError:
     tracer = None
 
 bin_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'binaries')
+bin_priv_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'binaries-private')
+
 if not os.path.isdir(bin_location):
     raise Exception("Can't find the angr/binaries repo for holding testcases. It should be cloned into the same folder as the rest of your angr modules.")
 
