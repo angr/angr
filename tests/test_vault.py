aa4e9e568dc178ba39ee4352c757ff97d0b138a8
Fish <fish@cs.ucsb.edu>
2020-01-31 11:02:36 -0700

    debug

--- tests/test_vault.py
+++ tests/test_vault.py
@@ -1,6 +1,8 @@
 import claripy
 import angr
 
+import nose.tools
+
 class A:
 	n = 0
 
@@ -16,7 +18,7 @@ def do_vault_identity(v_factory):
 	c.n = 2
 
 	aid = v.store(a)
-	assert len(v.keys()) == 1
+	nose.tools.assert_equal(len(v.keys()), 1, msg="Current keys: %s" % v.keys())
 	bid = v.store(b)
 	assert len(v.keys()) == 2
 	cid = v.store(c)
