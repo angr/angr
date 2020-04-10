19a8c647016c93e11b5912ba46247e4b7e966a84
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2020-04-10 16:08:39 -0700

    Add object reference equality check
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

--- angr/engines/soot/expressions/instancefieldref.py
+++ angr/engines/soot/expressions/instancefieldref.py
@@ -1,8 +1,15 @@
 
 from .base import SimSootExpr
+from ..values.local import SimSootValue_Local
+from .... import sim_options as options
 
 
 class SimSootExpr_InstanceFieldRef(SimSootExpr):
     def _execute(self):
         field_ref = self._translate_value(self.expr)
+        this_ref = self.state.memory.load(SimSootValue_Local.from_sootvalue(self.expr.base, self.state))
+
+        if options.JAVA_TRACK_ATTRIBUTES in self.state.options:
+            this_ref.attributes.add((field_ref.field_name, field_ref.type))
+
         self.expr = self.state.memory.load(field_ref, none_if_missing=True)
