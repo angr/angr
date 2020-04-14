64a3ae1e48d54683c356f0b9e897d06c361f6f5c
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2020-04-14 12:40:42 -0700

    Add cast operation on arrays
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

--- angr/engines/soot/expressions/cast.py
+++ angr/engines/soot/expressions/cast.py
@@ -5,6 +5,7 @@ from archinfo import ArchSoot
 
 from .base import SimSootExpr
 from ..values.thisref import SimSootValue_ThisRef
+from ..values.arrayref import SimSootValue_ArrayBaseRef
 
 l = logging.getLogger("angr.engines.soot.expressions.cast")
 
@@ -20,6 +21,10 @@ class SimSootExpr_Cast(SimSootExpr):
             self.expr = javavm_simos.cast_primitive(self.state,
                                                     value_uncasted,
                                                     to_type=self.expr.cast_type)
+        # We are casting an array
+        elif isinstance(value_uncasted, SimSootValue_ArrayBaseRef):
+            self.expr = SimSootValue_ArrayBaseRef(value_uncasted.heap_alloc_id, self.expr.cast_type.replace('[]', ''),
+                                                  value_uncasted.size)
         else:
             self.expr = SimSootValue_ThisRef(heap_alloc_id=value_uncasted.heap_alloc_id,
                                              type_=self.expr.cast_type)
