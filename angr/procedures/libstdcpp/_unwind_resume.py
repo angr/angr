a9f1da0deb3bdd18c17abd1b8d9110a21cc85040
Fish <fishw@asu.edu>
2020-04-26 02:50:23 -0700

    Add SimProcedure _Unwind_Resume.

---
+++ angr/procedures/libstdcpp/_unwind_resume.py
@@ -0,0 +1,13 @@
+import angr
+
+######################################
+# __unwind_resume
+######################################
+
+class _Unwind_Resume(angr.SimProcedure): #pylint:disable=redefined-builtin
+    #pylint:disable=arguments-differ
+
+    NO_RET = True
+
+    def run(self):
+        pass
