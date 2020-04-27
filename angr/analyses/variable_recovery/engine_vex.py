43dceacdf9b661b5fbc0c89af0d5a15da5ce031e
Fish <fishw@asu.edu>
2020-04-26 19:13:02 -0700

    VariableRecoveryFast: Add two more operations to the VEX engine.

--- angr/analyses/variable_recovery/engine_vex.py
+++ angr/analyses/variable_recovery/engine_vex.py
@@ -162,6 +162,24 @@ class SimEngineVRVEX(
             self.l.warning(e)
             return RichR(None)
 
+    def _handle_Or(self, expr):
+        arg0, arg1 = expr.args
+        r0 = self._expr(arg0)
+        r1 = self._expr(arg1)
+
+        try:
+            if isinstance(r0.data, int) and isinstance(r1.data, int):
+                # constants
+                return RichR(r0.data | r1.data)
+
+            r = None
+            if r0.data is not None and r1.data is not None:
+                r = r0.data | r1.data
+            return RichR(r)
+        except TypeError as e:
+            self.l.warning(e)
+            return RichR(None)
+
     def _handle_Mul(self, expr):
         arg0, arg1 = expr.args
         r0 = self._expr(arg0)
@@ -184,6 +202,31 @@ class SimEngineVRVEX(
             return RichR(None)
 
     def _handle_Shr(self, expr):
+        arg0, arg1 = expr.args
+        r0 = self._expr(arg0)
+        r1 = self._expr(arg1)
+
+        try:
+            if isinstance(r0.data, int) and isinstance(r1.data, int):
+                # constants
+                result_size = expr.result_size(self.tyenv)
+                return RichR(r0.data >> r1.data,
+                             typevar=typeconsts.int_type(result_size),
+                             type_constraints=None)
+
+            r = None
+            if r0.data is not None and r1.data is not None:
+                r = r0.data >> r1.data
+
+            return RichR(r,
+                         typevar=r0.typevar,
+                         )
+
+        except TypeError as e:
+            self.l.warning(e)
+            return RichR(None)
+
+    def _handle_Sar(self, expr):
         arg0, arg1 = expr.args
         r0 = self._expr(arg0)
         r1 = self._expr(arg1)
