f2ee902c437ab42b563a25904ef19a14ce8c98dd
Fish <ltfish@users.noreply.github.com>
2020-05-01 14:48:08 -0700

    Eliminate the "real_return" edge type. Closes #2101. (#2105)

--- angr/knowledge_plugins/functions/function.py
+++ angr/knowledge_plugins/functions/function.py
@@ -847,7 +847,7 @@ class Function(Serializable):
         self._local_transition_graph = None
 
     def _return_from_call(self, from_func, to_node, to_outside=False):
-        self.transition_graph.add_edge(from_func, to_node, type='real_return', to_outside=to_outside)
+        self.transition_graph.add_edge(from_func, to_node, type='return', to_outside=to_outside)
         for _, _, data in self.transition_graph.in_edges(to_node, data=True):
             if 'type' in data and data['type'] == 'fake_return':
                 data['confirmed'] = True
