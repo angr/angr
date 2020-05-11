24c7ee04beb8d5ddf707d695259478bd1c525dd2
Kyle ZENG <jkjh1jkjh1@gmail.com>
2020-05-10 17:17:54 -0700

    add several syscalls

--- angr/procedures/linux_kernel/sigaction.py
+++ angr/procedures/linux_kernel/sigaction.py
@@ -1,6 +1,11 @@
 import angr
 
 class sigaction(angr.SimProcedure):
-    def run(self, addr, length): #pylint:disable=arguments-differ,unused-argument
+    def run(self, signum, act, oldact): #pylint:disable=arguments-differ,unused-argument
+        # TODO: actually do something
+        return self.state.solver.BVV(0, self.state.arch.bits)
+
+class rt_sigaction(angr.SimProcedure):
+    def run(self, signum, act, oldact, sigsetsize): #pylint:disable=arguments-differ,unused-argument
         # TODO: actually do something
         return self.state.solver.BVV(0, self.state.arch.bits)
