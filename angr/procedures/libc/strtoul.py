7c710f2b11f4918651bd9f087cdf883e2fa79b6c
wwwzbwcom <zbwhome@outlook.com>
2020-05-26 22:28:06 +0000

    fix for lint

--- angr/procedures/libc/strtoul.py
+++ angr/procedures/libc/strtoul.py
@@ -4,7 +4,6 @@ class strtoul(angr.SimProcedure):
     #pylint:disable=arguments-differ
     def run(self, nptr, endptr, base):
         strtol = angr.SIM_PROCEDURES['libc']['strtol']
-        
-        result = self.inline_call(strol, nptr, endptr, base).ret_expr
+        result = self.inline_call(strtol, nptr, endptr, base).ret_expr
         return result
         
