a6c1c59a751f3d5e6ca8a8410f2343ba9c5d6c6e
wwwzbwcom <zbwhome@outlook.com>
2020-05-26 16:41:10 +0000

    fix mis-spell

--- angr/procedures/libc/strtoul.py
+++ angr/procedures/libc/strtoul.py
@@ -7,7 +7,7 @@ l = logging.getLogger(name=__name__)
 
 # note: this does not handle skipping white space
 
-class strtol(angr.SimProcedure):
+class strtoul(angr.SimProcedure):
     #pylint:disable=arguments-differ
     def run(self, nptr, endptr, base):
         strtol = angr.SIM_PROCEDURES['libc']['strtol']
