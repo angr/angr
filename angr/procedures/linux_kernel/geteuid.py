9c3d61df105df4a6947fb5eea1ec7e373622aeed
Lukas Dresel <Lukas-Dresel@users.noreply.github.com>
2020-05-29 19:50:06 -0700

    added simple stub simprocedures for get*id systemcalls

---
+++ angr/procedures/linux_kernel/geteuid.py
@@ -0,0 +1,19 @@
+import angr
+from angr.sim_type import SimTypeInt
+
+######################################
+# geteuid
+######################################
+
+
+class geteuid(angr.SimProcedure):
+    # pylint: disable=arguments-differ
+    def run(self):
+        self.return_type = SimTypeInt(16, True)
+        return 1000
+
+class geteuid32(angr.SimProcedure):
+    # pylint: disable=arguments-differ
+    def run(self):
+        self.return_type = SimTypeInt(32, True)
+        return 1000
---
+++ angr/procedures/linux_kernel/getgid.py
@@ -0,0 +1,20 @@
+import angr
+from angr.sim_type import SimTypeInt
+
+######################################
+# getgid
+######################################
+
+
+class getgid(angr.SimProcedure):
+    # pylint: disable=arguments-differ
+    def run(self):
+        self.return_type = SimTypeInt(16, True)
+        return 1000
+
+
+class getgid32(angr.SimProcedure):
+    # pylint: disable=arguments-differ
+    def run(self):
+        self.return_type = SimTypeInt(32, True)
+        return 1000
