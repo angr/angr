99602538b3300d7ccefa1b08f0e3f265c8535998
zd99921 <61439820+zd99921@users.noreply.github.com>
2020-05-31 22:35:11 +0800

    Add procedures in libc

--- angr/procedures/libc/strncat.py
+++ angr/procedures/libc/strncat.py
@@ -1,18 +1,15 @@
 import angr
-import claripy
-import logging
-from angr.errors import SimProcedureError
-
-l = logging.getLogger(name=__name__)
 
 class strncat(angr.SimProcedure):
-    def run(self, dest, src, n):
+    # pylint: disable=arguments-differ
+    def run(self, dst, src, limit):
         strncpy = angr.SIM_PROCEDURES['libc']['strncpy']
         strlen = angr.SIM_PROCEDURES['libc']['strlen']
-        dest_len = self.inline_call(strlen, dest).ret_expr
+        dst_len = self.inline_call(strlen, dst).ret_expr
         src_len = self.inline_call(strlen, src).ret_expr
-        self.inline_call(strncpy, dest + dest_len, src, n, src_len)
-        return dest
+
+        self.inline_call(strncpy, dst + dst_len, src, limit, src_len=src_len)
+        return dst
 
 
 
