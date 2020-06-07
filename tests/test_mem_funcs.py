cdae7e08190a589e196d2085705d744687657a53
Fish <fishw@asu.edu>
2020-06-07 01:04:15 -0700

    Fix test_mem_funcs.

--- tests/test_mem_funcs.py
+++ tests/test_mem_funcs.py
@@ -11,21 +11,21 @@ def test_memmove():
     proj = angr.Project(os.path.join(test_location, 'x86_64', 'memmove'), load_options={'auto_load_libs': True}, exclude_sim_procedures_list=['memmove'])
     explorer = proj.factory.simulation_manager().explore(find=[0x4005D7])
     s = explorer.found[0]
-    result = s.solver.eval(s.memory.load(s.registers.load(16), 13), cast_to=bytes)
+    result = s.solver.eval(s.memory.load(s.registers.load(16, 8), 13), cast_to=bytes)
     nose.tools.assert_equal(result, b'very useful.\x00')
 
 def test_memcpy():
     proj = angr.Project(os.path.join(test_location, 'x86_64', 'memcpy'), load_options={'auto_load_libs': True}, exclude_sim_procedures_list=['memcpy'])
     explorer = proj.factory.simulation_manager().explore(find=[0x40065A])
     s = explorer.found[0]
-    result = s.solver.eval(s.memory.load(s.registers.load(16), 19), cast_to=bytes)
+    result = s.solver.eval(s.memory.load(s.registers.load(16, 8), 19), cast_to=bytes)
     nose.tools.assert_equal(result, b"let's test memcpy!\x00")
 
 def test_memset():
     proj = angr.Project(os.path.join(test_location, 'x86_64', 'memset'), load_options={'auto_load_libs': True}, exclude_sim_procedures_list=['memset'])
     explorer = proj.factory.simulation_manager().explore(find=[0x400608])
     s = explorer.found[0]
-    result = s.solver.eval(s.memory.load(s.registers.load(16), 50), cast_to=bytes)
+    result = s.solver.eval(s.memory.load(s.registers.load(16, 8), 50), cast_to=bytes)
     nose.tools.assert_equal(result, b'BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB\x00')
 
 if __name__ == "__main__":
