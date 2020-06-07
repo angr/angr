4934562449d6e9a34a609b8345527a6291c764ea
Fish <fishw@asu.edu>
2020-06-07 01:41:55 -0700

    Allow setting maximum value in SimConcretizationStrategyMax().

--- angr/concretization_strategies/max.py
+++ angr/concretization_strategies/max.py
@@ -1,9 +1,23 @@
+from typing import Optional
+
+from ..errors import SimSolverError
 from . import SimConcretizationStrategy
+
 
 class SimConcretizationStrategyMax(SimConcretizationStrategy):
     """
     Concretization strategy that returns the maximum address.
     """
 
+    def __init__(self, max_addr: Optional[int]=None):
+        super().__init__()
+        self._max_addr = max_addr
+
     def _concretize(self, memory, addr):
-        return [ self._max(memory, addr) ]
+        if self._max_addr is None:
+            return [ self._max(memory, addr) ]
+        else:
+            try:
+                return [ self._max(memory, addr, extra_constraints=(addr <= self._max_addr,)) ]
+            except SimSolverError:
+                return [ self._max(memory, addr) ]
