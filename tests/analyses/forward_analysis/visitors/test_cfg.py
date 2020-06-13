9ed5d8e17b137b70871d86e07087849497e4d769
Fish <fishw@asu.edu>
2020-06-12 18:15:48 -0700

    Get rid of mock.patch function decorators because nose2 does not like them.

--- tests/analyses/forward_analysis/visitors/test_cfg.py
+++ tests/analyses/forward_analysis/visitors/test_cfg.py
@@ -10,8 +10,6 @@ from angr.knowledge_plugins.cfg.cfg_node import CFGNode
 from angr.project import Project
 
 
-# `reset` is called at each `__init__` and calls `sort_nodes` which lacks a good CFG mock to properly work.
-@mock.patch.object(CFGVisitor, 'reset')
 class TestCFGVisitor(TestCase):
     def setUp(self) -> None:
         binary_path = os.path.join(
@@ -25,56 +23,61 @@ class TestCFGVisitor(TestCase):
         self.printf = cfg.kb.functions.function(name='printf', plt=False)
         self.printf_node = cfg.model.get_all_nodes(self.printf.addr)[0]
 
-    @mock.patch.object(CFGNode, 'successors', new_callable=mock.PropertyMock)
-    def test_successors_of_a_node_delegate_the_logic_to_the_CFGNode_successor(self, mock_successors, _):
-        addr, size, cfg = 1, None, None
-        node = CFGNode(0x42, addr, size, cfg, block_id=1)
+    def test_successors_of_a_node_delegate_the_logic_to_the_CFGNode_successor(self):
+        with mock.patch.object(CFGVisitor, 'reset'):
+            with mock.patch.object(CFGNode, 'successors', new_callable=mock.PropertyMock) as mock_successors:
+                addr, size, cfg = 1, None, None
+                node = CFGNode(0x42, addr, size, cfg, block_id=1)
 
-        cfg_visitor = CFGVisitor(None)
-        _ = cfg_visitor.successors(node)
+                cfg_visitor = CFGVisitor(None)
+                _ = cfg_visitor.successors(node)
 
-        mock_successors.assert_called_once_with()
+                mock_successors.assert_called_once_with()
 
-    @mock.patch.object(CFGNode, 'predecessors', new_callable=mock.PropertyMock)
-    def test_predecessors_of_a_node_delegate_the_logic_to_the_CFGNode_predecessor(self, mock_predecessors, _):
-        addr, size, cfg = 1, None, None
-        node = CFGNode(0x42, addr, size, cfg, block_id=1)
+    def test_predecessors_of_a_node_delegate_the_logic_to_the_CFGNode_predecessor(self):
+        with mock.patch.object(CFGVisitor, 'reset'):
+            with mock.patch.object(CFGNode, 'predecessors', new_callable=mock.PropertyMock) as mock_predecessors:
+                addr, size, cfg = 1, None, None
+                node = CFGNode(0x42, addr, size, cfg, block_id=1)
 
-        cfg_visitor = CFGVisitor(None)
-        _ = cfg_visitor.predecessors(node)
+                cfg_visitor = CFGVisitor(None)
+                _ = cfg_visitor.predecessors(node)
 
-        mock_predecessors.assert_called_once_with()
+                mock_predecessors.assert_called_once_with()
 
-    @mock.patch.object(CFGUtils, 'quasi_topological_sort_nodes')
-    def test_sort_nodes(self, mock_quasi_topological_sort, _):
-        class CFGVisitorMock(CFGVisitor):
-            def __init__(self, *args):
-                super().__init__(*args)
-                self._cfg = CFGMock()
-            @property
-            def cfg(self):
-                return CFGMock()
-        class CFGMock():
-            @property
-            def graph(self):
-                return 'mock_graph_return'
+    def test_sort_nodes(self):
+        with mock.patch.object(CFGVisitor, 'reset'):
+            with mock.patch.object(CFGUtils, 'quasi_topological_sort_nodes') as mock_quasi_topological_sort:
+                class CFGVisitorMock(CFGVisitor):
+                    def __init__(self, *args):
+                        super().__init__(*args)
+                        self._cfg = CFGMock()
+                    @property
+                    def cfg(self):
+                        return CFGMock()
+                class CFGMock():
+                    @property
+                    def graph(self):
+                        return 'mock_graph_return'
 
-        cfg_visitor = CFGVisitorMock(None)
-        _ = cfg_visitor.sort_nodes()
+                cfg_visitor = CFGVisitorMock(None)
+                _ = cfg_visitor.sort_nodes()
 
-        mock_quasi_topological_sort.assert_called_once_with('mock_graph_return')
+                mock_quasi_topological_sort.assert_called_once_with('mock_graph_return')
 
-    def test_remove_from_sorted_nodes(self, _):
+    def test_remove_from_sorted_nodes(self, *args):
         """
         Test the side-effect of a method on an ihnerited private property...
         """
-        cfg_visitor = CFGVisitor(None)
 
-        arbitrarily_chosen_nodes = [self.printf_node] + self.printf_node.predecessors
-        cfg_visitor._sorted_nodes = OrderedSet(arbitrarily_chosen_nodes)
+        with mock.patch.object(CFGVisitor, 'reset'):
+            cfg_visitor = CFGVisitor(None)
 
-        visited_blocks = self.printf_node.predecessors
+            arbitrarily_chosen_nodes = [self.printf_node] + self.printf_node.predecessors
+            cfg_visitor._sorted_nodes = OrderedSet(arbitrarily_chosen_nodes)
 
-        cfg_visitor.remove_from_sorted_nodes(visited_blocks)
+            visited_blocks = self.printf_node.predecessors
 
-        self.assertListEqual(list(cfg_visitor._sorted_nodes), [self.printf_node])
+            cfg_visitor.remove_from_sorted_nodes(visited_blocks)
+
+            self.assertListEqual(list(cfg_visitor._sorted_nodes), [self.printf_node])
