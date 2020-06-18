2aad075a09f6620001805b668de9b352e757b327
degrigis <degrigis@ucsb.edu>
2020-06-18 15:56:54 -0700

    fix space
    Signed-off-by: degrigis <degrigis@ucsb.edu>

--- angr/knowledge_plugins/key_definitions/definition.py
+++ angr/knowledge_plugins/key_definitions/definition.py
@@ -25,7 +25,7 @@ class ParamTag(Tag):
 
 class RetValueTag(Tag):
     """
-    A tag for a definiton of a return value 
+    A tag for a definiton of a return value
     of a function.
     """
 
@@ -39,7 +39,7 @@ class InitValueTag(Tag):
     """
     A tag for a definiton of an initial value
     """
-    
+
     def __init__(self, metadata: object=None):
         super(InitValueTag, self).__init__()
         self.metadata = metadata
