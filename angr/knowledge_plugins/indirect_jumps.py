019d8a33b1ffb293060dd706b7a5aeb265d92205
mahaloz <zbasque@asu.edu>
2020-06-19 15:27:39 -0700

    added a way to save running resolves

--- angr/knowledge_plugins/indirect_jumps.py
+++ angr/knowledge_plugins/indirect_jumps.py
@@ -9,10 +9,15 @@ class IndirectJumps(KnowledgeBasePlugin, dict):
         self.resolved = set()
         self.unresolved = set()
 
+        # variable for storing addresses actively being resolved
+        # dict format:    {indirect_addr: [resolved_addr]}
+        self.active_resolves = {}
+
     def copy(self):
         o = IndirectJumps(self._kb)
         o.resolved.update(self.resolved)
         o.unresolved.update(self.unresolved)
+        o.active_resolves = self.active_resolves
 
 
 KnowledgeBasePlugin.register_default('indirect_jumps', IndirectJumps)
