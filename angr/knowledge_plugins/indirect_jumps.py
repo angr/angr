a8d483f2049f9231fedc3fff4e763be1c52527d4
mahaloz <zbasque@asu.edu>
2020-06-21 13:48:36 -0700

    Refactored resolved_addrs kb to store resolves

--- angr/knowledge_plugins/indirect_jumps.py
+++ angr/knowledge_plugins/indirect_jumps.py
@@ -6,13 +6,25 @@ class IndirectJumps(KnowledgeBasePlugin, dict):
     def __init__(self, kb):
         super(IndirectJumps, self).__init__()
         self._kb = kb
-        self.resolved = set()
         self.unresolved = set()
+
+        # dict format: {indirect_address: [resolved_addresses]}
+        self.resolved = {}
 
     def copy(self):
         o = IndirectJumps(self._kb)
-        o.resolved.update(self.resolved)
         o.unresolved.update(self.unresolved)
+        o.resolved = {k: v for k, v in self.resolved.items()}
+
+    def update_resolved_addrs(self, indirect_address: int, resolved_addresses: list):
+        # sanity check on usage
+        if indirect_address is None:
+            return
+
+        if indirect_address in self.resolved:
+            self.resolved[indirect_address] + resolved_addresses
+        else:
+            self.resolved[indirect_address] = resolved_addresses
 
 
 KnowledgeBasePlugin.register_default('indirect_jumps', IndirectJumps)
