c565292a2dd75a0eb77fad74a6b6dd2656216b1f
i301Void <i301void@outlook.com>
2020-06-25 20:11:59 +0800

    add memchr&strcspn

--- angr/procedures/libc/memchr.py
+++ angr/procedures/libc/memchr.py
@@ -5,39 +5,74 @@ l = logging.getLogger(name=__name__)
 
 
 class memchr(angr.SimProcedure):
-    # change from strncmp, only can deal with single valued
     # pylint:disable=arguments-differ
 
-    def run(self, a_addr, b, limit):  # pylint:disable=arguments-differ
+    def run(self, a_addr, b_int, limit):  # pylint:disable=arguments-differ
+        strlen = angr.SIM_PROCEDURES['libc']['strlen']
+
+        a_strlen = self.inline_call(strlen, a_addr)
+        a_len = a_strlen.ret_expr
+
+        b = b_int[7:0]
+        # b = self.state.solver.BVV(203, 8)
+
+        match_constraints = []
+        ret_expr = self.state.solver.Unconstrained(
+            "memchr_ret", self.state.arch.bits, key=('api', 'memchr'))
 
         concrete_run = False
-        if self.state.solver.single_valued(limit):
-            maxlen = self.state.solver.eval(limit)
+        if self.state.solver.single_valued(a_len) and self.state.solver.single_valued(limit):
+            c_a_len = self.state.solver.eval(a_len)
+            c_limit = self.state.solver.eval(limit)
+            maxlen = min(c_a_len, c_limit)
             concrete_run = True
+        else:
+            if self.state.solver.single_valued(limit):
+                c_limit = self.state.solver.eval(limit)
+                maxlen = min(a_strlen.max_null_index, c_limit)
+            else:
+                maxlen = a_strlen.max_null_index
 
         if maxlen == 0:
-            # return NULL
             return self.state.solver.BVV(0, self.state.arch.bits)
 
         # the string bytes
         a_bytes = self.state.memory.load(a_addr, maxlen, endness='Iend_BE')
 
-        if self.state.solver.single_valued(b):
-            b_conc = self.state.solver.eval(b)
+        for i in range(maxlen):
+            l.debug("Processing byte %d", i)
+            maxbit = (maxlen-i)*8
+            a_byte = a_bytes[maxbit-1:maxbit-8]
 
-            for i in range(maxlen):
-                l.debug("Processing byte %d", i)
-                maxbit = (maxlen-i)*8
-                a_byte = a_bytes[maxbit-1:maxbit-8]
+            if concrete_run and self.state.solver.single_valued(a_byte) and self.state.solver.single_valued(b):
+                a_conc = self.state.solver.eval(a_byte)
+                b_conc = self.state.solver.eval(b)
 
-                if concrete_run and self.state.solver.single_valued(a_byte):
-                    a_conc = self.state.solver.eval(a_byte)
-                    if a_conc == b_conc:
-                        l.debug(
-                            "... found matched concrete bytes 0x%x and 0x%x", a_conc, b_conc)
-                        return a_addr + self.state.solver.BVV(i, self.state.arch.bits)
-                else:
-                    concrete_run = False
+                if a_conc == b_conc:
+                    l.debug(
+                        "... found matched concrete bytes 0x%x and 0x%x", a_conc, b_conc)
+                    return a_addr + self.state.solver.BVV(i, self.state.arch.bits)
+            else:
+                concrete_run = False
 
-        # default return NULL
-        return self.state.solver.BVV(0, self.state.arch.bits)
+            byte_constraint = self.state.solver.And(a_byte == b, ret_expr == (
+                a_addr + self.state.solver.BVV(i, self.state.arch.bits)))
+            match_constraints.append(byte_constraint)
+
+        if concrete_run:
+            l.debug("concrete run made it to the end!")
+            return self.state.solver.BVV(0, self.state.arch.bits)
+
+        l.debug("returning symbolic")
+        match_constraint = self.state.solver.Or(*match_constraints)
+        nomatch_constraint = self.state.solver.Not(match_constraint)
+
+        match_case = self.state.solver.And(
+            maxlen != 0, match_constraint)
+        nomatch_case = self.state.solver.And(
+            nomatch_constraint, ret_expr == self.state.solver.BVV(0, self.state.arch.bits))
+
+        self.state.add_constraints(
+            self.state.solver.Or(match_case, nomatch_case))
+
+        return ret_expr
--- angr/procedures/libc/strcspn.py
+++ angr/procedures/libc/strcspn.py
@@ -5,12 +5,10 @@ l = logging.getLogger(name=__name__)
 
 
 class strcspn(angr.SimProcedure):
-    # simple version, only can deal with single valued string
     # pylint:disable=arguments-differ
 
     def run(self, a_addr, b_addr):  # pylint:disable=arguments-differ
         strlen = angr.SIM_PROCEDURES['libc']['strlen']
-        strncmp = angr.SIM_PROCEDURES['libc']['strncmp']
 
         a_strlen = self.inline_call(strlen, a_addr)
         b_strlen = self.inline_call(strlen, b_addr)
@@ -18,19 +16,58 @@ class strcspn(angr.SimProcedure):
         a_len = a_strlen.ret_expr
         b_len = b_strlen.ret_expr
 
-        if self.state.solver.single_valued(a_len) and self.state.solver.single_valued(b_len):
-            c_a_len = self.state.solver.eval(a_len)
-            c_b_len = self.state.solver.eval(b_len)
-
-            for i in range(c_a_len):
-                for j in range(c_b_len):
-                    ret_expr = self.inline_call(strncmp, a_addr+i, b_addr+j, 1).ret_expr
-                    if self.state.solver.single_valued(ret_expr):
-                        ret = self.state.solver.eval(ret_expr)
-                        if ret == 0:
-                            return self.state.solver.BVV(i, self.state.arch.bits)
-
-            return self.state.solver.BVV(c_a_len, self.state.arch.bits)
-
+        concrete_run = False
+        if self.state.solver.single_valued(a_len):
+            max_a_len = self.state.solver.eval(a_len)
+            concrete_run = True
         else:
-            return self.state.solver.BVV(c_a_len, self.state.arch.bits)
+            max_a_len = a_strlen.max_null_index
+
+        if self.state.solver.single_valued(b_len):
+            max_b_len = self.state.solver.eval(b_len)
+        else:
+            max_b_len = b_strlen.max_null_index
+            concrete_run = False
+
+        a_bytes = self.state.memory.load(a_addr, max_a_len, endness='Iend_BE')
+        b_bytes = self.state.memory.load(b_addr, max_b_len, endness='Iend_BE')
+
+        match_constraints = []
+        ret_expr = self.state.solver.Unconstrained(
+            "strcspn_ret", self.state.arch.bits, key=('api', 'strcspn'))
+
+        for i in range(max_a_len):
+            a_maxbit = (max_a_len-i)*8
+            a_byte = a_bytes[a_maxbit-1:a_maxbit-8]
+            for j in range(max_b_len):
+                b_maxbit = (max_b_len-j)*8
+                b_byte = b_bytes[b_maxbit-1:b_maxbit-8]
+
+                if concrete_run and self.state.solver.single_valued(a_byte) and self.state.solver.single_valued(b_byte):
+                    a_conc = self.state.solver.eval(a_byte)
+                    b_conc = self.state.solver.eval(b_byte)
+
+                    if a_conc == b_conc:
+                        return self.state.solver.BVV(i, self.state.arch.bits)
+                else:
+                    concrete_run = False
+
+                byte_constraint = self.state.solver.And(
+                    a_byte == b_byte, ret_expr == self.state.solver.BVV(i, self.state.arch.bits))
+                match_constraints.append(byte_constraint)
+
+        if concrete_run:
+            return self.state.solver.BVV(max_a_len, self.state.arch.bits)
+
+        match_constraint = self.state.solver.Or(*match_constraints)
+        nomatch_constraint = self.state.solver.Not(match_constraint)
+
+        match_case = self.state.solver.And(
+            max_a_len != 0, max_b_len != 0, match_constraint)
+        nomatch_case = self.state.solver.And(
+            nomatch_constraint, ret_expr == self.state.solver.BVV(max_a_len, self.state.arch.bits))
+
+        self.state.add_constraints(
+            self.state.solver.Or(match_case, nomatch_case))
+
+        return ret_expr
