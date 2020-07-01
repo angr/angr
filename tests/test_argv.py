31652db6195a4df9b900df93988b9c24d9a64078
trz19991228 <66503893+trz19991228@users.noreply.github.com>
2020-07-02 04:43:38 +0800

    Update test_argv.py

--- tests/test_argv.py
+++ tests/test_argv.py
@@ -8,17 +8,15 @@ import os
 test_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'binaries', 'tests')
 
 plate={
-    'mips':0x400768,
     'mipsel':0x400768,
     'i386':0x804845B,
     'amd64':0x400571,
     'arm':0x1048c,
     'ppc32':0x10000498
 }
-
-def test(arch):
-    proj = angr.Project(os.path.join(test_location, arch, 'argv_test'))
-    r_addr = plate[arch]
+def test_mips():
+    proj = angr.Project(os.path.join(test_location, 'mips', 'argv_test'))
+    r_addr = 0x400768
 
     s = proj.factory.entry_state(args = ['aaa', "Yan is a noob"], env ={"HOME": "/home/angr"})
     xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
@@ -39,8 +37,32 @@ def test(arch):
     conc = found.solver.eval(found.memory.load(found.registers.load('sp'), 400), cast_to=bytes)
 
     nose.tools.assert_equal(b"Yan is a noob" in conc, True)
+
+def test(arch):
+    proj = angr.Project(os.path.join(test_location, arch, 'argv_test'))
+    r_addr = plate[arch]
+
+    s = proj.factory.entry_state(args = ['aaa', "Yan is a noob"], env ={"HOME": "/home/angr"})
+    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+
+    nose.tools.assert_equal(len(xpl.found), 1)
+
+    s = proj.factory.entry_state(args = ['aaa', 'Yan is not a noob'], env ={"HOME": "/home/angr"})
+    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+
+    nose.tools.assert_equal(len(xpl.found), 0)
+
+    # symbolic command
+    s = proj.factory.entry_state(args = ['aaa', claripy.BVS('arg_2', 50*8)], env ={"HOME": "/home/angr"})
+    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+
+    found = xpl.found[0]
+    conc = found.solver.eval(found.memory.load(found.registers.load('sp'), 400), cast_to=bytes)
+
+    nose.tools.assert_equal(b"Yan is a noob" in conc, True)
     
     
 if __name__ == "__main__":
+    test_mips()
     for i in plate
         test(i)
