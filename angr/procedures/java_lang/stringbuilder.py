44601f0ed059d5b50346b12b3af27697fc551c7a
fabiano <fabian.fleischer@fau.de>
2020-07-06 16:43:37 -0700

    Implement StringBuilder.<init> for CharSequences

--- angr/procedures/java_lang/stringbuilder.py
+++ angr/procedures/java_lang/stringbuilder.py
@@ -11,12 +11,29 @@ class StringBuilderInit(JavaSimProcedure):
 
     __provides__ = (
         ('java.lang.StringBuilder', '<init>()'),
+        ('java.lang.StringBuilder', '<init>(java.lang.String)'),
+        ('java.lang.StringBuilder', '<init>(java.lang.CharSequence)'),
     )
 
-    def run(self, this_ref):
+    def run(self, this_ref, thing=None):
         log.debug('Called SimProcedure java.lang.StringBuilder.<init> with args: {}'.format(this_ref))
 
-        str_ref = SimSootValue_StringRef.new_object(self.state, claripy.StringV(''))
+        if thing:
+            if isinstance(thing, SimSootValue_StringRef):
+                thing_str = self.state.javavm_memory.load(thing)
+
+            elif thing.type == 'java.lang.StringBuilder':
+                thing_str_ref = thing.get_field(self.state, 'str', 'java.lang.String')
+                thing_str = self.state.javavm_memory.load(thing_str_ref)
+
+            else:
+                log.error('NotImplemented, unsupported type for StringBuilder.<init>')
+                thing_str = claripy.StringV('')
+
+        else:
+            thing_str = claripy.StringV('')
+
+        str_ref = SimSootValue_StringRef.new_object(self.state, thing_str)
         this_ref.store_field(self.state, 'str', 'java.lang.String', str_ref)
         return
 
