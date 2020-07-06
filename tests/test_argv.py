cb03c3265382f81c48a400d4071859b6c7dd977e
trz19991228 <66503893+trz19991228@users.noreply.github.com>
2020-07-07 04:24:12 +0800

    Update test_argv.py

--- tests/test_argv.py
+++ tests/test_argv.py
@@ -7,36 +7,14 @@ l = logging.getLogger("angr_tests")
 import os
 test_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'binaries', 'tests')
 
-plate={
+arches={
+    'mips':0x400768,
     'mipsel':0x400768,
     'i386':0x804845B,
     'x86_64':0x400571,
     'armel':0x1048c,
     'ppc':0x10000498
 }
-def test_mips():
-    proj = angr.Project(os.path.join(test_location, 'mips', 'argv_test'))
-    r_addr = 0x400768
-
-    s = proj.factory.entry_state(args = ['aaa', "Yan is a noob"], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
-
-    nose.tools.assert_equal(len(xpl.found), 1)
-
-    s = proj.factory.entry_state(args = ['aaa', 'Yan is not a noob'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
-
-    nose.tools.assert_equal(len(xpl.found), 0)
-
-    # symbolic command line argument
-    arg = claripy.BVS('arg_2', 50*8)
-    s = proj.factory.entry_state(args = ['aaa', arg], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
-
-    found = xpl.found[0]
-    conc = found.solver.eval(found.memory.load(found.registers.load('sp'), 400), cast_to=bytes)
-
-    nose.tools.assert_equal(b"Yan is a noob" in conc, True)
 
 def test(arch):
     proj = angr.Project(os.path.join(test_location, arch, 'argv_test'))
@@ -61,8 +39,8 @@ def test(arch):
 
     nose.tools.assert_equal(b"Yan is a noob" in conc, True)
     
-    
+def test_argv():
+    for arch in arches:
+        yield run_test, arch
 if __name__ == "__main__":
-    test_mips()
-    for i in plate
-        test(i)
+    test_argv()
