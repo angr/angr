1990490e37f89f8e5fa46b1d6deb4d42c298a663
trz19991228 <66503893+trz19991228@users.noreply.github.com>
2020-07-12 04:28:38 +0800

    And dont merge this too!

--- tests/test_argv.py
+++ tests/test_argv.py
@@ -8,129 +8,129 @@ import os
 test_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'binaries', 'tests')
 
 def test():
-    proj = angr.Project(os.path.join(test_location, 'mips', 'argv_test'))
+    proj1 = angr.Project(os.path.join(test_location, 'mips', 'argv_test'))
     r_addr = 0x400768
 
-    s = proj.factory.entry_state(args = ['aaa', "Yan is a noob"], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+    s = proj1.factory.entry_state(args = ['aaa', "Yan is a noob"], env ={"HOME": "/home/angr"})
+    xpl = proj1.factory.simulation_manager(s).explore(find=r_addr)
 
     nose.tools.assert_equal(len(xpl.found), 1)
 
-    s = proj.factory.entry_state(args = ['aaa', 'Yan is not a noob'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+    s = proj1.factory.entry_state(args = ['aaa', 'Yan is not a noob'], env ={"HOME": "/home/angr"})
+    xpl = proj1.factory.simulation_manager(s).explore(find=r_addr)
 
     nose.tools.assert_equal(len(xpl.found), 0)
 
     # Does this matter?
-    s = proj.factory.entry_state(args = ['aaa', claripy.BVS('arg_2', 50*8)], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+    s = proj1.factory.entry_state(args = ['aaa', claripy.BVS('arg_2', 50*8)], env ={"HOME": "/home/angr"})
+    xpl = proj1.factory.simulation_manager(s).explore(find=r_addr)
 
     found = xpl.found[0]
     conc = found.solver.eval(found.memory.load(found.registers.load('sp'), 400), cast_to=bytes)
 
     nose.tools.assert_equal(b"Yan is a noob" in conc, True)
     
-    proj = angr.Project(os.path.join(test_location, 'mipsel', 'argv_test'))
+    proj2 = angr.Project(os.path.join(test_location, 'mipsel', 'argv_test'))
     r_addr = 0x400768
-    s = proj.factory.entry_state(args = ['aaa', 'Yan is a noob'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+    s = proj2.factory.entry_state(args = ['aaa', 'Yan is a noob'], env ={"HOME": "/home/angr"})
+    xpl = proj2.factory.simulation_manager(s).explore(find=r_addr)
 
     nose.tools.assert_equal(len(xpl.found), 1)
 
-    s = proj.factory.entry_state(args = ['aaa', 'Yan is not a noob'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+    s = proj2.factory.entry_state(args = ['aaa', 'Yan is not a noob'], env ={"HOME": "/home/angr"})
+    xpl = proj2.factory.simulation_manager(s).explore(find=r_addr)
 
     nose.tools.assert_equal(len(xpl.found), 0)
 
     # symbolic args
-    s = proj.factory.entry_state(args = ['aaa', claripy.BVS('arg_2', 50*8)], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+    s = proj2.factory.entry_state(args = ['aaa', claripy.BVS('arg_2', 50*8)], env ={"HOME": "/home/angr"})
+    xpl = proj2.factory.simulation_manager(s).explore(find=r_addr)
 
     found = xpl.found[0]
     conc = found.solver.eval(found.memory.load(found.registers.load('sp'), 400), cast_to=bytes)
 
     nose.tools.assert_equal(b"Yan is a noob" in conc, True)
 
-    proj = angr.Project(os.path.join(test_location, 'i386', 'argv_test'))
+    proj3 = angr.Project(os.path.join(test_location, 'i386', 'argv_test'))
     r_addr = 0x804845B
-    s = proj.factory.entry_state(args = ['aaa', 'Yan is a noob'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+    s = proj3.factory.entry_state(args = ['aaa', 'Yan is a noob'], env ={"HOME": "/home/angr"})
+    xpl = proj3.factory.simulation_manager(s).explore(find=r_addr)
 
     nose.tools.assert_equal(len(xpl.found), 1)
 
-    s = proj.factory.entry_state(args = ['aaa', 'Yan is not a noob'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+    s = proj3.factory.entry_state(args = ['aaa', 'Yan is not a noob'], env ={"HOME": "/home/angr"})
+    xpl = proj3.factory.simulation_manager(s).explore(find=r_addr)
 
     nose.tools.assert_equal(len(xpl.found), 0)
 
     # symbolic args
-    s = proj.factory.entry_state(args = ['aaa', claripy.BVS('arg_2', 50*8)], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+    s = proj3.factory.entry_state(args = ['aaa', claripy.BVS('arg_2', 50*8)], env ={"HOME": "/home/angr"})
+    xpl = proj3.factory.simulation_manager(s).explore(find=r_addr)
 
     found = xpl.found[0]
     conc = found.solver.eval(found.memory.load(found.registers.load('sp'), 400), cast_to=bytes)
 
     nose.tools.assert_equal(b"Yan is a noob" in conc, True)
 
-    proj = angr.Project(os.path.join(test_location, 'x86_64', 'argv_test'))
+    proj4 = angr.Project(os.path.join(test_location, 'x86_64', 'argv_test'))
     r_addr = 0x400571
-    s = proj.factory.entry_state(args = ['aaa', 'Yan is a noob'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+    s = proj4.factory.entry_state(args = ['aaa', 'Yan is a noob'], env ={"HOME": "/home/angr"})
+    xpl = proj4.factory.simulation_manager(s).explore(find=r_addr)
 
     nose.tools.assert_equal(len(xpl.found), 1)
 
-    s = proj.factory.entry_state(args = ['aaa', 'Yan is not a noob'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+    s = proj4.factory.entry_state(args = ['aaa', 'Yan is not a noob'], env ={"HOME": "/home/angr"})
+    xpl = proj4.factory.simulation_manager(s).explore(find=r_addr)
 
     nose.tools.assert_equal(len(xpl.found), 0)
 
     # symbolic args
-    s = proj.factory.entry_state(args = ['aaa', claripy.BVS('arg_2', 50*8)], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+    s = proj4.factory.entry_state(args = ['aaa', claripy.BVS('arg_2', 50*8)], env ={"HOME": "/home/angr"})
+    xpl = proj4.factory.simulation_manager(s).explore(find=r_addr)
 
     found = xpl.found[0]
     conc = found.solver.eval(found.memory.load(found.registers.load('sp'), 400), cast_to=bytes)
 
     nose.tools.assert_equal(b"Yan is a noob" in conc, True)
 
-    proj = angr.Project(os.path.join(test_location, 'armel', 'argv_test'))
+    proj5 = angr.Project(os.path.join(test_location, 'armel', 'argv_test'))
     r_addr = 0x1048c
 
-    s = proj.factory.entry_state(args = ['aaa', 'Yan is a noob'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+    s = proj5.factory.entry_state(args = ['aaa', 'Yan is a noob'], env ={"HOME": "/home/angr"})
+    xpl = proj5.factory.simulation_manager(s).explore(find=r_addr)
 
     nose.tools.assert_equal(len(xpl.found), 1)
 
-    s = proj.factory.entry_state(args = ['aaa', 'Yan is not a noob'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+    s = proj5.factory.entry_state(args = ['aaa', 'Yan is not a noob'], env ={"HOME": "/home/angr"})
+    xpl = proj5.factory.simulation_manager(s).explore(find=r_addr)
 
     nose.tools.assert_equal(len(xpl.found), 0)
 
     # symbolic args
-    s = proj.factory.entry_state(args = ['aaa', claripy.BVS('arg_2', 50*8)], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+    s = proj5.factory.entry_state(args = ['aaa', claripy.BVS('arg_2', 50*8)], env ={"HOME": "/home/angr"})
+    xpl = proj5.factory.simulation_manager(s).explore(find=r_addr)
 
     found = xpl.found[0]
     conc = found.solver.eval(found.memory.load(found.registers.load('sp'), 400), cast_to=bytes)
 
     nose.tools.assert_equal(b"Yan is a noob" in conc, True)
 
-    proj = angr.Project(os.path.join(test_location, 'ppc', 'argv_test'))
+    proj6 = angr.Project(os.path.join(test_location, 'ppc', 'argv_test'))
     r_addr = 0x10000498
 
-    s = proj.factory.entry_state(args = ['aaa', 'Yan is a noob'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+    s = proj6.factory.entry_state(args = ['aaa', 'Yan is a noob'], env ={"HOME": "/home/angr"})
+    xpl = proj6.factory.simulation_manager(s).explore(find=r_addr)
 
     nose.tools.assert_equal(len(xpl.found), 1)
 
-    s = proj.factory.entry_state(args = ['aaa', 'Yan is not a noob'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+    s = proj6.factory.entry_state(args = ['aaa', 'Yan is not a noob'], env ={"HOME": "/home/angr"})
+    xpl = proj6.factory.simulation_manager(s).explore(find=r_addr)
 
     nose.tools.assert_equal(len(xpl.found), 0)
 
     # symbolic args
-    s = proj.factory.entry_state(args = ['aaa', claripy.BVS('arg_2', 50*8)], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+    s = proj6.factory.entry_state(args = ['aaa', claripy.BVS('arg_2', 50*8)], env ={"HOME": "/home/angr"})
+    xpl = proj6.factory.simulation_manager(s).explore(find=r_addr)
 
     found = xpl.found[0]
     conc = found.solver.eval(found.memory.load(found.registers.load('sp'), 400), cast_to=bytes)
