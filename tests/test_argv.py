88ac071ab76733fb5056b58bfaf20a4b056464fb
trz19991228 <66503893+trz19991228@users.noreply.github.com>
2020-07-12 04:04:48 +0800

    Don't merge this to master!Just try something

--- tests/test_argv.py
+++ tests/test_argv.py
@@ -7,7 +7,7 @@ l = logging.getLogger("angr_tests")
 import os
 test_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'binaries', 'tests')
 
-def test_mips():
+def test():
     proj = angr.Project(os.path.join(test_location, 'mips', 'argv_test'))
     r_addr = 0x400768
 
@@ -29,8 +29,7 @@ def test_mips():
     conc = found.solver.eval(found.memory.load(found.registers.load('sp'), 400), cast_to=bytes)
 
     nose.tools.assert_equal(b"Yan is a noob" in conc, True)
-
-def test_mipsel():
+    
     proj = angr.Project(os.path.join(test_location, 'mipsel', 'argv_test'))
     r_addr = 0x400768
     s = proj.factory.entry_state(args = ['aaa', 'Yan is a noob'], env ={"HOME": "/home/angr"})
@@ -52,7 +51,6 @@ def test_mipsel():
 
     nose.tools.assert_equal(b"Yan is a noob" in conc, True)
 
-def test_i386():
     proj = angr.Project(os.path.join(test_location, 'i386', 'argv_test'))
     r_addr = 0x804845B
     s = proj.factory.entry_state(args = ['aaa', 'Yan is a noob'], env ={"HOME": "/home/angr"})
@@ -74,7 +72,6 @@ def test_i386():
 
     nose.tools.assert_equal(b"Yan is a noob" in conc, True)
 
-def test_amd64():
     proj = angr.Project(os.path.join(test_location, 'x86_64', 'argv_test'))
     r_addr = 0x400571
     s = proj.factory.entry_state(args = ['aaa', 'Yan is a noob'], env ={"HOME": "/home/angr"})
@@ -96,7 +93,6 @@ def test_amd64():
 
     nose.tools.assert_equal(b"Yan is a noob" in conc, True)
 
-def test_arm():
     proj = angr.Project(os.path.join(test_location, 'armel', 'argv_test'))
     r_addr = 0x1048c
 
@@ -119,7 +115,6 @@ def test_arm():
 
     nose.tools.assert_equal(b"Yan is a noob" in conc, True)
 
-def test_ppc32():
     proj = angr.Project(os.path.join(test_location, 'ppc', 'argv_test'))
     r_addr = 0x10000498
 
@@ -143,9 +138,4 @@ def test_ppc32():
     nose.tools.assert_equal(b"Yan is a noob" in conc, True)
 
 if __name__ == "__main__":
-    test_mips()
-    test_mipsel()
-    test_arm()
-    test_i386()
-    test_amd64()
-    test_ppc32()
+    test()
