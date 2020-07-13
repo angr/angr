de654eae7c7fd1e8ff1206afafb46c52efaf92c0
Sebastiano Mariani <mariani.sebastiano@gmail.com>
2020-07-13 11:48:30 -0700

    Implemented add with index for java.util.list.add(int, obj)
    Signed-off-by: Sebastiano Mariani <mariani.sebastiano@gmail.com>

--- angr/procedures/java_util/list.py
+++ angr/procedures/java_util/list.py
@@ -13,7 +13,8 @@ class ListInit(JavaSimProcedure):
 
     __provides__ = (
         ('java.util.List', '<init>()'),
-        ('java.util.LinkedList', '<init>()')
+        ('java.util.LinkedList', '<init>()'),
+        ('java.util.ArrayList', '<init>()')
     )
 
     def run(self, this_ref):
@@ -30,7 +31,8 @@ class ListAdd(JavaSimProcedure):
 
     __provides__ = (
         ('java.util.List', 'add(java.lang.Object)'),
-        ('java.util.LinkedList', 'add(java.lang.Object)')
+        ('java.util.LinkedList', 'add(java.lang.Object)'),
+        ('java.util.ArrayList', 'add(java.lang.Object)')
     )
 
     def run(self, this_ref, obj_ref):
@@ -51,13 +53,52 @@ class ListAdd(JavaSimProcedure):
 
         return claripy.BoolV(1)
 
+class ListAddAtIndex(JavaSimProcedure):
+
+    __provides__ = (
+        ('java.util.List', 'add(int,java.lang.Object)'),
+        ('java.util.LinkedList', 'add(int,java.lang.Object)'),
+        ('java.util.ArrayList', 'add(int,java.lang.Object)')
+    )
+
+    def run(self, this_ref, idx, obj_ref):
+        log.debug('Called SimProcedure java.util.List.add with args: {} {}'.format(this_ref, obj_ref))
+
+        if this_ref.symbolic:
+            return claripy.BoolS('list.append')
+
+        try:
+            array_ref = this_ref.load_field(self.state, ELEMS, 'java.lang.Object[]')
+            array_len = this_ref.load_field(self.state, SIZE, 'int')
+
+            array_len_concrete = self.state.solver.eval(array_len)
+            # We don't support symbolic indexing for now
+            idx_concrete = self.state.solver.eval(idx)
+
+            if idx_concrete <= array_len_concrete - 1:
+                i = array_len_concrete - 1
+                # Shift all the element after the idx to the left
+                while i >= idx_concrete:
+                    elem = self.state.javavm_memory.load_array_element(array_ref, i)
+                    self.state.javavm_memory.store_array_element(array_ref, i+1, elem)
+                    i -= 1
+            # Insert the element
+            self.state.javavm_memory.store_array_element(array_ref, idx, obj_ref)
+            # Update size
+            new_array_len = claripy.BVV(self.state.solver.eval(array_len) + 1, 32)
+            this_ref.store_field(self.state, SIZE, 'int', new_array_len)
+        except KeyError:
+            log.warning('Could not add element to list {}'.format(this_ref))
+
+        return claripy.BoolV(1)
 
 
 class ListGet(JavaSimProcedure):
 
     __provides__ = (
         ('java.util.List', 'get(int)'),
-        ('java.util.LinkedList', 'get(int)')
+        ('java.util.LinkedList', 'get(int)'),
+        ('java.util.ArrayList', 'get(int)')
     )
 
     def run(self, this_ref, index):
@@ -79,7 +120,9 @@ class ListGet(JavaSimProcedure):
 class ListGetFirst(JavaSimProcedure):
 
     __provides__ = (
+        ('java.util.List', 'getFirst()'),
         ('java.util.LinkedList', 'getFirst()'),
+        ('java.util.ArrayList', 'getFirst()')
     )
 
     def run(self, this_ref):
@@ -103,7 +146,8 @@ class ListSize(JavaSimProcedure):
 
     __provides__ = (
         ('java.util.List', 'size()'),
-        ('java.util.LinkedList', 'size()')
+        ('java.util.LinkedList', 'size()'),
+        ('java.util.ArrayList', 'size()')
     )
 
     def run(self, this_ref):
