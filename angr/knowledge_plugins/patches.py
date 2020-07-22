419bb15f1afa8d1924cc2983cde425b1f9a8ab89
Fish <fishw@asu.edu>
2020-07-22 00:04:32 -0700

    Patches: Support applying patches to a binary. Add a test case.

--- angr/knowledge_plugins/patches.py
+++ angr/knowledge_plugins/patches.py
@@ -1,4 +1,6 @@
+from typing import Optional, List, Dict
 
+from cle.address_translator import AddressTranslator
 from sortedcontainers import SortedDict
 
 from .plugin import KnowledgeBasePlugin
@@ -6,9 +8,10 @@ from .plugin import KnowledgeBasePlugin
 
 # TODO: Serializable
 class Patch:
-    def __init__(self, addr, new_bytes):
+    def __init__(self, addr, new_bytes, comment: Optional[str]=None):
         self.addr = addr
         self.new_bytes = new_bytes
+        self.comment = comment
 
     def __len__(self):
         return len(self.new_bytes)
@@ -27,11 +30,14 @@ class PatchManager(KnowledgeBasePlugin):
     def __init__(self, kb):
         super().__init__()
 
-        self._patches = SortedDict()
+        self._patches: Dict[int,Patch] = SortedDict()
         self._kb = kb
 
-    def add_patch(self, addr, new_bytes):
-        self._patches[addr] = Patch(addr, new_bytes)
+    def add_patch(self, addr, new_bytes, comment: Optional[str]=None):
+        self._patches[addr] = Patch(addr, new_bytes, comment=comment)
+
+    def add_patch_obj(self, patch: Patch):
+        self._patches[patch.addr] = patch
 
     def remove_patch(self, addr):
         if addr in self._patches:
@@ -84,6 +90,25 @@ class PatchManager(KnowledgeBasePlugin):
     @staticmethod
     def overlap(a0, a1, b0, b1):
         return a0 <= b0 < a1 or a0 <= b1 < a1 or b0 <= a0 < b1
+
+    def apply_patches_to_binary(self, binary_bytes: Optional[bytes]=None, patches: Optional[List[Patch]]=None) -> bytes:
+        if patches is None:
+            patches = sorted(list(self._patches.values()), key=lambda x: x.addr)
+
+        if binary_bytes is None:
+            with open(self._kb._project.loader.main_object.binary, "rb") as f:
+                binary_bytes = f.read()
+        for patch in patches:
+            # convert addr to file offset
+            at = AddressTranslator.from_mva(patch.addr, self._kb._project.loader.main_object)
+            file_offset = at.to_raw()
+
+            if file_offset < len(binary_bytes) and file_offset + len(patch.new_bytes) < len(binary_bytes):
+                binary_bytes = binary_bytes[:file_offset] + \
+                               patch.new_bytes + \
+                               binary_bytes[file_offset + len(patch.new_bytes):]
+
+        return binary_bytes
 
 
 KnowledgeBasePlugin.register_default('patches', PatchManager)
---
+++ tests/test_patches.py
@@ -0,0 +1,28 @@
+import os
+
+import pyvex
+import angr
+from angr.analyses.decompiler.structured_codegen import CBinaryOp
+from angr.analyses.viscosity.edits import BytesEdit, MaskedBytesEdit
+
+
+test_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'binaries', 'tests')
+
+
+def test_patch_vulnerable_fauxware_amd64():
+    binpath = os.path.join(test_location, "x86_64", "vulns", "vulnerable_fauxware")
+    proj = angr.Project(binpath, auto_load_libs=False)
+
+    proj.kb.patches.add_patch(0x40094c, b"\x0a")
+    patched = proj.kb.patches.apply_patches_to_binary()
+
+    # manual patch
+    with open(binpath, "rb") as f:
+        binary_data = f.read()
+    binary_data = binary_data[:0x94c] + b"\x0a" + binary_data[0x94d:]
+
+    assert patched == binary_data
+
+
+if __name__ == "__main__":
+    test_patch_vulnerable_fauxware_amd64()
