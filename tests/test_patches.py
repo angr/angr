61c151a2fa4ed3f1d236fac642eb391df63435fa
Fish <ltfish@users.noreply.github.com>
2020-07-22 03:48:19 -0700

    Patches: Support applying patches to a binary. Add a test case. (#2269)
    * Patches: Support applying patches to a binary. Add a test case.
    
    * Lint the code. Trigger CI.

---
+++ tests/test_patches.py
@@ -0,0 +1,25 @@
+import os
+
+import angr
+
+
+test_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'binaries', 'tests')
+
+
+def test_patch_vulnerable_fauxware_amd64():
+    binpath = os.path.join(test_location, "x86_64", "vulns", "vulnerable_fauxware")
+    proj = angr.Project(binpath, auto_load_libs=False)
+
+    proj.kb.patches.add_patch(0x40094c, b"\x0a")
+    patched = proj.kb.patches.apply_patches_to_binary()
+
+    # manual patch
+    with open(binpath, "rb") as f:
+        binary_data = f.read()
+    binary_data = binary_data[:0x94c] + b"\x0a" + binary_data[0x94d:]
+
+    assert patched == binary_data
+
+
+if __name__ == "__main__":
+    test_patch_vulnerable_fauxware_amd64()
