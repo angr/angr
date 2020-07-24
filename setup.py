7178de25add6097da233fa08ea2342234202e5a4
Fish <fish@cs.ucsb.edu>
2020-07-24 00:35:45 -0700

    Pin to unicorn>=1.0.2rc4.

--- setup.py
+++ setup.py
@@ -129,7 +129,7 @@ if 'bdist_wheel' in sys.argv and '--plat-name' not in sys.argv:
         # https://www.python.org/dev/peps/pep-0425/
         sys.argv.append(name.replace('.', '_').replace('-', '_'))
 
-_UNICORN = "unicorn>=1.0.2rc2"
+_UNICORN = "unicorn>=1.0.2rc4"
 
 setup(
     name='angr',
