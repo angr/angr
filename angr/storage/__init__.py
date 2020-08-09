52499fd467347dc1937a74b4ff9dc60154f0666c
Audrey Dutcher <audrey@rhelmot.io>
2020-08-08 23:01:57 -0700

    Make import work

--- angr/storage/__init__.py
+++ angr/storage/__init__.py
@@ -1,7 +1,10 @@
+# misc
+
+DUMMY_SYMBOLIC_READ_VALUE = 0xc0deb4be
+
+#imports
+
 from .file import SimFile
 from .memory_object import SimMemoryObject
 from .memory_mixins import MemoryMixin, DefaultMemory
 
-# misc
-
-DUMMY_SYMBOLIC_READ_VALUE = 0xc0deb4be
