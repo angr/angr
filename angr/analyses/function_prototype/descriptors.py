f95db1327a3066e1f3a282a67d09a30b584742c8
Alex Lin <17103094+VoidMercy@users.noreply.github.com>
2020-08-19 02:11:26 -0400

    Null terminated based loop variable and buffer identification (#2297)
    * Null terminated based loop variable and string buffer identification
    
    * fix replace and load/get constraints

--- angr/analyses/function_prototype/descriptors.py
+++ angr/analyses/function_prototype/descriptors.py
@@ -2,6 +2,19 @@
 class BaseDescriptor:
     pass
 
+class CounterLoopVariable(BaseDescriptor):
+    def __init__(self, bits:int, lbound:int, interval:int, is_string: str=False):
+        self.bits = bits
+        self.lbound = lbound
+        self.ubound = None
+        self.interval = interval
+        self.is_string = is_string
+
+    def __repr__(self):
+        return "[LoopVar %s-?, step %s]" % (
+            self.lbound if self.lbound is not None else "unknown",
+            self.interval if self.interval is not None else "unknown",
+        )
 
 class SimpleLoopVariable(BaseDescriptor):
     def __init__(self, bits:int, lbound:int, ubound:int, interval:int):
