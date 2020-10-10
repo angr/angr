3f18ab9222d78e53be8c571b2dfee991cc31056d
Audrey Dutcher <audrey@rhelmot.io>
2020-10-09 21:02:12 -0700

    add ppc32 fstat64 struct

--- angr/procedures/linux_kernel/fstat64.py
+++ angr/procedures/linux_kernel/fstat64.py
@@ -1,14 +1,18 @@
 import angr
 
+# these structs can be easily-ish pulled out of qemu/linux-user/syscall_defs.h
+
 class fstat64(angr.SimProcedure):
 
     def run(self, fd, stat_buf): # pylint:disable=arguments-differ
         stat = self.state.posix.fstat(fd)
         # TODO: make arch-neutral
-        if self.arch.bits == 32:
+        if self.arch.name == 'X86':
             self._store_i386(stat_buf, stat)
-        else:
+        elif self.arch.name == 'AMD64':
             self._store_amd64(stat_buf, stat)
+        elif self.arch.name == 'PPC32':
+            self._store_ppc32(stat_buf, stat)
         return 0
 
     def _store_i386(self, stat_buf, stat):
@@ -55,3 +59,27 @@ class fstat64(angr.SimProcedure):
         store(0x78, self.state.solver.BVV(0, 64))
         store(0x80, self.state.solver.BVV(0, 64))
         store(0x88, self.state.solver.BVV(0, 64))
+
+    def _store_ppc32(self, stat_buf, stat):
+        store = lambda offset, val: self.state.memory.store(stat_buf + offset, val, endness=self.state.arch.memory_endness)
+
+        store(0x00, stat.st_dev)
+        store(0x08, stat.st_ino)
+        store(0x10, stat.st_mode)
+        store(0x14, stat.st_nlink)
+        store(0x18, stat.st_uid)
+        store(0x1c, stat.st_gid)
+        store(0x20, stat.st_rdev)
+        store(0x28, self.state.solver.BVV(0, 64))
+        store(0x30, stat.st_size)
+        store(0x38, stat.st_blksize)
+        store(0x3c, self.state.solver.BVV(0, 32))
+        store(0x40, stat.st_blocks)
+        store(0x48, stat.st_atime)
+        store(0x4c, stat.st_atimensec)
+        store(0x50, stat.st_mtime)
+        store(0x54, stat.st_mtimensec)
+        store(0x58, stat.st_ctime)
+        store(0x5c, stat.st_ctimensec)
+        store(0x60, self.state.solver.BVV(0, 32))
+        store(0x64, self.state.solver.BVV(0, 32))
