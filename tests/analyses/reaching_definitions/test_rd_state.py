d31304910ba6b1f48151ae287dc4dff92be9e863
Pamplemousse <xav.maso@gmail.com>
2020-10-30 11:02:37 -0700

    RDA: Favor `unittest` assertions to `nose`
    Signed-off-by: Pamplemousse <xav.maso@gmail.com>

--- tests/analyses/reaching_definitions/test_rd_state.py
+++ tests/analyses/reaching_definitions/test_rd_state.py
@@ -1,7 +1,7 @@
 import os
 import random
 
-import nose
+from unittest import TestCase
 
 import archinfo
 
@@ -25,27 +25,26 @@ class _MockFunctionSubject:
         self.cc = None
         self.content = self._MockFunction()
 
+class TestReachingDefinitionsState(TestCase):
+    def test_initializing_rd_state_for_ppc_without_rtoc_value_should_raise_an_error(self):
+        arch = archinfo.arch_ppc64.ArchPPC64()
+        self.assertRaises(
+           ValueError,
+           ReachingDefinitionsState, arch=arch, subject=_MockFunctionSubject()
+        )
 
-def test_initializing_rd_state_for_ppc_without_rtoc_value_should_raise_an_error():
-    arch = archinfo.arch_ppc64.ArchPPC64()
-    nose.tools.assert_raises(
-       ValueError,
-       ReachingDefinitionsState, arch=arch, subject=_MockFunctionSubject()
-    )
+    def test_initializing_rd_state_for_ppc_with_rtoc_value(self):
+        arch = archinfo.arch_ppc64.ArchPPC64()
+        rtoc_value = random.randint(0, 0xffffffffffffffff)
 
+        state = ReachingDefinitionsState(
+           arch=arch, subject=_MockFunctionSubject(), rtoc_value=rtoc_value
+        )
 
-def test_initializing_rd_state_for_ppc_with_rtoc_value():
-    arch = archinfo.arch_ppc64.ArchPPC64()
-    rtoc_value = random.randint(0, 0xffffffffffffffff)
+        rtoc_offset = arch.registers['rtoc'][0]
+        rtoc_definition = next(iter(
+            state.register_definitions.get_objects_by_offset(rtoc_offset)
+        ))
+        rtoc_definition_value = rtoc_definition.data.get_first_element()
 
-    state = ReachingDefinitionsState(
-       arch=arch, subject=_MockFunctionSubject(), rtoc_value=rtoc_value
-    )
-
-    rtoc_offset = arch.registers['rtoc'][0]
-    rtoc_definition = next(iter(
-        state.register_definitions.get_objects_by_offset(rtoc_offset)
-    ))
-    rtoc_definition_value = rtoc_definition.data.get_first_element()
-
-    nose.tools.assert_equals(rtoc_definition_value, rtoc_value)
+        self.assertEquals(rtoc_definition_value, rtoc_value)
