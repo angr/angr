5ca2f56b459a7599f11929afb6498643ee02034f
Pamplemousse <xav.maso@gmail.com>
2020-10-30 08:37:53 -0700

    ReachingDefinitionsModel: Document to avoid confusion
    Signed-off-by: Pamplemousse <xav.maso@gmail.com>

--- angr/knowledge_plugins/key_definitions/rd_model.py
+++ angr/knowledge_plugins/key_definitions/rd_model.py
@@ -7,6 +7,11 @@ from .live_definitions import LiveDefinitions
 # TODO: Make ReachingDefinitionsModel serializable
 class ReachingDefinitionsModel:
     def __init__(self, func_addr: Optional[int]=None):
+        """
+        Represents the results obtained at the end of a ReachingDefinitionsAnalysis.
+
+        *Note:* Not to confused with ReachingDefinitionsState, which stores the internal state of the analysis while it runs.
+        """
         self.func_addr = func_addr  # do not use. only for pretty-printing
         self.observed_results: Dict[Tuple[str, int, int], LiveDefinitions] = {}
         self.all_definitions = set()
