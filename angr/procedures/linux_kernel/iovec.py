abd709f9defd7375abdc3300b2ad6cb5e5d2a30a
Fish <ltfish@users.noreply.github.com>
2021-01-02 10:12:06 -0700

    Fix a bug in readv(). Close #2447. (#2453)

--- angr/procedures/linux_kernel/iovec.py
+++ angr/procedures/linux_kernel/iovec.py
@@ -17,7 +17,7 @@ class readv(angr.SimProcedure):
         iovcnt = self.state.solver.eval(iovcnt)
         res = 0
         for element in self.state.mem[iovec].struct.iovec.array(iovcnt).resolved:
-            tmpres = self.inline_call(read, fd, element.iov_base, element.iov_len)
+            tmpres = self.inline_call(read, fd, element.iov_base, element.iov_len).ret_expr
             if self.state.solver.is_true(self.state.solver.SLT(tmpres, 0)):
                 return tmpres
 
