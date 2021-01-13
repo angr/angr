123684a5fe7bd3c7a61b571f8578a64d45dedf75
Pamplemousse <xav.maso@gmail.com>
2021-01-13 15:55:09 -0700

    Allow to compute the difference between two `SpOffset`
    Signed-off-by: Pamplemousse <xav.maso@gmail.com>

---
+++ tests/engines/light/test_data.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+from angr.engines.light.data import SpOffset
+
+
+class TestSpOffset(TestCase):
+
+    def test_difference_between_two_sp_offset_is_concrete(self):
+        size = 8
+        first_offset = SpOffset(size, 10)
+        second_offset = SpOffset(size, 20)
+
+        self.assertEqual(first_offset - second_offset, -10)
