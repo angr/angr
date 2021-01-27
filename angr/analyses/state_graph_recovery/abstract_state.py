de502eb77dec41d968185a0b4a2796f15b9243e2
Fish <fishw@asu.edu>
2021-01-27 09:59:54 -0700

    Add the preliminary definitions of APIs.

--- angr/analyses/state_graph_recovery/abstract_state.py
+++ angr/analyses/state_graph_recovery/abstract_state.py
@@ -4,6 +4,16 @@ if TYPE_CHECKING:
     from angr import SimState
 
 
+class AbstractState:
+    """
+    Defines an abstract state in a state graph. An abstract state is defined by a set of fields (AbstractStateFields)
+    and the value of each field.
+    """
+    def __init__(self, fields: AbstractStateFields, values: Dict[str,Any]):
+        self.fields = fields
+        self.values = values
+
+
 class AbstractStateFields:
     def __init__(self, fields: Dict[str,Tuple[int,int]]):
         self.fields = fields
