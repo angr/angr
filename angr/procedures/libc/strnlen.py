ca4f0ea00619e2be6f392c642c7b3d3169e128aa
Kyle ZENG <jkjh1jkjh1@gmail.com>
2021-02-13 19:21:23 -0700

    make pylint happy

--- angr/procedures/libc/strnlen.py
+++ angr/procedures/libc/strnlen.py
@@ -4,7 +4,7 @@ import logging
 l = logging.getLogger(name=__name__)
 
 class strnlen(angr.SimProcedure):
-    def run(self, s, n, wchar=False):
+    def run(self, s, n, wchar=False): #pylint:disable=arguments-differ,unused-argument
         strlen = angr.SIM_PROCEDURES['libc']['strlen']
 
         maxlen = self.state.solver.eval_one(n)
