b94b11cb1d2d922b0671d280f7389a1448c326a0
Fish <fishw@asu.edu>
2021-03-05 10:01:52 -0700

    More documentation.

--- angr/analyses/state_graph_recovery/apis.py
+++ angr/analyses/state_graph_recovery/apis.py
@@ -42,12 +42,40 @@ class AddStatePatch(Patch):
 
 class EditDataPatch(Patch):
     """
-    Edit an existing constant value in the binary. Note that this constant value to be edited may or may not be in a
-    data section. In other words, you may use
+    Edit an existing constant value in the binary. Note that the constant value to be edited may or may not be in a
+    data section. In other words, you may use EditDataPatch to directly edit bytes anywhere in the binary.
+
+    If old_data is specified, it will be used to check against existing data before applying this patch. The patch will
+    not be applied if existing data does not match old_data.
+
+    Example: Changing the 4-byte chunk at 0x83200 from 04 00 00 00 to 13 37 00 00
+
+        0x83200    04 00 00 00
+
+        p = EditDataPatch(0x83200, b"\x13\x37\x00\x00", old_data=b"\x04\x00\x00\x00")
+
+    After applying this patch::
+
+        0x83200    13 37 00 00
     """
+    def __init__(self, addr: int, data: bytes, old_data: Optional[bytes]=None, name: Optional[str]=None):
+        super().__init__(name)
+        self.addr = addr
+        self.data = data
+        self.old_data = old_data
 
 
 class EditInstrPatch(Patch):
     """
-    TODO
+    Edit an existing instruction in the binary. Note that this patch is not always applicable.
+
+    For now, the following edits are supported.
+
+    - Changing comparison operators: (signed to unsigned, unsigned to signed, less than to greater than, less than or
+      equal to to greater than or equal to, etc.)
+    - Nopping instructions.
     """
+    def __init__(self, addr: int, new_instr: str, name: Optional[str]=None):
+        super().__init__(name)
+        self.addr = addr
+        self.new_instr = new_instr
