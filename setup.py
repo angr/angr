03f42a3c087cf33ef8603e53796666f474edb930
Audrey Dutcher <audrey@rhelmot.io>
2021-03-08 11:05:03 -0700

    Bump pinned unicorn version

--- setup.py
+++ setup.py
@@ -130,7 +130,7 @@ if 'bdist_wheel' in sys.argv and '--plat-name' not in sys.argv:
         # https://www.python.org/dev/peps/pep-0425/
         sys.argv.append(name.replace('.', '_').replace('-', '_'))
 
-_UNICORN = "unicorn==1.0.2rc4"
+_UNICORN = "unicorn==1.0.2"
 
 setup(
     name='angr',
