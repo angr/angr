785d805b381e3db82578787fb0705fabbb3173bc
Audrey Dutcher <audrey@rhelmot.io>
2021-03-10 23:08:25 -0700

    Change perf tests to perfharness format

---
+++ tests/perf/perf_concrete_execution.py
@@ -0,0 +1,38 @@
+# Performance tests on concrete code execution without invoking Unicorn engine
+# uses a stripped-down SimEngine to only test the essential pieces
+# TODO also use a stripped-down memory
+
+import os
+import time
+
+import angr
+import claripy
+
+# attempt to turn off claripy debug mode
+if hasattr(claripy, "set_debug"):
+    claripy.set_debug(False)
+
+test_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../../../binaries')
+
+
+class SkinnyEngine(angr.engines.SimEngineFailure,
+                   angr.engines.SimEngineSyscall,
+                   angr.engines.HooksMixin,
+                   angr.engines.HeavyVEXMixin):
+    pass
+
+arch = 'x86_64'
+b = angr.Project(os.path.join(test_location, 'tests', arch, "perf_tight_loops"), auto_load_libs=False)
+state = b.factory.full_init_state(plugins={'registers': angr.state_plugins.SimLightRegisters()},
+                                           remove_options={angr.sim_options.COPY_STATES})
+engine = SkinnyEngine(b)
+
+def main():
+    simgr = b.factory.simgr(state)
+    simgr.explore(engine=engine)
+
+if __name__ == '__main__':
+    tstart = time.time()
+    main()
+    tend = time.time()
+    print('Elapsed: %f sec' % (tend - tstart))
