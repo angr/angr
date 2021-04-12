ab8b0360b28276f990e78db01f8d2be38a5e4b1e
Fish <fishw@asu.edu>
2021-04-12 09:52:30 -0700

    ListPage: Consider endianness of MemoryObjects during merging.

---
+++ tests/test_memory_merge.py
@@ -0,0 +1,56 @@
+import claripy
+
+from angr.storage.memory_mixins import (
+    DataNormalizationMixin,
+    SizeNormalizationMixin,
+    AddressConcretizationMixin,
+    UltraPagesMixin,
+    ListPagesMixin,
+    PagedMemoryMixin,
+    SymbolicMergerMixin,
+    ConvenientMappingsMixin,
+)
+from angr import SimState
+
+class UltraPageMemory(
+    DataNormalizationMixin,
+    SizeNormalizationMixin,
+    AddressConcretizationMixin,
+    SymbolicMergerMixin,
+    ConvenientMappingsMixin,
+    UltraPagesMixin,
+    PagedMemoryMixin,
+):
+    pass
+
+class ListPageMemory(
+    DataNormalizationMixin,
+    SizeNormalizationMixin,
+    AddressConcretizationMixin,
+    SymbolicMergerMixin,
+    ConvenientMappingsMixin,
+    ListPagesMixin,
+    PagedMemoryMixin,
+):
+    pass
+
+
+def test_merge_memory_object_endness():
+
+    for memcls in [UltraPageMemory, ListPageMemory]:
+        state0 = SimState(arch='AMD64', mode='symbolic', plugins={'memory': memcls()})
+        state0.memory.store(0x20000, claripy.BVS("x", 64), endness="Iend_LE")
+
+        state1 = SimState(arch="AMD64", mode="symbolic", plugins={'memory': memcls()})
+        state1.memory.store(0x20000, claripy.BVS("y", 64), endness="Iend_LE")
+
+        state, _, _ = state0.merge(state1)
+        obj = state.memory.load(0x20000, size=8, endness="Iend_LE")
+        assert isinstance(obj, claripy.ast.Base)
+        # the original endness should be respected, and obj.op should not be Reverse
+        assert obj.op == "If"
+
+
+if __name__ == '__main__':
+    test_merge_memory_object_endness()
+    #import sys; sys.exit(0)
