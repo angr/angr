0ad114843213308908f824273f4aa1b097ff2120
Bonnie <fdong12@asu.edu>
2021-04-14 21:52:33 -0700

    implement div function for ARM

--- angr/procedures/libc/div.py
+++ angr/procedures/libc/div.py
@@ -9,14 +9,26 @@ import angr
 class div(angr.SimProcedure):
     #pylint:disable=arguments-differ
 
-    def run(self, x, y):
-        x = _raw_ast(x)
-        y = _raw_ast(y)
-        x = x[31:0]
-        y = y[31:0]
-        remainder = x / y
-        quotient = x % y
-        return claripy.Concat(remainder, quotient)
+    def run(self, x, y, z=None):
+        if self.arch.name == 'ARMHF':
+            x = _raw_ast(x)
+            y= _raw_ast(y)
+            z = _raw_ast(z)
+            y = y[31:0]
+            z = z[31:0]
+            quotient = y / z
+            remainder = y % z
+            self.state.memory.store(x, quotient)
+            self.state.memory.store(x + self.arch.bytes, remainder)
+            return x
+        else:
+            x = _raw_ast(x)
+            y = _raw_ast(y)
+            x = x[31:0]
+            y = y[31:0]
+            quotient = x / y
+            remainder = x % y
+            return claripy.Concat(quotient, remainder)
 
 
 from ...state_plugins.sim_action_object import _raw_ast
