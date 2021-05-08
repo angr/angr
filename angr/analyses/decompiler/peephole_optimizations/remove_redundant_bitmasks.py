590e70184cb9181d0dc9406b233f4987102db294
Fish <ltfish@users.noreply.github.com>
2021-05-07 20:26:22 -0700

    Decompiler: Add a few more optimizations. (#2623)
    * Decompiler: Add a few more optimizations.
    
    * Missed a file.
    
    * No more string comparisons.
    
    * oops
    
    * Lint the code.

--- angr/analyses/decompiler/peephole_optimizations/remove_redundant_bitmasks.py
+++ angr/analyses/decompiler/peephole_optimizations/remove_redundant_bitmasks.py
@@ -1,4 +1,4 @@
-from ailment.expression import BinaryOp, Convert, Const
+from ailment.expression import BinaryOp, Convert, Const, ITE
 
 from .base import PeepholeOptimizationExprBase
 
@@ -20,14 +20,23 @@ class RemoveRedundantBitmasks(PeepholeOptimizationExprBase):
 
         # And(Conv(M->N, expr), full_N_bitmask) ==> Conv(M->N, expr)
         # And(Conv(1->N, bool_expr), bitmask) ==> Conv(1->N, bool_expr)
-        if expr.op == "And" \
-                and isinstance(expr.operands[0], Convert) \
-                and isinstance(expr.operands[1], Const):
-            conv_expr = expr.operands[0]
-            if expr.operands[1].value == _MASKS.get(conv_expr.to_bits, None):
-                return expr.operands[1]
-            if self.is_bool_expr(conv_expr.operand):
-                # useless masking
-                return conv_expr
+        # And(ITE(?, const_expr, const_expr), bitmask) ==> ITE(?, const_expr, const_expr)
+        if expr.op == "And" and isinstance(expr.operands[1], Const):
+            if isinstance(expr.operands[0], Convert):
+                conv_expr = expr.operands[0]
+                if expr.operands[1].value == _MASKS.get(conv_expr.to_bits, None):
+                    return expr.operands[1]
+                if self.is_bool_expr(conv_expr.operand):
+                    # useless masking
+                    return conv_expr
+            if isinstance(expr.operands[0], ITE) and \
+                    isinstance(expr.operands[0].iftrue, Const) and \
+                    isinstance(expr.operands[0].iffalse, Const):
+                # is the masking unnecessary?
+                mask = expr.operands[1].value
+                ite = expr.operands[0]
+                if mask == 0xff and ite.iftrue.value <= 0xff and ite.iffalse.value <= 0xff:
+                    # yes!
+                    return ite
 
         return None
---
+++ angr/analyses/decompiler/peephole_optimizations/remove_redundant_ite_branch.py
@@ -0,0 +1,27 @@
+from ailment.expression import ITE, BinaryOp
+
+from .base import PeepholeOptimizationExprBase
+
+
+class RemoveRedundantITEBranches(PeepholeOptimizationExprBase):
+    __slots__ = ()
+
+    name = "Remove redundant ITE branches"
+    expr_classes = (ITE, )  # all expressions are allowed
+
+    def optimize(self, expr: ITE):
+
+        # ITE(cond, a, ITE(!cond, b, c)) ==> ITE(cond, a, b)
+        if isinstance(expr.iffalse, ITE):
+            # cascading ITEs
+            if isinstance(expr.cond, BinaryOp) and isinstance(expr.iffalse.cond, BinaryOp):
+                # are they negating each other?
+                if expr.cond.op in BinaryOp.COMPARISON_NEGATION and expr.iffalse.cond.op == BinaryOp.COMPARISON_NEGATION[expr.cond.op]:
+                    cond0_operands = expr.cond.operands
+                    cond1_operands = expr.iffalse.cond.operands
+                    if cond0_operands[0].likes(cond1_operands[0]) and cond0_operands[1].likes(cond1_operands[1]):
+                        # YES...
+                        expr = ITE(expr.idx, expr.cond, expr.iffalse.iftrue, expr.iftrue, **expr.tags)
+                        return expr
+
+        return None
