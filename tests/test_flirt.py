e69c0636f1962173c5a876d1032d6ac251b04843
Fish <fishw@asu.edu>
2021-05-25 01:10:24 -0700

    Re-enable _IO_file_open.

--- tests/test_flirt.py
+++ tests/test_flirt.py
@@ -8,15 +8,13 @@ from common import bin_location
 def test_amd64_elf_static_libc_ubuntu_2004():
     binary_path = os.path.join(bin_location, "tests", "x86_64", "elf_with_static_libc_ubuntu_2004_stripped")
     proj = angr.Project(binary_path, auto_load_libs=False, load_debug_info=False)
-    cfg = proj.analyses.CFGFast(show_progressbar=True)
+    cfg = proj.analyses.CFGFast(show_progressbar=True) #, detect_tail_calls=True)
     flirt_path = os.path.join(bin_location, "tests", "x86_64", "libc_ubuntu_2004.sig")
     proj.analyses.Flirt(flirt_path)
 
-    # does not work since we could not find the function 0x4140a0. instead we could 0x414098 because of imperfect nop
-    # matching... fix it later
-    # assert cfg.functions[0x415cc0].name == "_IO_file_open"
-    # assert cfg.functions[0x415cc0].is_default_name is False
-    # assert cfg.functions[0x415cc0].from_signature == "flirt"
+    assert cfg.functions[0x415cc0].name == "_IO_file_open"
+    assert cfg.functions[0x415cc0].is_default_name is False
+    assert cfg.functions[0x415cc0].from_signature == "flirt"
     assert cfg.functions[0x436980].name == "__mempcpy_chk_avx512_no_vzeroupper"
     assert cfg.functions[0x436980].is_default_name is False
     assert cfg.functions[0x436980].from_signature == "flirt"
