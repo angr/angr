84cae29cee00d07d17ae19f6f4675de55d8fb398
Zeming YU <yuzeming@gmail.com>
2021-06-11 03:01:26 +0800

    add `load_from_dwarf` for `knowledge_base` (#2713)
    * add `load_form_dwarf` for `knowledge_base`
    
    * fix test
    
    * fix test
    
    * Update variable_manager.py
    
    * fix typo
    
    * fix list bug
    
    * add test and fix bug
    
    * fix typo
    
    * linting

---
+++ tests/test_kb_plugins_dwarf.py
@@ -0,0 +1,25 @@
+import nose.tools
+import angr
+
+import os
+location = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'binaries', 'tests')
+
+
+def test_kb_plugins_dwarf():
+    p = angr.Project(os.path.join(location, 'x86_64', 'state_merge_0'), load_options={
+        "load_debug_info":True,
+        "auto_load_libs": False,
+    } )
+    nose.tools.assert_is_instance(p.kb.variables, angr.knowledge_plugins.VariableManager)
+    p.kb.variables.load_from_dwarf()
+
+
+    ret = p.kb.variables.global_manager.get_global_variables(6295620)
+    nose.tools.assert_equal(len(ret), 1)
+
+    v = ret.pop()
+    nose.tools.assert_equal(v.name, "buf")
+
+
+if __name__ == '__main__':
+    test_kb_plugins_dwarf()
