74e520cc8cd9ce50ec70b12ba24ec7f455ebb895
Fish <ltfish@users.noreply.github.com>
2021-06-17 21:43:48 -0700

    CFGBase: Speed up _process_irrational_functions. (#2734)
    * CFGBase: Speed up _process_irrational_functions.
    
    * Lint the code.

--- angr/analyses/cfg/cfg_base.py
+++ angr/analyses/cfg/cfg_base.py
@@ -12,7 +12,6 @@ import archinfo
 from archinfo.arch_soot import SootAddressDescriptor
 from archinfo.arch_arm import is_arm_arch, get_real_address_if_arm
 
-from ...sim_options import SYMBOL_FILL_UNCONSTRAINED_REGISTERS, SYMBOL_FILL_UNCONSTRAINED_MEMORY
 from ...knowledge_plugins.functions import FunctionManager, Function
 from ...knowledge_plugins.cfg import IndirectJump, CFGNode, CFGENode, CFGModel  # pylint:disable=unused-import
 from ...misc.ux import deprecated
@@ -1533,7 +1532,7 @@ class CFGBase(Analysis):
             if max_unresolved_jump_addr <= startpoint_addr or max_unresolved_jump_addr >= endpoint_addr:
                 continue
 
-            # scan forward from the endpoint to include any function tail jumps
+            # scan forward from the endpoint to include any function-tail jumps
             # Here is an example:
             # loc_8049562:
             #       mov eax, ebp
@@ -1548,33 +1547,26 @@ class CFGBase(Analysis):
             #       jmp loc_8049562
             #
             last_addr = endpoint_addr
-            tmp_state = self.project.factory.blank_state(mode='fastpath', add_options={
-                SYMBOL_FILL_UNCONSTRAINED_REGISTERS,
-                SYMBOL_FILL_UNCONSTRAINED_MEMORY,
-            })
             while True:
                 try:
                     # do not follow hooked addresses (such as SimProcedures)
                     if self.project.is_hooked(last_addr):
                         break
 
-                    # using successors is slow, but acceptable since we won't be creating millions of blocks here...
-                    tmp_state.ip = last_addr
-                    b = self.project.factory.successors(tmp_state, jumpkind='Ijk_Boring')
-                    if len(b.successors) != 1:
+                    next_block = self._lift(last_addr)
+                    next_block_irsb = next_block.vex_nostmt
+                    if next_block_irsb.jumpkind not in ('Ijk_Boring', 'Ijk_InvalICache'):
                         break
-                    if b.successors[0].history.jumpkind not in ('Ijk_Boring', 'Ijk_InvalICache'):
+                    if not isinstance(next_block_irsb.next, pyvex.IRExpr.Const):
                         break
-                    if b.successors[0].ip.symbolic:
-                        break
-                    suc_addr = b.successors[0].ip._model_concrete
+                    suc_addr = next_block_irsb.next.con.value
                     if max(startpoint_addr, the_endpoint.addr - 0x40) <= suc_addr < the_endpoint.addr + the_endpoint.size:
                         # increment the endpoint_addr
-                        endpoint_addr = b.addr + b.artifacts['irsb_size']
+                        endpoint_addr = next_block.addr + next_block.size
                     else:
                         break
 
-                    last_addr = b.addr + b.artifacts['irsb_size']
+                    last_addr = next_block.addr + next_block.size
 
                 except (SimTranslationError, SimMemoryError, SimIRSBError, SimEngineError):
                     break
@@ -1591,7 +1583,7 @@ class CFGBase(Analysis):
                 if f_addr == func_addr:
                     continue
                 if max_unresolved_jump_addr < f_addr < endpoint_addr and \
-                        all([max_unresolved_jump_addr < b_addr < endpoint_addr for b_addr in f.block_addrs]):
+                        all(max_unresolved_jump_addr < b_addr < endpoint_addr for b_addr in f.block_addrs):
                     if f_addr in functions_to_remove:
                         # this function has already been merged with other functions before... it cannot be merged with
                         # this function anymore
