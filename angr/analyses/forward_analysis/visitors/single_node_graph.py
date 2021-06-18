f6c8d77b25eb2f94f135c2b130a278655b9f7597
Fish <fishw@asu.edu>
2021-06-17 18:40:09 -0700

    Optimize SingleNodeGraphVisitor.

--- angr/analyses/forward_analysis/visitors/single_node_graph.py
+++ angr/analyses/forward_analysis/visitors/single_node_graph.py
@@ -5,11 +5,22 @@ class SingleNodeGraphVisitor(GraphVisitor):
     """
     :param node: The single node that should be in the graph.
     """
+
+    __slots__ = ('node', 'node_returned', )
+
     def __init__(self, node):
         super(SingleNodeGraphVisitor, self).__init__()
         self.node = node
+        self.node_returned = False
 
-        self.reset()
+    def reset(self):
+        self.node_returned = False
+
+    def next_node(self):
+        if self.node_returned:
+            return None
+        self.node_returned = True
+        return self.node
 
     def successors(self, node):
         return [ ]
