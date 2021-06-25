b5a29f07a9803c48621ec7b19c16007991956ce0
AOS002 <ashwin2sudhir@gmail.com>
2021-06-25 14:24:58 -0700

    refactored the code

--- angr/analyses/vtable.py
+++ angr/analyses/vtable.py
@@ -12,7 +12,7 @@ class Vtable:
 class VtableFinder(Analysis):
     def __init__(self):
         self.cfg = self.project.analyses.CFGFast(cross_references=True)
-        self.find_vtables()
+        self.analyze()
 
     def is_cross_referenced(self, addr):
         if addr in self.cfg.kb.xrefs.xrefs_by_dst:
@@ -26,32 +26,43 @@ class VtableFinder(Analysis):
         else:
             return False
 
-    def find_vtables(self):
+    def analyze(self):
+        # finding candidate starting vtable addresses
+        # "current location is referenced from a code segment andits value is a pointer to a function,
+        # then it is marked as astart of vtable"
+        # taken from - Reconstruction of Class Hierarchies for Decompilation of C++ Programs
         list_vtables = []
-        in_vtable = False
-        cur_vtable = None
         for sec in self.project.loader.main_object.sections:
             if sec.name in ['.data.rel.ro', '.rodata', '.data']:
                 for offset in range(0, sec.memsize, self.project.arch.bytes):
                     cur_addr = sec.vaddr + offset
                     possible_func_addr = self.project.loader.memory.unpack_word(cur_addr)
-                    # Check if this address is referenced in the code segment
-                    if self.is_cross_referenced(cur_addr) and not in_vtable:
-                        #check if it is also a function
+                    # check if this address is referenced in the code segment
+                    if self.is_cross_referenced(cur_addr):
+                        # check if it is also a function, if so then it is possibly a vtable start
                         if self.is_function(possible_func_addr):
-                            #found a vtable start_addr
-                            in_vtable = True
-                            cur_vtable = Vtable(cur_addr, self.project.arch.bytes)
-                            cur_vtable.func_addrs.append(possible_func_addr)
-                            list_vtables.append(cur_vtable)
-                    elif in_vtable and self.is_function(possible_func_addr) and not self.is_cross_referenced(cur_addr):
-                        cur_vtable.func_addrs.append(possible_func_addr)
-                        cur_vtable.size += self.project.arch.bytes
-                    elif in_vtable and (not self.is_function(possible_func_addr) or self.is_cross_referenced(cur_addr)):
-                        in_vtable = False
-                        cur_vtable = None
+                            new_vtable = self.create_extract_vtable(cur_addr, sec.memsize)
+                            list_vtables.append(new_vtable)
 
+        print(list_vtables)
+        import ipdb;ipdb.set_trace()
         return list_vtables
+
+    def create_extract_vtable(self, start_addr, sec_size):
+        # using the starting address extracting the vtable
+        # "Other elements of vtable must be unreferenced pointers to function"
+        # "Vtable ends with the first location that is either referencedfrom the program code, or is not a pointer to a function"
+        # taken from - Reconstruction of Class Hierarchies for Decompilation of C++ Programs
+        first_func_addr = self.project.loader.memory.unpack_word(start_addr)
+        cur_vtable = Vtable(start_addr, self.project.arch.bytes, [first_func_addr])
+        for cur_addr in range(start_addr + self.project.arch.bytes, start_addr+sec_size, self.project.arch.bytes):
+            possible_func_addr = self.project.loader.memory.unpack_word(cur_addr)
+            if self.is_function(possible_func_addr) and not self.is_cross_referenced(cur_addr):
+                cur_vtable.func_addrs.append(possible_func_addr)
+                cur_vtable.size += self.project.arch.bytes
+            elif not self.is_function(possible_func_addr) or self.is_cross_referenced(cur_addr):
+                return cur_vtable
+
 
 from angr.analyses import AnalysesHub
 AnalysesHub.register_default('VtableFinder', VtableFinder)
