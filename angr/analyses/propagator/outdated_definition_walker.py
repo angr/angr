a039932754a49dd88b924742fdc5062891481531
Fish <fishw@asu.edu>
2021-08-25 04:49:37 -0700

    Decompiler: Avoid self-replacing Load expressions.

--- angr/analyses/propagator/outdated_definition_walker.py
+++ angr/analyses/propagator/outdated_definition_walker.py
@@ -12,19 +12,30 @@ class OutdatedDefinitionWalker(AILBlockWalker):
     """
     Walks an AIL expression to find outdated definitions.
     """
-    def __init__(self, expr, state: 'PropagatorAILState'):
+    def __init__(self, expr, state: 'PropagatorAILState', avoid: Optional[Expr.Expression]=None):
         super().__init__()
         self.expr = expr
         self.state = state
+        self.avoid = avoid
         self.expr_handlers[Expr.Register] = self._handle_Register
+        self.expr_handlers[Expr.Load] = self._handle_Load
         self.out_dated = False
 
     # pylint:disable=unused-argument
     def _handle_Register(self, expr_idx: int, reg_expr: Expr.Register, stmt_idx: int, stmt: Stmt.Assignment,
                          block: Optional[Block]):
-        v = self.state.load_register(reg_expr)
-        if v is not None:
-            if not self.expr.likes(v):
-                self.out_dated = True
-            elif isinstance(v, Expr.TaggedObject) and v.tags.get('def_at', None) != self.expr.tags.get('def_at', None):
-                self.out_dated = True
+        if self.avoid is not None and reg_expr == self.avoid:
+            self.out_dated = True
+        else:
+            v = self.state.load_register(reg_expr)
+            if v is not None:
+                if not self.expr.likes(v):
+                    self.out_dated = True
+                elif isinstance(v, Expr.TaggedObject) and v.tags.get('def_at', None) != self.expr.tags.get('def_at', None):
+                    self.out_dated = True
+
+    def _handle_Load(self, expr_idx: int, expr: Expr.Load, stmt_idx: int, stmt: Stmt.Statement, block: Optional[Block]):
+        if self.avoid is not None and (expr == self.avoid or expr.addr == self.avoid):
+            self.out_dated = True
+        else:
+            super()._handle_Load(expr_idx, expr, stmt_idx, stmt, block)
