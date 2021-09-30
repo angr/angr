9956138dcf0b032fa85a036872d30acdd4f95ac7
Audrey Dutcher <audrey@rhelmot.io>
2021-09-30 13:26:35 -0700

    ah shit.

--- angr/exploration_techniques/tracer.py
+++ angr/exploration_techniques/tracer.py
@@ -202,7 +202,7 @@ class Tracer(ExplorationTechnique):
             # if we don't know whether there is any slide, we need to identify the slides via heuristics
             for obj in self.project.loader.all_objects:
                 # do not analyze pseudo-objects
-                if obj.binary.binary_basename.startswith('cle##'):
+                if obj.binary_basename.startswith('cle##'):
                     continue
 
                 # heuristic 1: non-PIC  objects are loaded without aslr slides
@@ -239,7 +239,7 @@ class Tracer(ExplorationTechnique):
             # if we know there is no slides, just trust the address in the loader
             for obj in self.project.loader.all_objects:
                 # do not analyze pseudo-objects
-                if obj.binary.binary_basename.startswith('cle##'):
+                if obj.binary_basename.startswith('cle##'):
                     continue
                 self._aslr_slides[obj] = 0
             self._current_slide = 0
