ca1caaf3cc9bccf6bd47380608d80edd4d17487d
Fish <fishw@asu.edu>
2021-10-14 22:13:40 -0700

    Compatibility.

--- angr/misc/import_hooks.py
+++ angr/misc/import_hooks.py
@@ -1,11 +1,34 @@
+# pylint:disable=import-outside-toplevel
 import os.path
 import sys
 import importlib.util
+from importlib.machinery import ModuleSpec
 
 
-class FastPkgResources:
-    @staticmethod
-    def resource_filename(package, resource_name):
+class FastPkgResources():
+
+    def __getattribute__(self, name):
+        try:
+            return object.__getattribute__(self, name)
+        except AttributeError:
+            # fallback to the real pkg_resources
+            remove_fake_pkg_resources()
+            import pkg_resources
+            return getattr(pkg_resources, name)
+
+    def __spec__(self):
+        return ModuleSpec("pkg_resources", None)
+
+    def resource_filename(self, package, resource_name):
+        r = self._resource_filename(package, resource_name)
+        if r is None:
+            # fallback to the real pkg_resources
+            remove_fake_pkg_resources()
+            import pkg_resources
+            return pkg_resources.resource_filename(package, resource_name)
+        return r
+
+    def _resource_filename(self, package, resource_name):
         spec = importlib.util.find_spec(package)
         if spec is None:
             return None
@@ -24,9 +47,9 @@ class FastPkgResources:
 
 def import_fake_pkg_resources(force=False):
     if force or 'pkg_resources' not in sys.modules:
-        sys.modules['pkg_resources'] = FastPkgResources
+        sys.modules['pkg_resources'] = FastPkgResources()
 
 
 def remove_fake_pkg_resources():
-    if sys.modules.get('pkg_resources') is FastPkgResources:
+    if isinstance(sys.modules.get('pkg_resources', None), FastPkgResources):
         sys.modules.pop('pkg_resources')
