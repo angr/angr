eb8dbe2927e48ce38a3f4fd1e2800d168cce590c
mauricesvp <maurice@walny.de>
2021-10-29 11:09:23 +0200

    Add newline back in

--- angr/procedures/libc/strtoul.py
+++ angr/procedures/libc/strtoul.py
@@ -6,3 +6,4 @@ class strtoul(angr.SimProcedure):
         strtol = angr.SIM_PROCEDURES['libc']['strtol']
         result = self.inline_call(strtol, nptr, endptr, base).ret_expr
         return result
+
