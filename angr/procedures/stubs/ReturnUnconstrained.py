2304606eb3c49a7ff51f7d2e55f9732b9d2935e9
Audrey Dutcher <audrey@rhelmot.io>
2021-12-08 16:05:25 -0700

    More ints

--- angr/procedures/stubs/ReturnUnconstrained.py
+++ angr/procedures/stubs/ReturnUnconstrained.py
@@ -13,7 +13,8 @@ class ReturnUnconstrained(angr.SimProcedure):
 
         return_val = kwargs.pop('return_val', None)
         if return_val is None:
-            o = self.state.solver.Unconstrained("unconstrained_ret_%s" % self.display_name, self.state.arch.bits, key=('api', '?', self.display_name))
+            size = self.prototype.returnty.size
+            o = self.state.solver.Unconstrained("unconstrained_ret_%s" % self.display_name, size, key=('api', '?', self.display_name))
         else:
             o = return_val
 
