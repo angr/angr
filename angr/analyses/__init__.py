5524d738f0cffa289f06d81a5747cd343d92f6ad
woadey <ssmits@asu.edu>
2021-12-11 11:17:39 -0700

    Replacing old proximity with the new

--- angr/analyses/__init__.py
+++ angr/analyses/__init__.py
@@ -37,7 +37,6 @@ from .init_finder import InitializationFinder
 from .complete_calling_conventions import CompleteCallingConventionsAnalysis
 from .typehoon import Typehoon
 from .proximity_graph import ProximityGraphAnalysis
-from .new_prox_graph import NewProximityGraphAnalysis
 from .vtable import VtableFinder
 from .find_objects_static import StaticObjectFinder
 from .class_identifier import ClassIdentifier
