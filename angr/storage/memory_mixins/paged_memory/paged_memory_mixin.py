f2bff00a8a239dcc0d035ef5c9a74c5c8f2186cc
icegrave0391 <zhangchuqi1999@gmail.com>
2021-12-21 16:15:08 +0800

    fix changed_pages()

--- angr/storage/memory_mixins/paged_memory/paged_memory_mixin.py
+++ angr/storage/memory_mixins/paged_memory/paged_memory_mixin.py
@@ -479,7 +479,7 @@ class PagedMemoryMixin(MemoryMixin):
         my_pages = set(self._pages)
         other_pages = set(other._pages)
         intersection = my_pages.intersection(other_pages)
-        difference = my_pages.symmetric_difference(other_pages)
+        difference = my_pages.difference(other_pages)
 
         changes = set()
         for pageno in difference:
@@ -502,8 +502,8 @@ class PagedMemoryMixin(MemoryMixin):
         my_pages = set(self._pages)
         other_pages = set(other._pages)
         intersection = my_pages.intersection(other_pages)
-        difference = my_pages.difference(other_pages) | other_pages.difference(my_pages)
-
+        difference = my_pages.symmetric_difference(other_pages)
+        
         changes: Dict[int,Optional[Set[int]]] = dict((d, None) for d in difference)
 
         for pageno in intersection:
