d95f4b75a167578836a56fae4c0550b4b97a0cd8
Audrey Dutcher <audrey@rhelmot.io>
2021-12-21 22:37:26 -0700

    lint

--- angr/concretization_strategies/nonzero.py
+++ angr/concretization_strategies/nonzero.py
@@ -5,7 +5,7 @@ class SimConcretizationStrategyNonzero(SimConcretizationStrategy):
     Concretization strategy that returns any non-zero solution.
     """
 
-    def _concretize(self, memory, addr, extra_constraints=None, **kwargs):
+    def _concretize(self, memory, addr, **kwargs):
         child_constraints = (addr != 0,)
         extra_constraints = kwargs.pop('extra_constraints', None)
         if extra_constraints is not None:
