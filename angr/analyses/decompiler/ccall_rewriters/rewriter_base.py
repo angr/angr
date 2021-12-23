bd398651a3dead82239a6fa37721bfe550d94b86
Fish <fishw@asu.edu>
2021-12-23 10:53:17 -0700

    Add missing files.

---
+++ angr/analyses/decompiler/ccall_rewriters/rewriter_base.py
@@ -0,0 +1,14 @@
+from typing import Optional
+
+import ailment
+
+
+class CCallRewriterBase:
+
+    __slots__ = ('result', )
+
+    def __init__(self, ccall: ailment.Expr.VEXCCallExpression):
+        self.result: Optional[ailment.Expr.Expression] = self._rewrite(ccall)
+
+    def _rewrite(self, ccall: ailment.Expr.VEXCCallExpression) -> Optional[ailment.Expr.Expression]:
+        raise NotImplementedError()
