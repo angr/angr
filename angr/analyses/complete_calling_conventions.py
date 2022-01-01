aee43c043abb7089928c05d897cc17396f60adac
Fish <ltfish@users.noreply.github.com>
2022-01-01 12:15:23 -0700

    CCA: Support function size threshold. (#3050)
    * CCA: Support function size threshold.
    
    * Document CCA.

--- angr/analyses/complete_calling_conventions.py
+++ angr/analyses/complete_calling_conventions.py
@@ -11,9 +11,30 @@ _l = logging.getLogger(name=__name__)
 
 
 class CompleteCallingConventionsAnalysis(Analysis):
+    """
+    Implements full-binary calling convention analysis. During the initial analysis of a binary, you may set
+    `recover_variables` to True so that it will perform variable recovery on each function before performing calling
+    convention analysis.
+    """
 
     def __init__(self, recover_variables=False, low_priority=False, force=False, cfg: Optional[CFGModel]=None,
-                 analyze_callsites: bool=False, skip_signature_matched_functions: bool=False):
+                 analyze_callsites: bool=False, skip_signature_matched_functions: bool=False,
+                 max_function_blocks: Optional[int]=None, max_function_size: Optional[int]=None):
+        """
+
+        :param recover_variables:   Recover variables on each function before performing calling convention analysis.
+        :param low_priority:        Run in the background - periodically release GIL.
+        :param force:               Perform calling convention analysis on functions even if they have calling
+                                    conventions or prototypes already specified (or previously recovered).
+        :param cfg:                 The control flow graph model, which will be passed to CallingConventionAnalysis.
+        :param analyze_callsites:   Consider artifacts at call sites when performing calling convention analysis.
+        :param skip_signature_matched_functions:    Do not perform calling convention analysis on functions that match
+                                                    against existing FLIRT signatures.
+        :param max_function_blocks: Do not perform calling convention analysis on functions with more than the
+                                    specified number of blocks. Setting it to None disables this check.
+        :param max_function_size:   Do not perform calling convention analysis on functions whose sizes are more than
+                                    `max_function_size`. Setting it to None disables this check.
+        """
 
         self._recover_variables = recover_variables
         self._low_priority = low_priority
@@ -21,6 +42,8 @@ class CompleteCallingConventionsAnalysis(Analysis):
         self._cfg = cfg
         self._analyze_callsites = analyze_callsites
         self._skip_signature_matched_functions = skip_signature_matched_functions
+        self._max_function_blocks = max_function_blocks
+        self._max_function_size = max_function_size
 
         self._analyze()
 
@@ -48,6 +71,19 @@ class CompleteCallingConventionsAnalysis(Analysis):
                 if self._skip_signature_matched_functions and func.from_signature:
                     # this function matches against a known library function. skip it.
                     continue
+
+                if self._max_function_size is not None:
+                    func_size = sum(block.size for block in func.blocks)
+                    if func_size > self._max_function_size:
+                        _l.info("Skipping variable recovery for %r since its size (%d) is greater than the cutoff "
+                                "size (%d).", func, func_size, self._max_function_size)
+                        continue
+
+                if self._max_function_blocks is not None:
+                    if len(func.block_addrs_set) > self._max_function_blocks:
+                        _l.info("Skipping variable recovery for %r since its number of blocks (%d) is greater than the "
+                                "cutoff number (%d).", func, len(func.block_addrs_set), self._max_function_blocks)
+                        continue
 
                 # if it's a normal function, we attempt to perform variable recovery
                 if self._recover_variables and self.function_needs_variable_recovery(func):
