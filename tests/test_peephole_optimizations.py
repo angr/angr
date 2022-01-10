05deb7c4e928808124e0ecad156f0bcc44db8d8d
Fish <fishw@asu.edu>
2022-01-10 06:11:02 -0700

    Fix the test case.

--- tests/test_peephole_optimizations.py
+++ tests/test_peephole_optimizations.py
@@ -16,38 +16,19 @@ def test_constant_dereference():
 
     proj = angr.Project(os.path.join(test_location, "armel", "decompiler", "rm"), auto_load_libs=False)
 
-    stmt = ailment.Assignment(None,
-                              ailment.Register(None, None, proj.arch.registers['r0'][0],
-                                               proj.arch.registers['r0'][1] * proj.arch.byte_width,
-                                               ins_addr=0x400100
-                                               ),
-                              ailment.Expr.Load(None, ailment.Expr.Const(None, None, 0xa000, proj.arch.bits),
-                                                proj.arch.bytes, archinfo.Endness.LE
-                                                ),
-                              ins_addr=0x400100,
-                              )
+    expr = ailment.Expr.Load(None, ailment.Expr.Const(None, None, 0xa000, proj.arch.bits),
+                             proj.arch.bytes, archinfo.Endness.LE, ins_addr=0x400100)
     opt = ConstantDereferences(proj)
-    optimized = opt.optimize(stmt)
-    assert isinstance(optimized, ailment.Assignment)
-    assert optimized.dst is stmt.dst
-    assert isinstance(optimized.src, ailment.Const)
-    assert optimized.src.value == 0x183f8
+    optimized = opt.optimize(expr)
+    assert isinstance(optimized, ailment.Const)
+    assert optimized.value == 0x183f8
     assert optimized.tags.get('ins_addr', None) == 0x400100, "Peephole optimizer lost tags."
 
     # multiple cases that no optimization should happen
     # a. Loading a pointer from a writable location
-    stmt = ailment.Assignment(None,
-                              ailment.Register(None, None, proj.arch.registers['r0'][0],
-                                               1,
-                                               ins_addr=0x400100
-                                               ),
-                              ailment.Expr.Load(None, ailment.Expr.Const(None, None, 0x21df4, proj.arch.bits),
-                                                1, archinfo.Endness.LE
-                                                ),
-                              ins_addr=0x400100,
-                              )
+    expr = ailment.Expr.Load(None, ailment.Expr.Const(None, None, 0x21df4, proj.arch.bits), 1, archinfo.Endness.LE)
     opt = ConstantDereferences(proj)
-    optimized = opt.optimize(stmt)
+    optimized = opt.optimize(expr)
     assert optimized is None
 
 
