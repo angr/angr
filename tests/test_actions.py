24c92553007e363c5a5da96d48502d447996b3c9
ASUAD\aganes17 <adersh.g.97@gmail.com>
2022-01-11 14:41:51 -0700

    remove bare functions first part

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -2,18 +2,23 @@ import angr
 from angr import SimState, SIM_PROCEDURES
 from angr.engines import ProcedureEngine
 
+import unittest
+
 FAKE_ADDR = 0x100000
 
-def test_procedure_actions():
-    s = SimState(arch='AMD64')
 
-    s.registers.store('rbx', 2)
-    proc = SIM_PROCEDURES['testing']['retreg'](reg='rbx')
-    succ = ProcedureEngine(None).process(s, procedure=proc)
-    rbx = succ.artifacts['procedure'].ret_expr
-    assert type(rbx) is angr.state_plugins.SimActionObject
-    assert s.solver.eval(rbx) == 2
-    assert rbx.reg_deps == { s.arch.registers['rbx'][0] }
+class TestActions(unittest.TestCase):
+    def test_procedure_actions(self):
+        s = SimState(arch='AMD64')
+
+        s.registers.store('rbx', 2)
+        proc = SIM_PROCEDURES['testing']['retreg'](reg='rbx')
+        succ = ProcedureEngine(None).process(s, procedure=proc)
+        rbx = succ.artifacts['procedure'].ret_expr
+        assert type(rbx) is angr.state_plugins.SimActionObject
+        assert s.solver.eval(rbx) == 2
+        assert rbx.reg_deps == {s.arch.registers['rbx'][0]}
+
 
 if __name__ == '__main__':
-    test_procedure_actions()
+    unittest.main()
--- tests/test_cacher.py
+++ tests/test_cacher.py
@@ -1,35 +1,37 @@
 import tempfile
 import os
 import logging
-
+import unittest
 
 import angr
-
 
 l = logging.getLogger("angr_tests.managers")
 
 location = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'binaries', 'tests')
 
-def broken_cacher():
-    p = angr.Project(os.path.join(location, 'x86_64', 'fauxware'), load_options={'auto_load_libs': False})
 
-    tmp_dir = tempfile.mkdtemp(prefix='test_cacher_container')
-    container = os.path.join(tmp_dir, '%s.cache' % os.path.basename(p.filename))
+class TestCacher(unittest.TestCase):
+    def test_broken_cacher(self):
+        p = angr.Project(os.path.join(location, 'x86_64', 'fauxware'), load_options={'auto_load_libs': False})
 
-    pg = p.factory.simulation_manager()
-    pg.use_technique(angr.exploration_techniques.Cacher(when=0x4006ee, container=container))
-    pg.run()
+        tmp_dir = tempfile.mkdtemp(prefix='test_cacher_container')
+        container = os.path.join(tmp_dir, '%s.cache' % os.path.basename(p.filename))
 
-    pg2 = p.factory.simulation_manager()
-    pg2.use_technique(angr.exploration_techniques.Cacher(container=container))
-    assert pg2.active[0].addr == 0x4006ed
+        pg = p.factory.simulation_manager()
+        pg.use_technique(angr.exploration_techniques.Cacher(when=0x4006ee, container=container))
+        pg.run()
 
-    pg2.run()
+        pg2 = p.factory.simulation_manager()
+        pg2.use_technique(angr.exploration_techniques.Cacher(container=container))
+        assert pg2.active[0].addr == 0x4006ed
 
-    assert len(pg2.deadended) == len(pg.deadended)
-    assert pg2.deadended[0].addr in [s.addr for s in pg.deadended]
-    assert pg2.deadended[1].addr in [s.addr for s in pg.deadended]
-    assert pg2.deadended[2].addr in [s.addr for s in pg.deadended]
+        pg2.run()
+
+        assert len(pg2.deadended) == len(pg.deadended)
+        assert pg2.deadended[0].addr in [s.addr for s in pg.deadended]
+        assert pg2.deadended[1].addr in [s.addr for s in pg.deadended]
+        assert pg2.deadended[2].addr in [s.addr for s in pg.deadended]
+
 
 if __name__ == "__main__":
-    broken_cacher()
+    unittest.main()
