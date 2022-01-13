585eb9c34492a62e36c0701b8345b02b8b58cf83
Adersh <36945281+Adersh97@users.noreply.github.com>
2022-01-13 13:53:15 -0700

    Angr remove bare function Part(1) (#3085)
    * test commit
    
    * remove bare functions first part
    
    * fix lint errors
    
    * add descriptive name for pylint disable
    
    * add decorator to broken func
    
    * fix lint errors

--- tests/test_argc.py
+++ tests/test_argc.py
@@ -1,96 +1,98 @@
+import logging
+import os
+import unittest
+
 import angr
 
-import logging
 l = logging.getLogger("angr_tests")
 
-import os
 test_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'binaries', 'tests')
 
-def test_mips():
-    proj = angr.Project(os.path.join(test_location, 'mips', 'argc_decide'), auto_load_libs=False)
-    r_addr = 0x4006f4
 
-    s = proj.factory.entry_state(args = ['aaa'], env = {"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+# pylint: disable=missing-class-docstring
+# pylint: disable=no-self-use
+class TestArgc(unittest.TestCase):
+    def test_mips(self):
+        proj = angr.Project(os.path.join(test_location, 'mips', 'argc_decide'), auto_load_libs=False)
+        r_addr = 0x4006f4
 
-    assert len(xpl.found) == 1
+        s = proj.factory.entry_state(args=['aaa'], env={"HOME": "/home/angr"})
+        xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
 
-    s = proj.factory.entry_state(args = ['aaa', 'bbb'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+        assert len(xpl.found) == 1
 
-    assert len(xpl.found) == 0
+        s = proj.factory.entry_state(args=['aaa', 'bbb'], env={"HOME": "/home/angr"})
+        xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
 
-def test_mipsel():
-    proj = angr.Project(os.path.join(test_location, 'mipsel', 'argc_decide'), auto_load_libs=False)
-    r_addr = 0x400708
-    s = proj.factory.entry_state(args = ['aaa', 'bbb'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+        assert len(xpl.found) == 0
 
-    assert len(xpl.found) == 1
+    def test_mipsel(self):
+        proj = angr.Project(os.path.join(test_location, 'mipsel', 'argc_decide'), auto_load_libs=False)
+        r_addr = 0x400708
+        s = proj.factory.entry_state(args=['aaa', 'bbb'], env={"HOME": "/home/angr"})
+        xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
 
-    s = proj.factory.entry_state(args = ['aaa'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+        assert len(xpl.found) == 1
 
-    assert len(xpl.found) == 0
+        s = proj.factory.entry_state(args=['aaa'], env={"HOME": "/home/angr"})
+        xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
 
-def test_i386():
-    proj = angr.Project(os.path.join(test_location, 'i386', 'argc_decide'), auto_load_libs=False)
-    r_addr = 0x80483d4
-    s = proj.factory.entry_state(args = ['aaa'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+        assert len(xpl.found) == 0
 
-    assert len(xpl.found) == 1
+    def test_i386(self):
+        proj = angr.Project(os.path.join(test_location, 'i386', 'argc_decide'), auto_load_libs=False)
+        r_addr = 0x80483d4
+        s = proj.factory.entry_state(args=['aaa'], env={"HOME": "/home/angr"})
+        xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
 
-    s = proj.factory.entry_state(args = ['aaa', 'bbb'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+        assert len(xpl.found) == 1
 
-    assert len(xpl.found) == 0
+        s = proj.factory.entry_state(args=['aaa', 'bbb'], env={"HOME": "/home/angr"})
+        xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
 
-def test_amd64():
-    proj = angr.Project(os.path.join(test_location, 'x86_64', 'argc_decide'), auto_load_libs=False)
-    r_addr = 0x4004c7
-    s = proj.factory.entry_state(args = ['aaa'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+        assert len(xpl.found) == 0
 
-    assert len(xpl.found) == 1
+    def test_amd64(self):
+        proj = angr.Project(os.path.join(test_location, 'x86_64', 'argc_decide'), auto_load_libs=False)
+        r_addr = 0x4004c7
+        s = proj.factory.entry_state(args=['aaa'], env={"HOME": "/home/angr"})
+        xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
 
-    s = proj.factory.entry_state(args = ['aaa', 'bbb'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+        assert len(xpl.found) == 1
 
-    assert len(xpl.found) == 0
+        s = proj.factory.entry_state(args=['aaa', 'bbb'], env={"HOME": "/home/angr"})
+        xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
 
-def test_arm():
-    proj = angr.Project(os.path.join(test_location, 'armel', 'argc_decide'), auto_load_libs=False)
-    r_addr = 0x1040c
+        assert len(xpl.found) == 0
 
-    s = proj.factory.entry_state(args = ['aaa'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+    def test_arm(self):
+        proj = angr.Project(os.path.join(test_location, 'armel', 'argc_decide'), auto_load_libs=False)
+        r_addr = 0x1040c
 
-    assert len(xpl.found) == 1
+        s = proj.factory.entry_state(args=['aaa'], env={"HOME": "/home/angr"})
+        xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
 
-    s = proj.factory.entry_state(args = ['aaa', 'bbb'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+        assert len(xpl.found) == 1
 
-    assert len(xpl.found) == 0
+        s = proj.factory.entry_state(args=['aaa', 'bbb'], env={"HOME": "/home/angr"})
+        xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
 
-def test_ppc32():
-    proj = angr.Project(os.path.join(test_location, 'ppc', 'argc_decide'), auto_load_libs=False)
-    r_addr = 0x10000404
+        assert len(xpl.found) == 0
 
-    s = proj.factory.entry_state(args = ['aaa'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+    def test_ppc32(self):
+        proj = angr.Project(os.path.join(test_location, 'ppc', 'argc_decide'), auto_load_libs=False)
+        r_addr = 0x10000404
 
-    assert len(xpl.found) == 1
+        s = proj.factory.entry_state(args=['aaa'], env={"HOME": "/home/angr"})
+        xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
 
-    s = proj.factory.entry_state(args = ['aaa', 'bbb'], env ={"HOME": "/home/angr"})
-    xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+        assert len(xpl.found) == 1
 
-    assert len(xpl.found) == 0
+        s = proj.factory.entry_state(args=['aaa', 'bbb'], env={"HOME": "/home/angr"})
+        xpl = proj.factory.simulation_manager(s).explore(find=r_addr)
+
+        assert len(xpl.found) == 0
+
 
 if __name__ == "__main__":
-    test_mips()
-    test_mipsel()
-    test_arm()
-    test_i386()
-    test_amd64()
-    test_ppc32()
+    unittest.main()
