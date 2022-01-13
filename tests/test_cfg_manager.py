c5ad0ac60fd34602d2e8a5c73abf4db9be2feec7
ASUAD\aganes17 <adersh.g.97@gmail.com>
2022-01-13 12:12:32 -0700

    remove bare functions (part 2)

--- tests/test_cfg_manager.py
+++ tests/test_cfg_manager.py
@@ -1,24 +1,31 @@
 import os
-
+import unittest
 
 from angr.project import Project
 
 
-def test_cfg_manager_copies_cfg_graphs():
-    binary_path = os.path.join(
-        os.path.dirname(os.path.realpath(__file__)),
-        '..', '..', 'binaries', 'tests', 'x86_64',
-        'all'
-    )
-    project = Project(binary_path, auto_load_libs=False)
-    _ = project.analyses.CFGFast()
+# pylint: disable=missing-class-docstring
+# pylint: disable=no-self-use
+class TestCfgManager(unittest.TestCase):
+    def test_cfg_manager_copies_cfg_graphs(self):
+        binary_path = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            '..', '..', 'binaries', 'tests', 'x86_64',
+            'all'
+        )
+        project = Project(binary_path, auto_load_libs=False)
+        _ = project.analyses.CFGFast()
 
-    original_cfgs = project.kb.cfgs
-    new_cfgs = project.kb.cfgs.copy()
+        original_cfgs = project.kb.cfgs
+        new_cfgs = project.kb.cfgs.copy()
 
-    original_graph = original_cfgs.cfgs['CFGFast'].graph
-    new_graph = new_cfgs.cfgs['CFGFast'].graph
+        original_graph = original_cfgs.cfgs['CFGFast'].graph
+        new_graph = new_cfgs.cfgs['CFGFast'].graph
 
-    assert original_graph.edges() == new_graph.edges()
-    assert original_graph.nodes() == new_graph.nodes()
-    assert not original_graph is new_graph
+        assert original_graph.edges() == new_graph.edges()
+        assert original_graph.nodes() == new_graph.nodes()
+        assert not original_graph is new_graph
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_cfgfast_soot.py
+++ tests/test_cfgfast_soot.py
@@ -1,25 +1,26 @@
 import os
+import unittest
+
 import angr
 
 test_location = os.path.join(os.path.dirname(os.path.realpath(str(__file__))), '..', '..', 'binaries', 'tests')
 
-def test_simple1():
-    binary_path = os.path.join(test_location, "java", "simple1.jar")
-    p = angr.Project(binary_path, main_opts={'entry_point': 'simple1.Class1.main'}, auto_load_libs=False)
-    cfg = p.analyses.CFGFastSoot()
-    assert cfg.graph.nodes()
 
+# pylint: disable=missing-class-docstring
+# pylint: disable=no-self-use
+class TestCfgfastSoot(unittest.TestCase):
+    def test_simple1(self):
+        binary_path = os.path.join(test_location, "java", "simple1.jar")
+        p = angr.Project(binary_path, main_opts={'entry_point': 'simple1.Class1.main'}, auto_load_libs=False)
+        cfg = p.analyses.CFGFastSoot()
+        assert cfg.graph.nodes()
 
-def test_simple2():
-    binary_path = os.path.join(test_location, "java", "simple2.jar")
-    p = angr.Project(binary_path, main_opts={'entry_point': 'simple2.Class1.main'}, auto_load_libs=False)
-    cfg = p.analyses.CFGFastSoot()
-    assert cfg.graph.nodes()
+    def test_simple2(self):
+        binary_path = os.path.join(test_location, "java", "simple2.jar")
+        p = angr.Project(binary_path, main_opts={'entry_point': 'simple2.Class1.main'}, auto_load_libs=False)
+        cfg = p.analyses.CFGFastSoot()
+        assert cfg.graph.nodes()
 
-
-def main():
-    test_simple1()
-    test_simple2()
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
