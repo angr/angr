b105605818c9b8459671346a90f89e772c2b839b
Mohit Patil <91758557+mohitrpatil@users.noreply.github.com>
2022-01-20 09:47:04 -0700

    Remove test generators (#3015)
    * Removed generators in test_checkbyte.py
    
    * Removed generators in test_boyscout.py
    Removed unused list in test_checkbyte.py
    
    * Removed generators in test_relro_perm.py
    
    * Removed generators in test_accuracy.py
    
    * Removed generators in test_0div.py
    
    * Removed lint errors in test_relro_perm.py
    
    * Fixed Lint errors in below files:
    test_0div.py
    test_accuracy.py
    test_echo.py
    
    * Removed generators in test_echo.py
    
    * Removed generators in test_unique.py
    
    * Reverted changes in test_echo.py
    
    * Removed lint errors in test_checkbyte.py
    
    * Removed generators in below files:
    1. test_keystone.py
    2. test_veritesting.py
    
    * Resolved missing ending newline in test_0div.py
    
    * Removed extra comment and double imports in test_keystone.py and test_veritesting.py
    
    * Removed lint errors in below files:
    test_0div.py
    test_boyscout.py
    test_veritesting.py
    
    * Removed generators in test_stochastic.py
    
    * Fixed minor formatting errors in test_stochastic.py

--- tests/test_stochastic.py
+++ tests/test_stochastic.py
@@ -1,4 +1,6 @@
 import os
+import unittest
+
 import angr
 
 location = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'binaries', 'tests')
@@ -13,27 +15,25 @@ criteria = {
     'veritesting_a': lambda input_found: input_found.count(b'B') == 10
 }
 
-def run_stochastic(binary, arch):
-    proj = angr.Project(os.path.join(location, arch, binary),
-                        auto_load_libs=False)
-    simgr = proj.factory.simulation_manager()
-    start_state = simgr.active[0]
-    technique = angr.exploration_techniques.StochasticSearch(start_state)
-    simgr.use_technique(technique)
+class TestRun(unittest.TestCase):
+    def _run_stochastic(self,binary, arch):
+        proj = angr.Project(os.path.join(location, arch, binary),
+                            auto_load_libs=False)
+        simgr = proj.factory.simulation_manager()
+        start_state = simgr.active[0]
+        technique = angr.exploration_techniques.StochasticSearch(start_state)
+        simgr.use_technique(technique)
 
-    def found(simgr):
-        return simgr.active[0].addr == find[binary][arch]
-    simgr.run(until=found)
-    assert simgr.active[0].addr == find[binary][arch]
+        def found(simgr):
+            return simgr.active[0].addr == find[binary][arch]
+        simgr.run(until=found)
+        assert simgr.active[0].addr == find[binary][arch]
 
-    input_found = simgr.active[0].posix.dumps(0)
-    assert criteria[binary](input_found)
+        input_found = simgr.active[0].posix.dumps(0)
+        assert criteria[binary](input_found)
 
-def test_stochastic():
-    for binary in find:
-        for arch in find[binary]:
-            yield run_stochastic, binary, arch
+    def test_stochastic(self):
+        self._run_stochastic('veritesting_a','x86_64')
 
 if __name__ == "__main__":
-    for test_func, test_binary, test_arch in test_stochastic():
-        test_func(test_binary, test_arch)
+    unittest.main()
--- tests/test_unique.py
+++ tests/test_unique.py
@@ -1,4 +1,5 @@
 import os
+import unittest
 
 
 import angr
@@ -15,26 +16,24 @@ criteria = {
     'veritesting_a': lambda input_found: input_found.count(b'B') == 10
 }
 
-def run_unique(binary, arch):
-    proj = angr.Project(os.path.join(location, arch, binary),
-                        auto_load_libs=False)
-    simgr = proj.factory.simulation_manager()
-    technique = angr.exploration_techniques.UniqueSearch()
-    simgr.use_technique(technique)
+class TestRunUnique(unittest.TestCase):
+    def _run_unique(self,binary, arch):
+        proj = angr.Project(os.path.join(location, arch, binary),
+                            auto_load_libs=False)
+        simgr = proj.factory.simulation_manager()
+        technique = angr.exploration_techniques.UniqueSearch()
+        simgr.use_technique(technique)
 
-    def found(simgr):
-        return simgr.active[0].addr == find[binary][arch]
-    simgr.run(until=found)
-    assert simgr.active[0].addr == find[binary][arch]
+        def found(simgr):
+            return simgr.active[0].addr == find[binary][arch]
+        simgr.run(until=found)
+        assert simgr.active[0].addr == find[binary][arch]
 
-    input_found = simgr.active[0].posix.dumps(0)
-    assert criteria[binary](input_found)
+        input_found = simgr.active[0].posix.dumps(0)
+        assert criteria[binary](input_found)
 
-def test_unique():
-    for binary in find:
-        for arch in find[binary]:
-            yield run_unique, binary, arch
+    def test_unique(self):
+        self._run_unique('veritesting_a','x86_64')
 
 if __name__ == "__main__":
-    for test_func, test_binary, test_arch in test_unique():
-        test_func(test_binary, test_arch)
+    unittest.main()
