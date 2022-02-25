db2436d4b14a3d18f8d225439a20338433de6fe6
ASUAD\aganes17 <adersh.g.97@gmail.com>
2022-02-25 14:33:45 -0700

    revert conflict change

--- tests/test_echo.py
+++ tests/test_echo.py
@@ -9,17 +9,9 @@ l = logging.getLogger("angr.tests")
 
 test_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'binaries', 'tests')
 
-target_arches = {
-    # 'i386',
-    'x86_64',
-    # 'ppc',
-    # 'armel',
-    # 'mips',
-}
-
 
 class TestEcho(unittest.TestCase):
-    def run_echo_haha(self, arch):
+    def _run_echo_haha(self, arch):
         # auto_load_libs can't be disabled as the test fails
         p = angr.Project(os.path.join(test_location, arch, 'echo'), use_sim_procedures=False)
         s = p.factory.full_init_state(mode='symbolic_approximating', args=['echo', 'haha'],
@@ -33,8 +25,7 @@ class TestEcho(unittest.TestCase):
         assert pg.deadended[0].posix.stdout.concretize() == [b'haha\n']
 
     def test_echo_haha(self):
-        for arch in target_arches:
-            yield self.run_echo_haha, arch
+        self._run_echo_haha('x86_64')
 
 
 if __name__ == "__main__":
