af485b8e6c181293234ff6ce05ca1ec5c79bd81b
Audrey Dutcher <audrey@rhelmot.io>
2022-03-11 09:23:27 -0700

    Fix syscall_stub returning the wrong size

--- angr/procedures/stubs/syscall_stub.py
+++ angr/procedures/stubs/syscall_stub.py
@@ -14,7 +14,13 @@ class syscall(angr.SimProcedure):
         if self.successors is not None:
             self.successors.artifacts['resolves'] = resolves
 
-        return self.state.solver.Unconstrained("syscall_stub_%s" % self.display_name, self.state.arch.bits, key=('syscall', '?', self.display_name))
+        # code duplicated from ReturnUnconstrained
+        size = self.prototype.returnty.size
+        # ummmmm do we really want to rely on this behavior?
+        if size is NotImplemented:
+            return None
+        else:
+            return self.state.solver.Unconstrained("syscall_stub_%s" % self.display_name, size, key=('syscall', '?', self.display_name))
 
     def __repr__(self):
         if 'resolves' in self.kwargs:
