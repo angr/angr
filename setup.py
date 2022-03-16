d8788df8a97960d4477f7d94e1f9c9dcb5513a27
Kevin Phoenix <kevin@kphoenix.us>
2022-03-16 13:14:56 -0700

    Remove automatic platform tag on Linux (#3187)

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ except ImportError:
     from distutils.core import setup
     packages = [x.strip('./').replace('/','.') for x in os.popen('find -name "__init__.py" | xargs -n1 dirname').read().strip().split('\n')]
 
-from distutils.util import get_platform
 from distutils.errors import LibError
 from distutils.command.build import build as _build
 from distutils.command.clean import clean as _clean
@@ -118,17 +117,6 @@ try:
     cmdclass['develop'] = develop
 except ImportError:
     pass
-
-if 'bdist_wheel' in sys.argv and '--plat-name' not in sys.argv:
-    sys.argv.append('--plat-name')
-    name = get_platform()
-    if 'linux' in name:
-        # linux_* platform tags are disallowed because the python ecosystem is fubar
-        # linux builds should be built in the centos 5 vm for maximum compatibility
-        sys.argv.append('manylinux1_' + platform.machine())
-    else:
-        # https://www.python.org/dev/peps/pep-0425/
-        sys.argv.append(name.replace('.', '_').replace('-', '_'))
 
 _UNICORN = "unicorn==1.0.2rc4"
 
