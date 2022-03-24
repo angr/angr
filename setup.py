2d6b95eec5f2228befed6afb4a38172aacef0285
Kevin Phoenix <kevin@kphoenix.us>
2022-03-24 16:50:33 -0700

    Use setup.cfg

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-# pylint: disable=no-name-in-module,import-error,unused-variable,missing-class-docstring
+# pylint: disable=missing-class-docstring
 import os
 import sys
 import subprocess
 import pkg_resources
 import shutil
-import platform
 import glob
 from distutils.command.build import build as st_build
 
@@ -93,50 +92,4 @@ cmdclass = {
     'develop': develop,
 }
 
-_UNICORN = "unicorn==1.0.2rc4"
-
-setup(
-    name='angr',
-    version='9.2.0.dev0',
-    python_requires='>=3.6',
-    description='A multi-architecture binary analysis toolkit, with the ability to perform dynamic symbolic execution and various static analyses on binaries',
-    url='https://github.com/angr/angr',
-    packages=find_packages(),
-    install_requires=[
-        'sortedcontainers',
-        'cachetools',
-        # capstone 5.0.0rc2 returns incorrect insn_name for nop instructions in ARM THUMB blocks
-        'capstone>=3.0.5rc2,!=5.0.0rc2',
-        'dpkt',
-        'mulpyplexer',
-        'networkx>=2.0',
-        'progressbar2>=3',
-        'rpyc',
-        'cffi>=1.14.0',
-        _UNICORN,
-        'archinfo==9.2.0.dev0',
-        'claripy==9.2.0.dev0',
-        'cle==9.2.0.dev0',
-        'pyvex==9.2.0.dev0',
-        'ailment==9.2.0.dev0',
-        'GitPython',
-        'psutil',
-        'pycparser>=2.18',
-        'itanium_demangler',
-        'CppHeaderParser',
-        'protobuf>=3.12.0',
-        'nampa',
-        'sympy',
-    ],
-    setup_requires=[_UNICORN, 'pyvex'],
-    extras_require={
-        'AngrDB': ['sqlalchemy'],
-        'pcode': ['pypcode==1.0.5'],
-        ':sys_platform == "win32"': ['colorama'],
-    },
-    cmdclass=cmdclass,
-    include_package_data=True,
-    package_data={
-        'angr': ['lib/*', "py.typed"]
-    }
-)
+setup(cmdclass=cmdclass)
