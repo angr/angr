dcaf3b302d445be386019e3f311ecf91aff6631e
Kevin Phoenix <kevin@kphoenix.us>
2022-03-29 14:59:43 -0700

    Revert to distutils build (#3240)
    * Revert to distutils build
    
    * Implement custom develop to call build

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ import pkg_resources
 import shutil
 import platform
 import glob
+from distutils.command.build import build as st_build
 
 from setuptools import setup, find_packages, Command
-from setuptools.command.build_ext import build_ext as st_build_ext
+from setuptools.command.develop import develop as st_develop
 from setuptools.errors import LibError
 
 if sys.platform == 'darwin':
@@ -64,7 +65,7 @@ def _clean_native():
     for fname in oglob:
         os.unlink(fname)
 
-class build_ext(st_build_ext):
+class build(st_build):
     def run(self, *args):
         self.execute(_build_native, (), msg='Building angr_native')
         super().run(*args)
@@ -81,9 +82,15 @@ class clean_native(Command):
     def run(self, *args):
         self.execute(_clean_native, (), msg='Cleaning angr_native')
 
+class develop(st_develop):
+    def run(self):
+        self.run_command("build")
+        super().run()
+
 cmdclass = {
-    'build_ext': build_ext,
+    'build': build,
     'clean_native': clean_native,
+    'develop': develop,
 }
 
 _UNICORN = "unicorn==1.0.2rc4"
