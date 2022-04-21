a939e0d6dc4594b94345c2abf1fbd412c96f08b3
Kevin Phoenix <kevin@kphoenix.us>
2022-04-21 16:53:33 -0700

    Re-add hardcoded platform tag

--- setup.py
+++ setup.py
@@ -105,8 +105,13 @@ cmdclass = {
     "develop": develop,
 }
 
-if "bdist_wheel" in sys.argv and "--plat-name" not in sys.argv:
-    sys.argv.append("--plat-name")
-    sys.argv.append(get_platform())
+if 'bdist_wheel' in sys.argv and '--plat-name' not in sys.argv:
+    sys.argv.append('--plat-name')
+    name = get_platform()
+    if 'linux' in name:
+        sys.argv.append('manylinux2014_' + platform.machine())
+    else:
+        # https://www.python.org/dev/peps/pep-0425/
+        sys.argv.append(name.replace('.', '_').replace('-', '_'))
 
 setup(cmdclass=cmdclass)
