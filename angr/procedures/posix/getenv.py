224f4bf39b0d9410d0476eaf93cb19c418ca99e0
Zeming Yu <yuzeming@gmail.com>
2022-04-25 18:04:35 -0700

    add test and fix bug

--- angr/procedures/posix/getenv.py
+++ angr/procedures/posix/getenv.py
@@ -20,7 +20,7 @@ class getenv(angr.SimProcedure):
 
         p = self.state.posix.environ
         if p is None:
-            return self.state.solver.BVS(b"getenv__" + name, self.state.arch.bits)
+            return self.state.solver.BVS(b"getenv__" + name, self.state.arch.bits, key=('api', 'getenv', name.decode()))
 
         while True:
             m_line = self.state.memory.load(p, self.state.arch.byte_width, endness=self.arch.memory_endness)
@@ -29,8 +29,8 @@ class getenv(angr.SimProcedure):
             line_len = self.inline_call(strlen, m_line)
             line_expr = self.state.memory.load(m_line, line_len.max_null_index, endness='Iend_BE')
             line = self.state.solver.eval(line_expr, cast_to=bytes)
-            kv = line.split(b'=', maxsplit=2)
+            kv = line.split(b'=', maxsplit=1)
             if len(kv) == 2 and kv[0] == name:
                 return m_line + (name_len.max_null_index + 1)
             p += self.state.arch.bytes
-        return self.state.solver.BVV(0, self.state.arch.bits)
+        return 0
---
+++ tests/test_getenv.py
@@ -0,0 +1,54 @@
+import os
+import logging
+
+import unittest
+
+import angr
+
+l = logging.getLogger("angr.tests.getenv")
+
+test_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'binaries', 'tests')
+
+class TestRunEcho(unittest.TestCase):
+    flag = "!! Bingo this_is_a_string!"
+
+    def _run_getenv_with_env(self,arch):
+        env =  { "PATH":"/home/angr/", "TEST_ENV1":"this_is_a_string!", "JAVA_HOME":"jdk-install-dir"}
+        TEST_name = [ "TEST_ENV1" , "TEST_ENV2" ]
+        p = angr.Project(os.path.join(test_location, arch, 'test_getenv'))
+        s = p.factory.entry_state(env = env)
+        simgr = p.factory.simulation_manager(s)
+        simgr.explore()
+
+        assert len(simgr.deadended) == 1
+
+        output_lines = simgr.deadended[0].posix.dumps(1).decode().splitlines(keepends=False)
+        expect_output = ["# {k}={v}".format(k=k,v=v) for k,v in env.items()] + \
+            [ "{k}: {v}".format(k=k, v=env.get(k,"__NULL__"))  for k in TEST_name ] + \
+                [self.flag]
+        
+        output_lines.sort()
+        expect_output.sort()
+
+        assert output_lines == expect_output
+    
+    def _run_getenv_without_env(self,arch):
+        p = angr.Project(os.path.join(test_location, arch, 'test_getenv'))
+        s = p.factory.entry_state()
+        simgr = p.factory.simulation_manager(s)
+        simgr.explore()
+
+        assert len(simgr.deadended) == 2
+
+        bingo_count = 0
+        for s in simgr.deadended:
+            bingo_count += int(self.flag in s.posix.dumps(1).decode())
+
+        assert bingo_count == 1
+
+    def test_run_x86_64(self):
+        self._run_getenv_with_env('x86_64')
+        self._run_getenv_without_env('x86_64')
+
+if __name__ == "__main__":
+    unittest.main()
