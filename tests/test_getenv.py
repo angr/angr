cacb8e1c3245a8e1ca539e95f7dc4019bccfdc67
Zeming Yu <yuzeming@gmail.com>
2022-04-27 16:08:35 -0700

    add more test

--- tests/test_getenv.py
+++ tests/test_getenv.py
@@ -3,6 +3,8 @@ import logging
 
 import unittest
 
+import claripy
+
 import angr
 
 l = logging.getLogger("angr.tests.getenv")
@@ -10,12 +12,12 @@ l = logging.getLogger("angr.tests.getenv")
 test_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'binaries', 'tests')
 
 class TestRunEcho(unittest.TestCase): #pylint: disable=missing-class-docstring
-    flag = "!! Bingo this_is_a_string!"
+    flag = "this_is_a_string!"
 
-    def _run_getenv_with_env(self,arch):
+    def test_run_getenv_with_env(self):
         env =  { "PATH":"/home/angr/", "TEST_ENV1":"this_is_a_string!", "JAVA_HOME":"jdk-install-dir"}
         TEST_name = [ "TEST_ENV1" , "TEST_ENV2" ]
-        p = angr.Project(os.path.join(test_location, arch, 'test_getenv'))
+        p = angr.Project(os.path.join(test_location, 'x86_64', 'test_getenv'))
         s = p.factory.entry_state(env = env)
         simgr = p.factory.simulation_manager(s)
         simgr.explore()
@@ -25,15 +27,15 @@ class TestRunEcho(unittest.TestCase): #pylint: disable=missing-class-docstring
         output_lines = simgr.deadended[0].posix.dumps(1).decode().splitlines(keepends=False)
         expect_output = ["# {k}={v}".format(k=k,v=v) for k,v in env.items()] + \
             [ "{k}: {v}".format(k=k, v=env.get(k,"__NULL__"))  for k in TEST_name ] + \
-                [self.flag]
+                ["!! Bingo "+self.flag]
 
         output_lines.sort()
         expect_output.sort()
 
         assert output_lines == expect_output
 
-    def _run_getenv_without_env(self,arch):
-        p = angr.Project(os.path.join(test_location, arch, 'test_getenv'))
+    def test_run_getenv_without_env(self):
+        p = angr.Project(os.path.join(test_location, 'x86_64', 'test_getenv'))
         s = p.factory.entry_state()
         simgr = p.factory.simulation_manager(s)
         simgr.explore()
@@ -42,13 +44,28 @@ class TestRunEcho(unittest.TestCase): #pylint: disable=missing-class-docstring
 
         bingo_count = 0
         for s in simgr.deadended:
-            bingo_count += int(self.flag in s.posix.dumps(1).decode())
+            bingo_count += int("Bingo" in s.posix.dumps(1).decode())
 
         assert bingo_count == 1
 
-    def test_run_x86_64(self):
-        self._run_getenv_with_env('x86_64')
-        self._run_getenv_without_env('x86_64')
+    def test_run_getenv_with_symbolic_env(self):
+        flag = claripy.Concat(*[claripy.BVS('flag_%d' % i, 8) for i in range(30)])
+        env =  { "PATH":"/home/angr/", "TEST_ENV1":flag, "JAVA_HOME":"jdk-install-dir"}
+        p = angr.Project(os.path.join(test_location, 'x86_64', 'test_getenv'))
+        s = p.factory.entry_state(env = env)
+        simgr = p.factory.simulation_manager(s)
+        simgr.explore()
+
+        assert len(simgr.deadended) == 2
+
+        solved_flag = []
+        for s in simgr.deadended:
+            if "Bingo" in s.posix.dumps(1).decode():
+                solved_flag.append(s.solver.eval(flag,cast_to=bytes).strip(b'\x00') )
+
+        assert len(solved_flag) == 1
+        assert solved_flag[0].decode() == self.flag
+
 
 if __name__ == "__main__":
     unittest.main()
