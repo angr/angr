e0f08c5ecd7ac19ad9a8a10ee4c3cf1e5bfb5737
Fish <fishw@asu.edu>
2022-06-10 02:38:35 -0700

    MultiValues: Fix a bug leading to inconsistent slot sizes.

--- angr/storage/memory_mixins/paged_memory/pages/multi_values.py
+++ angr/storage/memory_mixins/paged_memory/pages/multi_values.py
@@ -15,10 +15,59 @@ class MultiValues:
             if not isinstance(vs, set):
                 raise TypeError("Each value in offset_to_values must be a set!")
 
-    def add_value(self, offset, value) -> None:
+    def add_value(self, offset: int, value: claripy.ast.Base) -> None:
         if offset not in self.values:
             self.values[offset] = set()
+
+        # if value overlaps with existing values, we need to break them down
+
+        succ_offset = self._adjacent_offset(offset, before=False)
+        if succ_offset is not None:
+            value_end = offset + value.length // 8
+            if value_end > succ_offset:
+                # value is too long. we need to break value into two
+                mid_value_size = succ_offset - offset
+                remaining_value = value[value.length - mid_value_size * 8 - 1 : 0]
+                # update value
+                value = value[value.length - 1: value.length - mid_value_size * 8]
+                self.add_value(succ_offset, remaining_value)
+
+        if self.values[offset]:
+            curr_value_size = next(iter(self.values[offset])).length // 8
+            if curr_value_size > value.length // 8:
+                # we need to break existing values
+                new_curr_values = set()
+                remaining_values = set()
+                for v in self.values[offset]:
+                    new_curr_values.add(v[v.length - 1 : v.length - value.length])
+                    remaining_values.add(v[v.length - value.length - 1 : 0])
+                self.values[offset] = new_curr_values
+                for v in remaining_values:
+                    self.add_value(offset + value.length // 8, v)
+            elif curr_value_size < value.length // 8:
+                # value is too long. we need to break value into two
+                remaining_value = value[value.length - curr_value_size * 8 - 1 : 0]
+                # update value
+                value = value[value.length - 1 : value.length - curr_value_size * 8]
+                self.add_value(offset + curr_value_size, remaining_value)
+
         self.values[offset].add(value)
+
+        pre_offset = self._adjacent_offset(offset, before=True)
+        if pre_offset is not None:
+            pre_values = self.values[pre_offset]
+            pre_values_size = next(iter(pre_values)).length // 8
+            if pre_offset + pre_values_size > offset:
+                # we need to break the preceding values
+                new_pre_value_size = offset - pre_offset
+                new_pre_values = set()
+                remaining_values = set()
+                for v in pre_values:
+                    new_pre_values.add(v[v.length - 1 : v.length - new_pre_value_size * 8])
+                    remaining_values.add(v[v.length - new_pre_value_size * 8 - 1 : 0])
+                self.values[pre_offset] = new_pre_values
+                for v in remaining_values:
+                    self.add_value(offset, v)
 
     def one_value(self) -> Optional[claripy.ast.Base]:
         if len(self.values) == 1 and len(self.values[0]) == 1:
@@ -51,3 +100,30 @@ class MultiValues:
 
     def __repr__(self):
         return f"<{self.__class__.__name__}({self.values})>"
+
+    #
+    # Private methods
+    #
+
+    def _adjacent_offset(self, offset: int, before: bool=True) -> Optional[int]:
+        """
+        Find the offset that is right before or after the given offset.
+
+        :param offset:  The specified offset.
+        :param before:  True if we want to find the offset right before the specified offset, False if we want to find
+                        the offset right after the specified offset.
+        :return:        The adjacent offset as requested. If the requested adjacent offset does not exist, return None.
+        """
+
+        sorted_offsets = list(sorted(self.values.keys()))
+
+        for i, off in enumerate(sorted_offsets):
+            if off == offset:
+                if before:
+                    return sorted_offsets[i - 1] if i > 0 else None
+                else:
+                    return sorted_offsets[i + 1] if i + 1 < len(sorted_offsets) else None
+            if off > offset:
+                # we missed it...
+                return None
+        return None
