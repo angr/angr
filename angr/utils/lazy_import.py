524ad2cb29cabc787e9a5023e1c3e3e8e94ee22b
Fish <fishw@asu.edu>
2022-07-05 10:45:06 -0700

    Lazy import sympy.

---
+++ angr/utils/lazy_import.py
@@ -0,0 +1,12 @@
+import importlib.util
+import sys
+
+
+def lazy_import(name):
+    spec = importlib.util.find_spec(name)
+    loader = importlib.util.LazyLoader(spec.loader)
+    spec.loader = loader
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[name] = module
+    loader.exec_module(module)
+    return module
