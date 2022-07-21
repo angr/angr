17fc72ec20d39d756dae6c6df134bb03f2864a7c
Kevin Phoenix <kevin@kphoenix.us>
2022-07-21 16:53:14 -0700

    AIL: Rewrite test_irsb to handle missing dependencies gracefully (angr/ailment#75)
    * Rewrite test_irsb to handle missing dependencies gracefully
    
    * Fix uneccesary angr requirement for test_convert_from_vex_isrb
    
    * Fix lint
    
    * Fix pylint disables
    
    * Fix lint

--- tests/ailment/test_irsb.py
+++ tests/ailment/test_irsb.py
@@ -1,39 +1,45 @@
-import angr
-import pyvex
-import archinfo
+import unittest
 
 import ailment
 
-block_bytes = bytes.fromhex("554889E54883EC40897DCC488975C048C745F89508400048C745F0B6064000488B45C04883C008488B00BEA70840004889C7E883FEFFFF")
-block_addr = 0x4006c6
+try:
+    import angr
+    import archinfo
+except ImportError:
+    angr = None
 
-def test_convert_from_vex_irsb():
+try:
+    import pyvex
+except ImportError:
+    pyvex = None
 
-    arch = archinfo.arch_from_id('AMD64')
 
-    manager = ailment.Manager(arch=arch)
+class TestIrsb(unittest.TestCase):
+    # pylint: disable=missing-class-docstring
+    block_bytes = bytes.fromhex("554889E54883EC40897DCC488975C048C745F89508400048C745F0B6064000488B45C04883C008488B00BEA70840004889C7E883FEFFFF")  # pylint: disable=line-too-long
+    block_addr = 0x4006c6
 
-    irsb = pyvex.IRSB(block_bytes, block_addr, arch, opt_level=0)
+    @unittest.skipUnless(pyvex, "pyvex required")
+    def test_convert_from_vex_irsb(self):
+        arch = archinfo.arch_from_id('AMD64')
+        manager = ailment.Manager(arch=arch)
+        irsb = pyvex.IRSB(self.block_bytes, self.block_addr, arch, opt_level=0)
+        ablock = ailment.IRSBConverter.convert(irsb, manager)
+        assert ablock  # TODO: test if this conversion is valid
 
-    ablock = ailment.IRSBConverter.convert(irsb, manager)
+    @unittest.skipUnless(
+        angr and hasattr(angr.engines, "UberEnginePcode"),
+        "angr and pypcode required"
+    )
+    def test_convert_from_pcode_irsb(self):
+        arch = archinfo.arch_from_id('AMD64')
+        manager = ailment.Manager(arch=arch)
+        p = angr.load_shellcode(self.block_bytes, arch, self.block_addr, self.block_addr,
+                                engine=angr.engines.UberEnginePcode)
+        irsb = p.factory.block(self.block_addr).vex
+        ablock = ailment.IRSBConverter.convert(irsb, manager)
+        assert ablock  # TODO: test if this conversion is valid
 
-    print(str(ablock))
-
-def test_convert_from_pcode_irsb():
-
-    arch = archinfo.arch_from_id('AMD64')
-
-    manager = ailment.Manager(arch=arch)
-
-    p = angr.load_shellcode(block_bytes, arch, block_addr, block_addr,
-                            engine=angr.engines.UberEnginePcode)
-
-    irsb = p.factory.block(block_addr).vex
-
-    ablock = ailment.IRSBConverter.convert(irsb, manager)
-
-    print(str(ablock))
 
 if __name__ == "__main__":
-    test_convert_from_vex_irsb()
-    test_convert_from_pcode_irsb()
+    unittest.main()
