65b94ed7e35b534fa40e84bc4b6c210522022530
Aydalanche <Aydenrm25@gmail.com>
2022-07-22 21:14:37 -0700

    Added Chroot SimProcedure

---
+++ angr/procedures/posix/chroot.py
@@ -0,0 +1,20 @@
+import angr
+import os
+
+class chroot(angr.SimProcedure):
+    def run(self, file_path):
+        size = 0
+        while True:
+            s_bv = self.state.memory.load(file_path, size)
+            newPath = self.state.solver.eval(s_bv, cast_to=bytes)
+            newPath = str(newPath)
+            if newPath[size:size+1] == "\\":
+                newPath = newPath[2:size]
+                break
+            size += 1
+
+        if not os.path.exists(newPath):
+            return -1
+        
+        self.state.fs.mount('/', angr.SimHostFilesystem(newPath))
+        return 0
