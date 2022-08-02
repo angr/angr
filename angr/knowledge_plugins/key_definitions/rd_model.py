f24a261a1db5a5886dcb46bca9c9d363c21a6e6d
Clasm <wfgibbs@asu.edu>
2022-08-02 12:37:25 -0700

    Add merge function to rd_models

--- angr/knowledge_plugins/key_definitions/rd_model.py
+++ angr/knowledge_plugins/key_definitions/rd_model.py
@@ -27,3 +27,15 @@ class ReachingDefinitionsModel:
         new.all_definitions = self.all_definitions.copy()
         new.all_uses = self.all_uses.copy()
         return new
+
+    def merge(self, model: 'ReachingDefinitionsModel'):
+        for k, v in model.observed_results.items():
+            if k not in self.observed_results:
+                self.observed_results[k] = v
+            else:
+                merged, merge_occured = self.observed_results[k].merge(v)
+                if merge_occured:
+                    self.observed_results[k] = merged
+        self.all_definitions.union(model.all_definitions)
+        self.all_uses.merge(model.all_uses)
+
