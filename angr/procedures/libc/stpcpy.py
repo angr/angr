68929796937f3a3ffbb7f89415986e87a60d206e
Sebastian Schrittwieser <10422786+sschritt@users.noreply.github.com>
2022-08-05 12:22:32 +0200

    Add SimProcedure for stpcpy (#3465)
    * Add SimProcedure for stpcpy
    
    * Fix the lint ci pass

---
+++ angr/procedures/libc/stpcpy.py
@@ -0,0 +1,14 @@
+import angr
+
+class stpcpy(angr.SimProcedure):
+    """stpcpy"""
+    # pylint:disable=arguments-differ
+
+    def run(self, dst, src):
+        strlen = angr.SIM_PROCEDURES['libc']['strlen']
+        strncpy = angr.SIM_PROCEDURES['libc']['strncpy']
+        src_len = self.inline_call(strlen, src)
+
+        ret_expr = self.inline_call(
+            strncpy, dst, src, src_len.ret_expr+1, src_len=src_len.ret_expr).ret_expr+src_len.ret_expr
+        return ret_expr
