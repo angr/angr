e25074d795edc86bb9e694246a08ec76c1167375
Arvind <dnivra@users.noreply.github.com>
2022-08-16 17:33:50 -0700

    More lint fixes

--- angr/procedures/tracer/random.py
+++ angr/procedures/tracer/random.py
@@ -1,16 +1,8 @@
-import angr
-import claripy
-
 from ..cgc.random import random as orig_random
 
 
 class random(orig_random):
-    #pylint:disable=arguments-differ
-
-    def run(self, buf, count, rnd_bytes, concrete_data=None):
-        """
-        This a passthrough to the CGC version which pretty much implements same and more. Removing this requires
-        regenerating angrop caches used by rex and so this is being retained
-        """
-
-        return super(random, self).run(buf, count, rnd_bytes, concrete_data)
+    """
+    This a passthrough to the CGC version. Removing this requires regenerating angrop caches used by rex and so this is
+    being retained.
+    """
