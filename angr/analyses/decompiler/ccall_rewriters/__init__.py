dca045717d839a48ef1edcbce7849d6ad1273e3e
Fish <fishw@asu.edu>
2022-09-04 22:21:19 -0700

    oops

--- angr/analyses/decompiler/ccall_rewriters/__init__.py
+++ angr/analyses/decompiler/ccall_rewriters/__init__.py
@@ -1,9 +1,6 @@
 from .amd64_ccalls import AMD64CCallRewriter
-from .arm_ccalls import ARMCCallRewriter
 
 
 CCALL_REWRITERS = {
     'AMD64': AMD64CCallRewriter,
-    'ARMHF': ARMCCallRewriter,
-    'ARMEL': ARMCCallRewriter,
 }
