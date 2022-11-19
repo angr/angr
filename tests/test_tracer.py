21dd02725fd18839b0b15651bf8b397578fd405b
Arvind <dnivra@users.noreply.github.com>
2022-11-19 16:29:01 -0700

    Update options to enable in test case

--- tests/test_tracer.py
+++ tests/test_tracer.py
@@ -537,7 +537,8 @@ def test_concretize_unsupported_vex_irops():
     pov_file = os.path.join(bin_location, "tests_data", "cgc_povs", "CROMU_00020_POV_00000.xml")
     output = (b"\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x15\x00\x00\x00\x00\x00\x00\x00\x15" +
               b"\x00\x00\x00\x00\x00\x00\x00\x15\x00\x00\x00\x00\x00\x00\x00")
-    add_options = set([angr.options.UNSUPPORTED_FORCE_CONCRETIZE])
+    add_options = {angr.options.UNSUPPORTED_FORCE_CONCRETIZE, angr.options.UNICORN_HANDLE_CGC_RECEIVE_SYSCALL,
+                   angr.options.UNICORN_HANDLE_SYMBOLIC_ADDRESSES, angr.options.UNICORN_HANDLE_SYMBOLIC_CONDITIONS}
     trace_cgc_with_pov_file(binary, "tracer_concretize_unsupported_vex_irops", pov_file, output, add_options=add_options)
 
 def test_skip_some_symbolic_memory_writes():
