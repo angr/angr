0301519906b030bd05f2795db140d83787a40e55
mahaloz <zionbasque@gmail.com>
2022-11-18 23:57:17 -0700

    Added a tracking system for Gotos generated by simplifying regions

--- angr/analyses/decompiler/region_simplifiers/goto.py
+++ angr/analyses/decompiler/region_simplifiers/goto.py
@@ -1,5 +1,6 @@
 # pylint:disable=unused-argument,arguments-differ
 from typing import Set
+import logging
 
 import ailment
 
@@ -7,13 +8,24 @@ from ..sequence_walker import SequenceWalker
 from ..structuring.structurer_nodes import SequenceNode, CodeNode, MultiNode, LoopNode, ConditionNode, \
     CascadingConditionNode
 from .node_address_finder import NodeAddressFinder
+from ....knowledge_plugins.gotos import Goto
+
+
+l = logging.getLogger(name=__name__)
 
 
 class GotoSimplifier(SequenceWalker):
     """
     Remove unnecessary Jump statements.
+    This simplifier also has the side effect of detecting Gotos that can't be reduced in the 
+    structuring and eventual decompilation output. Because of this, when this analysis is run, 
+    gotos in decompilation will be detected and stored in the kb.gotos. See the 
+    _handle_irreducible_goto function below.
+
+    TODO:
+    Move the recording of Gotos outside of this function
     """
-    def __init__(self, node):
+    def __init__(self, node, function=None, kb=None):
         handlers = {
             SequenceNode: self._handle_sequencenode,
             CodeNode: self._handle_codenode,
@@ -23,6 +35,8 @@ class GotoSimplifier(SequenceWalker):
             CascadingConditionNode: self._handle_cascadingconditionnode,
             ailment.Block: self._handle_block,
         }
+        self._function = function
+        self._kb = kb
 
         super().__init__(handlers)
         self._node_addrs: Set[int] = NodeAddressFinder(node).addrs
@@ -92,6 +106,7 @@ class GotoSimplifier(SequenceWalker):
 
     def _handle_block(self, block, successor=None, **kwargs):  # pylint:disable=no-self-use
         """
+        This will also record irreducible gotos into the kb if found.
 
         :param ailment.Block block:
         :return:
@@ -108,7 +123,19 @@ class GotoSimplifier(SequenceWalker):
                     can_remove = True
                 else:
                     can_remove = False
+                    self._handle_irreducible_goto(block, goto_stmt)
 
                 if can_remove:
                     # we can remove this statement
                     block.statements = block.statements[:-1]
+
+    def _handle_irreducible_goto(self, block, goto_stmt: ailment.Stmt.Jump):
+        if not self._kb or not self._function:
+            l.debug(f"Unable ot store a goto at {block.addr} because simplifier is kb or functionless")
+            return
+
+        goto = Goto(addr=block.addr, target_addr=goto_stmt.target.value)
+        l.debug(f"Storing {goto} in kb.gotos")
+        self._kb.gotos.locations[self._function.addr].add(
+            goto
+        )
