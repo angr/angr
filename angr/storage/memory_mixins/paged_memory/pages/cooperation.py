88de7813f7f477b44e26b1c34af1b1a2428632c0
Arvind <dnivra@users.noreply.github.com>
2023-01-03 18:28:38 -0700

    Fix bug in decomposing large symbolic BVs before storing in memory (#3716)
    * Do not assume elements are byte sized when decomposing symbolic BVs with
    Concat operator
    
    * Fix next element size computation and reduce number of Concat performed

--- angr/storage/memory_mixins/paged_memory/pages/cooperation.py
+++ angr/storage/memory_mixins/paged_memory/pages/cooperation.py
@@ -101,12 +101,29 @@ class MemoryObjectMixin(CooperationBase):
             memory_object = SimMemoryObject(data, cur_addr, endness, byte_width=byte_width)
         else:
             memory_object = SimLabeledMemoryObject(data, cur_addr, endness, byte_width=byte_width, label=label)
+
+        if data.symbolic and data.op == 'Concat':
+            next_elem_size_left = data.args[0].size() // 8
+            next_elem_index = 0
+
         size = yield
         while True:
             if data.symbolic and data.op == 'Concat' and data.size() > size * 8:
-                start_offset = cur_addr - addr
                 # Generate new memory object with only size bytes to speed up extracting bytes
-                cur_data = data.args[start_offset].concat(*data.args[start_offset + 1:start_offset + size])
+                cur_data_size = 0
+                cur_data = []
+                while cur_data_size < size:
+                    if next_elem_size_left == 0:
+                        next_elem_index += 1
+
+                    next_elem = data.args[next_elem_index]
+                    cur_data.append(next_elem)
+                    next_elem_size_left = next_elem.size() // 8
+                    added_size = min(size - cur_data_size, next_elem.size() // 8)
+                    cur_data_size += added_size
+                    next_elem_size_left = next_elem_size_left - added_size
+
+                cur_data = claripy.Concat(*cur_data)
                 if label is None:
                     memory_object = SimMemoryObject(cur_data, cur_addr, endness, byte_width=byte_width)
                 else:
