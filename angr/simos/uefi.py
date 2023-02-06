b65744c434bd1568c2d7ad9b7ff1c28207b45e23
Audrey Dutcher <audrey@rhelmot.io>
2023-02-06 10:21:19 -0700

    in-progress

---
+++ angr/simos/uefi.py
@@ -0,0 +1,79 @@
+from typing import TYPE_CHECKING
+
+from . import SimOS
+from ..sim_type import parse_file
+
+headers = parse_file(
+    """
+typedef struct {
+    UINT64 Signature;
+    UINT32 Revision;
+    UINT32 HeaderSize;
+    UINT32 CRC32;
+    UINT32 Reserved;
+} EFI_TABLE_HEADER;
+
+typedef struct {
+    EFI_TABLE_HEADER Hdr;
+    //
+    // Time Services
+    //
+    EFI_GET_TIME GetTime;
+    EFI_SET_TIME SetTime;
+    EFI_GET_WAKEUP_TIME GetWakeupTime;
+    EFI_SET_WAKEUP_TIME SetWakeupTime;
+    //
+    // Virtual Memory Services
+    //
+    EFI_SET_VIRTUAL_ADDRESS_MAP SetVirtualAddressMap;
+    EFI_CONVERT_POINTER ConvertPointer;
+    //
+    // Variable Services
+    //
+    EFI_GET_VARIABLE GetVariable;
+    EFI_GET_NEXT_VARIABLE_NAME GetNextVariableName;
+    EFI_SET_VARIABLE SetVariable;
+    //
+    // Miscellaneous Services
+    //
+    EFI_GET_NEXT_HIGH_MONO_COUNT GetNextHighMonotonicCount;
+    EFI_RESET_SYSTEM ResetSystem;
+    //
+    // UEFI 2.0 Capsule Services
+    //
+    EFI_UPDATE_CAPSULE UpdateCapsule;
+    EFI_QUERY_CAPSULE_CAPABILITIES QueryCapsuleCapabilities;
+    //
+    // Miscellaneous UEFI 2.0 Service
+    //
+    EFI_QUERY_VARIABLE_INFO QueryVariableInfo;
+} EFI_RUNTIME_SERVICES;
+
+typedef struct {
+    EFI_TABLE_HEADER Hdr;
+    CHAR16 *FirmwareVendor;
+    UINT32 FirmwareRevision;
+    EFI_HANDLE ConsoleInHandle;
+    EFI_SIMPLE_TEXT_INPUT_PROTOCOL *ConIn;
+    EFI_HANDLE ConsoleOutHandle;
+    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut;
+    EFI_HANDLE StandardErrorHandle;
+    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *StdErr;
+    EFI_RUNTIME_SERVICES *RuntimeServices;
+    EFI_BOOT_SERVICES *BootServices;
+    UINTN NumberOfTableEntries;
+    EFI_CONFIGURATION_TABLE *ConfigurationTable;
+} EFI_SYSTEM_TABLE;
+"""
+)
+
+if TYPE_CHECKING:
+    import angr
+
+
+class SimUEFI(SimOS):
+    def __init__(self, project: "angr.Project", name=None):
+        super().__init__(project, name)
+
+    def configure_project(self):
+        super().configure_project()
