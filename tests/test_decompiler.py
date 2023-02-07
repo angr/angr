2751cfaf487fbd2e5f94b55ac0ca52dc316c295f
Fish <fishw@asu.edu>
2023-02-06 22:17:05 -0700

    Phoenix: Fix premature last-resort refinement for regions with loop ancestors.

--- tests/test_decompiler.py
+++ tests/test_decompiler.py
@@ -2047,6 +2047,19 @@ class TestDecompiler(unittest.TestCase):
         assert "goto " not in d.codegen.text
 
     @structuring_algo("phoenix")
+    def test_decompiling_printenv_main(self, decompiler_options=None):
+        # when a subgraph inside a loop cannot be structured, instead of entering last-resort refinement, we should
+        # return the subgraph and let structuring resume with the knowledge of the loop.
+        # otherwise, in this function, we will see a goto while in reality we do not need any gotos.
+        bin_path = os.path.join(test_location, "x86_64", "decompiler", "printenv.o")
+        proj = angr.Project(bin_path, auto_load_libs=False)
+        cfg = proj.analyses.CFGFast(normalize=True, data_references=True)
+        f = proj.kb.functions["main"]
+        d = proj.analyses[Decompiler].prep()(f, cfg=cfg.model, options=decompiler_options)
+        self._print_decompilation_result(d)
+        assert "goto " not in d.codegen.text
+
+    @structuring_algo("phoenix")
     def test_reverting_switch_lowering_cksum_digest_print_filename(self, decompiler_options=None):
         bin_path = os.path.join(test_location, "x86_64", "decompiler", "cksum-digest.o")
         proj = angr.Project(bin_path, auto_load_libs=False)
