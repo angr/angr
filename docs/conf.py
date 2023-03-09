5051023056aee3ccef7be1446f0aeefbb250bf66
Kevin Phoenix <kevin@kphoenix.us>
2023-03-08 18:06:10 -0700

    Add in-tree docs (#3849)
    * Add in-tree docs
    
    * Add build dependencies to RTD build
    
    * Also install newer pip
    
    * Install angrdb and pcode when building docs
    
    * Sync ci docs with rtd config
    
    * Use pipe for multiline string
    
    * Fix typo
    
    * Replace copyright with project_copyright
    
    * Ignore type aliases in sphinx coverage
    
    * Add links to plugin documentation
    
    * Use coverage_ignore_pyobjects to ignore aliases
    
    * Attribute copyright to contributors

---
+++ docs/conf.py
@@ -0,0 +1,59 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import datetime
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "angr"
+project_copyright = f"{datetime.datetime.now().year}, The angr Project contributors"
+author = "The angr Project"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx_autodoc_typehints",
+    "myst_parser",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+# -- Options for autodoc -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#configuration
+autoclass_content = "class"
+autodoc_default_options = {
+    "members": True,
+    "member-order": "bysource",
+    "inherited-members": True,
+    "show-inheritance": True,
+    "undoc-members": True,
+}
+autodoc_inherit_docstrings = True
+autodoc_typehints = "both"
+
+# -- Options for coverage ----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/extensions/coverage.html
+coverage_write_headline = False
+
+coverage_ignore_pyobjects = [
+    "angr.analyses.decompiler.structured_codegen.c.StructuredCodeGenerator",  # Alias to CStructuredCodeGenerator
+    "angr.sim_type.SimTypeFixedSizeArray",  # Alias to SimTypeArray
+]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
