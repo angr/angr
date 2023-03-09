402beb15ba785447a7b2172db42c8ba58130380b
Colin Stephenne <colin.stephenne@zoho.com>
2023-03-09 15:46:09 -0500

    Add 3 concretization strategies

---
+++ angr/concretization_strategies/logging.py
@@ -0,0 +1,17 @@
+from . import SimConcretizationStrategy
+import logging
+
+class SimConcretizationStrategyLogging(SimConcretizationStrategy):
+    def __init__(self, strategy: SimConcretizationStrategy, is_read: bool):
+        super().__init__()
+        self._strategy = strategy
+        self._is_read = is_read
+
+    def _concretize(self, memory, addr, **kwargs):
+        answers = self._strategy._concretize(memory, addr, **kwargs)
+        if answers is not None:
+            if self._is_read:
+                logging.debug(f"Read strategy {type(self._strategy).__name__} on {addr} gave [{', '.join(map(hex, answers))}]")
+            else:
+                logging.debug(f"Write strategy {type(self._strategy).__name__} on {addr} gave [{', '.join(map(hex, answers))}]")
+        return answers
