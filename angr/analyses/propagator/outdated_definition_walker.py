f4a63574076c761d6180ab12f810480945eb023a
Fish <fishw@asu.edu>
2023-03-11 22:25:46 -0700

    Implement memory read checks for OutdatedDefinitionWalker.

--- angr/analyses/propagator/outdated_definition_walker.py
+++ angr/analyses/propagator/outdated_definition_walker.py
@@ -1,4 +1,4 @@
-from typing import Optional, Any, Callable, TYPE_CHECKING
+from typing import Optional, Callable, TYPE_CHECKING
 
 from ailment import Block, Stmt, Expr
 
@@ -86,46 +86,6 @@ class OutdatedDefinitionWalker(AILBlockWalker):
             if codelocs_defat != codelocs_currentloc:
                 self.out_dated = True
 
-    @staticmethod
-    def _check_store_precedes_load(store_defat: Optional[CodeLocation], load_defat: Optional[CodeLocation]) -> bool:
-        """
-        Check if store precedes load based on their AIL statement IDs.
-        """
-        if store_defat is None or load_defat is None:
-            return True
-        return store_defat.block_addr == load_defat.block_addr and store_defat.stmt_idx <= load_defat.stmt_idx
-
-    @staticmethod
-    def _check_global_store_conflicts_load(
-        store_block_addr: int,
-        store_stmt_idx: int,
-        addr: Any,
-        store: Stmt.Store,
-        load_block_addr: int,
-        load_stmt_idx: int,
-        load: Expr.Load,
-    ) -> bool:
-        """
-        Check if the load may conflict with any existing stores that happened in the past.
-
-        :param addr:
-        :param size:
-        :param store:
-        :param load:
-        :return:
-        """
-        if store_block_addr == load_block_addr and store_stmt_idx >= load_stmt_idx:
-            written = set()
-            if isinstance(addr, Expr.Const) and isinstance(store.size, int):
-                written |= {addr.value + i for i in range(store.size)}
-            else:
-                return False  # FIXME: This is unsafe
-            if isinstance(load.addr, Expr.Const) and isinstance(load.size, int):
-                read = {load.addr.value + i for i in range(load.size)}
-                return bool(written.intersection(read))
-            return True
-        return False
-
     def _handle_Load(self, expr_idx: int, expr: Expr.Load, stmt_idx: int, stmt: Stmt.Statement, block: Optional[Block]):
         if self.avoid is not None and (
             expr == self.avoid or expr.addr == self.avoid
@@ -133,62 +93,61 @@ class OutdatedDefinitionWalker(AILBlockWalker):
             self.out_dated = True
         elif isinstance(expr.addr, Expr.StackBaseOffset):
             sp_offset = self.extract_offset_to_sp(expr.addr)
+
             if sp_offset is not None:
-                # if expr.addr can be resolved to a concrete stack offset, perform the read and detect if the variables
-                # on the stack are updated after their values are loaded into self.expr
-                curr_stackvar = self.state.load_stack_variable(sp_offset, expr.size, endness=expr.endness)
-                if curr_stackvar is None:
-                    # the variable does not exist!
-                    pass
-                else:
-                    for details in curr_stackvar.offset_and_details.values():
-                        if details.def_at is None:
-                            # not sure where the variable was defined - assume it's ok
-                            # breakpoint()
-                            pass
-                        elif not self._check_store_precedes_load(details.def_at, self.expr_defat):
-                            self.out_dated = True
-                            break
-                    if not self.out_dated:
-                        # if there has been a stack store whose address cannot be resolved or concretized, we see if
-                        # this store happens after the current definition. if so, we mark it as out-dated
-                        if self.state.last_stack_store is not None and not self._check_store_precedes_load(
-                            CodeLocation(*self.state.last_stack_store[:2]), self.expr_defat
-                        ):
-                            self.out_dated = True
+                stack_addr = self.livedefs_defat.stack_offset_to_stack_addr(sp_offset)
+                try:
+                    mem_vals: "MultiValues" = self.livedefs_defat.stack_definitions.load(
+                        stack_addr, size=expr.size, endness=expr.endness
+                    )
+                    defs_defat = list(self.livedefs_defat.extract_defs_from_mv(mem_vals))
+                except SimMemoryMissingError:
+                    defs_defat = []
+
+                try:
+                    mem_vals: "MultiValues" = self.livedefs_currentloc.stack_definitions.load(
+                        stack_addr, size=expr.size, endness=expr.endness
+                    )
+                    defs_currentloc = list(self.livedefs_defat.extract_defs_from_mv(mem_vals))
+                except SimMemoryMissingError:
+                    defs_currentloc = []
+
+                codelocs_defat = {def_.codeloc for def_ in defs_defat}
+                codelocs_currentloc = {def_.codeloc for def_ in defs_currentloc}
+
+                if codelocs_defat != codelocs_currentloc:
+                    self.out_dated = True
             else:
                 # in cases where expr.addr cannot be resolved to a concrete stack offset, we play safe and assume
                 # it's outdated
                 self.out_dated = True
-        elif (
-            isinstance(expr.addr, Expr.Const)
-            and self.state.global_stores
-            and any(
-                self._check_global_store_conflicts_load(
-                    store_block_addr,
-                    store_stmt_idx,
-                    addr,
-                    store,
-                    self.expr_defat.block_addr,
-                    self.expr_defat.stmt_idx,
-                    expr,
+
+        elif isinstance(expr.addr, Expr.Const):
+            mem_addr = expr.addr.value
+            try:
+                mem_vals: "MultiValues" = self.livedefs_defat.memory_definitions.load(
+                    mem_addr, size=expr.size, endness=expr.endness
                 )
-                for store_block_addr, store_stmt_idx, addr, store in self.state.global_stores
-            )
-        ):
-            self.out_dated = True
-        elif not isinstance(expr.addr, (Expr.StackBaseOffset, Expr.Const)) and (
-            self.state.global_stores
-            and not all(
-                self._check_store_precedes_load(CodeLocation(store_block_addr, store_stmt_idx), self.expr_defat)
-                for store_block_addr, store_stmt_idx, addr, store in self.state.global_stores
-            )
-            or self.state.last_stack_store is not None
-            and not self._check_store_precedes_load(CodeLocation(*self.state.last_stack_store[:2]), self.expr_defat)
-        ):
-            # check both stack and global stores if the load address is unknown
-            self.out_dated = True
+                defs_defat = list(self.livedefs_defat.extract_defs_from_mv(mem_vals))
+            except SimMemoryMissingError:
+                defs_defat = []
+
+            try:
+                mem_vals: "MultiValues" = self.livedefs_currentloc.memory_definitions.load(
+                    mem_addr, size=expr.size, endness=expr.endness
+                )
+                defs_currentloc = list(self.livedefs_defat.extract_defs_from_mv(mem_vals))
+            except SimMemoryMissingError:
+                defs_currentloc = []
+
+            codelocs_defat = {def_.codeloc for def_ in defs_defat}
+            codelocs_currentloc = {def_.codeloc for def_ in defs_currentloc}
+
+            if codelocs_defat != codelocs_currentloc:
+                self.out_dated = True
+
         else:
+            # the address is not concrete - we check the address instead
             super()._handle_Load(expr_idx, expr, stmt_idx, stmt, block)
 
     def _handle_VEXCCallExpression(
