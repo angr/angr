07fbc8c573b87a9433dc21eeb2a760b36525836e
Lukas Grätz <lukas.graetz@tu-darmstadt.de>
2023-04-11 16:23:11 +0200

    make linter happier

--- tests/test_symbolic_fd.py
+++ tests/test_symbolic_fd.py
@@ -2,18 +2,20 @@
 import os
 import unittest
 import angr
-from angr.state_plugins.posix import Flags
 
 
 test_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "binaries", "tests")
 
 
+def find(s):
+    return s.posix.dumps(1) == b"c0de\n"
+
+def avoid(s):
+    return s.posix.dumps(1) == b"nothing\n"
+
 class TestSymbolicFd(unittest.TestCase):
     def test_symbolic_fd(self):
         project = angr.Project(os.path.join(test_location, "x86_64", "symbolic_fd"))
-
-        find = lambda s: s.posix.dumps(1) == b"c0de\n"
-        avoid = lambda s: s.posix.dumps(1) == b"nothing\n"
 
         for method_name in ("stat_test", "fstat_test", "open_test", "fopen_test", "fdopen_test"):
             addr = project.loader.find_symbol(method_name).rebased_addr
