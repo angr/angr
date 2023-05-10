e110641f614724754b4a28cbd77deaa21c380a03
Fish <ltfish@users.noreply.github.com>
2023-05-11 02:25:28 +0800

    RemoveRedundantITEComparisons: Support CmpNE. (#3957)
    * RemoveRedundantITEComparisons: Support CmpNE.
    
    * oops

--- angr/analyses/decompiler/peephole_optimizations/remove_redundant_ite_comparisons.py
+++ angr/analyses/decompiler/peephole_optimizations/remove_redundant_ite_comparisons.py
@@ -16,6 +16,8 @@ class RemoveRedundantITEComparisons(PeepholeOptimizationExprBase):
     def optimize(self, expr: BinaryOp):
         # ITE(cond, a, b) == a  ==>  cond
         # ITE(cond, a, b) == b  ==>  !cond
+        # ITE(cond, a, b) != a  ==>  !cond
+        # ITE(cond, a, b) != b  ==>  cond
         if expr.op == "CmpEQ":
             if isinstance(expr.operands[0], UnaryOp) and expr.operands[0].op == "Not":
                 negate = True
@@ -23,20 +25,30 @@ class RemoveRedundantITEComparisons(PeepholeOptimizationExprBase):
             else:
                 negate = False
                 inner_expr = expr.operands[0]
+        elif expr.op == "CmpNE":
+            if isinstance(expr.operands[0], UnaryOp) and expr.operands[0].op == "Not":
+                negate = False
+                inner_expr = expr.operands[0].operand
+            else:
+                negate = True
+                inner_expr = expr.operands[0]
+        else:
+            negate = None
+            inner_expr = None
 
-            if isinstance(inner_expr, ITE):
-                a, b = inner_expr.iftrue, inner_expr.iffalse
-                if isinstance(expr.operands[1], Const):
-                    if isinstance(a, Const) and a.value == expr.operands[1].value:
-                        pass
-                    elif isinstance(b, Const) and b.value == expr.operands[1].value:
-                        negate = not negate
-                    else:
-                        return None
+        if inner_expr is not None and isinstance(inner_expr, ITE):
+            a, b = inner_expr.iftrue, inner_expr.iffalse
+            if isinstance(expr.operands[1], Const):
+                if isinstance(a, Const) and a.value == expr.operands[1].value:
+                    pass
+                elif isinstance(b, Const) and b.value == expr.operands[1].value:
+                    negate = not negate
+                else:
+                    return None
 
-                    if not negate:
-                        return inner_expr.cond
-                    else:
-                        return UnaryOp(None, "Not", inner_expr.cond, **expr.tags)
+                if not negate:
+                    return inner_expr.cond
+                else:
+                    return UnaryOp(None, "Not", inner_expr.cond, **expr.tags)
 
         return None
