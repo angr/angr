87b1b906296b922d45438aa7f4c522bbb60051e2
Arvind <dnivra@users.noreply.github.com>
2023-07-22 16:15:38 +0530

    Fix set_cgc_syscall_details setup prototype

--- angr/state_plugins/unicorn_engine.py
+++ angr/state_plugins/unicorn_engine.py
@@ -481,6 +481,7 @@ def _load_native():
             ctypes.c_uint64,
             ctypes.c_uint32,
             ctypes.c_uint64,
+            ctypes.c_uint64,
             ctypes.c_uint32,
             ctypes.c_uint64,
         )
