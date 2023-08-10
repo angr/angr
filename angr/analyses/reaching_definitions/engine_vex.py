angr/angr

commit 82673ff9d3b558369267720ffd6c500660b70e26

Fix zero division issues and flipped arguments in RDA DIV handling (#4059)
Co-authored-by: Florian Magin <fmagin@users.noreply.github.com>

angr/analyses/reaching_definitions/engine_vex.py
@@ -671,12 +671,14 @@ class SimEngineRDVEX(
             # we do not support division between two real multivalues
             r = MultiValues(self.state.top(bits))
         elif expr0_v is None and expr1_v is not None:
-            if expr0.count() == 1 and 0 in expr0:
+            if expr1_v == 0:
+                r = MultiValues(self.state.top(bits))
+            elif expr0.count() == 1 and 0 in expr0:
                 vs = {v / expr1_v for v in expr0[0]}
                 r = MultiValues(offset_to_values={0: vs})
         elif expr0_v is not None and expr1_v is None:
             if expr1.count() == 1 and 0 in expr1:
-                vs = {v / expr0_v for v in expr1[0]}
+                vs = {expr0_v / v for v in expr1[0] if (not v.concrete) or v.concrete_value != 0}
                 r = MultiValues(offset_to_values={0: vs})
         else:
             if expr0_v.concrete and expr1_v.concrete:

commit fef9c950ce41da90c01db77274a57ad2206df260

Fixes a bug accidentally introduced in https://github.com/angr/angr/pull/4059

angr/analyses/reaching_definitions/engine_vex.py
@@ -682,7 +682,7 @@ class SimEngineRDVEX(
             # we do not support division between two real multivalues
             r = MultiValues(self.state.top(bits))
         elif expr0_v is None and expr1_v is not None:
-            if expr1_v == 0:
+            if expr1_v.concrete and expr1_v.concrete_value == 0:
                 r = MultiValues(self.state.top(bits))
             elif expr0.count() == 1 and 0 in expr0:
                 vs = {v / expr1_v for v in expr0[0]}