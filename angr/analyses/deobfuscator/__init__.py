12d22022b5f64de7749e2b801660bb0899f15b58
Fish <fish@cs.ucsb.edu>
2023-09-06 11:26:05 -0700

    String deobfuscation finder: Strike 1

---
+++ angr/analyses/deobfuscator/__init__.py
@@ -0,0 +1,4 @@
+# deobfuscator is a collection of analyses that automatically identifies functions where obfuscation techniques are
+# in-use.
+
+from .string_obf_finder import StringObfuscationFinder
