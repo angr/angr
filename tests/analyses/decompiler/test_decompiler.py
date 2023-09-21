angr/angr

commit 88dfa42be4ff841f58d6e37ffcfdb8ef751ab0db

Propagator: Fix an over-propagation in AIL. (#4191)

angr/knowledge_plugins/propagations/states.py
@@ -67,6 +67,7 @@ class PropagatorState:
         "_store_tops",
         "_gp",
         "_max_prop_expr_occurrence",
+        "model",
         "__weakref__",
     )
 
@@ -83,6 +84,7 @@ class PropagatorState:
         store_tops: bool = True,
         gp: Optional[int] = None,
         max_prop_expr_occurrence: int = 1,
+        model=None,
     ):
         self.arch = arch
         self.gpr_size = arch.bits // arch.byte_width  # size of the general-purpose registers
@@ -99,6 +101,7 @@ class PropagatorState:
         self._gp: Optional[int] = gp  # Value of gp for MIPS32 and 64 binaries
 
         self.project = project
+        self.model = model
 
     def __repr__(self):
         return "<PropagatorState>"
@@ -115,7 +118,7 @@ class PropagatorState:
         max_prop_expr_occurrence=None,
         initial_codeloc=None,
     ):
-        raise NotImplementedError
+        raise NotImplementedError()
 
     def _get_weakref(self):
         return weakref.proxy(self)
@@ -288,6 +291,7 @@ class PropagatorVEXState(PropagatorState):
         store_tops=True,
         gp=None,
         max_prop_expr_occurrence: int = 1,
+        model=None,
     ):
         super().__init__(
             arch,
@@ -298,6 +302,7 @@ class PropagatorVEXState(PropagatorState):
             store_tops=store_tops,
             gp=gp,
             max_prop_expr_occurrence=max_prop_expr_occurrence,
+            model=model,
         )
         self.do_binops = do_binops
         self._registers = (
@@ -328,6 +333,7 @@ class PropagatorVEXState(PropagatorState):
         func_addr=None,
         max_prop_expr_occurrence=None,
         initial_codeloc=None,
+        model=None,
     ):
         state = cls(
             project.arch,
@@ -337,6 +343,7 @@ class PropagatorVEXState(PropagatorState):
             store_tops=store_tops,
             gp=gp,
             max_prop_expr_occurrence=max_prop_expr_occurrence,
+            model=model,
         )
         spoffset_var = SimEngineLight.sp_offset(project.arch.bits, 0)
         state.store_register(
@@ -379,6 +386,7 @@ class PropagatorVEXState(PropagatorState):
             store_tops=self._store_tops,
             gp=self._gp,
             max_prop_expr_occurrence=self._max_prop_expr_occurrence,
+            model=self.model,
         )
 
         return cp
@@ -487,6 +495,7 @@ class PropagatorAILState(PropagatorState):
         block_initial_reg_values=None,
         max_prop_expr_occurrence: int = 1,
         sp_adjusted: bool = False,
+        model=None,
     ):
         super().__init__(
             arch,
@@ -497,6 +506,7 @@ class PropagatorAILState(PropagatorState):
             equivalence=equivalence,
             gp=gp,
             max_prop_expr_occurrence=max_prop_expr_occurrence,
+            model=model,
         )
 
         self._stack_variables = (
@@ -539,6 +549,7 @@ class PropagatorAILState(PropagatorState):
         func_addr=None,
         max_prop_expr_occurrence=None,
         initial_codeloc=None,
+        model=None,
     ):
         state = cls(
             project.arch,
@@ -546,6 +557,7 @@ class PropagatorAILState(PropagatorState):
             only_consts=only_consts,
             gp=gp,
             max_prop_expr_occurrence=max_prop_expr_occurrence,
+            model=model,
         )
         spoffset_var = ailment.Expr.StackBaseOffset(None, project.arch.bits, 0)
         sp_value = PropValue(
@@ -609,6 +621,7 @@ class PropagatorAILState(PropagatorState):
             gp=self._gp,
             max_prop_expr_occurrence=self._max_prop_expr_occurrence,
             sp_adjusted=self._sp_adjusted,
+            model=self.model,
         )
 
         return rd
@@ -731,6 +744,10 @@ class PropagatorAILState(PropagatorState):
         if self.is_top(new):
             # eliminate the past propagation of this expression
             self._replacements[codeloc][old] = self.top(1)  # placeholder
+            for codeloc_ in self._expr_used_locs[new]:
+                for key, replace_with in list(self.model.replacements[codeloc_].items()):
+                    if replace_with == new:
+                        self.model.replacements[codeloc_][key] = self.top(1)
             return
 
         # count-based propagation rule only matters when we are performing a full-function copy propagation
@@ -769,6 +786,10 @@ class PropagatorAILState(PropagatorState):
                 for codeloc_ in self._replacements:
                     if old in self._replacements[codeloc_]:
                         self._replacements[codeloc_][old] = self.top(1)
+                for codeloc_ in self._expr_used_locs[new]:
+                    for key, replace_with in list(self.model.replacements[codeloc_].items()):
+                        if replace_with == new:
+                            self.model.replacements[codeloc_][key] = self.top(1)
 
     def add_equivalence(self, codeloc, old, new):
         eq = Equivalence(codeloc, old, new)

tests/analyses/decompiler/test_decompiler.py
@@ -1972,6 +1972,22 @@ class TestDecompiler(unittest.TestCase):
         assert "amd64g_calculate_condition" not in d.codegen.text  # we should rewrite the ccall to expr == 0
         assert "a1 == a1" not in d.codegen.text
 
+    @structuring_algo("phoenix")
+    def test_decompiling_uname_main(self, decompiler_options=None):
+        bin_path = os.path.join(test_location, "x86_64", "decompiler", "uname.o")
+        proj = angr.Project(bin_path, auto_load_libs=False)
+
+        cfg = proj.analyses.CFGFast(normalize=True, data_references=True)
+
+        f = proj.kb.functions["main"]
+
+        d = proj.analyses[Decompiler].prep()(f, cfg=cfg.model, options=decompiler_options)
+        self._print_decompilation_result(d)
+
+        # the ternary expression should not be propagated. however, we fail to narrow the ebx expression at 0x400c4f,
+        # so we over-propagate the ternary expression once
+        assert d.codegen.text.count("?") in (1, 2)
+
     @for_all_structuring_algos
     def test_decompiling_prototype_recovery_two_blocks(self, decompiler_options=None):
         # we must analyze both 0x40021d and 0x400225 to determine the prototype of xstrtol

angr/analyses/propagator/propagator.py
@@ -229,6 +229,7 @@ class PropagatorAnalysis(ForwardAnalysis, Analysis):  # pylint:disable=abstract-
             func_addr=self._func_addr,
             max_prop_expr_occurrence=1 if self.flavor == "function" else 0,
             initial_codeloc=self._initial_codeloc,
+            model=self.model,
         )
         return self._initial_state
 

commit 5a5fc5ddd94471c0517cda65da7fc81fd632f0f6

Fix insufficient propagation caused by #4191.

angr/knowledge_plugins/propagations/states.py
@@ -741,7 +741,7 @@ class PropagatorAILState(PropagatorState):
             if callexpr_finder.has_call:
                 return
 
-        if self.is_top(new):
+        if self.is_top(new) or self.is_expression_too_deep(new):
             # eliminate the past propagation of this expression
             self._replacements[codeloc][old] = self.top(1)  # placeholder
             for codeloc_ in self._expr_used_locs[new]:
@@ -786,11 +786,29 @@ class PropagatorAILState(PropagatorState):
                 for codeloc_ in self._replacements:
                     if old in self._replacements[codeloc_]:
                         self._replacements[codeloc_][old] = self.top(1)
-                for codeloc_ in self._expr_used_locs[new]:
-                    for key, replace_with in list(self.model.replacements[codeloc_].items()):
-                        if replace_with == new:
-                            self.model.replacements[codeloc_][key] = self.top(1)
 
     def add_equivalence(self, codeloc, old, new):
         eq = Equivalence(codeloc, old, new)
         self._equivalence.add(eq)
+
+    @staticmethod
+    def is_expression_too_deep(expr: ailment.Expr.Expression) -> bool:
+        # determine if the expression is too deep to propagate
+        if expr.depth >= 6:
+            return True
+        if PropagatorAILState.has_ternary_expr(expr):
+            return True
+        return False
+
+    @staticmethod
+    def has_ternary_expr(expr: ailment.Expr.Expression) -> bool:
+        class _has_ternary_expr:
+            v = False
+
+        def _handle_ITE(*args, **kwargs):
+            _has_ternary_expr.v = True
+
+        walker = ailment.AILBlockWalkerBase()
+        walker.expr_handlers[ailment.Expr.ITE] = _handle_ITE
+        walker.walk_expression(expr, 0, None, None)
+        return _has_ternary_expr.v

tests/analyses/decompiler/test_decompiler.py
@@ -2970,6 +2970,40 @@ class TestDecompiler(unittest.TestCase):
         assert "extern" not in text
         assert "std::rt::lang_start::h9b2e0b6aeda0bae0(rust_hello_world::main::h932c4676a11c63c3" in text
 
+    @structuring_algo("phoenix")
+    def test_decompiling_remove_rm_fts(self, decompiler_options=None):
+        bin_path = os.path.join(test_location, "x86_64", "decompiler", "remove.o")
+        proj = angr.Project(bin_path, auto_load_libs=False)
+
+        cfg = proj.analyses.CFGFast(normalize=True, data_references=True)
+
+        f = proj.kb.functions["rm_fts"]
+        proj.analyses.CompleteCallingConventions(cfg=cfg, recover_variables=True)
+
+        # disable eager returns simplifier
+        all_optimization_passes = angr.analyses.decompiler.optimization_passes.get_default_optimization_passes(
+            "AMD64", "linux"
+        )
+        all_optimization_passes = [
+            p
+            for p in all_optimization_passes
+            if p is not angr.analyses.decompiler.optimization_passes.EagerReturnsSimplifier
+        ]
+
+        d = proj.analyses[Decompiler].prep()(
+            f, cfg=cfg.model, options=decompiler_options, optimization_passes=all_optimization_passes
+        )
+        self._print_decompilation_result(d)
+
+        lines = d.codegen.text.split("\n")
+        func_starting_line = [idx for idx, line in enumerate(lines) if "rm_fts" in line][0]
+        lines = lines[func_starting_line:]
+        end_of_variable_list_line = [idx for idx, line in enumerate(lines) if not line.strip(" ")][0]
+        lines = lines[end_of_variable_list_line + 1 :]
+        # the first line of the code should be an if statement. all other variables should have been eliminated by
+        # proper propagation
+        assert lines[0].strip(" ").startswith("if (")
+
 
 if __name__ == "__main__":
     unittest.main()