43cb1b3200a40b448f15c6c3d79bda55930a5767
Fish <fish@cs.ucsb.edu>
2023-09-27 02:58:42 -0700

    fixes

--- tests/test_string_obf_finder.py
+++ tests/test_string_obf_finder.py
@@ -4,6 +4,7 @@ import os
 
 import angr
 from angr.analyses import deobfuscator
+from angr.sim_type import parse_signature
 
 binaries_base = os.path.join(
     os.path.dirname(os.path.realpath(str(__file__))),
@@ -18,18 +19,32 @@ class TestStringObfFinder(TestCase):
     def test_smoketest(self):
         bin_path = os.path.join(binaries_base, "x86_64", "netfilter_b64.sys")
 
-        # this is a Windows 64-bit binary!
-        angr.DEFAULT_CC["AMD64"] = angr.calling_conventions.SimCCMicrosoftAMD64
-        angr.calling_conventions.CC["AMD64"] = [angr.calling_conventions.SimCCMicrosoftAMD64]
-
         proj = angr.Project(bin_path, auto_load_libs=False)
         cfg = proj.analyses.CFG(normalize=True)
+
+        # sadly we do not yet have function prototypes for Windows kernel
+        # gotta manually specify prototypes for a few Windows kernel APIs
+        proj.kb.functions["PsLookupProcessByProcessId"].prototype = parse_signature(
+            "int PsLookupProcessByProcessId(uint64_t a, uint64_t b);"
+        ).with_arch(proj.arch)
+
+        # also sadly we do not yet identify "__security_check_cookie" on Windows binaries
+        # hard-code it for now
+        proj.kb.functions[0x1400070B0].name = "_security_check_cookie"
+        proj.kb.functions[0x1400070B0].is_default_name = False
+
         proj.analyses.CompleteCallingConventions(recover_variables=True, workers=4)
 
         # it will update kb.obfuscations
         finder = proj.analyses.StringObfuscationFinder()
+        assert proj.kb.obfuscations.type1_deobfuscated_strings
+        assert proj.kb.obfuscations.type2_deobfuscated_strings
 
         dec = proj.analyses.Decompiler(proj.kb.functions[0x140005174])
+        print(dec.codegen.text)
+        dec = proj.analyses.Decompiler(proj.kb.functions[0x1400035A0])
+        print(dec.codegen.text)
+        dec = proj.analyses.Decompiler(proj.kb.functions[0x140003504])
         print(dec.codegen.text)
         dec = proj.analyses.Decompiler(proj.kb.functions[0x140006208])
         print(dec.codegen.text)
