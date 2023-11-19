ea07f080bd7acf1a92e748d9b9990533639ca3e0
bluesadi <yiboliume@gmail.com>
2023-11-19 16:44:04 -0700

    Add two test cases for MultiValues

--- tests/storage/test_multivalues.py
+++ tests/storage/test_multivalues.py
@@ -21,6 +21,23 @@ class TestMultiValues(TestCase):
         assert mv._values[5] == {claripy.BVV(0xCC, 8), claripy.BVV(0x38, 8), claripy.BVV(0, 8)}
         assert mv._values[6] == {claripy.BVV(1, 16), claripy.BVV(0x1338, 16)}
 
+    def test_multivalues_empty(self):
+        mv = MultiValues()
+        assert mv._single_value is None
+        assert mv._values == {}
+        assert len(mv) == 0
+
+    def test_multivalues_single_value(self):
+        v = claripy.BVV(0x1338133813371337, 64)
+        mv = MultiValues(v)
+        assert mv._single_value is not None
+        assert v.concrete_value == mv._single_value.concrete_value
+        assert len(mv) == 64
+
+        mv2 = MultiValues(mv)
+        assert mv2._single_value is not None
+        assert v.concrete_value == mv2._single_value.concrete_value
+        assert len(mv) == 64
 
 if __name__ == "__main__":
     main()
