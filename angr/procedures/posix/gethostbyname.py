d40207b802341e698aac455b87159bf08be5838b
Matthew Pruett <matthewtpruett@yahoo.com>
2024-01-12 04:26:49 -0500

    Fix writes to hostent struct (#4417)

--- angr/procedures/posix/gethostbyname.py
+++ angr/procedures/posix/gethostbyname.py
@@ -4,24 +4,39 @@ import angr
 class gethostbyname(angr.SimProcedure):
     def run(self, name):
         malloc = angr.SIM_PROCEDURES["libc"]["malloc"]
-        place = self.inline_call(malloc, 32).ret_expr
-        self.state.memory.store(
-            place, self.state.solver.BVS("h_name", 64, key=("api", "gethostbyname", "h_name")), endness="Iend_LE"
-        )
-        self.state.memory.store(
-            place, self.state.solver.BVS("h_aliases", 64, key=("api", "gethostbyname", "h_aliases")), endness="Iend_LE"
-        )
+        int_size_bits = self.arch.sizeof["int"]
+        int_size = int_size_bits // 8
+        ptr_size_bits = self.arch.bits
+        ptr_size = ptr_size_bits // 8
+        hostent_size = (2 * int_size) + (3 * ptr_size)
+        place = self.inline_call(malloc, hostent_size).ret_expr
         self.state.memory.store(
             place,
-            self.state.solver.BVS("h_addrtype", 64, key=("api", "gethostbyname", "h_addrtype")),
+            self.state.solver.BVS("h_name", ptr_size_bits, key=("api", "gethostbyname", "h_name")),
             endness="Iend_LE",
         )
+        next_addr = place + ptr_size
         self.state.memory.store(
-            place, self.state.solver.BVS("h_length", 64, key=("api", "gethostbyname", "h_length")), endness="Iend_LE"
+            next_addr,
+            self.state.solver.BVS("h_aliases", ptr_size_bits, key=("api", "gethostbyname", "h_aliases")),
+            endness="Iend_LE",
         )
+        next_addr += ptr_size
         self.state.memory.store(
-            place,
-            self.state.solver.BVS("h_addr_list", 64, key=("api", "gethostbyname", "h_addr_list")),
+            next_addr,
+            self.state.solver.BVS("h_addrtype", int_size_bits, key=("api", "gethostbyname", "h_addrtype")),
+            endness="Iend_LE",
+        )
+        next_addr += int_size
+        self.state.memory.store(
+            next_addr,
+            self.state.solver.BVS("h_length", int_size_bits, key=("api", "gethostbyname", "h_length")),
+            endness="Iend_LE",
+        )
+        next_addr += int_size
+        self.state.memory.store(
+            next_addr,
+            self.state.solver.BVS("h_addr_list", ptr_size_bits, key=("api", "gethostbyname", "h_addr_list")),
             endness="Iend_LE",
         )
         return place
