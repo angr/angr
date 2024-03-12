53ac3e31f8814f20f4082e3632448dcbdd572abd
bluesadi <yiboliume@gmail.com>
2024-03-12 11:39:26 -0700

    Refine StringSimplifier

--- angr/rust/optimization_passes/string_simplifier.py
+++ angr/rust/optimization_passes/string_simplifier.py
@@ -1,7 +1,6 @@
 import ailment
 
 from ...analyses.decompiler.optimization_passes.engine_base import SimplifierAILState
-from ...knowledge_plugins.cfg import MemoryDataSort
 from ...analyses.decompiler.optimization_passes.optimization_pass import OptimizationPass, OptimizationPassStage
 from ..sim_type import RustSimTypeStr, RustSimTypePointer
 from ..ailment.expression import Str
@@ -22,30 +21,33 @@ class StringSimplifier(OptimizationPass):
     def _check(self):
         return True, None
 
+    def try_simplify_string(self, stmt):
+        if isinstance(stmt, ailment.statement.Store) and isinstance(stmt.data, ailment.Const):
+            data = stmt.data
+            section = self.project.loader.find_section_containing(data.value)
+            if section and section.is_readable:
+                memory = self.project.loader.memory
+                str_addr = memory.unpack(stmt.data.value, self.project.arch.struct_fmt())[0]
+                section = self.project.loader.find_section_containing(str_addr)
+                if section and section.is_readable and not section.is_writable:
+                    str_len = memory.unpack(
+                        stmt.data.value + self.project.arch.bytes, self.project.arch.struct_fmt()
+                    )[0]
+                    try:
+                        decoded_str = memory.load(str_addr, str_len).decode("utf-8")
+                    except UnicodeDecodeError:
+                        return False
+                    new_expr = Str(data.idx, data.variable, data.value, data.bits, decoded_str)
+                    stmt.data = new_expr
+                    # Set variable type to &str
+                    self._variable_kb.variables.get_function_manager(self._func.addr).set_variable_type(
+                        stmt.variable, RustSimTypePointer(RustSimTypeStr()), mark_manual=True
+                    )
+                    return True
+        return False
+
     def _analyze(self, cache=None):
         for block in list(self._graph.nodes()):
             block: ailment.Block
             for stmt in block.statements:
-                if isinstance(stmt, ailment.statement.Store) and isinstance(stmt.data, ailment.Const):
-                    data = stmt.data
-                    section = self.project.loader.find_section_containing(data.value)
-                    if section and section.is_readable:
-                        str_addr = self.project.loader.memory.unpack(stmt.data.value, self.project.arch.struct_fmt())[0]
-                        section = self.project.loader.find_section_containing(str_addr)
-                        if section and section.is_readable and not section.is_writable:
-                            str_len = self.project.loader.memory.unpack(
-                                stmt.data.value + self.project.arch.bytes, self.project.arch.struct_fmt()
-                            )[0]
-                            cfg = self.project.kb.cfgs.get_most_accurate()
-                            if cfg is not None and str_addr in cfg.memory_data:
-                                memory_data = cfg.memory_data[str_addr]
-                                if memory_data.sort == MemoryDataSort.String:
-                                    new_memory_data = memory_data.copy()
-                                    new_memory_data.content = memory_data.content[:str_len]
-                                    new_memory_data.size = str_len
-                                    new_expr = Str(data.idx, data.variable, data.value, data.bits, new_memory_data)
-                                    stmt.data = new_expr
-                                    # Set variable type to &str
-                                    self._variable_kb.variables.get_function_manager(self._func.addr).set_variable_type(
-                                        stmt.variable, RustSimTypePointer(RustSimTypeStr())
-                                    )
+                self.try_simplify_string(stmt)
