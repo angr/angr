016ecb591abd2ded78dd6280d0c451daf2f92f51
ashwin <ashwin2sudhir@gmail.com>
2024-03-14 15:30:16 -0700

    skip artificial registers from the check

--- angr/analyses/decompiler/ail_simplifier.py
+++ angr/analyses/decompiler/ail_simplifier.py
@@ -236,7 +236,7 @@ class AILSimplifier(Analysis):
                 # only do this for general purpose register
                 skip_def = False
                 for reg in self.project.arch.register_list:
-                    if reg.vex_offset == def_.atom.reg_offset and not reg.general_purpose:
+                    if not reg.artificial and reg.vex_offset == def_.atom.reg_offset and not reg.general_purpose:
                         skip_def = True
                         break
 
