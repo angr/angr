1f764c4c230c81e5af888e7b1ba5c904c8bd0612
Fish <fishw@asu.edu>
2024-03-28 01:13:18 -0700

    Update test cases.

--- angr/knowledge_plugins/key_definitions/live_definitions.py
+++ angr/knowledge_plugins/key_definitions/live_definitions.py
@@ -850,16 +850,14 @@ class LiveDefinitions:
     @overload
     def get_concrete_value(
         self, spec: Union[Atom, Definition[Atom], Iterable[Atom], Iterable[Definition[Atom]]], cast_to: Type[int] = ...
-    ) -> Optional[int]:
-        ...
+    ) -> Optional[int]: ...
 
     @overload
     def get_concrete_value(
         self,
         spec: Union[Atom, Definition[Atom], Iterable[Atom], Iterable[Definition[Atom]]],
         cast_to: Type[bytes] = ...,
-    ) -> Optional[bytes]:
-        ...
+    ) -> Optional[bytes]: ...
 
     def get_concrete_value(
         self,
@@ -953,8 +951,7 @@ class LiveDefinitions:
         pointer: Union[MultiValues, Atom, Definition, Iterable[Atom], Iterable[Definition]],
         size: Union[int, DerefSize],
         endness: archinfo.Endness = ...,
-    ) -> Set[MemoryLocation]:
-        ...
+    ) -> Set[MemoryLocation]: ...
 
     @overload
     def deref(
@@ -962,8 +959,7 @@ class LiveDefinitions:
         pointer: Union[int, claripy.ast.BV, HeapAddress, SpOffset],
         size: Union[int, DerefSize],
         endness: archinfo.Endness = ...,
-    ) -> Optional[MemoryLocation]:
-        ...
+    ) -> Optional[MemoryLocation]: ...
 
     def deref(self, pointer, size, endness=archinfo.Endness.BE):
         if isinstance(pointer, (Atom, Definition)):
