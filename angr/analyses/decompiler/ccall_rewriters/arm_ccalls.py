a75a8014ef1c4f3316800f6c1449a256e8f75b69
Fish <fishw@asu.edu>
2024-05-14 06:55:38 -0700

    Decompiler: Add ARM ccall rewriter.

---
+++ angr/analyses/decompiler/ccall_rewriters/arm_ccalls.py
@@ -0,0 +1,73 @@
+from typing import Optional
+
+from ailment import Expr
+
+from angr.engines.vex.claripy.ccall import (
+    ARMCondEQ,
+    ARMCondNE,
+    ARMCondHS,
+    ARMCondLO,
+    ARMCondMI,
+    ARMCondPL,
+    # TODO:
+    ARMG_CC_OP_COPY,
+    ARMG_CC_OP_ADD,
+    ARMG_CC_OP_SUB,
+    ARMG_CC_OP_ADC,
+    ARMG_CC_OP_SBB,
+    ARMG_CC_OP_LOGIC,
+    # TODO:
+)
+from .rewriter_base import CCallRewriterBase
+
+
+class ARMCCallRewriter(CCallRewriterBase):
+    """
+    Implements ccall rewriter for AMD64.
+    """
+
+    __slots__ = ()
+
+    def _rewrite(self, ccall: Expr.VEXCCallExpression) -> Optional[Expr.Expression]:
+        if ccall.cee_name == "armg_calculate_condition":
+            cond_n_op = ccall.operands[0]
+
+            if isinstance(cond_n_op, Expr.Const) and isinstance(cond_n_op, Expr.Const):
+                concrete_cond_n_op = cond_n_op.value
+                cond_v = concrete_cond_n_op >> 4
+                op_v = concrete_cond_n_op & 0xf
+                inv = cond_v & 1
+
+                dep_1 = ccall.operands[1]
+                dep_2 = ccall.operands[2]
+                dep_3 = ccall.operands[3]
+
+                if cond_v in { ARMCondHS, ARMCondLO }:
+                    # armg_calculate_flag_c
+                    if op_v == ARMG_CC_OP_SBB:
+                        # dep_1 >= dep_2 if dep_3 == 0 else dep_1 > dep_2,
+                        #   and then negate the result if inv == 1
+                        if dep_3.value == 0:
+                            r = Expr.BinaryOp(ccall.idx, "CmpGE" if inv == 0 else "CmpLT",
+                                              (dep_1, dep_2),
+                                              False,
+                                              **ccall.tags)
+                        else:
+                            r = Expr.BinaryOp(ccall.idx, "CmpGT" if inv == 0 else "CmpLE",
+                                              (dep_1, dep_2),
+                                              False,
+                                              **ccall.tags)
+                        return Expr.Convert(None, r.bits, ccall.bits, False, r, **ccall.tags)
+
+                elif cond_v in { ARMCondMI, ARMCondPL }:
+                    # armg_calculate_flag_n
+                    if op_v == ARMG_CC_OP_SUB:
+                        # dep_1 < dep_2,
+                        #   and then negate the result if inv == 1
+                        r = Expr.BinaryOp(ccall.idx, "CmpLT" if inv == 0 else "CmpGE",
+                                          (dep_1, dep_2),
+                                          False,
+                                          **ccall.tags)
+                        return Expr.Convert(None, r.bits, ccall.bits, False, r, **ccall.tags)
+
+        return None
