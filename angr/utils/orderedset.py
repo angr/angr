c77219d754098086294b370bd8b566345c9e2ddd
Kevin Phoenix <kevin@kphoenix.us>
2024-07-22 19:54:12 -0700

    Improve lint

--- angr/utils/orderedset.py
+++ angr/utils/orderedset.py
@@ -20,15 +20,15 @@ class OrderedSet(collections.abc.MutableSet):
     def __contains__(self, key):
         return key in self.map
 
-    def add(self, key):
-        if key not in self.map:
+    def add(self, value):
+        if value not in self.map:
             end = self.end
             curr = end[1]
-            curr[2] = end[1] = self.map[key] = [key, curr, end]
+            curr[2] = end[1] = self.map[value] = [value, curr, end]
 
-    def discard(self, key):
-        if key in self.map:
-            key, prev, next_ = self.map.pop(key)
+    def discard(self, value):
+        if value in self.map:
+            value, prev, next_ = self.map.pop(value)
             prev[2] = next_
             next_[1] = prev
 
