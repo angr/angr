15ab6e8b51217d83c8f3137355dabe8b2eee5f7e
Fish <fishw@asu.edu>
2024-07-28 21:08:30 -0700

    PeepholeOptimizations: Fix overly aggressive removal of Convert().

--- angr/analyses/decompiler/peephole_optimizations/remove_cascading_conversions.py
+++ angr/analyses/decompiler/peephole_optimizations/remove_cascading_conversions.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from ailment.expression import Convert
+from ailment.expression import Convert, BinaryOp, Const
 
 from .base import PeepholeOptimizationExprBase
 
@@ -14,7 +14,20 @@ class RemoveCascadingConversions(PeepholeOptimizationExprBase):
         if isinstance(expr.operand, Convert):
             inner = expr.operand
             if inner.from_bits == expr.to_bits:
-                return inner.operand
+                if inner.from_bits < inner.to_bits:
+                    # extension -> truncation
+                    return inner.operand
+                else:
+                    # truncation -> extension
+                    # we must clear the top truncated bits
+                    mask = (1 << inner.to_bits) - 1
+                    return BinaryOp(
+                        expr.idx,
+                        "And",
+                        [inner.operand, Const(None, None, mask, inner.operand.bits)],
+                        False,
+                        **expr.tags,
+                    )
             return Convert(expr.idx, inner.from_bits, expr.to_bits, expr.is_signed, inner.operand, **expr.tags)
 
         return None
