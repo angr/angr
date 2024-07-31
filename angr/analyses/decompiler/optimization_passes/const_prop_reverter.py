e679f6acc6bc4ac354f51661615a28e5d8d22f87
mahaloz <zion@zionbasque.com>
2024-07-31 14:56:03 -0700

    Move all SAILR specific changes to a new SAILR structurer

--- angr/analyses/decompiler/optimization_passes/const_prop_reverter.py
+++ angr/analyses/decompiler/optimization_passes/const_prop_reverter.py
@@ -12,6 +12,7 @@ from ailment.expression import Convert, Register, Expression
 
 from .optimization_pass import OptimizationPass, OptimizationPassStage
 from ..structuring import SAILRStructurer, DreamStructurer
+from .optimization_pass import OptimizationPass, OptimizationPassStage, SAILRStructurer
 from ....knowledge_plugins.key_definitions.atoms import MemoryLocation
 from ....knowledge_plugins.key_definitions.constants import OP_BEFORE
 
