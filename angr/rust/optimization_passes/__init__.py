5a38180e5011bdd5f1beccd9bdfacf71310767cc
bluesadi <yiboliume@gmail.com>
2024-08-02 14:55:43 -0700

    Create UnwrapSimplifier (But it's not completed yet)

--- angr/rust/optimization_passes/__init__.py
+++ angr/rust/optimization_passes/__init__.py
@@ -1,4 +1,5 @@
 from ...analyses.decompiler.optimization_passes import _all_optimization_passes
+from .unwrap_simplifier import UnwrapSimplifier
 from .lib_function_identifier import LibFunctionIdentifier
 from .string_simplifier import StringSimplifier
 from .junk_remover import JunkRemover
@@ -12,6 +13,7 @@ _all_optimization_passes.extend(
         (LibFunctionIdentifier, True),
         (JunkRemover, True),
         (CallsiteMaker, True),
+        (UnwrapSimplifier, True),
         # (StringSimplifier, True),
         (AllocSimplifier, True),
         # (TypeCorrector, True),
