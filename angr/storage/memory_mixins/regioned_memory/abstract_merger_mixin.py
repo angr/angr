6144909be1566cedd49a174ab0287babe0829404
Kevin Phoenix <kevin@kphoenix.us>
2024-09-06 12:54:18 -0700

    Use Base.identical instead of BackendVSA.convert()

--- angr/storage/memory_mixins/regioned_memory/abstract_merger_mixin.py
+++ angr/storage/memory_mixins/regioned_memory/abstract_merger_mixin.py
@@ -25,7 +25,7 @@ class AbstractMergerMixin(MemoryMixin):
             merged_val = merged_val.union(tm)
             l.info("... Merged to %s", merged_val)
 
-        if not values[0][0].uninitialized and claripy.backends.vsa.identical(merged_val, values[0][0]):
+        if not values[0][0].uninitialized and merged_val.identical(values[0][0]):
             return None
 
         return merged_val
