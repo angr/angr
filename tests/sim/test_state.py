1b8714fe488e0e1ca14a63082bef641fae7363ff
Kevin Phoenix <kevin@kphoenix.us>
2024-09-06 14:23:15 -0700

    Another one

--- tests/sim/test_state.py
+++ tests/sim/test_state.py
@@ -135,8 +135,8 @@ class TestState(unittest.TestCase):
         c.memory.store(addr, claripy.BVV(70, 32), endness="Iend_LE")
 
         merged, _, _ = a.merge(b, c)
-        actual = claripy.backends.vsa.convert(merged.memory.load(addr, 4, endness="Iend_LE"))
-        expected = claripy.backends.vsa.convert(claripy.SI(bits=32, stride=10, lower_bound=50, upper_bound=70))
+        actual = merged.memory.load(addr, 4, endness="Iend_LE")
+        expected = claripy.SI(bits=32, stride=10, lower_bound=50, upper_bound=70)
         assert actual.identical(expected)
 
     def test_state_merge_3way(self):
