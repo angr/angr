11c8650040a66ff2965d0bb8df5329a6fe7f7ca3
Fish <ltfish@users.noreply.github.com>
2024-09-07 18:01:43 -0600

    CallingConventionAnalysis: Supports tail jumps when collecting call-site effects. (#4821)
    * CallingConventionAnalysis: Supports tail jumps when collecting call-site effects.
    
    * CCA: Skip alignment stubs when collecting callsite facts.

--- angr/analyses/calling_convention.py
+++ angr/analyses/calling_convention.py
@@ -400,13 +400,13 @@ class CallingConventionAnalysis(Analysis):
         call_sites_by_function: dict[Function, list[tuple[int, int]]] = defaultdict(list)
         for src, _, data in sorted(in_edges, key=lambda x: x[0].addr):
             edge_type = data.get("jumpkind", "Ijk_Call")
-            if edge_type != "Ijk_Call":
+            if not (edge_type == "Ijk_Call" or edge_type == "Ijk_Boring" and self._cfg.graph.out_degree[src] == 1):
                 continue
             if not self.kb.functions.contains_addr(src.function_address):
                 continue
             caller = self.kb.functions[src.function_address]
-            if caller.is_simprocedure:
-                # do not analyze SimProcedures
+            if caller.is_simprocedure or caller.is_alignment:
+                # do not analyze SimProcedures or alignment stubs
                 continue
             call_sites_by_function[caller].append((src.addr, src.instruction_addrs[-1]))
 
