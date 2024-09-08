ee5f3a6659f7f1d1e0c5b88f261424ec3df8fe56
Fish <ltfish@users.noreply.github.com>
2024-09-07 23:37:54 -0600

    CCA: Improve callsite info collection for PLT stubs. (#4822)
    * CCA: Improve callsite info collection for PLT stubs.
    
    * CCA: Also be slightly less conservative when analyzing callsites for args.
    
    * Remove test_decompiling_remove_rm_fts.

--- angr/analyses/calling_convention.py
+++ angr/analyses/calling_convention.py
@@ -398,6 +398,17 @@ class CallingConventionAnalysis(Analysis):
         in_edges = self._cfg.graph.in_edges(node, data=True)
 
         call_sites_by_function: dict[Function, list[tuple[int, int]]] = defaultdict(list)
+
+        if len(in_edges) == 1:
+            src, _, data = next(iter(in_edges))
+            if (
+                data.get("jumpkind", "Ijk_Call") == "Ijk_Boring"
+                and self.kb.functions.contains_addr(src.function_address)
+                and self.kb.functions[src.function_address].is_plt
+            ):
+                # find callers to the PLT stub instead
+                in_edges = self._cfg.graph.in_edges(src, data=True)
+
         for src, _, data in sorted(in_edges, key=lambda x: x[0].addr):
             edge_type = data.get("jumpkind", "Ijk_Call")
             if not (edge_type == "Ijk_Call" or edge_type == "Ijk_Boring" and self._cfg.graph.out_degree[src] == 1):
@@ -584,8 +595,10 @@ class CallingConventionAnalysis(Analysis):
         default_type_cls = SimTypeInt if self.project.arch.bits == 32 else SimTypeLongLong
         arg_session = cc.arg_session(default_type_cls().with_arch(self.project.arch))
         temp_args: list[SimFunctionArgument | None] = []
+        expected_args: list[SimFunctionArgument] = []
         for _ in range(30):  # at most 30 arguments
             arg_loc = cc.next_arg(arg_session, default_type_cls().with_arch(self.project.arch))
+            expected_args.append(arg_loc)
             if isinstance(arg_loc, SimRegArg):
                 reg_offset = self.project.arch.registers[arg_loc.reg_name][0]
                 # is it initialized?
@@ -602,6 +615,12 @@ class CallingConventionAnalysis(Analysis):
                     break
             else:
                 break
+
+        if None in temp_args:
+            first_none_idx = temp_args.index(None)
+            # test if there is at least one argument set after None; if so, we ignore the first None
+            if any(arg is not None for arg in temp_args[first_none_idx:]):
+                temp_args[first_none_idx] = expected_args[first_none_idx]
 
         if None in temp_args:
             # we be very conservative here and ignore all arguments starting from the first missing one
--- tests/analyses/decompiler/test_decompiler.py
+++ tests/analyses/decompiler/test_decompiler.py
@@ -2870,7 +2870,7 @@ class TestDecompiler(unittest.TestCase):
         #         qsort();
         #     }
         #     return;
-        assert re.search(r"if\(.+?\)\{.+?\}return", text) is not None
+        assert re.search(r"if\(.+?\)\{{0,1}.+?\}{0,1}return", text) is not None
 
     @for_all_structuring_algos
     def test_ret_dedupe_fakeret_2(self, decompiler_options=None):
@@ -3047,35 +3047,6 @@ class TestDecompiler(unittest.TestCase):
         text = d.codegen.text
         assert "extern" not in text
         assert "std::rt::lang_start::h9b2e0b6aeda0bae0(rust_hello_world::main::h932c4676a11c63c3" in text
-
-    @structuring_algo("sailr")
-    def test_decompiling_remove_rm_fts(self, decompiler_options=None):
-        bin_path = os.path.join(test_location, "x86_64", "decompiler", "remove.o")
-        proj = angr.Project(bin_path, auto_load_libs=False)
-
-        cfg = proj.analyses.CFGFast(normalize=True, data_references=True)
-
-        f = proj.kb.functions["rm_fts"]
-        proj.analyses.CompleteCallingConventions(cfg=cfg, recover_variables=True)
-
-        # disable eager returns simplifier
-        all_optimization_passes = angr.analyses.decompiler.optimization_passes.get_default_optimization_passes(
-            "AMD64", "linux", disable_opts=DUPLICATING_OPTS
-        )
-
-        d = proj.analyses[Decompiler].prep()(
-            f, cfg=cfg.model, options=decompiler_options, optimization_passes=all_optimization_passes
-        )
-        self._print_decompilation_result(d)
-
-        lines = d.codegen.text.split("\n")
-        func_starting_line = next(idx for idx, line in enumerate(lines) if "rm_fts" in line)
-        lines = lines[func_starting_line:]
-        end_of_variable_list_line = next(idx for idx, line in enumerate(lines) if not line.strip(" "))
-        lines = lines[end_of_variable_list_line + 1 :]
-        # the second line of the code should be an if statement. all other variables should have been eliminated by
-        # proper propagation
-        assert lines[1].strip(" ").startswith("if (")
 
     @structuring_algo("sailr")
     def test_decompiling_incorrect_duplication_chcon_main(self, decompiler_options=None):
