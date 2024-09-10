060dedd8781744e0995cd6a5601f4b89c3feaafb
Fish <fishw@asu.edu>
2024-09-10 01:40:12 -0700

    Implement another case for ConstMullAShift.

--- angr/analyses/decompiler/peephole_optimizations/const_mull_a_shift.py
+++ angr/analyses/decompiler/peephole_optimizations/const_mull_a_shift.py
@@ -36,7 +36,7 @@ class ConstMullAShift(PeepholeOptimizationExprBase):
         if isinstance(expr, BinaryOp) and expr.op == "Shr" and isinstance(expr.operands[1], Const):
             # (N * a) >> M  ==>  a / N1
             inner = expr.operands[0]
-            if isinstance(inner, BinaryOp) and inner.op in {"Mull", "Mul"}:
+            if isinstance(inner, BinaryOp) and inner.op in {"Mull", "Mul"} and not inner.signed:
                 if isinstance(inner.operands[0], Const) and not isinstance(inner.operands[1], Const):
                     C = inner.operands[0].value
                     X = inner.operands[1]
@@ -64,36 +64,91 @@ class ConstMullAShift(PeepholeOptimizationExprBase):
                 and isinstance(expr1, BinaryOp)
                 and expr1.op in {"Shr", "Sar"}
                 and isinstance(expr1.operands[1], Const)
-            ) and (
-                isinstance(expr0.operands[0], BinaryOp)
-                and expr0.operands[0].op in {"Mull", "Mul"}
-                and isinstance(expr0.operands[0].operands[1], Const)
             ):
                 if (
                     isinstance(expr0.operands[0], BinaryOp)
                     and expr0.operands[0].op in {"Mull", "Mul"}
+                    and expr0.operands[0].signed
                     and isinstance(expr0.operands[0].operands[1], Const)
                 ):
                     a0 = expr0.operands[0].operands[0]
                     a1 = expr1.operands[0]
-                    if a0.likes(a1):
-                        # (a * x >> M1) +/- (a >> M2)  ==>  a / N
-                        C = expr0.operands[0].operands[1].value
-                        X = a0
-                        V = expr0.operands[1].value
-                        ndigits = 5 if V == 32 else 6
-                        divisor = self._check_divisor(pow(2, V), C, ndigits)
-                        if divisor is not None:
-                            new_const = Const(None, None, divisor, X.bits)
-                            new_div = BinaryOp(
-                                expr0.operands[0].idx,
-                                "Div",
-                                [X, new_const],
-                                expr0.operands[0].signed,
-                                **expr0.operands[0].tags,
-                            )
-                            # we cannot drop the convert in this case
-                            return new_div
+                elif (
+                    isinstance(expr1.operands[0], BinaryOp)
+                    and expr1.operands[0].op in {"Mull", "Mul"}
+                    and expr1.operands[0].signed
+                    and isinstance(expr1.operands[0].operands[1], Const)
+                ):
+                    a1 = expr0.operands[0].operands[0]
+                    a0 = expr1.operands[0]
+                else:
+                    a0, a1 = None, None
+                if a0 is not None and a1 is not None and a0.likes(a1):
+                    # (a * x >> M1) +/- (a >> M2)  ==>  a / N
+                    C = expr0.operands[0].operands[1].value
+                    X = a0
+                    V = expr0.operands[1].value
+                    ndigits = 5 if V == 32 else 6
+                    divisor = self._check_divisor(pow(2, V), C, ndigits)
+                    if divisor is not None:
+                        new_const = Const(None, None, divisor, X.bits)
+                        # we cannot drop the convert in this case
+                        return BinaryOp(
+                            expr0.operands[0].idx,
+                            "Div",
+                            [X, new_const],
+                            expr0.operands[0].signed,
+                            **expr0.operands[0].tags,
+                        )
+
+            # with Convert in consideration
+            if (
+                isinstance(expr0, BinaryOp)
+                and expr0.op in {"Shr", "Sar"}
+                and isinstance(expr0.operands[1], Const)
+                and expr0.operands[1].value == 0x3F
+                and isinstance(expr1, BinaryOp)
+                and expr1.op in {"Shr", "Sar"}
+                and isinstance(expr1.operands[1], Const)
+                and expr1.operands[1].value == 0x20
+            ):
+                expr0_operand = expr0.operands[0]
+                expr1_operand = expr1.operands[0]
+                if (
+                    isinstance(expr0_operand, BinaryOp)
+                    and expr0_operand.op in {"Mull", "Mul"}
+                    and expr0_operand.signed
+                    and isinstance(expr0_operand.operands[1], Const)
+                ):
+                    a0 = expr0_operand.operands[0]
+                    a1 = expr1_operand.operands[0]
+                elif (
+                    isinstance(expr1_operand, BinaryOp)
+                    and expr1_operand.op in {"Mull", "Mul"}
+                    and expr1_operand.signed
+                    and isinstance(expr1_operand.operands[1], Const)
+                ):
+                    a0 = expr1_operand.operands[0]
+                    a1 = expr0_operand.operands[0]
+                else:
+                    a0, a1 = None, None
+
+                if a0 is not None and a1 is not None and a0.likes(a1):
+                    # (a * x >> 0x3f) +/- (a * x >> 0x20)  ==>  a / N
+                    C = expr0_operand.operands[1].value
+                    X = a0
+                    V = 32
+                    ndigits = 5 if V == 32 else 6
+                    divisor = self._check_divisor(pow(2, V), C, ndigits)
+                    if divisor is not None:
+                        new_const = Const(None, None, divisor, X.bits)
+                        return BinaryOp(
+                            expr0_operand.idx,
+                            "Div",
+                            [X, new_const],
+                            expr0_operand.signed,
+                            **expr0_operand.tags,
+                        )
 
         return None
 
