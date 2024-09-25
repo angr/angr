5860f893a5767c802788d366986d7a47b9b3f757
Kevin Phoenix <kevin@kphoenix.us>
2024-09-25 15:08:16 -0700

    Refactor s_reaching_definitions analysis into subpackage

---
+++ angr/analyses/s_reaching_definitions/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from .s_rda_model import SRDAModel
+from .s_rda_view import SRDAView
+from .s_reaching_definitions import SReachingDefinitionsAnalysis
+
+
+__all__ = (
+    "SRDAModel",
+    "SRDAView",
+    "SReachingDefinitionsAnalysis",
+)
