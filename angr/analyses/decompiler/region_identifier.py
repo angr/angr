42caa66cccaa9bfefc5f47d3dfc1c2ceb40b8cea
Fish <ltfish@users.noreply.github.com>
2024-09-25 17:38:23 -0700

    Decompiler: Respect updated entry node address in optimizations. (#4892)
    * Decompiler: Respect updated entry node address in optimizations.
    
    * RegionIdentifier: Handle cases where func is not specified.

--- angr/analyses/decompiler/region_identifier.py
+++ angr/analyses/decompiler/region_identifier.py
@@ -42,8 +42,12 @@ class RegionIdentifier(Analysis):
         largest_successor_tree_outside_loop=True,
         force_loop_single_exit=True,
         complete_successors=False,
+        entry_node_addr: tuple[int, int | None] | None = None,
     ):
         self.function = func
+        self.entry_node_addr: tuple[int, int | None] | None = (
+            entry_node_addr if entry_node_addr is not None else (func.addr, None) if func is not None else None
+        )
         self.cond_proc = (
             cond_proc
             if cond_proc is not None
@@ -144,10 +148,20 @@ class RegionIdentifier(Analysis):
         except StopIteration:
             pass
 
-        try:
-            return next(n for n in graph.nodes() if n.addr == self.function.addr)
-        except StopIteration as ex:
-            raise AngrRuntimeError("Cannot find the start node from the graph!") from ex
+        if self.entry_node_addr is not None:
+            try:
+                return next(
+                    n
+                    for n in graph.nodes()
+                    if (
+                        (n.addr, n.idx) == self.entry_node_addr
+                        if isinstance(n, Block)
+                        else n.addr == self.entry_node_addr[0]
+                    )
+                )
+            except StopIteration as ex:
+                raise AngrRuntimeError("Cannot find the start node from the graph!") from ex
+        raise AngrRuntimeError("Cannot find the start node from the graph!")
 
     def _test_reducibility(self):
         # make a copy of the graph
