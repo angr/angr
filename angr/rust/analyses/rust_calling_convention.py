60f652d3300774625f0bee5a94c4c2075652341c
bluesadi <yiboliume@gmail.com>
2024-10-01 10:27:09 -0700

    Refine RustCallingConventionAnalysis

--- angr/rust/analyses/rust_calling_convention.py
+++ angr/rust/analyses/rust_calling_convention.py
@@ -2,23 +2,26 @@ import logging
 import traceback
 
 from ailment import BinaryOp, Const, AILBlockWalker, Block
-from ailment.expression import BasePointerOffset, VirtualVariable
-from ailment.statement import Store
+from ailment.expression import BasePointerOffset, VirtualVariable, Phi
+from ailment.statement import Store, Call, Statement
 
-from ..definitions.structs import Option
 from ..knowledge_plugins.rust_calling_conventions import RustCallingConventionModel
 from ..sim_type import RustSimTypeInt, RustSimTypeReference, RustSimStruct, RustSimTypeFunction
 from ..utils.library import normalize
 from ...analyses import Analysis, AnalysesHub
+from ...analyses.s_reaching_definitions import SRDAView
 from ...knowledge_plugins import Function
 
 l = logging.getLogger(name=__name__)
 
 
 class FactsCollector(AILBlockWalker):
-    def __init__(self, model: RustCallingConventionModel):
+    def __init__(self, context: "RustCallingConventionAnalysis", srda_view: SRDAView):
         super().__init__()
-        self.model = model
+        self.context = context
+        self.project = context.model.clinic.project
+        self.model = context.model
+        self.srda_view = srda_view
 
     def add_memory_write(self, arg_idx, block, offset, expr):
         if block not in self.model.memory_writes[arg_idx]:
@@ -29,6 +32,24 @@ class FactsCollector(AILBlockWalker):
         if block not in self.model.memory_reads[arg_idx]:
             self.model.memory_reads[arg_idx][block] = {}
         self.model.memory_reads[arg_idx][block][offset] = (expr, self.model.clinic.function.addr)
+
+    def get_vvar_value(self, vvar):
+        value = vvar
+        for i in range(100):  # Avoid unexpected infinite loop
+            value = self.srda_view.get_vvar_value(vvar)
+            if isinstance(value, VirtualVariable):
+                vvar = value
+            elif isinstance(value, Phi):
+                result = set()
+                for _, phi_vvar in value.src_and_vvars:
+                    result.add(self.get_vvar_value(phi_vvar))
+                if len(result) == 1:
+                    return next(iter(result))
+                else:
+                    return vvar
+            else:
+                return vvar
+        return value
 
     def _handle_Store(self, stmt_idx: int, stmt: Store, block: Block | None):
         addr = stmt.addr
@@ -41,9 +62,32 @@ class FactsCollector(AILBlockWalker):
         ):
             offset = addr.operands[1].value
             addr = addr.operands[0]
-        addr: VirtualVariable
-        if addr.was_parameter:
+        addr = self.get_vvar_value(addr)
+        if isinstance(addr, VirtualVariable) and addr.was_parameter:
             self.add_memory_write(addr.varid, block, offset, stmt.data)
+
+    def handle_Call(self, call: Call):
+        if (
+            isinstance(call.target, Const)
+            and call.target.value in self.project.kb.functions
+            and call.args
+            and isinstance(call.args[0], VirtualVariable)
+            and call.args[0].was_parameter
+            and call.args[0].varid == 0
+        ):
+            func = self.project.kb.functions[call.target.value]
+            result = self.project.analyses.RustCallingConvention(
+                func, self.context.clinic.graph, depth=self.context.depth + 1, max_depth=self.context.max_depth
+            )
+            self.model.memory_writes[0] |= result.model.memory_writes[0]
+
+    def _handle_Call(self, stmt_idx: int, stmt: Call, block: Block | None):
+        self.handle_Call(stmt)
+        super()._handle_Call(stmt_idx, stmt, block)
+
+    def _handle_CallExpr(self, expr_idx: int, expr: Call, stmt_idx: int, stmt: Statement, block: Block | None):
+        self.handle_Call(expr)
+        super()._handle_CallExpr(expr_idx, expr, stmt_idx, stmt, block)
 
 
 class RustCallingConventionAnalysis(Analysis):
@@ -52,57 +96,53 @@ class RustCallingConventionAnalysis(Analysis):
         self.model = RustCallingConventionModel()
         self.model.caller_graph = caller_graph
 
-        self._depth = depth
-        self._max_depth = max_depth
+        self.depth = depth
+        self.max_depth = max_depth
 
-        if self._depth <= self._max_depth:
+        if self.depth <= self.max_depth:
             try:
                 self._analyze()
             except Exception as e:
                 l.debug(f"Rust calling convention analysis failed for {normalize(self.func.name)}")
                 l.debug("".join(traceback.format_exception(e)))
 
+    def _merge_struct_types(self, struct_types) -> RustSimStruct | None:
+        """
+        Merge a list of struct types
+        If the types have different sizes, merge them into one enum type
+        """
+        if len(struct_types) == 0:
+            return None
+        sizes = {ty.size for ty in struct_types}
+        if len(sizes) == 1:
+            return next(iter(struct_types))
+        # Return an enum type
+        return next(iter(struct_types))
+
     def _infer_arg_type(self, arg_idx):
         fields = {}
         memory_writes = self.model.memory_writes[arg_idx]
-        # If we find potential discriminant in collected facts, the type maybe Result or Option
-        discriminant = None
-        overlapping_discriminant = False
         struct_types = []
 
-        def _maybe_discriminant(block_memory_writes):
-            return (
-                len(block_memory_writes) == 1
-                and 0 in block_memory_writes
-                and isinstance(block_memory_writes[0][0], Const)
-            )
-
         for block in memory_writes:
             block_memory_writes = memory_writes[block]
-            if _maybe_discriminant(block_memory_writes):
-                discriminant = block_memory_writes[0][0].value
-
-        for block in memory_writes:
-            block_memory_writes = memory_writes[block]
-            if _maybe_discriminant(block_memory_writes):
-                continue
             for offset in sorted(block_memory_writes.keys()):
                 expr, func_addr = block_memory_writes[offset]
                 arg_ty = RustSimTypeInt(expr.bits, signed=False)
                 fields[f"field_{offset}"] = arg_ty
-            ty = RustSimTypeReference(
+            struct_types.append(
                 RustSimStruct(
                     fields,
                     name=f"struct{sum(field.size if field.size else 0 for field in fields.values()) // 8}",
                     pack=True,
-                )
-            ).with_arch(self.project.arch)
-            struct_types.append(ty)
+                ).with_arch(self.project.arch)
+            )
 
-        if discriminant is not None and discriminant != 0 and len(struct_types):
-            struct_ty = Option(struct_types[0].pts_to, overlapping_discriminant=overlapping_discriminant)
-            return RustSimTypeReference(struct_ty).with_arch(self.project.arch)
-        return struct_types[0] if len(struct_types) else None
+        final_ty = self._merge_struct_types(struct_types)
+        if final_ty:
+            final_ty = RustSimTypeReference(final_ty).with_arch(self.project.arch)
+
+        return final_ty
 
     def _infer_prototype(self):
         args = []
@@ -184,11 +224,14 @@ class RustCallingConventionAnalysis(Analysis):
             cfg = self.kb.cfgs.get_most_accurate()
             self.clinic = self.project.analyses.Clinic(self.func, cfg=cfg, optimization_passes=[])
 
-        walker = FactsCollector(self.model)
+        srda = self.project.analyses.SReachingDefinitions(subject=self.func, func_graph=self.clinic.graph)
+        srda_view = SRDAView(srda.model)
+        walker = FactsCollector(self, srda_view)
         for block in self.clinic.graph.nodes:
             walker.walk(block)
 
         prototype = self._infer_prototype()
+
         self.model.inferred_prototype = prototype
         self.kb.rust_calling_conventions.cache[self.func.addr] = self.model
         l.debug(f"Analysis result for {normalize(self.func.name)} (addr: {hex(self.func.addr)}): {str(self.model)}")
