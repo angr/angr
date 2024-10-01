1b9ae1133ee9e41cda3a0b91baf47defe24c7f3d
Kevin Phoenix <kevin@kphoenix.us>
2024-09-30 17:03:27 -0700

    Remove unused IRange utility class

--- angr/misc/__init__.py
+++ angr/misc/__init__.py
@@ -3,7 +3,6 @@ from . import ux
 from . import autoimport
 from . import weakpatch
 from .loggers import Loggers
-from .range import IRange
 from .plugins import PluginHub, PluginPreset
 from .hookset import HookSet
 from .picklable_lock import PicklableLock
