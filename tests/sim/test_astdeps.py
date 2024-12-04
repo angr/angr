9d39f658761c5d74fc40450a6f03c2f094810679
Kevin Phoenix <kevin@kphoenix.us>
2024-12-04 13:49:28 -0700

    Improve lint

--- tests/sim/test_astdeps.py
+++ tests/sim/test_astdeps.py
@@ -6,12 +6,16 @@ import angr
 
 
 class TestAstDeps(unittest.TestCase):
-    # Previously we had a bug where with the AST_DEPS option enabled, SAO would
-    # be passed to claripy, which would cause an exception to be raised.
+    """
+    Previously we had a bug where with the AST_DEPS option enabled, SAO would be
+    passed to claripy, which would cause an exception to be raised.
 
-    # TODO: These tests only test that we can step with AST_DEPS enabled. We
-    # should also test that the option is actually used and an expected result
-    # is produced.
+    TODO: These tests only test that we can step with AST_DEPS enabled. We
+    should also test that the option is actually used and an expected result is
+    produced.
+    """
+
+    # pylint: disable=no-self-use
 
     def test_stepping_vex(self):
         code = b"\x48\x8d\x54\x24\x08"  # lea rdx, [rsp + 0x8]
