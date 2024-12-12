015911557f6b1a23bc2d62708dbbc42f53035784
Fish <fishw@asu.edu>
2024-12-12 00:31:26 -0700

    Do not reference ConvertType (yet).

--- angr/analyses/decompiler/peephole_optimizations/remove_noop_conversions.py
+++ angr/analyses/decompiler/peephole_optimizations/remove_noop_conversions.py
@@ -1,6 +1,6 @@
 # pylint: disable=missing-class-docstring,no-self-use
 from __future__ import annotations
-from ailment.expression import Convert, ConvertType
+from ailment.expression import Convert
 
 from .base import PeepholeOptimizationExprBase
 
@@ -22,7 +22,7 @@ class RemoveNoopConversions(PeepholeOptimizationExprBase):
                 and expr.to_bits < expr.from_bits
                 and expr.from_bits == inner.to_bits
                 and expr.is_signed == inner.is_signed
-                and expr.from_type == expr.to_type == inner.from_type == inner.to_type == ConvertType.TYPE_INT
+                and expr.from_type == expr.to_type == inner.from_type == inner.to_type == Convert.TYPE_INT
             ):
                 # extension then truncation (e.g., 1->64->1) can be removed, but truncation then extension cannot be
                 # removed (e.g., the high 32 bits must be removed during 64->32->64)
@@ -32,7 +32,7 @@ class RemoveNoopConversions(PeepholeOptimizationExprBase):
                 and expr.from_bits == inner.to_bits
                 and inner.to_bits <= inner.from_bits
                 and expr.is_signed == inner.is_signed
-                and expr.from_type == expr.to_type == inner.from_type == inner.to_type == ConvertType.TYPE_INT
+                and expr.from_type == expr.to_type == inner.from_type == inner.to_type == Convert.TYPE_INT
             ):
                 # merging two truncations into one
                 return Convert(
