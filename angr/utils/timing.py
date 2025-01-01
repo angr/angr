f4c67a0c1e16bbeb4a9a9e69021ae55a67a5e04b
Fish <fishw@asu.edu>
2024-12-31 23:00:06 -0700

    timing: Add print_timing_total. Allow setting TD using envvar.

--- angr/utils/timing.py
+++ angr/utils/timing.py
@@ -7,11 +7,28 @@ from collections import defaultdict
 
 TIMING = os.environ.get("TIMING", "").lower() not in {"", "no", "0", "false"}
 PRINT = os.environ.get("PRINT", "").lower() not in {"", "no", "0", "false"}
-TIME_DISTRIBUTION = False
+TIME_DISTRIBUTION = os.environ.get("TIMING_DIST", "").lower() not in {"", "no", "0", "false"}
 
 total_time = defaultdict(float)
 time_distribution = defaultdict(list)
 depth = 0
+
+
+def print_timing_total():
+    sorted_keys = sorted(time_distribution.keys(), key=lambda x: sum(time_distribution[x]), reverse=True)
+    for func in sorted_keys:
+        millisec = sum(time_distribution[func])
+        sec = millisec / 1000
+        if sec > 1.0:
+            print(f"[timing] {func.__name__}: {sec} seconds.")
+        else:
+            print(f"[timing] {func.__name__}: {millisec} milliseconds.")
+
+        # list top-ten slowest calls
+        sorted_calls = sorted(time_distribution[func], reverse=True)
+        print(f"[timing]   Slowest top 10:")
+        for idx, call in enumerate(sorted_calls[:10]):
+            print(f"[timing]     {idx + 1}: {call} ms")
 
 
 def _t():
