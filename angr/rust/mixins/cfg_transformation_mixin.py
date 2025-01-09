48fcb118bad30a6c4a7b2e10beb4b5d52fe2145b
Fish <fishw@asu.edu>
2025-01-09 15:08:17 -0700

    Fix bugs in _update_phi_variables_after_removing_block.

--- angr/rust/mixins/cfg_transformation_mixin.py
+++ angr/rust/mixins/cfg_transformation_mixin.py
@@ -68,19 +68,35 @@ class CFGTransformationMixin:
 
     @staticmethod
     def _update_phi_variables_after_removing_block(graph, preds, removed_block: Block) -> None:
+        rblock_phi_to_src: dict[int, dict[tuple[int, int | None], int]] = {}
+        for stmt in removed_block.statements:
+            if isinstance(stmt, Assignment) and isinstance(stmt.dst, VirtualVariable) and isinstance(stmt.src, Phi):
+                rblock_phi_to_src[stmt.dst.varid] = dict(stmt.src.src_and_vvars)
+
         for block in graph.nodes:
             for idx in range(len(block.statements)):  # pylint:disable=consider-using-enumerate
                 stmt = block.statements[idx]
                 if isinstance(stmt, Assignment) and isinstance(stmt.src, Phi) and isinstance(stmt.dst, VirtualVariable):
                     # remove the variable from the specified source
-                    new_src_and_vvars = []
+                    new_src_and_vvars = {}
                     for src, vvar in stmt.src.src_and_vvars:
                         if src != (removed_block.addr, removed_block.idx):
-                            new_src_and_vvars.append((src, vvar))
+                            new_src_and_vvars[src] = vvar
                         else:
-                            for pred in preds:
-                                new_src_and_vvars.append(((pred.addr, pred.idx), vvar))
-                    new_phi = Phi(stmt.src.idx, stmt.src.bits, new_src_and_vvars, **stmt.src.tags)
+                            # we need to handle two cases:
+                            # - vvar might be defined in the removed block
+                            # - pred is already a predecessor of the current block
+                            if vvar.varid in rblock_phi_to_src:
+                                # vvar is defined in the removed block
+                                replacements = rblock_phi_to_src[vvar.varid]
+                            else:
+                                replacements = {(pred.addr, pred.idx): vvar for pred in preds}
+                            for repl_src, repl_vvar in replacements.items():
+                                if repl_src not in new_src_and_vvars:
+                                    new_src_and_vvars[repl_src] = repl_vvar
+                    # make it a list
+                    new_src_and_vvars_lst = list(new_src_and_vvars.items())
+                    new_phi = Phi(stmt.src.idx, stmt.src.bits, new_src_and_vvars_lst, **stmt.src.tags)
                     block.statements[idx] = Assignment(stmt.idx, stmt.dst, new_phi, **stmt.tags)
 
     def remove_block(self, block: Block):
