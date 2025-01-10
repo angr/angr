47e18884e9e43fbc057150f19f3a5e238cf21eb0
Fish <fishw@asu.edu>
2025-01-09 18:19:58 -0700

    FactCollector: Properly handle tail-call optimizations.

--- angr/analyses/calling_convention/fact_collector.py
+++ angr/analyses/calling_convention/fact_collector.py
@@ -276,13 +276,21 @@ class FactCollector(Analysis):
             call_succ, ret_succ = None, None
             for _, succ, data in func_graph.out_edges(node, data=True):
                 edge_type = data.get("type")
+                outside = data.get("outside", False)
                 if succ not in traversed and depth + 1 <= self._max_depth:
                     if edge_type == "fake_return":
                         ret_succ = succ
-                    elif edge_type == "transition":
+                    elif edge_type == "transition" and not outside:
                         successor_added = True
                         queue.append((depth + 1, state.copy(), succ, None))
-                    elif edge_type == "call":
+                    elif edge_type == "call" or (edge_type == "transition" and outside):
+                        # a call or a tail-call
+                        if not isinstance(succ, Function):
+                            if self.kb.functions.contains_addr(succ.addr):
+                                succ = self.kb.functions.get_by_addr(succ.addr)
+                            else:
+                                # not sure who we are calling
+                                continue
                         call_succ = succ
             if call_succ is not None:
                 successor_added = True
