c09f9177f9c688ca0e82378007d8ac5add9226f5
Fish <fishw@asu.edu>
2025-01-14 00:19:29 -0700

    type check pcode lifter.py

--- angr/engines/pcode/lifter.py
+++ angr/engines/pcode/lifter.py
@@ -7,7 +7,7 @@
 from __future__ import annotations
 
 import logging
-from typing import Optional
+from typing import Any, TYPE_CHECKING
 from collections.abc import Iterable, Sequence
 
 import archinfo
@@ -33,6 +33,12 @@ try:
     import pypcode
 except ImportError:
     pypcode = None
+
+
+if TYPE_CHECKING:
+    # this is to make pyright happy; otherwise it believes pypcode is None
+    import pypcode
+    from pypcode import PcodeOp, Context
 
 
 l = logging.getLogger(__name__)
@@ -130,8 +136,8 @@ class IRSB:
 
     _direct_next: bool | None
     _exit_statements: Sequence[tuple[int, int, ExitStatement]]
-    _instruction_addresses: Sequence[int] | None
-    _ops: Sequence[pypcode.PcodeOp]  # FIXME: Merge into _statements
+    _instruction_addresses: list[int] | None
+    _ops: list[PcodeOp]  # FIXME: Merge into _statements
     _size: int | None
     _statements: Iterable  # Note: currently unused
     _disassembly: PcodeDisassemblerBlock | None
@@ -140,7 +146,7 @@ class IRSB:
     behaviors: BehaviorFactory | None
     data_refs: Sequence  # Note: currently unused
     const_vals: Sequence  # Note: currently unused
-    default_exit_target: Optional  # Note: currently used
+    default_exit_target: Any  # Note: currently used
     jumpkind: str | None
     next: int | None
 
@@ -199,7 +205,7 @@ class IRSB:
         self._direct_next = None
         self._exit_statements = []
         self._instruction_addresses = None
-        self._ops = []
+        self._ops: list[PcodeOp] = []
         self._size = None
         self._statements = []
         self.addr = mem_addr
@@ -248,7 +254,7 @@ class IRSB:
 
     @property
     def has_statements(self) -> bool:
-        return self.statements is not None and self.statements
+        return bool(self.statements is not None and self.statements)
 
     @property
     def exit_statements(self) -> Sequence[tuple[int, int, ExitStatement]]:
@@ -320,7 +326,7 @@ class IRSB:
         return len(self.statements)
 
     @property
-    def offsIP(self) -> int:
+    def offsIP(self) -> int | None:
         return self.arch.ip_offset
 
     @property
@@ -459,10 +465,10 @@ class IRSB:
         jumpkind: str | None = None,
         direct_next: bool | None = None,
         size: int | None = None,
-        ops: Sequence[pypcode.PcodeOp] | None = None,
-        instruction_addresses: Iterable[int] | None = None,
+        ops: list[PcodeOp] | None = None,
+        instruction_addresses: list[int] | None = None,
         exit_statements: Sequence[tuple[int, int, ExitStatement]] | None = None,
-        default_exit_target: Optional | None = None,
+        default_exit_target: Any = None,
     ) -> None:
         # pylint: disable=unused-argument
         self._statements = statements if statements is not None else []
@@ -490,7 +496,7 @@ class IRSB:
         )
 
     @property
-    def statements(self) -> Iterable:
+    def statements(self) -> list:
         # FIXME: For compatibility, may want to implement Ist_IMark and
         # pyvex.IRStmt.Exit to ease analyses.
         l.debug("Returning empty statements list!")
@@ -807,7 +813,7 @@ class PcodeBasicBlockLifter:
     Lifts basic blocks to P-code
     """
 
-    context: pypcode.Context
+    context: Context
     behaviors: BehaviorFactory
 
     def __init__(self, arch: archinfo.Arch):
@@ -1137,6 +1143,7 @@ class PcodeLifterEngineMixin(SimEngineBase):
 
         # phase 1: parameter defaults
         if addr is None:
+            assert state is not None
             addr = state.solver.eval(state._ip)
         if size is not None:
             size = min(size, IRSB_MAX_SIZE)
@@ -1158,6 +1165,7 @@ class PcodeLifterEngineMixin(SimEngineBase):
                     " disabled."
                 )
             opt_level = 0
+            assert state is not None
             if state and o.OPTIMIZE_IR in state.options:
                 state.options.remove(o.OPTIMIZE_IR)
         if skip_stmts is not True:
@@ -1278,6 +1286,7 @@ class PcodeLifterEngineMixin(SimEngineBase):
                     )
                 return irsb
 
+            raise SimEngineError("Unreachable code reached")
         # phase x: error handling
         except PyVEXError as e:
             l.debug("Translation error at %#x", addr)
