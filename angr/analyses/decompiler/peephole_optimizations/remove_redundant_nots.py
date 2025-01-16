d44f84b6b346968d3f74e5133a5ac193697c9d56
Fish <ltfish@users.noreply.github.com>
2025-01-16 10:56:20 -0700

    PeepholeOpt: Remove unnecessary negations around comparisons. (#5185)

--- angr/analyses/decompiler/peephole_optimizations/remove_redundant_nots.py
+++ angr/analyses/decompiler/peephole_optimizations/remove_redundant_nots.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
-from ailment.expression import UnaryOp
+
+from ailment.expression import BinaryOp, UnaryOp
 
 from .base import PeepholeOptimizationExprBase
 
@@ -12,7 +13,24 @@ class RemoveRedundantNots(PeepholeOptimizationExprBase):
 
     def optimize(self, expr: UnaryOp, **kwargs):
         # Not(Not(expr)) ==> expr
-        if expr.op == "Not" and isinstance(expr.operand, UnaryOp) and expr.operand.op == "Not":
-            return expr.operand.operand
+        if expr.op == "Not":
+            if isinstance(expr.operand, UnaryOp):
+                if expr.operand.op == "Not":
+                    return expr.operand.operand
+            elif isinstance(expr.operand, BinaryOp) and expr.operand.op in BinaryOp.COMPARISON_NEGATION:
+                inner = expr.operand
+                negated_op = BinaryOp.COMPARISON_NEGATION[expr.operand.op]
+                return BinaryOp(
+                    inner.idx,
+                    negated_op,
+                    inner.operands,
+                    inner.signed,
+                    bits=inner.bits,
+                    floating_point=inner.floating_point,
+                    rounding_mode=inner.rounding_mode,
+                    vector_count=inner.vector_count,
+                    vector_size=inner.vector_size,
+                    **inner.tags,
+                )
 
         return None
