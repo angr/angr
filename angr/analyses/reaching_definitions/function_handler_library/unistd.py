9ce64f1d0a8f65e67de2bfda282e028e80933c91
Wil Gibbs <wfgibbs@asu.edu>
2025-02-15 20:05:37 -0700

    Better FD tracking

--- angr/analyses/reaching_definitions/function_handler_library/unistd.py
+++ angr/analyses/reaching_definitions/function_handler_library/unistd.py
@@ -1,17 +1,42 @@
 from __future__ import annotations
+import random
 from angr.analyses.reaching_definitions.function_handler import FunctionCallDataUnwrapped, FunctionHandler
 from angr.analyses.reaching_definitions.function_handler_library.stdio import StdinAtom, StdoutAtom
 from angr.analyses.reaching_definitions.rd_state import ReachingDefinitionsState
+from angr.knowledge_plugins.key_definitions.atoms import Atom
 
 # pylint: disable=no-self-use,missing-class-docstring,unused-argument
 
+class FDAtom(Atom):
+    def __init__(self, fd: int | None, source: str, size: int = 1):
+        self.source = source
+        self.fd = fd
+        self.nonce = random.randint(0, 999999999999)
+        super().__init__(size)
+
+    def _identity(self):
+        if self.fd is not None:
+            return (self.fd,)
+        return (self.nonce,)
 
 class LibcUnistdHandlers(FunctionHandler):
     @FunctionCallDataUnwrapped.decorate
     def handle_impl_read(self, state: ReachingDefinitionsState, data: FunctionCallDataUnwrapped):
         size = state.get_concrete_value(data.args_atoms[2]) or 1
         dst_atom = state.deref(data.args_atoms[1], size)
-        data.depends(dst_atom, StdinAtom(data.function.name, size))
+        real_fd = state.get_concrete_value(data.args_atoms[0])
+        if real_fd is not None:
+            if real_fd == 0:
+                fd_atom = StdinAtom(data.function.name, size)
+            else:
+                fd_atom = FDAtom(real_fd, data.function.name, size)
+        else:
+            fd_atom = FDAtom(None, data.function.name, size)
+        if size is not None:
+            buf_data = state.top(size*8)
+        else:
+            buf_data = state.top(state.arch.bits)
+        data.depends(dst_atom, fd_atom, value=buf_data)
 
     handle_impl_recv = handle_impl_recvfrom = handle_impl_read
 
