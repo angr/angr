cb5e8a385573cad7b6e329f3549f2f7047a6b740
Fish <fishw@asu.edu>
2025-02-19 10:05:48 -0700

    Type check.

--- angr/analyses/decompiler/peephole_optimizations/simplify_pc_relative_loads.py
+++ angr/analyses/decompiler/peephole_optimizations/simplify_pc_relative_loads.py
@@ -20,15 +20,21 @@ class SimplifyPcRelativeLoads(PeepholeOptimizationExprBase):
         if expr.op == "Add" and len(expr.operands) == 2 and isinstance(expr.operands[0], Load):
             op0, op1 = expr.operands
 
+            assert self.project is not None
+            assert expr.ins_addr is not None
+
             # check if op1 is PC
             if (
                 isinstance(op1, Const)
+                and op1.is_int
                 and hasattr(expr, "ins_addr")
-                and is_pc(self.project, expr.ins_addr, op1.value)
+                and is_pc(self.project, expr.ins_addr, op1.value)  # type: ignore
                 and isinstance(op0.addr, Const)
+                and op0.addr.is_int
             ):
                 # check if op0.addr points to a read-only section
                 addr = op0.addr.value
+                assert isinstance(addr, int)
                 if is_in_readonly_section(self.project, addr) or is_in_readonly_segment(self.project, addr):
                     # found it!
                     # do the load first
