db423aada8d496ae01b1b6687fe0b683ca3169aa
mborgerson <contact@mborgerson.com>
2025-03-19 03:44:49 -0700

    Function: Only count local blocks toward size (#5366)

--- angr/knowledge_plugins/functions/function.py
+++ angr/knowledge_plugins/functions/function.py
@@ -612,7 +612,7 @@ class Function(Serializable):
 
     @property
     def size(self):
-        return sum(self._block_sizes.values())
+        return sum(self._block_sizes[addr] for addr in self._local_blocks)
 
     @property
     def binary(self):
