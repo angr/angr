4bb4ca8162ccf4fb0c030df580020aa9cf58ed7e
Audrey Dutcher <audrey@rhelmot.io>
2025-04-07 17:32:31 -0700

    try to get it working

--- corpus_tests/test_corpus.py
+++ corpus_tests/test_corpus.py
@@ -86,36 +86,7 @@ def create_diffable_decompilation(decompiler_output: dict) -> str | None:
     return decompiler_json_newlined
 
 
-def pytest_insta_snapshot_name(binary: str) -> str:
-    """
-    Return the undecorated snapshot name that `pytest-insta` will use to
-    retrieve the snapshot from local storage in `./snapshots/`. Note that this
-    will *not* contain the decorations that `pytest-insta` uses, like its
-    `"<test-name>__<case-name>__"` prefix and `"__<#>.<ext>"` suffix.
-    """
-    return re.sub("/", "_", re.sub("^binaries/", "", binary)) + ".json.txt"
-
-
 def test_decompilation(binary, snapshot):
-    """
-    In order to accommodate insta's need to have snapshots stored in a single
-    file directly in the local `./snapshots/` directory, but also allow a
-    reasonable comparison with the snapshots repo using github's pull request
-    comparison, we pull down each snapshot from deeper within the snapshot
-    repo's directory structure, but place it with a flat name (path delimiters
-    replaced) in the `./snapshots/`, tweaking it to work with the standard
-    way `pytest-insta` works (see `pytest_insta_snapshot_name()` above).
-
-    Note that the snapshot should already be downloaded (by the workflow or
-    manually) and placed in the snapshots directory. The name should be
-    'corpus__decompilation__<binary-subpath-/-escaped>.json.txt__0.txt'. For
-    example, the binary 'binaries/my/path/binary.exe' should be named
-    'corpus__decompilation__my_path_binary.exe.json.txt__0.txt' in the
-    local `./snapshots/` directory.
-
-    This needs to stay in sync with the code that downloads the snapshots
-    in `corpus_test.yml`.
-    """
     decompilation = analyze_binary(binary)
     if not decompilation:
         # Message already emitted.
@@ -124,8 +95,5 @@ def test_decompilation(binary, snapshot):
     # Adds newlines after each newline literal '\\n'.
     diffable_decompilation = create_diffable_decompilation(decompilation)
 
-    # This replaces path delimiters with underscores and appends ".json.txt".
-    snapshot_name = pytest_insta_snapshot_name(binary)
-
-    print(f'Loading snapshot "{snapshot_name}".')
-    assert snapshot(snapshot_name) == diffable_decompilation
+    print(f'Loading snapshot "{binary}".')
+    assert snapshot(binary) == diffable_decompilation
