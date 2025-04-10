3e80c785ffe8b60de22e3ea212080cbad690fe02
Matt Borgerson <contact@mborgerson.com>
2025-04-10 16:19:12 -0700

    CLI: Use argparse subparsers for command argument parsing

--- angr/__main__.py
+++ angr/__main__.py
@@ -7,31 +7,22 @@ from angr.analyses.decompiler.structuring import STRUCTURER_CLASSES, DEFAULT_STR
 from angr.analyses.decompiler.utils import decompile_functions
 
 
-class COMMANDS:
-    """
-    The commands that the angr CLI supports.
-    """
-
-    DECOMPILE = "decompile"
-    ALL_COMMANDS = [DECOMPILE]
+def decompile(args):
+    decompilation = decompile_functions(
+        args.binary,
+        functions=args.functions,
+        structurer=args.structurer,
+        catch_errors=args.catch_exceptions,
+        show_casts=not args.no_casts,
+        base_address=args.base_addr,
+        preset=args.preset,
+    )
+    print(decompilation)
 
 
 def main():
     parser = argparse.ArgumentParser(description="The angr CLI allows you to decompile and analyze binaries.")
-    parser.add_argument(
-        "command",
-        help="""
-        The analysis type to run on the binary. All analysis is output to stdout.""",
-        choices=COMMANDS.ALL_COMMANDS,
-    )
     parser.add_argument("binary", help="The path to the binary to analyze.")
-    parser.add_argument(
-        "--functions",
-        help="""
-        The functions to analyze under the current command. Functions can either be expressed as names found in the
-        symbols of the binary or as addresses like: 0x401000.""",
-        nargs="+",
-    )
     parser.add_argument(
         "--catch-exceptions",
         help="""
@@ -48,40 +39,38 @@ def main():
         type=lambda x: int(x, 0),
         default=None,
     )
-    # decompilation-specific arguments
-    parser.add_argument(
+    subparsers = parser.add_subparsers(metavar="command", required=True)
+
+    decompile_cmd_parser = subparsers.add_parser("decompile", help="Decompile functions.")
+    decompile_cmd_parser.set_defaults(func=decompile)
+    decompile_cmd_parser.add_argument(
         "--structurer",
         help="The structuring algorithm to use for decompilation.",
         choices=STRUCTURER_CLASSES.keys(),
         default=DEFAULT_STRUCTURER.NAME,
     )
-    parser.add_argument(
+    decompile_cmd_parser.add_argument(
         "--no-casts",
         help="Do not show type casts in the decompiled output.",
         action="store_true",
         default=False,
     )
-    parser.add_argument(
+    decompile_cmd_parser.add_argument(
         "--preset",
         help="The configuration preset to use for decompilation.",
         choices=DECOMPILATION_PRESETS,
         default="default",
     )
+    decompile_cmd_parser.add_argument(
+        "--functions",
+        help="""
+        The functions to decompile. Functions can either be expressed as names found in the
+        symbols of the binary or as addresses like: 0x401000.""",
+        nargs="+",
+    )
 
     args = parser.parse_args()
-    if args.command == COMMANDS.DECOMPILE:
-        decompilation = decompile_functions(
-            args.binary,
-            functions=args.functions,
-            structurer=args.structurer,
-            catch_errors=args.catch_exceptions,
-            show_casts=not args.no_casts,
-            base_address=args.base_addr,
-            preset=args.preset,
-        )
-        print(decompilation)
-    else:
-        parser.print_help()
+    args.func(args)
 
 
 if __name__ == "__main__":
