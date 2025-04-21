405316e6db552ab82066873ca21d90d61f34ca15
Kevin Phoenix <kevin@kphoenix.us>
2025-04-21 13:35:59 -0700

    Refactor SegmentList tests into own file (#5421)
    * Refactor SegmentList tests into own file
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fix lint
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ tests/utils/test_segment_list.py
@@ -0,0 +1,113 @@
+from __future__ import annotations
+
+import unittest
+
+from angr.utils.segment_list import SegmentList
+
+
+class TestSegmentList(unittest.TestCase):
+    """
+    Test the SegmentList class.
+    """
+
+    # pylint: disable=no-self-use
+
+    def test_occupy(self):
+        seg_list = SegmentList()
+        seg_list.occupy(0, 1, "code")
+        seg_list.occupy(2, 3, "code")
+
+        assert len(seg_list) == 2
+        assert seg_list._list[0].end == 1
+        assert seg_list._list[1].end == 5
+        assert seg_list.is_occupied(4)
+        assert seg_list.is_occupied(5) is False
+
+    def test_merging(self):
+        seg_list = SegmentList()
+
+        # They should be merged
+        seg_list.occupy(0, 1, "code")
+        seg_list.occupy(1, 2, "code")
+
+        assert len(seg_list) == 1
+        assert seg_list._list[0].start == 0
+        assert seg_list._list[0].end == 3
+
+    def test_not_merged(self):
+        seg_list = SegmentList()
+
+        # They should not be merged
+        seg_list.occupy(0, 1, "code")
+        seg_list.occupy(1, 2, "data")
+
+        assert len(seg_list) == 2
+        assert seg_list._list[0].start == 0
+        assert seg_list._list[0].end == 1
+        assert seg_list._list[1].start == 1
+        assert seg_list._list[1].end == 3
+
+    def test_multi_merge(self):
+        seg_list = SegmentList()
+
+        # They should be merged, and create three different segments
+        seg_list.occupy(0, 5, "code")
+        seg_list.occupy(5, 5, "code")
+        seg_list.occupy(1, 2, "data")
+
+        assert len(seg_list) == 3
+
+        assert seg_list._list[0].start == 0
+        assert seg_list._list[0].end == 1
+        assert seg_list._list[0].sort == "code"
+
+        assert seg_list._list[1].start == 1
+        assert seg_list._list[1].end == 3
+        assert seg_list._list[1].sort == "data"
+
+        assert seg_list._list[2].start == 3
+        assert seg_list._list[2].end == 10
+        assert seg_list._list[2].sort == "code"
+
+    def test_fully_overlapping(self):
+        seg_list = SegmentList()
+
+        seg_list.occupy(5, 5, "code")
+        seg_list.occupy(4, 1, "code")
+        seg_list.occupy(2, 2, "code")
+
+        assert len(seg_list) == 1
+        assert seg_list._list[0].start == 2
+        assert seg_list._list[0].end == 10
+
+    def test_overlapping_not_merged(self):
+        seg_list = SegmentList()
+
+        seg_list.occupy(5, 5, "data")
+        seg_list.occupy(4, 1, "code")
+        seg_list.occupy(2, 2, "data")
+
+        assert len(seg_list) == 3
+        assert seg_list._list[0].start == 2
+        assert seg_list._list[2].end == 10
+
+        seg_list.occupy(3, 2, "data")
+
+        assert len(seg_list) == 1
+        assert seg_list._list[0].start == 2
+        assert seg_list._list[0].end == 10
+
+    def test_partially_overlapping_not_merged(self):
+        seg_list = SegmentList()
+
+        seg_list.occupy(10, 20, "code")
+        seg_list.occupy(9, 2, "data")
+
+        assert len(seg_list) == 2
+        assert seg_list._list[0].start == 9
+        assert seg_list._list[0].end == 11
+        assert seg_list._list[0].sort == "data"
+
+        assert seg_list._list[1].start == 11
+        assert seg_list._list[1].end == 30
+        assert seg_list._list[1].sort == "code"
