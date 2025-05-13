790ef14b734f0d7f317c4b728b8e1aad4be90d95
Kevin Phoenix <kevin@kphoenix.us>
2025-05-12 18:06:39 -0700

    Fix import

--- angr/storage/memory_mixins/paged_memory/pages/history_tracking_mixin.py
+++ angr/storage/memory_mixins/paged_memory/pages/history_tracking_mixin.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 
 from angr.storage.memory_mixins.memory_mixin import MemoryMixin
-from angr.utils.segment_list import SegmentList
+from angr.rustylib import SegmentList
 from .refcount_mixin import RefcountMixin
 
 
