63e829f61995ff3efde2d90ab5cf4342a7f75c3a
Fish <fishw@asu.edu>
2025-06-19 21:10:18 -0700

    SAILR: Fix indentation (and the heuristics).

--- angr/analyses/decompiler/structuring/sailr.py
+++ angr/analyses/decompiler/structuring/sailr.py
@@ -46,15 +46,16 @@ class SAILRStructurer(PhoenixStructurer):
         except StopIteration:
             entry_node = None
 
+        edges = sorted(edges, key=lambda edge: (edge[0].addr, edge[1].addr))
         best_edges = edges
         if entry_node is not None:
             # the first few heuristics are based on the post-dominator count of the edge
             # so we collect them for each candidate edge
             edge_postdom_count = {}
             edge_sibling_count = {}
+            graph_copy = networkx.DiGraph(graph)
             for edge in edges:
                 _, dst = edge
-                graph_copy = networkx.DiGraph(graph)
                 graph_copy.remove_edge(*edge)
                 sibling_cnt = graph_copy.in_degree(dst)
                 if sibling_cnt == 0:
@@ -67,46 +68,48 @@ class SAILRStructurer(PhoenixStructurer):
                     if not isinstance(postdom_node, TemporaryNode) and not isinstance(dominatee, TemporaryNode):
                         post_doms.add((postdom_node, dominatee))
                 edge_postdom_count[edge] = len(post_doms)
+                # add back the edge
+                graph_copy.add_edge(*edge)
 
-                # H1: the edge that has the least amount of sibling edges should be virtualized first
-                # this is believed to reduce the amount of virtualization needed in future rounds and increase
-                # the edges that enter a single outer-scope if-stmt
-                if edge_sibling_count:
-                    min_sibling_count = min(edge_sibling_count.values())
-                    best_edges = [edge for edge, cnt in edge_sibling_count.items() if cnt == min_sibling_count]
-                    if len(best_edges) == 1:
-                        return best_edges
-
-                    # create the next heuristic based on the best edges from the previous heuristic
-                    filtered_edge_postdom_count = edge_postdom_count.copy()
-                    for edge in list(edge_postdom_count.keys()):
-                        if edge not in best_edges:
-                            del filtered_edge_postdom_count[edge]
-                    if filtered_edge_postdom_count:
-                        edge_postdom_count = filtered_edge_postdom_count
-
-                # H2: the edge, when removed, that causes the most post-dominators of the graph should be virtualized
-                # first. this is believed to make the code more linear looking be reducing the amount of scopes.
-                # informally, we believe post-dominators to be an inverse indicator of the number of scopes present
-                if edge_postdom_count:
-                    max_postdom_count = max(edge_postdom_count.values())
-                    best_edges = [edge for edge, cnt in edge_postdom_count.items() if cnt == max_postdom_count]
-                    if len(best_edges) == 1:
-                        return best_edges
-
-                # H3: the edge that goes directly to a return statement should be virtualized first
-                # this is believed to be good because it can be corrected in later optimization by duplicating
-                # the return
-                candidate_edges = best_edges
-                best_edges = []
-                for src, dst in candidate_edges:
-                    if graph.has_node(dst) and structured_node_is_simple_return(dst, graph):
-                        best_edges.append((src, dst))
-
+            # H1: the edge that has the least amount of sibling edges should be virtualized first
+            # this is believed to reduce the amount of virtualization needed in future rounds and increase
+            # the edges that enter a single outer-scope if-stmt
+            if edge_sibling_count:
+                min_sibling_count = min(edge_sibling_count.values())
+                best_edges = [edge for edge, cnt in edge_sibling_count.items() if cnt == min_sibling_count]
                 if len(best_edges) == 1:
                     return best_edges
-                if not best_edges:
-                    best_edges = candidate_edges
+
+                # create the next heuristic based on the best edges from the previous heuristic
+                filtered_edge_postdom_count = edge_postdom_count.copy()
+                for edge in list(edge_postdom_count.keys()):
+                    if edge not in best_edges:
+                        del filtered_edge_postdom_count[edge]
+                if filtered_edge_postdom_count:
+                    edge_postdom_count = filtered_edge_postdom_count
+
+            # H2: the edge, when removed, that causes the most post-dominators of the graph should be virtualized
+            # first. this is believed to make the code more linear looking be reducing the amount of scopes.
+            # informally, we believe post-dominators to be an inverse indicator of the number of scopes present
+            if edge_postdom_count:
+                max_postdom_count = max(edge_postdom_count.values())
+                best_edges = [edge for edge, cnt in edge_postdom_count.items() if cnt == max_postdom_count]
+                if len(best_edges) == 1:
+                    return best_edges
+
+            # H3: the edge that goes directly to a return statement should be virtualized first
+            # this is believed to be good because it can be corrected in later optimization by duplicating
+            # the return
+            candidate_edges = best_edges
+            best_edges = []
+            for src, dst in candidate_edges:
+                if graph.has_node(dst) and structured_node_is_simple_return(dst, graph):
+                    best_edges.append((src, dst))
+
+            if len(best_edges) == 1:
+                return best_edges
+            if not best_edges:
+                best_edges = candidate_edges
 
         # if we have another tie, or we never used improved heuristics, then we do the default ordering.
         return super()._order_virtualizable_edges(graph, best_edges, node_seq)
