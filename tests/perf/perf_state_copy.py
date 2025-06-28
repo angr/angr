ea38feeb592949961bd30c4f2badf697094e4c69
Audrey Dutcher <audrey@rhelmot.io>
2025-06-28 15:52:08 -0700

    ok codspeed time

--- tests/perf/perf_state_copy.py
+++ tests/perf/perf_state_copy.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python3
 from __future__ import annotations
-
 import os
-import time
+import pytest
+import unittest
 
 import angr
 import claripy
@@ -10,9 +9,6 @@ import claripy
 bvs = claripy.BVS("foo", 8)
 
 test_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../../binaries")
-state = angr.Project(
-    os.path.join(test_location, "tests", "x86_64", "fauxware"), main_opts={"base_addr": 0x400000}, auto_load_libs=True
-).factory.full_init_state(add_options={angr.options.REVERSE_MEMORY_NAME_MAP})
 
 
 def cycle(s):
@@ -21,14 +17,20 @@ def cycle(s):
     return s
 
 
-def main():
-    s = cycle(state)
-    for _ in range(20000):
-        s = cycle(s)
+class TestPerfStateCopy(unittest.TestCase):
+    def __init__(self):
+        self.state = angr.Project(
+            os.path.join(test_location, "tests", "x86_64", "fauxware"),
+            main_opts={"base_addr": 0x400000},
+            auto_load_libs=True,
+        ).factory.full_init_state(add_options={angr.options.REVERSE_MEMORY_NAME_MAP})
+
+    @pytest.mark.benchmark
+    def test_main(self):
+        s = cycle(self.state)
+        for _ in range(20000):
+            s = cycle(s)
 
 
 if __name__ == "__main__":
-    tstart = time.time()
-    main()
-    tend = time.time()
-    print("Elapsed: %f sec" % (tend - tstart))
+    unittest.main()
