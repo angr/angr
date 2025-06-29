eb61185a16fd9440a89fe55f9bc1e2ea5c976968
Audrey Dutcher <audrey@rhelmot.io>
2025-06-29 15:00:15 -0700

    whack????

--- tests/perf/perf_unicorn.py
+++ tests/perf/perf_unicorn.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 import os
-import pytest
 import unittest
 
 import angr
@@ -9,26 +8,24 @@ from angr import options as so
 test_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../../binaries")
 
 
-class TestPerfUnicorn(unittest.TestCase):
-    @pytest.mark.benchmark
-    def test_unicorn_0(self, benchmark):
-        p = angr.Project(os.path.join(test_location, "tests", "x86_64", "perf_unicorn_0"))
-        s_unicorn = p.factory.entry_state(
-            add_options=so.unicorn | {so.STRICT_PAGE_ACCESS}, remove_options={so.LAZY_SOLVES}
-        )  # unicorn
-        sm_unicorn = p.factory.simulation_manager(s_unicorn)
+def test_unicorn_0(benchmark):
+    p = angr.Project(os.path.join(test_location, "tests", "x86_64", "perf_unicorn_0"))
+    s_unicorn = p.factory.entry_state(
+        add_options=so.unicorn | {so.STRICT_PAGE_ACCESS}, remove_options={so.LAZY_SOLVES}
+    )  # unicorn
+    sm_unicorn = p.factory.simulation_manager(s_unicorn)
 
-        benchmark(sm_unicorn.run)
+    benchmark(sm_unicorn.run)
 
-    @pytest.mark.benchmark
-    def test_unicorn_1(self, benchmark):
-        p = angr.Project(os.path.join(test_location, "tests", "x86_64", "perf_unicorn_1"))
-        s_unicorn = p.factory.entry_state(
-            add_options=so.unicorn | {so.STRICT_PAGE_ACCESS}, remove_options={so.LAZY_SOLVES}
-        )  # unicorn
-        sm_unicorn = p.factory.simulation_manager(s_unicorn)
 
-        benchmark(sm_unicorn.run)
+def test_unicorn_1(benchmark):
+    p = angr.Project(os.path.join(test_location, "tests", "x86_64", "perf_unicorn_1"))
+    s_unicorn = p.factory.entry_state(
+        add_options=so.unicorn | {so.STRICT_PAGE_ACCESS}, remove_options={so.LAZY_SOLVES}
+    )  # unicorn
+    sm_unicorn = p.factory.simulation_manager(s_unicorn)
+
+    benchmark(sm_unicorn.run)
 
 
 if __name__ == "__main__":
