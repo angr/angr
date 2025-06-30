f2d297e428b4aba7bd5466a9e3b0b2290f3fb801
Kevin Phoenix <kevin@kphoenix.us>
2025-06-30 14:38:47 -0700

    Switch to scikit-build-core and cmake for building unicornlib

--- setup.py
+++ setup.py
@@ -1,19 +1,10 @@
-# pylint: disable=missing-class-docstring
 from __future__ import annotations
-import glob
+
+import os.path
 import importlib
 import importlib.resources
-import os
-import platform
-import shutil
-import subprocess
-import sys
-from distutils.command.build import build as st_build
-from distutils.util import get_platform
 
-from setuptools import Command, setup
-from setuptools.command.develop import develop as st_develop
-from setuptools.errors import LibError
+from setuptools import setup
 
 # Import setuptools_rust to ensure an error is raised if not installed
 try:
@@ -21,112 +12,14 @@ try:
 except ImportError as err:
     raise Exception("angr requires setuptools-rust to build") from err
 
-if sys.platform == "darwin":
-    library_file = "unicornlib.dylib"
-elif sys.platform in ("win32", "cygwin"):
-    library_file = "unicornlib.dll"
-else:
-    library_file = "unicornlib.so"
 
+PYVEX_INCLUDE_PATH = str(importlib.resources.files("pyvex").joinpath("include"))
+PYVEX_LIB_PATH = str(importlib.resources.files("pyvex").joinpath("lib"))
 
-def build_unicornlib():
-    try:
-        importlib.import_module("pyvex")
-    except ImportError as e:
-        raise LibError("You must install pyvex before building angr") from e
+CMAKE_SOURCE_DIR = str(os.path.join(os.path.dirname(__file__), "native", "unicornlib"))
+CMAKE_ARGS = [f"-DPYVEX_INCLUDE_PATH={PYVEX_INCLUDE_PATH}", f"-DPYVEX_LIB_PATH={PYVEX_LIB_PATH}"]
 
-    env = os.environ.copy()
-    env_data = (
-        ("PYVEX_INCLUDE_PATH", "pyvex", "include"),
-        ("PYVEX_LIB_PATH", "pyvex", "lib"),
-        ("PYVEX_LIB_FILE", "pyvex", "lib\\pyvex.lib"),
-    )
-    for var, pkg, fnm in env_data:
-        base = importlib.resources.files(pkg)
-        for child in fnm.split("\\"):
-            base = base.joinpath(child)
-        env[var] = str(base)
-
-    if sys.platform == "win32":
-        cmd = ["nmake", "/f", "Makefile-win"]
-    elif shutil.which("gmake") is not None:
-        cmd = ["gmake"]
-    else:
-        cmd = ["make"]
-    try:
-        subprocess.run(cmd, cwd="native/unicornlib", env=env, check=True)
-    except FileNotFoundError as err:
-        raise LibError("Couldn't find " + cmd[0] + " in PATH") from err
-    except subprocess.CalledProcessError as err:
-        raise LibError("Error while building unicornlib: " + str(err)) from err
-
-    shutil.rmtree("angr/lib", ignore_errors=True)
-    os.mkdir("angr/lib")
-    shutil.copy(os.path.join("native/unicornlib", library_file), "angr")
-
-
-def clean_unicornlib():
-    oglob = glob.glob("native/*.o")
-    oglob += glob.glob("native/*.obj")
-    oglob += glob.glob("native/*.so")
-    oglob += glob.glob("native/*.dll")
-    oglob += glob.glob("native/*.dylib")
-    for fname in oglob:
-        os.unlink(fname)
-
-
-class build(st_build):
-    def run(self, *args):
-        self.execute(build_unicornlib, (), msg="Building unicornlib")
-        super().run(*args)
-
-
-class clean(Command):
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        self.execute(clean, (), msg="Cleaning unicornlib")
-
-
-class develop(st_develop):
-    def run(self):
-        self.run_command("build")
-        super().run()
-
-
-cmdclass = {
-    "build": build,
-    "clean_unicornlib": clean,
-    "develop": develop,
-}
-
-
-try:
-    from setuptools.command.editable_wheel import editable_wheel as st_editable_wheel
-
-    class editable_wheel(st_editable_wheel):
-        def run(self):
-            self.run_command("build")
-            super().run()
-
-    cmdclass["editable_wheel"] = editable_wheel
-except ModuleNotFoundError:
-    pass
-
-
-if "bdist_wheel" in sys.argv and "--plat-name" not in sys.argv:
-    sys.argv.append("--plat-name")
-    name = get_platform()
-    if "linux" in name:
-        sys.argv.append("manylinux2014_" + platform.machine())
-    else:
-        # https://www.python.org/dev/peps/pep-0425/
-        sys.argv.append(name.replace(".", "_").replace("-", "_"))
-
-setup(cmdclass=cmdclass)
+setup(
+    cmake_source_dir=CMAKE_SOURCE_DIR,
+    cmake_args=CMAKE_ARGS,
+)
