cff71d9a08fb3ef6327fd8f5820bc652c9634fc3
Kevin Phoenix <kevin@kphoenix.us>
2025-07-09 17:22:13 -0700

    Remove linux platform tag override in setup.py

--- setup.py
+++ setup.py
@@ -120,13 +120,4 @@ except ModuleNotFoundError:
     pass
 
 
-if "bdist_wheel" in sys.argv and "--plat-name" not in sys.argv:
-    sys.argv.append("--plat-name")
-    name = get_platform()
-    if "linux" in name:
-        sys.argv.append("manylinux2014_" + platform.machine())
-    else:
-        # https://www.python.org/dev/peps/pep-0425/
-        sys.argv.append(name.replace(".", "_").replace("-", "_"))
-
 setup(cmdclass=cmdclass)
