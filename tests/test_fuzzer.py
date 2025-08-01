34f1456f6406d177b111dc8ea561c747efae064f
Kevin Phoenix <kevin@kphoenix.us>
2025-07-31 18:38:03 -0700

    WIP Fuzzer

---
+++ tests/test_fuzzer.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+import os.path
+
+import angr
+from angr.rustylib.fuzzer import Fuzzer, InMemoryCorpus
+
+from tests.common import bin_location
+
+
+def test_fuzzer():
+    project = angr.Project(
+        os.path.join(bin_location, "tests", "x86_64", "fauxware"), auto_load_libs=True, use_sim_procedures=False
+    )
+    stdin = angr.SimFile(content=b"", concrete=True)
+    base_state = project.factory.entry_state(stdin=stdin, add_options={angr.options.STRICT_PAGE_ACCESS})
+
+    def apply_fn(state: SimState, input: bytes):
+        print("Apply function called with input:", input, flush=True)
+        state.posix.stdin.write(0, input)
+        print("Done")
+
+    corpus = InMemoryCorpus.from_list([b"S"])
+    solutions = InMemoryCorpus()
+
+    fuzzer = Fuzzer(base_state, corpus, solutions, apply_fn, 0, 0)
+
+    new_corpus_entry = fuzzer.run_once()
+    print("New corpus entry:", new_corpus_entry, flush=True)
+    print("Value: ", corpus[new_corpus_entry], flush=True)
+    print("Fuzzer run completed", flush=True)
+
+    new_corpus_entry = fuzzer.run_once()
+    print("New corpus entry:", new_corpus_entry, flush=True)
+    print("Value: ", corpus[new_corpus_entry], flush=True)
+    print("Fuzzer run completed", flush=True)
