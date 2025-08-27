d402429054ad26b15bc1022c0a467bb7c4bc7608
Fish <fishw@asu.edu>
2025-08-26 23:30:41 -0700

    Add a test case.

---
+++ tests/analyses/decompiler/test_peephole_wstrcpy.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# pylint: disable=missing-class-docstring,no-self-use,no-member
+from __future__ import annotations
+
+__package__ = __package__ or "tests.analyses.decompiler"  # pylint:disable=redefined-builtin
+
+import logging
+import os
+import unittest
+
+import angr
+
+from tests.common import bin_location, print_decompilation_result, WORKER
+
+
+test_location = os.path.join(bin_location, "tests")
+
+l = logging.Logger(__name__)
+
+
+class TestDecompilerWstrcpy(unittest.TestCase):
+    def test_ipnathlp_IcsRegReadFromLocation(self):
+        bin_path = os.path.join(test_location, "x86_64", "windows", "ipnathlp.dll")
+        proj = angr.Project(bin_path, auto_load_libs=False)
+
+        cfg = proj.analyses.CFGFast(show_progressbar=not WORKER, fail_fast=True, normalize=True)
+        func = cfg.functions[0x18003CA70]
+        assert func is not None
+        dec = proj.analyses.Decompiler(func, cfg=cfg)
+        assert dec.codegen is not None and dec.codegen.text is not None
+        print_decompilation_result(dec)
+
+        assert (
+            'L"System\\\\CurrentControlSet\\\\Services\\\\Tcpip6\\\\Parameters\\\\Interfaces\\\\", 63)'
+            in dec.codegen.text
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
