ec2f1193a98abde314ee1be63208fb4d566b4ef2
Fish <ltfish@users.noreply.github.com>
2025-09-24 13:36:17 -0700

    Decompiler: Rewrite VEX-ccalls to _ccall(). (#5682)
    * Decompiler: Rewrite VEX-ccalls to _ccall().
    
    * Type check.

--- angr/analyses/decompiler/ccall_rewriters/rewriter_base.py
+++ angr/analyses/decompiler/ccall_rewriters/rewriter_base.py
@@ -12,9 +12,13 @@ class CCallRewriterBase:
         "result",
     )
 
-    def __init__(self, ccall: ailment.Expr.VEXCCallExpression, arch):
+    def __init__(self, ccall: ailment.Expr.VEXCCallExpression, arch, rename_ccalls: bool = False):
         self.arch = arch
         self.result: ailment.Expr.Expression | None = self._rewrite(ccall)
+        if rename_ccalls and self.result is None and ccall.callee != "_ccall":
+            renamed = ccall.copy()
+            renamed.callee = "_ccall"
+            self.result = renamed
 
     def _rewrite(self, ccall: ailment.Expr.VEXCCallExpression) -> ailment.Expr.Expression | None:
         raise NotImplementedError
