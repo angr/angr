7da846506ca266645f136b3b16d88400195a9a38
Fish <fishw@asu.edu>
2025-10-22 03:48:53 -0700

    Undo the changes to the msvcrt_setsbuplow test case.

--- tests/analyses/decompiler/test_decompiler.py
+++ tests/analyses/decompiler/test_decompiler.py
@@ -4407,11 +4407,10 @@ class TestDecompiler(unittest.TestCase):
         # intended:
         #   do
         #   {
-        #       *(v11) = v10;
-        #       v9 += 1;
-        #       v10 = v9;
-        #       v11 += 1;
-        #   } while ((unsigned int)v9 < 0x100);
+        #       *(v12) = (char)v11;
+        #       v11 = (unsigned int)v11 + 1;
+        #       v12 += 1;
+        #   } while ((unsigned int)v11 < 0x100);
         lines = [line.strip(" ") for line in d.codegen.text.split("\n")]
         while True:
             # find the do-while loop
@@ -4421,11 +4420,10 @@ class TestDecompiler(unittest.TestCase):
                 assert False, "Cannot find the do-while loop in this function"
             if (
                 lines[start_idx + 1] == "{"
-                and re.match(r"\*\(v\d+\) = v\d+;", lines[start_idx + 2])
-                and re.match(r"v\d+ \+= 1;", lines[start_idx + 3])
-                and re.match(r"v\d+ = v\d+;", lines[start_idx + 4])
-                and re.match(r"v\d+ \+= 1;", lines[start_idx + 5])
-                and re.match(r"} while \(v\d+ < 0x100\);", lines[start_idx + 6])
+                and re.match(r"\*\(v\d+\) = \(char\)v\d+;", lines[start_idx + 2])
+                and re.match(r"v\d+ = \(unsigned int\)v\d+ \+ 1;", lines[start_idx + 3])
+                and re.match(r"v\d+ \+= 1;", lines[start_idx + 4])
+                and re.match(r"} while \(\(unsigned int\)v\d+ < 0x100\);", lines[start_idx + 5])
             ):
                 # found it!
                 break
