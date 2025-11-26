7cdfd1bef9403417276b3fb44932f478bf664add
Matt Borgerson <contact@mborgerson.com>
2025-11-26 16:57:39 -0700

    X86CCallRewriter: Rewrite reads from fs:[0x18, 0x30] to NtGetCurrent{T,P}eb

---
+++ tests/analyses/decompiler/test_ccall_rewriting.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# pylint: disable=missing-class-docstring,no-self-use,no-member
+from __future__ import annotations
+
+__package__ = __package__ or "tests.analyses.decompiler"  # pylint:disable=redefined-builtin
+
+import os
+import unittest
+
+import angr
+
+from tests.common import bin_location
+
+
+test_location = os.path.join(bin_location, "tests")
+
+
+class TestCCallRewriting(unittest.TestCase):
+    def test_NtGetCurrentPeb(self):
+        bin_path = os.path.join(
+            test_location, "i386", "windows", "48460c9633d06cad3e3b41c87de04177d129906610c5bbdebc7507a211100e98"
+        )
+        proj = angr.Project(bin_path)
+        cfg = proj.analyses.CFGFast(fail_fast=True, normalize=True)
+        func = cfg.functions[0x401030]
+        assert func is not None
+
+        dec = proj.analyses.Decompiler(func, cfg=cfg)
+        assert dec.codegen is not None and dec.codegen.text is not None
+
+        assert "PEB *v0;" in dec.codegen.text
+        assert "v0 = NtGetCurrentPeb();" in dec.codegen.text
+
+
+if __name__ == "__main__":
+    unittest.main()
