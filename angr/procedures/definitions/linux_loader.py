26f8a2804bbb5bb3dfaef5ae54daeb9d6498de13
Matt Borgerson <contact@mborgerson.com>
2025-11-26 17:21:17 -0700

    pyproject.toml: Include definitions for Black formatting

--- angr/procedures/definitions/linux_loader.py
+++ angr/procedures/definitions/linux_loader.py
@@ -3,5 +3,5 @@ from . import SimLibrary
 from angr.procedures import SIM_PROCEDURES as P
 
 lib = SimLibrary()
-lib.set_library_names('ld.so', 'ld-linux.so', 'ld.so.2', 'ld-linux.so.2', 'ld-linux-x86-64.so.2')
-lib.add_all_from_dict(P['linux_loader'])
+lib.set_library_names("ld.so", "ld-linux.so", "ld.so.2", "ld-linux.so.2", "ld-linux-x86-64.so.2")
+lib.add_all_from_dict(P["linux_loader"])
--- angr/procedures/definitions/msvcr.py
+++ angr/procedures/definitions/msvcr.py
@@ -5,12 +5,13 @@ from angr.procedures import SIM_PROCEDURES as P
 from angr.calling_conventions import SimCCMicrosoftAMD64
 
 libc = SimLibrary()
-libc.set_library_names('msvcrt.dll', 'msvcr71.dll', 'msvcr100.dll', 'msvcr110.dll', 'msvcrt20.dll', 'msvcrt40.dll',
-                       'msvcr120.dll')
-libc.add_all_from_dict(P['libc'])
-libc.add_all_from_dict(P['msvcr']) # overwrite any that are also defined in libc
-libc.set_non_returning('_exit', 'abort', 'exit', '_invoke_watson')
+libc.set_library_names(
+    "msvcrt.dll", "msvcr71.dll", "msvcr100.dll", "msvcr110.dll", "msvcrt20.dll", "msvcrt40.dll", "msvcr120.dll"
+)
+libc.add_all_from_dict(P["libc"])
+libc.add_all_from_dict(P["msvcr"])  # overwrite any that are also defined in libc
+libc.set_non_returning("_exit", "abort", "exit", "_invoke_watson")
 
-libc.add_alias('_initterm', '_initterm_e')
+libc.add_alias("_initterm", "_initterm_e")
 
-libc.set_default_cc('AMD64', SimCCMicrosoftAMD64)
+libc.set_default_cc("AMD64", SimCCMicrosoftAMD64)
