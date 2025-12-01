6ba342cb16fb059d016d96104f3a19309b2c0c7e
Audrey Dutcher <audrey@rhelmot.io>
2025-12-01 12:46:34 -0700

    Add end_node option to inverted_idoms; make original_nodes a list

--- angr/analyses/decompiler/utils.py
+++ angr/analyses/decompiler/utils.py
@@ -497,8 +497,8 @@ def _merge_ail_nodes(graph, node_a: ailment.Block, node_b: ailment.Block) -> ail
     in_edges = list(graph.in_edges(node_a, data=True))
     out_edges = list(graph.out_edges(node_b, data=True))
 
-    a_ogs = graph.nodes[node_a].get("original_nodes", set())
-    b_ogs = graph.nodes[node_b].get("original_nodes", set())
+    a_ogs = graph.nodes[node_a].get("original_nodes", [])
+    b_ogs = graph.nodes[node_b].get("original_nodes", [])
     new_node = node_a.copy() if node_a.addr <= node_b.addr else node_b.copy()
     old_node = node_b if new_node == node_a else node_a
     # remove jumps in the middle of nodes when merging
@@ -511,7 +511,7 @@ def _merge_ail_nodes(graph, node_a: ailment.Block, node_b: ailment.Block) -> ail
     graph.remove_node(node_b)
 
     if new_node is not None:
-        graph.add_node(new_node, original_nodes=a_ogs.union(b_ogs))
+        graph.add_node(new_node, original_nodes=a_ogs + b_ogs)
         for src, _, data in in_edges:
             if src is node_b:
                 src = new_node
@@ -536,7 +536,7 @@ def to_ail_supergraph(transition_graph: networkx.DiGraph, allow_fake=False) -> n
     """
     # make a copy of the graph
     transition_graph = networkx.DiGraph(transition_graph)
-    networkx.set_node_attributes(transition_graph, {node: {node} for node in transition_graph.nodes}, "original_nodes")
+    networkx.set_node_attributes(transition_graph, {node: [node] for node in transition_graph.nodes}, "original_nodes")
 
     while True:
         for src, dst, data in transition_graph.edges(data=True):
--- angr/utils/graph.py
+++ angr/utils/graph.py
@@ -30,7 +30,9 @@ def shallow_reverse(g: networkx.DiGraph[T]) -> networkx.DiGraph[T]:
     return new_g
 
 
-def inverted_idoms(graph: networkx.DiGraph[T]) -> tuple[networkx.DiGraph[T], dict[T, T] | None]:
+def inverted_idoms(
+    graph: networkx.DiGraph[T], end_node: T | None = None
+) -> tuple[networkx.DiGraph[T], dict[T, T] | None]:
     """
     Invert the given graph and generate the immediate dominator tree on the inverted graph. This is useful for
     computing post-dominators.
@@ -39,8 +41,8 @@ def inverted_idoms(graph: networkx.DiGraph[T]) -> tuple[networkx.DiGraph[T], dic
     :return:        A tuple of the inverted graph and the immediate dominator tree.
     """
 
-    end_nodes = {n for n in graph.nodes() if graph.out_degree(n) == 0}
     inverted_graph: networkx.DiGraph = shallow_reverse(graph)
+    end_nodes = {n for n in graph.nodes() if graph.out_degree(n) == 0} if end_node is None else {end_node}
     if end_nodes:
         if len(end_nodes) > 1:
             # make sure there is only one end node
