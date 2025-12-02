3f4963e07b2b7112d1c040f325ccd89ad1c90b5c
Matt Borgerson <contact@mborgerson.com>
2025-12-02 14:28:27 -0700

    TestCCallRewriting: Test function prototype is correctly deduced

--- tests/analyses/decompiler/test_ccall_rewriting.py
+++ tests/analyses/decompiler/test_ccall_rewriting.py
@@ -28,6 +28,7 @@ class TestCCallRewriting(unittest.TestCase):
         dec = proj.analyses.Decompiler(func, cfg=cfg)
         assert dec.codegen is not None and dec.codegen.text is not None
 
+        assert "PEB * sub_401030()" in dec.codegen.text
         assert "PEB *v0;" in dec.codegen.text
         assert "v0 = NtGetCurrentPeb();" in dec.codegen.text
 
