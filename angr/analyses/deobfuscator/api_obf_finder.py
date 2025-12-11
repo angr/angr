3d2aaaac3b57e0113ee8fcba8ec43e2ce0556af1
Fish <fishw@asu.edu>
2025-12-11 13:48:20 -0700

    Oops.

--- angr/analyses/deobfuscator/api_obf_finder.py
+++ angr/analyses/deobfuscator/api_obf_finder.py
@@ -113,7 +113,13 @@ class APIObfuscationFinder(Analysis):
                 self.kb.obfuscations.type1_deobfuscated_apis.update(type1_deobfuscated)
 
         APIObfuscationType2Finder(self.project, self.variable_kb).analyze()
-        self.project.analyses[HashLookupAPIDeobfuscator].prep(fail_fast=self._fail_fast)()
+        self.project.analyses[HashLookupAPIDeobfuscator].prep(fail_fast=self._fail_fast)(
+            self._hash_lookup_api_deobfuscator_lifter
+        )
+
+    def _hash_lookup_api_deobfuscator_lifter(self, func):
+        d = self.project.analyses.Decompiler(func)
+        return d.clinic
 
     def _find_type1(self):
         cfg = self.kb.cfgs.get_most_accurate()
