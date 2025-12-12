8dcf8aa2a40b4f07c8a4108d28754e071c88383b
Audrey Dutcher <audrey@rhelmot.io>
2025-12-12 14:13:42 -0700

    Add testcase

---
+++ tests/test_load_shellcode.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+import unittest
+
+import angr
+
+
+class TestLoadShellcode(unittest.TestCase):
+    def test_simos(self):
+        for os in "windows", "linux":
+            p = angr.load_shellcode(b"\xc3", arch="x86", simos=os)
+            p.factory.entry_state()
+
+
+if __name__ == "__main__":
+    unittest.main()
