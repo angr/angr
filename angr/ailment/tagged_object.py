c24b28a10c24a3d7ea5722a728f60eb15c2fa649
Matt Borgerson <contact@mborgerson.com>
2025-12-22 16:14:57 -0700

    TaggedObject: Type check tags

--- angr/ailment/tagged_object.py
+++ angr/ailment/tagged_object.py
@@ -1,28 +1,53 @@
 from __future__ import annotations
 
+from typing import Any, TypedDict, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing_extensions import Unpack
+
+    from angr.sim_type import SimType
+    from angr.sim_variable import SimVariable
+
+
+class TagDict(TypedDict, total=False):
+    """
+    Typed dict of tags for TaggedObject.
+    """
+
+    always_propagate: bool
+    block_idx: int
+    custom_string: bool
+    deref_src_addr: int
+    ins_addr: int
+    is_prototype_guessed: bool
+    keep_in_slice: bool
+    orig_ins_addr: int
+    reference_values: dict[SimType, Any]
+    reference_variable_offset: int
+    reference_variable: SimVariable
+    reg_name: str
+    struct_member_info: tuple[int, SimVariable, SimType]
+    type: dict[str, SimType]
+    vex_block_addr: int
+    vex_stmt_idx: int
+    write_size: int
+
 
 class TaggedObject:
     """
-    A class that takes arbitrary tags.
+    A class that takes tags.
     """
 
     __slots__ = (
         "_hash",
-        "_tags",
         "idx",
+        "tags",
     )
 
-    def __init__(self, idx: int | None, **kwargs):
-        self._tags = None
+    def __init__(self, idx: int | None, **kwargs: Unpack[TagDict]):
+        self.tags: TagDict = kwargs
         self.idx = idx
         self._hash = None
-        if kwargs:
-            self.initialize_tags(kwargs)
-
-    def initialize_tags(self, tags):
-        self._tags = {}
-        for k, v in tags.items():
-            self._tags[k] = v
 
     def __hash__(self) -> int:
         if self._hash is None:
@@ -31,9 +56,3 @@ class TaggedObject:
 
     def _hash_core(self):
         raise NotImplementedError
-
-    @property
-    def tags(self) -> dict:
-        if not self._tags:
-            self._tags = {}
-        return self._tags
