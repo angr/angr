f3651b8120b353c0f039e072984390b48f4c9bb6
Kyle Zeng <zengyhkyle@gmail.com>
2025-12-29 14:38:19 -0700

    merge changes in address_concretization_mixin

--- angr/storage/memory_mixins/address_concretization_mixin.py
+++ angr/storage/memory_mixins/address_concretization_mixin.py
@@ -269,15 +269,18 @@ class AddressConcretizationMixin(MemoryMixin):
                 self.state.solver.eval(addr), True, addr, condition, size, read_value=None, **kwargs
             )
 
-        if self.state.solver.symbolic(addr) and options.AVOID_MULTIVALUED_READS in self.state.options:
+        if options.AVOID_MULTIVALUED_READS in self.state.options:
             return self._default_value(addr, size, name="symbolic_read_unconstrained", **kwargs)
 
-        try:
-            concrete_addrs = self._interleave_ints(sorted(self.concretize_read_addr(addr, condition=condition)))
-        except SimMemoryError:
-            if options.CONSERVATIVE_READ_STRATEGY in self.state.options:
-                return self._default_value(addr, size, name="symbolic_read_unconstrained", **kwargs)
-            raise
+        if addr.variables.intersection(self.state.solver._solver.variables):
+            try:
+                concrete_addrs = self._interleave_ints(sorted(self.concretize_read_addr(addr, condition=condition)))
+            except SimMemoryError:
+                if options.CONSERVATIVE_READ_STRATEGY in self.state.options:
+                    return self._default_value(addr, size, name="symbolic_read_unconstrained", **kwargs)
+                raise
+        elif options.CONSERVATIVE_READ_STRATEGY in self.state.options:
+            return self._default_value(None, size, name="symbolic_read_unconstrained", **kwargs)
 
         # quick optimization so as to not involve the solver if not necessary
         trivial = len(concrete_addrs) == 1 and (addr == concrete_addrs[0]).is_true()
