a66169209260abebc53d98939ec2a3171d5561b1
Audrey Dutcher <audrey@rhelmot.io>
2026-01-15 13:43:50 -0700

    Add prototypes for intrinsics and peephole for const conversions

--- angr/analyses/decompiler/peephole_optimizations/__init__.py
+++ angr/analyses/decompiler/peephole_optimizations/__init__.py
@@ -58,6 +58,7 @@ from .a_mul_const_sub_a import AMulConstSubA
 from .rewrite_cxx_operator_calls import RewriteCxxOperatorCalls
 from .remove_cxx_destructor_calls import RemoveCxxDestructorCalls
 from .rewrite_conv_mul import RewriteConvMul
+from .evaluate_const_conversions import EvaluateConstConversions
 from .base import PeepholeOptimizationExprBase, PeepholeOptimizationStmtBase, PeepholeOptimizationMultiStmtBase
 
 
@@ -119,6 +120,7 @@ ALL_PEEPHOLE_OPTS: list[Any] = [
     RewriteCxxOperatorCalls,
     RemoveCxxDestructorCalls,
     RewriteConvMul,
+    EvaluateConstConversions,
 ]
 
 MULTI_STMT_OPTS: list[type[PeepholeOptimizationMultiStmtBase]] = [
