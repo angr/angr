6286230d2dadf4b5b2362e824041cf4c8d9a2499
Kevin Phoenix <kevin@kphoenix.us>
2026-01-20 18:22:42 -0500

    Update libafl to 0.15.4

--- native/angr/src/fuzzer.rs
+++ native/angr/src/fuzzer.rs
@@ -8,7 +8,7 @@ use libafl::{
     NopInputFilter, StdFuzzer,
     events::SimpleEventManager,
     feedbacks::{CrashFeedback, MaxMapFeedback},
-    inputs::{BytesInput, NopBytesConverter},
+    inputs::{BytesInput, NopToTargetBytes},
     mutators::{HavocMutationsType, HavocScheduledMutator, havoc_mutations},
     observers::OwnedMapObserver,
     schedulers::QueueScheduler,
@@ -40,7 +40,7 @@ pub(crate) type OT = tuple_list_type!(O);
 pub(crate) type Z = StdFuzzer<
     QueueScheduler,
     MaxMapFeedback<O, O>,
-    NopBytesConverter,
+    NopToTargetBytes,
     NopInputFilter,
     CrashFeedback,
 >;
@@ -107,7 +107,7 @@ impl Fuzzer {
         let fuzzer: StdFuzzer<
             QueueScheduler,
             MaxMapFeedback<OwnedMapObserver<u8>, OwnedMapObserver<u8>>,
-            NopBytesConverter,
+            NopToTargetBytes,
             NopInputFilter,
             CrashFeedback,
         > = StdFuzzer::new(QueueScheduler::new(), feedback, objective);
