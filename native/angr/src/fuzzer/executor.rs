c116e5e9b225b27b7dccefa5edfa25616d1b1cb6
Kevin Phoenix <kevin@kphoenix.us>
2026-01-20 16:23:06 -0700

    Update libafl to 0.15.4 (#6038)

--- native/angr/src/fuzzer/executor.rs
+++ native/angr/src/fuzzer/executor.rs
@@ -191,8 +191,4 @@ impl<S> HasTimeout for PyExecutorInner<S> {
     fn timeout(&self) -> Duration {
         self.timeout.unwrap_or(Duration::ZERO)
     }
-
-    fn set_timeout(&mut self, timeout: Duration) {
-        self.timeout = Some(timeout);
-    }
 }
