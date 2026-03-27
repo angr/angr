2a8fd4d1a48816d911092c8c19f9a006ad7982dd
bluesadi <yiboliume@gmail.com>
2026-03-27 14:17:07 -0700

    Get default sig dir from angr/flirt_signatures Python package

--- angr/rust/utils/rust_sigs.py
+++ angr/rust/utils/rust_sigs.py
@@ -1,24 +1,33 @@
 from __future__ import annotations
 
+import logging
 import os
 import sys
 
-import angr
 from angr.utils.env import is_pyinstaller
 
 
+l = logging.getLogger(name=__name__)
+
 def get_default_sig_dir(arch_name: str = "x86_64", platform: str = "linux") -> str | None:
     if is_pyinstaller():
-        base_dir = os.path.dirname(sys.executable)
+        base_dir = os.path.join(os.path.dirname(sys.executable), "flirt_signatures")
     else:
         if "angrmanagement" in sys.modules:
             angrm_dir = os.path.dirname(os.path.realpath(sys.modules["angrmanagement"].__file__))
-            base_dir = os.path.join(angrm_dir, "resources")
+            base_dir = os.path.join(angrm_dir, "resources", "flirt_signatures")
         else:
-            base_dir = os.path.dirname(os.path.realpath(angr.__file__))
+            base_dir = None
 
-    for parent_level in range(3):
-        sig_dir = os.path.join(base_dir, *[".."] * parent_level, "flirt_signatures", arch_name, platform, "rust")
+    if base_dir is None:
+        try:
+            from flirt_signatures import signatures_path
+            base_dir = signatures_path()
+        except ImportError:
+            l.warning("Could not import flirt_signatures. Please install https://github.com/angr/flirt_signatures.git in your environment.")
+
+    if base_dir:
+        sig_dir = os.path.join(base_dir, arch_name, platform, "rust")
         if os.path.isdir(sig_dir):
             return os.path.normpath(sig_dir)
     return None
