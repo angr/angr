sefcom/oxidizer

Support simplify write! and writeln!

angr/analyses/decompiler/condition_processor.py
@@ -891,7 +891,7 @@ class ConditionProcessor:
             (ailment.Expr.VEXCCallExpression, ailment.Expr.BasePointerOffset, ailment.Expr.ITE, StringLiteral),
         ):
             return _dummy_bvs(condition, self._condition_mapping)
-        if isinstance(condition, ailment.Stmt.Call):
+        if isinstance(condition, (ailment.Stmt.Call, ailment.Stmt.FunctionLikeMacro)):
             return _dummy_bvs(condition, self._condition_mapping, name_suffix=hex(condition.tags.get("ins_addr", 0)))
         if isinstance(condition, (ailment.Expr.Load, ailment.Expr.Register, ailment.Expr.VirtualVariable)):
             # does it have a variable associated?

angr/utils/ssa/__init__.py
@@ -18,7 +18,7 @@ from angr.ailment.expression import (
     DirtyExpression,
     ITE,
 )
-from angr.ailment.statement import Statement, Assignment, Call, Store, CAS
+from angr.ailment.statement import Statement, Assignment, Call, Store, CAS, FunctionLikeMacro
 from angr.ailment.block_walker import AILBlockWalkerBase
 
 from angr.knowledge_plugins.key_definitions import atoms
@@ -210,7 +210,7 @@ class AILBlacklistExprTypeWalker(AILBlockWalkerBase):
 
 def is_const_and_vvar_assignment(stmt: Statement) -> bool:
     if isinstance(stmt, Assignment):
-        walker = AILBlacklistExprTypeWalker((Tmp, Load, Register, Phi, Call, DirtyExpression))
+        walker = AILBlacklistExprTypeWalker((Tmp, Load, Register, Phi, Call, DirtyExpression, FunctionLikeMacro))
         walker.walk_expression(stmt.src)
         return not walker.has_blacklisted_exprs
     return False
@@ -218,7 +218,7 @@ def is_const_and_vvar_assignment(stmt: Statement) -> bool:
 
 def is_const_vvar_tmp_assignment(stmt: Statement) -> bool:
     if isinstance(stmt, Assignment):
-        walker = AILBlacklistExprTypeWalker((Load, Register, Phi, Call, DirtyExpression))
+        walker = AILBlacklistExprTypeWalker((Load, Register, Phi, Call, DirtyExpression, FunctionLikeMacro))
         walker.walk_expression(stmt.src)
         return not walker.has_blacklisted_exprs
     return False
@@ -226,7 +226,7 @@ def is_const_vvar_tmp_assignment(stmt: Statement) -> bool:
 
 def is_const_vvar_load_assignment(stmt: Statement) -> bool:
     if isinstance(stmt, Assignment):
-        walker = AILBlacklistExprTypeWalker((Tmp, Register, Phi, Call, DirtyExpression))
+        walker = AILBlacklistExprTypeWalker((Tmp, Register, Phi, Call, DirtyExpression, FunctionLikeMacro))
         walker.walk_expression(stmt.src)
         return not walker.has_blacklisted_exprs
     return False
@@ -234,7 +234,7 @@ def is_const_vvar_load_assignment(stmt: Statement) -> bool:
 
 def is_const_vvar_load_dirty_assignment(stmt: Statement) -> bool:
     if isinstance(stmt, Assignment):
-        walker = AILBlacklistExprTypeWalker((Tmp, Register, Phi, Call))
+        walker = AILBlacklistExprTypeWalker((Tmp, Register, Phi, Call, FunctionLikeMacro))
         walker.walk_expression(stmt.src)
         return not walker.has_blacklisted_exprs
     return False

angr/angr

angr/analyses/decompiler/condition_processor.py
@@ -925,7 +925,7 @@ class ConditionProcessor:
             (ailment.Expr.VEXCCallExpression, ailment.Expr.BasePointerOffset, ailment.Expr.ITE, StringLiteral),
         ):
             return _dummy_bvs(condition, self._condition_mapping, must_bool=must_bool)
-        if isinstance(condition, ailment.Expr.Call):
+        if isinstance(condition, (ailment.Expr.Call, ailment.Stmt.FunctionLikeMacro)):
             return _dummy_bvs(
                 condition,
                 self._condition_mapping,

angr/utils/ssa/__init__.py
@@ -23,7 +23,7 @@ from angr.ailment.expression import (
     UnaryOp,
 )
 from angr.ailment.expression import Call
-from angr.ailment.statement import Statement, Assignment, Store, CAS, SideEffectStatement
+from angr.ailment.statement import Statement, Assignment, Store, CAS, SideEffectStatement, FunctionLikeMacro
 from angr.ailment.block_walker import AILBlockViewer
 
 from angr.knowledge_plugins.key_definitions import atoms
@@ -229,7 +229,7 @@ class AILBlacklistExprTypeWalker(AILBlockViewer):
 
 def is_const_and_vvar_assignment(stmt: Statement) -> bool:
     if isinstance(stmt, Assignment):
-        walker = AILBlacklistExprTypeWalker((Tmp, Load, Register, Phi, Call, DirtyExpression))
+        walker = AILBlacklistExprTypeWalker((Tmp, Load, Register, Phi, Call, DirtyExpression, FunctionLikeMacro))
         walker.walk_expression(stmt.src)
         return not walker.has_blacklisted_exprs
     return False
@@ -237,7 +237,7 @@ def is_const_and_vvar_assignment(stmt: Statement) -> bool:
 
 def is_const_vvar_tmp_assignment(stmt: Statement) -> bool:
     if isinstance(stmt, Assignment):
-        walker = AILBlacklistExprTypeWalker((Load, Register, Phi, Call, DirtyExpression))
+        walker = AILBlacklistExprTypeWalker((Load, Register, Phi, Call, DirtyExpression, FunctionLikeMacro))
         walker.walk_expression(stmt.src)
         return not walker.has_blacklisted_exprs
     return False
@@ -245,7 +245,7 @@ def is_const_vvar_tmp_assignment(stmt: Statement) -> bool:
 
 def is_const_vvar_load_assignment(stmt: Statement) -> bool:
     if isinstance(stmt, Assignment):
-        walker = AILBlacklistExprTypeWalker((Tmp, Register, Phi, Call, DirtyExpression))
+        walker = AILBlacklistExprTypeWalker((Tmp, Register, Phi, Call, DirtyExpression, FunctionLikeMacro))
         walker.walk_expression(stmt.src)
         return not walker.has_blacklisted_exprs
     return False
@@ -253,7 +253,7 @@ def is_const_vvar_load_assignment(stmt: Statement) -> bool:
 
 def is_const_vvar_load_dirty_assignment(stmt: Statement) -> bool:
     if isinstance(stmt, Assignment):
-        walker = AILBlacklistExprTypeWalker((Tmp, Register, Phi, Call))
+        walker = AILBlacklistExprTypeWalker((Tmp, Register, Phi, Call, FunctionLikeMacro))
         walker.walk_expression(stmt.src)
         return not walker.has_blacklisted_exprs
     return False