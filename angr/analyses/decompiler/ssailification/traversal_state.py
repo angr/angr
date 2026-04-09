3d711d21b67535d07c8f6ab247000fd95c7a4914
Fish <ltfish@users.noreply.github.com>
2026-04-08 22:13:52 -0700

    SSA Traversal: Remove .difference() for faster merge. (#6331)

--- angr/analyses/decompiler/ssailification/traversal_state.py
+++ angr/analyses/decompiler/ssailification/traversal_state.py
@@ -119,14 +119,20 @@ class TraversalState:
 
         for o in others:
             for k, v in o.live_registers.items():
-                merge_occurred |= bool(v.difference(self.live_registers[k]))
+                old_len = len(self.live_registers[k])
                 self.live_registers[k].update(v)
+                merge_occurred |= len(self.live_registers[k]) > old_len
+
             for k, v in o.live_stackvars.items():
-                merge_occurred |= bool(v.difference(self.live_stackvars[k]))
+                old_len = len(self.live_stackvars[k])
                 self.live_stackvars[k].update(v)
+                merge_occurred |= len(self.live_stackvars[k]) > old_len
+
             for k, v in o.live_vvars.items():
-                merge_occurred |= bool(v.difference(self.live_vvars[k]))
+                old_len = len(self.live_vvars[k])
                 self.live_vvars[k].update(v)
+                merge_occurred |= len(self.live_vvars[k]) > old_len
+
             for k0, (k1, s1) in o.stackvar_bases.items():
                 k2, s2 = self.stackvar_bases.get(k0, (k0, 0))
                 k3 = min(k1, k2)
@@ -134,6 +140,7 @@ class TraversalState:
                 if (k2, s2) != (k3, s3):
                     merge_occurred = True
                     self.stackvar_bases[k0] = (k3, s3)
+
             for k0, (k1, s1) in o.register_bases.items():
                 k2, s2 = self.register_bases.get(k0, (k0, 0))
                 k3 = min(k1, k2)
@@ -141,17 +148,23 @@ class TraversalState:
                 if (k2, s2) != (k3, s3):
                     merge_occurred = True
                     self.register_bases[k0] = (k3, s3)
+
             for k, d in o.stackvar_defs.items():
-                merge_occurred |= bool(d.difference(self.stackvar_defs[k]))
+                old_len = len(self.stackvar_defs[k])
                 self.stackvar_defs[k].update(d)
+                merge_occurred |= len(self.stackvar_defs[k]) > old_len
+
             for k, d in o.register_defs.items():
-                merge_occurred |= bool(d.difference(self.register_defs[k]))
+                old_len = len(self.register_defs[k])
                 self.register_defs[k].update(d)
-            merge_occurred |= bool(
-                o.pending_ptr_defines_nonlocal_live.difference(self.pending_ptr_defines_nonlocal_live)
-            )
+                merge_occurred |= len(self.register_defs[k]) > old_len
+
+            old_len = len(self.pending_ptr_defines_nonlocal_live)
             self.pending_ptr_defines_nonlocal_live.update(o.pending_ptr_defines_nonlocal_live)
-            merge_occurred |= bool(o.register_blackout.difference(self.register_blackout))
+            merge_occurred |= len(self.pending_ptr_defines_nonlocal_live) > old_len
+
+            old_len = len(self.register_blackout)
             self.register_blackout.update(o.register_blackout)
+            merge_occurred |= len(self.register_blackout) > old_len
 
         return merge_occurred
