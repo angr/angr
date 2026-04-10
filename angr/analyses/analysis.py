571d88c23d572212bbd1a2a111990bda99e8699a
Fish <fishw@asu.edu>
2026-04-10 16:15:31 -0700

    Lint and type check.

--- angr/analyses/analysis.py
+++ angr/analyses/analysis.py
@@ -1,12 +1,13 @@
+# pylint:disable=missing-class-docstring
 from __future__ import annotations
 import functools
 import os
 import sys
 import contextlib
 from collections import defaultdict
+from collections.abc import Callable
 from inspect import Signature
 from typing import TYPE_CHECKING, TypeVar, Generic, cast, Any
-from collections.abc import Callable
 from types import NoneType
 from itertools import chain
 from traceback import format_exception
@@ -123,7 +124,7 @@ class AnalysesHub(PluginVendor[Any]):
         self.project = project
 
     def _init_plugin(self, plugin_cls: type[A]) -> AnalysisFactory[A]:
-        return functools.wraps(plugin_cls)(AnalysisFactory(self.project, plugin_cls))
+        return functools.wraps(plugin_cls)(AnalysisFactory(self.project, plugin_cls))  # type: ignore
 
     def __getstate__(self):  # type: ignore[reportIncompatibleMethodOverride]
         s = super().__getstate__()
@@ -134,7 +135,7 @@ class AnalysesHub(PluginVendor[Any]):
         super().__setstate__(s)
 
     def __getitem__(self, plugin_cls: type[A]) -> AnalysisFactory[A]:
-        return functools.wraps(plugin_cls)(AnalysisFactory(self.project, plugin_cls))
+        return functools.wraps(plugin_cls)(AnalysisFactory(self.project, plugin_cls))  # type: ignore
 
 
 class KnownAnalysesPlugin(typing.Protocol):
@@ -289,10 +290,11 @@ class Analysis:
     named_errors: defaultdict[str, list[AnalysisLogEntry]] = defaultdict(list)
     _ram_usage: float | None = None
     _last_ramusage_update: float = 0.0
-    _progress_callback = None
+    _progress_callback: Callable | None = None
     _show_progressbar = False
     _progressbar = None
     _task = None
+    log: list
 
     _PROGRESS_WIDGETS = [
         progress.TaskProgressColumn(),
@@ -311,17 +313,17 @@ class Analysis:
         except exception:  # pylint:disable=broad-except
             if self._fail_fast:
                 raise
+            error = AnalysisLogEntry("exception occurred", exc_info=True)
+            l.error(
+                "Caught and logged %s with resilience: %s",
+                error.exc_type.__name__ if error.exc_type is not None else "UnknownException",
+                error.exc_value,  # type: ignore
+                exc_info=True,
+            )
+            if name is None:
+                self.errors.append(error)
             else:
-                error = AnalysisLogEntry("exception occurred", exc_info=True)
-                l.error(
-                    "Caught and logged %s with resilience: %s",
-                    error.exc_type.__name__,
-                    error.exc_value,  # type: ignore
-                )
-                if name is None:
-                    self.errors.append(error)
-                else:
-                    self.named_errors[name].append(error)
+                self.named_errors[name].append(error)
 
     def _initialize_progressbar(self):
         """
