eaeef84768ee4a949b821d5b92d03235bb3c201d
Fish <fishw@asu.edu>
2026-04-24 16:11:26 +0000

    Lint code.

--- tests/angrdb/test_angrdb_load_missing_binary.py
+++ tests/angrdb/test_angrdb_load_missing_binary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# pylint:disable=missing-class-docstring
+# pylint:disable=missing-class-docstring,no-self-use
 from __future__ import annotations
 
 __package__ = __package__ or "tests.angrdb"  # pylint:disable=redefined-builtin
