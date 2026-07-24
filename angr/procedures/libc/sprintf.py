95bac61843b88f6a8d1368d9cab770fd0054b5e5
max-amb <max_a@e.email>
2026-07-24 18:18:58 +0100

    Add __sprintf_chk as a simprocedure
    http://refspecs.linux-foundation.org/LSB_4.0.0/LSB-Core-generic/LSB-Core-generic/libc---sprintf-chk-1.html

--- angr/procedures/libc/sprintf.py
+++ angr/procedures/libc/sprintf.py
@@ -24,3 +24,21 @@ class sprintf(FormatParser):
         )
 
         return out_str.size() // self.arch.byte_width
+
+class __sprintf_chk(FormatParser):
+    # pylint:disable=arguments-differ
+
+    def run(self, dst_ptr, flag, size, fmt):  # pylint:disable=unused-argument
+        # See http://refspecs.linux-foundation.org/LSB_4.0.0/LSB-Core-generic/LSB-Core-generic/libc---sprintf-chk-1.html
+        # for argument layout
+
+        fmt_str = self._parse(fmt)
+        out_str = fmt_str.replace(self.va_arg)
+        self.state.memory.store(dst_ptr, out_str)
+
+        # place the terminating null byte
+        self.state.memory.store(
+            dst_ptr + (out_str.size() // self.arch.byte_width), claripy.BVV(0, self.arch.byte_width)
+        )
+
+        return out_str.size() // self.arch.byte_width
