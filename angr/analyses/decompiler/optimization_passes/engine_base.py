f7f88cca4bed31deea72f0d46eb9cebb7f774fb2
Yan <yans@yancomm.net>
2026-08-01 21:32:51 +0000

    Decompiler: guard integer constant conversion folding

--- angr/analyses/decompiler/optimization_passes/engine_base.py
+++ angr/analyses/decompiler/optimization_passes/engine_base.py
@@ -241,6 +241,7 @@ class SimplifierAILEngine(
             )
         if (
             isinstance(operand_expr, ailment.expression.Const)
+            and isinstance(operand_expr.value, int)
             and expr.from_type == ailment.expression.Convert.TYPE_INT
             and expr.to_type == ailment.expression.Convert.TYPE_INT
         ):
