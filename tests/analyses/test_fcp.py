angr/angr

commit a31f3df8f70d47bb90174d3af49c6113ef1d068f

FunctionManager: Spill to external storage. (#5976)
* FunctionManager: Spill to external storage.

* Remove atexit registration. Reduce map size.

* Introduce FuncNode in function graphs; Fix multiple issues with SpillingFunctionDict.

* Implement Function.dirty.

* A bit more optimization.

* FunctionManager loads only meta data for functions when graphs are not accessed; Save .info for functions.

* Only load meta data for Functions in more places.

* Remove FunctionManager.block_map because it's never really used.

* Retire blockaddr_to_function and replace it with blockaddr_to_funcaddr.

* More optimizations and fixes.

* More refactor and fixes.

* More optimizations.

* Fix the bug in lmdb spilling after raising MapFullError.

* Introduce RuntimeDb in KB. Migrate SpillingFunctionsDict to use RuntimeDb.

* Prioritize the basedir of the main executable for the runtime db path.

* Cache non-returning function addrs, unknown-returning function addrs, and function block count in FunctionManager.

* Bug fixes.

* Fix a bug in FunctionParser.

* CFGBase.make_functions: Copy over function metadata when creating functions in the first place.

* Update MockFunctionManager.

* Type check codenode.py and fix an RDA test case.

* Fix serialization tests; Introduce KnowledgeBasePlugin.set_kb(); SpillingFunctionDict now derives from UserDict; Fix KB.name stored in KB._plugins; Fix FunctionDict.__setstate__ swapping Function objects and function addresses.

* Update FactCollector to support FuncNode.

* FunctionInfo: Update Function.dirty and perform type checks on keys and values.

* Make function cache limit configurable.

* Adjust FunctionInfo type check.

* More fixes.

* More updates to account for FuncNode in function graphs.

* Update a test case.

* Fix another test case (do not use the size of FuncNodes).

* Fix Reassembler.

* CFGBase.make_functions: Add a missing insertion to _updated_nonreturning_functions.

* Update FunctionManager.rebuild_callgraph.

angr/analyses/fcp/fcp.py
@@ -12,7 +12,7 @@ from angr.block import Block
 from angr.analyses.analysis import Analysis
 from angr.analyses import AnalysesHub
 from angr.knowledge_plugins.functions import Function
-from angr.codenode import BlockNode, HookNode
+from angr.codenode import BlockNode, HookNode, FuncNode
 from angr.engines.light import SimEngineNostmtVEX, SimEngineLight, SpOffset, RegisterOffset
 from angr.calling_conventions import SimStackArg, default_cc
 from angr.analyses.propagator.vex_vars import VEXReg, VEXTmp
@@ -333,10 +333,12 @@ class FastConstantPropagation(Analysis):
                     node = self.kb.functions.get_by_addr(node.addr)
                 else:
                     continue
-            if isinstance(node, Function):
-                if node.calling_convention is not None and node.prototype is not None:
-                    # consume args and overwrite the return register
-                    self._handle_function(state, node)
+            if isinstance(node, FuncNode):
+                if self.kb.functions.contains_addr(node.addr):
+                    callee = self.kb.functions.get_by_addr(node.addr)
+                    if callee.calling_convention is not None and callee.prototype is not None:
+                        # consume args and overwrite the return register
+                        self._handle_function(state, callee)
                 continue
 
             block = self.project.factory.block(node.addr, size=node.size, cross_insn_opt=self._vex_cross_insn_opt)
@@ -345,14 +347,13 @@ class FastConstantPropagation(Analysis):
             # if the node ends with a function call, call _handle_function
             if node in func_graph_with_callees:
                 succs = list(func_graph_with_callees.successors(node))
-                if any(isinstance(succ, (Function, HookNode)) for succ in succs):
-                    callee = next(succ for succ in succs if isinstance(succ, (Function, HookNode)))
-                    if isinstance(callee, HookNode):
-                        # attempt to convert it into a function
-                        if self.kb.functions.contains_addr(callee.addr):
-                            callee = self.kb.functions.get_by_addr(callee.addr)
-                        else:
-                            callee = None
+                if any(isinstance(succ, (FuncNode, HookNode)) for succ in succs):
+                    callee = next(succ for succ in succs if isinstance(succ, (FuncNode, HookNode)))
+                    # attempt to convert it into a function
+                    if self.kb.functions.contains_addr(callee.addr):
+                        callee = self.kb.functions.get_by_addr(callee.addr)
+                    else:
+                        callee = None
                     state = self._handle_function(state, callee)
 
             states[node] = state

angr/analyses/calling_convention/fact_collector.py
@@ -11,7 +11,7 @@ from angr.block import Block
 from angr.analyses.analysis import Analysis
 from angr.analyses import AnalysesHub
 from angr.knowledge_plugins.functions import Function
-from angr.codenode import BlockNode, HookNode
+from angr.codenode import BlockNode, HookNode, FuncNode
 from angr.engines.light import SimEngineNostmtVEX, SimEngineLight, SpOffset, RegisterOffset
 from angr.calling_conventions import SimRegArg, SimStackArg, default_cc
 from angr.sim_type import SimTypeBottom, SimTypeFunction
@@ -276,7 +276,12 @@ class FactCollector(Analysis):
 
         traversed = set()
         queue: list[
-            tuple[int, FactCollectorState, CodeNode | BlockNode | HookNode | Function, BlockNode | HookNode | None]
+            tuple[
+                int,
+                FactCollectorState,
+                CodeNode | BlockNode | HookNode | FuncNode,
+                BlockNode | HookNode | FuncNode | None,
+            ]
         ] = [(0, init_state, startpoint, None)]
         end_states: list[FactCollectorState] = []
         while queue:
@@ -291,17 +296,18 @@ class FactCollector(Analysis):
 
             if isinstance(node, BlockNode) and node.size == 0:
                 continue
-            if isinstance(node, HookNode):
+            func: Function | None = None
+            if isinstance(node, (HookNode, FuncNode)):
                 # attempt to convert it into a function
                 if self.kb.functions.contains_addr(node.addr):
-                    node = self.kb.functions.get_by_addr(node.addr)
+                    func = self.kb.functions.get_by_addr(node.addr)
                 else:
                     continue
-            if isinstance(node, Function):
-                if node.calling_convention is not None and node.prototype is not None:
+            if func is not None:
+                if func.calling_convention is not None and func.prototype is not None:
                     # consume args and overwrite the return register
-                    self._handle_function(state, node)
-                if node.returning is False or retnode is None:
+                    self._handle_function(state, func)
+                if func.returning is False or retnode is None:
                     # the function call does not return
                     end_states.append(state)
                 else:
@@ -331,12 +337,9 @@ class FactCollector(Analysis):
                     elif edge_type == "call" or (edge_type == "transition" and outside):
                         # a call or a tail-call
                         # note that it's ok to traverse a called function multiple times
-                        if not isinstance(succ, Function):
-                            if self.kb.functions.contains_addr(succ.addr):
-                                succ = self.kb.functions.get_by_addr(succ.addr)
-                            else:
-                                # not sure who we are calling
-                                continue
+                        if not isinstance(succ, FuncNode) and not self.kb.functions.contains_addr(succ.addr):
+                            # not sure who we are calling
+                            continue
                         call_succ = succ
             if call_succ is not None:
                 successor_added = True
@@ -393,7 +396,7 @@ class FactCollector(Analysis):
         retval_sizes = []
         for endpoint in self.function.endpoints:
             traversed = set()
-            queue: list[tuple[int, BlockNode | HookNode]] = [(0, endpoint)]
+            queue: list[tuple[int, CodeNode]] = [(0, endpoint)]
             while queue:
                 depth, node = queue.pop(0)
                 if isinstance(node, BlockNode) and node in traversed:
@@ -406,21 +409,22 @@ class FactCollector(Analysis):
                 if isinstance(node, BlockNode) and node.size == 0:
                     continue
 
-                if isinstance(node, HookNode):
+                func = None
+                if isinstance(node, (FuncNode, HookNode)):
                     # attempt to convert it into a function
                     if self.kb.functions.contains_addr(node.addr):
-                        node = self.kb.functions.get_by_addr(node.addr)
+                        func = self.kb.functions.get_by_addr(node.addr)
                     else:
                         continue
-                if isinstance(node, Function):
+                if func is not None:
                     if (
-                        node.calling_convention is not None
-                        and node.prototype is not None
-                        and node.prototype.returnty is not None
-                        and not isinstance(node.prototype.returnty, SimTypeBottom)
+                        func.calling_convention is not None
+                        and func.prototype is not None
+                        and func.prototype.returnty is not None
+                        and not isinstance(func.prototype.returnty, SimTypeBottom)
                     ):
                         # assume the function overwrites the return variable
-                        returnty_size = node.prototype.returnty.with_arch(self.project.arch).size
+                        returnty_size = func.prototype.returnty.with_arch(self.project.arch).size
                         assert returnty_size is not None
                         retval_size = returnty_size // self.project.arch.byte_width
                         retval_sizes.append(retval_size)
@@ -430,16 +434,15 @@ class FactCollector(Analysis):
                 func_succs = [
                     succ
                     for succ in func_graph.successors(node)
-                    if isinstance(succ, (Function, HookNode)) or self.kb.functions.contains_addr(succ.addr)
+                    if isinstance(succ, (FuncNode, HookNode)) or self.kb.functions.contains_addr(succ.addr)
                 ]
                 if len(func_succs) == 1:
-                    func_succ = func_succs[0]
-                    if isinstance(func_succ, (BlockNode, HookNode)) and self.kb.functions.contains_addr(func_succ.addr):
+                    succ = func_succs[0]
+                    func_succ: Function | None = None
+                    if isinstance(succ, (BlockNode, HookNode, FuncNode)) and self.kb.functions.contains_addr(succ.addr):
                         # attempt to convert it into a function
-                        func_succ = self.kb.functions.get_by_addr(func_succ.addr)
-                    if isinstance(func_succ, Function) and func_succ.name not in {  # noqa:SIM102
-                        "_security_check_cookie"
-                    }:
+                        func_succ = self.kb.functions.get_by_addr(succ.addr)
+                    if func_succ is not None and func_succ.name not in {"_security_check_cookie"}:  # noqa:SIM102
                         if (
                             func_succ.calling_convention is not None
                             and func_succ.prototype is not None
@@ -506,7 +509,7 @@ class FactCollector(Analysis):
         }
         for endpoint in self.function.endpoints:
             traversed = set()
-            queue: list[tuple[int, BlockNode | HookNode]] = [(0, endpoint)]
+            queue: list[tuple[int, CodeNode]] = [(0, endpoint)]
             while queue:
                 depth, node = queue.pop(0)
                 traversed.add(node)
@@ -516,7 +519,7 @@ class FactCollector(Analysis):
 
                 if isinstance(node, BlockNode) and node.size == 0:
                     continue
-                if isinstance(node, (HookNode, Function)):
+                if isinstance(node, (HookNode, FuncNode)):
                     continue
 
                 block = self.project.factory.block(node.addr, size=node.size)

commit 9f589f7062d03a0615c71609cf445e1b24c1ff5a

FCP: Do not lift hook nodes as zero-byte blocks
The node loop converted a HookNode into the Function that the knowledge
base holds for its address and then tested the result with
isinstance(node, FuncNode). A Function is not a FuncNode, so the test
never matched and every hook node fell through to
factory.block(node.addr, size=node.size) with the function's size, which
is 0 for a SimProcedure stub. Lifting that raises SimTranslationError,
and nothing between CFGFast and ConstantResolver catches it, so a single
hook node in a function graph destroys the whole CFG. This has been
broken since a31f3df8f, which replaced the isinstance(node, Function)
test the conversion used to feed.

Merge the two branches so a HookNode takes the path a FuncNode already
takes: look the callee up in the knowledge base, and lift nothing.

Annotate the states mapping with CodeNode, which is what Function.graph
is declared to hold. BlockNode was the same wrong assumption at the type
level.

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>

angr/analyses/fcp/fcp.py
@@ -12,7 +12,7 @@ from angr.analyses.analysis import AnalysesHub, Analysis
 from angr.analyses.propagator.vex_vars import VEXReg, VEXTmp
 from angr.block import Block
 from angr.calling_conventions import SimStackArg
-from angr.codenode import BlockNode, FuncNode, HookNode
+from angr.codenode import BlockNode, CodeNode, FuncNode, HookNode
 from angr.engines.light import RegisterOffset, SimEngineLight, SimEngineNostmtVEX, SpOffset
 from angr.knowledge_plugins.functions import Function
 from angr.utils.bits import s2u
@@ -313,7 +313,7 @@ class FastConstantPropagation(Analysis):
         if self._blocks:
             block_addrs = {b.addr for b in self._blocks}
 
-        states: dict[BlockNode, FCPState] = {}
+        states: dict[CodeNode, FCPState] = {}
         for node in sorted_nodes:
             preds = func_graph.predecessors(node)
             input_states = [states[pred] for pred in preds if pred in states]
@@ -327,13 +327,8 @@ class FastConstantPropagation(Analysis):
             # process the block
             if isinstance(node, BlockNode) and node.size == 0:
                 continue
-            if isinstance(node, HookNode):
-                # attempt to convert it into a function
-                if self.kb.functions.contains_addr(node.addr):
-                    node = self.kb.functions.get_by_addr(node.addr, meta_only=True)
-                else:
-                    continue
-            if isinstance(node, FuncNode):
+            if isinstance(node, (FuncNode, HookNode)):
+                # a callee, not a block of this function
                 if self.kb.functions.contains_addr(node.addr):
                     callee = self.kb.functions.get_by_addr(node.addr, meta_only=True)
                     if callee.calling_convention is not None and callee.prototype is not None:

tests/analyses/test_fcp.py
@@ -9,6 +9,7 @@ import unittest
 
 import angr
 from angr.analyses.propagator.vex_vars import VEXReg
+from angr.codenode import HookNode
 from tests.common import bin_location
 
 test_location = os.path.join(bin_location, "tests")
@@ -24,6 +25,19 @@ class TestFCP(unittest.TestCase):
         prop = p.analyses.FastConstantPropagation(func)
 
         assert len(prop.replacements) > 0
+
+    def test_hook_nodes_are_not_lifted(self):
+        # every SimProcedure stub is a function whose graph is a single hook node
+        bin_path = os.path.join(test_location, "x86_64", "fauxware")
+        p = angr.Project(bin_path, auto_load_libs=False)
+        cfg = p.analyses.CFGFast(normalize=True)
+
+        funcs = [func for func in cfg.functions.values() if any(isinstance(node, HookNode) for node in func.graph)]
+        assert funcs
+
+        for func in funcs:
+            prop = p.analyses.FastConstantPropagation(func)
+            assert not prop.replacements
 
     def test_register_propagation_across_calls(self):
         call_targets = [