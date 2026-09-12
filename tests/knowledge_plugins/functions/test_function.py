f125af04e6a7bace9862dcc904808bdb0d907a0a
Fish <fishw@asu.edu>
2026-09-12 08:16:02 +0000

    Lint and type check.

--- tests/knowledge_plugins/functions/test_function.py
+++ tests/knowledge_plugins/functions/test_function.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# pylint:disable=missing-class-docstring,protected-access,no-self-use
 from __future__ import annotations
 
 import os
@@ -10,7 +11,7 @@ import networkx
 import angr
 from angr.codenode import BlockNode
 from angr.knowledge_plugins.functions import Function
-from angr.sim_type import parse_defns
+from angr.sim_type import SimTypeFunction, parse_defns
 from tests.common import bin_location
 
 test_location = os.path.join(bin_location, "tests")
@@ -112,6 +113,7 @@ class TestFunction(TestCase):
         parsed_proto = parse_defns("int func(int, char*);")["func"]
         function.prototype = parsed_proto.with_arch(archinfo.arch_from_id("AMD64"))
 
+        assert isinstance(function.prototype, SimTypeFunction)
         assert len(function.prototype.args) == 2
         assert len(function.prototype.arg_names) == 2
         # default function argument names apply
@@ -121,9 +123,11 @@ class TestFunction(TestCase):
     def test_function_set_prototype_missing_a_parameter_name(self):
         function = makeFunction(self.function_manager, 0x42, "function")
         parsed_proto = parse_defns("int func(int, char*);")["func"]
-        parsed_proto.arg_names = ["", "a3"]
+        assert isinstance(parsed_proto, SimTypeFunction)
+        parsed_proto.arg_names = ("", "a3")
         function.prototype = parsed_proto.with_arch(archinfo.arch_from_id("AMD64"))
 
+        assert isinstance(function.prototype, SimTypeFunction)
         assert len(function.prototype.args) == 2
         assert len(function.prototype.arg_names) == 2
         # default function argument names apply
@@ -147,8 +151,9 @@ class TestLocalTransitionGraphCache(TestCase):
     (e.g. a GUI thread during CFG recovery) leaves get_node() pointing at a node the cached graph does not contain."""
 
     def test_registering_a_node_invalidates_the_cached_graph(self):
-        proj = angr.Project(os.path.join(test_location, "x86_64", "fauxware"), auto_load_libs=False)
+        proj = angr.Project(os.path.join(test_location, "x86_64", "fauxware"))
         func = proj.kb.functions.function(0x40071D, create=True)
+        assert func is not None
         assert func.get_node(0x40071D) is None
         cached = func.graph  # populate the cache while the function is still empty
 
@@ -158,4 +163,4 @@ class TestLocalTransitionGraphCache(TestCase):
         assert func.get_node(0x40071D) is node
         assert func.graph is not cached
         assert node in func.graph
-        assert list(func.graph.successors(node)) == []
+        assert not list(func.graph.successors(node))
